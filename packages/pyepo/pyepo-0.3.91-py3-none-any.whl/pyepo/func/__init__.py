6a7686821e7debc38a9a883079c9a6a5e38662b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 15:04:25 +0000

    Add package pyepo/0.3.91/pyepo-0.3.91-py3-none-any.whl

---
+++ packages/pyepo/pyepo-0.3.91-py3-none-any.whl/pyepo/func/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+Pytorch autograd function for end-to-end training
+"""
+
+from pyepo.func.spoplus import SPOPlus
+from pyepo.func.blackbox import blackboxOpt, negativeIdentity
+from pyepo.func.perturbed import perturbedOpt, perturbedFenchelYoung, implicitMLE, adaptiveImplicitMLE
+from pyepo.func.contrastive import NCE, contrastiveMAP
+from pyepo.func.rank import listwiseLTR, pairwiseLTR, pointwiseLTR
