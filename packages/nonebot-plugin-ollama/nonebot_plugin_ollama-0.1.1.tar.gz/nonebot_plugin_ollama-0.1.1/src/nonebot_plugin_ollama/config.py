44d3708361ca7892b0f65cc0ed51239c1fa5777d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 17:04:17 +0000

    Add package nonebot-plugin-ollama/0.1.1/nonebot_plugin_ollama-0.1.1.tar.gz

---
+++ packages/nonebot-plugin-ollama/nonebot_plugin_ollama-0.1.1.tar.gz/nonebot_plugin_ollama-0.1.1/src/nonebot_plugin_ollama/config.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, field_validator
+
+class ScopedConfig(BaseModel):
+    model: str = 'qwen2.5:0.5b'
+    url: str = 'http://127.0.0.1:11434/'
+    priority: int = 5
+
+    @field_validator('priority')
+    @classmethod
+    def check_priority(cls, v: int):
+        if v >= 1:
+            return v
+        raise ValueError('ollama command priority must be greater than 1')
+    
+    @field_validator('url')
+    @classmethod
+    def check_url(cls, v: str):
+        if v.startswith('http://') and v.endswith('/'):
+            return v
+        raise ValueError('ollama url must start with http:// and end with /')
+    
+class Config(BaseModel):
+    ollama: ScopedConfig = ScopedConfig()
