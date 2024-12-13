0743cfece5b426da92bde4120e8d8405ddfb3200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 16:04:09 +0000

    Add package Zxy-Privated/0.0.4/zxy_privated-0.0.4.tar.gz

---
+++ packages/Zxy-Privated/zxy_privated-0.0.4.tar.gz/zxy_privated-0.0.4/OhMyChat_API/__init__.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+'''
+@Project_name    :  __init__.py
+@ProjectDescription: ..........
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2023/8/8 16:52     zxy       1.0         None
+'''
+
+from .Get_API import GetAPI
+
---
+++ packages/Zxy-Privated/zxy_privated-0.0.4.tar.gz/zxy_privated-0.0.4/setup.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+import setuptools
+
+setuptools.setup(
+    name="Zxy_Privated",
+    version="0.0.4",
+    author="AnQing",
+    description="需要到 https://aigptx.top/ 注册并获取api秘钥(废弃),使用秋雨所写的chatGPT3接口",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+)
