ccf0d91dca9a813393786235dc54cb198262b064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 16:04:35 +0000

    Add package mtmai/0.4.160/mtmai-0.4.160.tar.gz

---
+++ packages/mtmai/mtmai-0.4.160.tar.gz/mtmai-0.4.160/mtmai/workflows/workers.py
@@ -0,0 +1,43 @@
+from mtmai.agents.assisant_graph import MtmAssistantGraph
+from mtmai.agents.base import GraphBuilder
+from mtmai.workflows.graphflowhelper import build_graph_flow
+
+
+def get_graphs() -> list[GraphBuilder]:
+    return [
+        MtmAssistantGraph(),
+    ]
+
+
+async def deploy_mtmai_workers(backend_url: str):
+    import asyncio
+
+    from mtmai.workflows.agentcall import AgentCall
+
+    # from mtmai.workflows.assisant_flow import AssisantFlow
+    from mtmai.workflows.wfapp import wfapp
+
+    # 获取配置文件
+    # response = httpx.get("http://localhost:8383/api/v1/worker/config")
+    # hatchet = Hatchet(debug=True)
+    # list: WorkflowList = await wfapp.rest.aio.default_api.worker_config()
+    worker = wfapp.worker("pyworker")
+    if not worker:
+        raise ValueError("worker not found")
+    # worker.register_workflow(BasicRagWorkflow())
+    # worker.register_workflow(FlowMcpClientExample())
+    # worker.register_workflow(FlowArticleGen())
+    # worker.register_workflow(FlowWriteChapter())
+    # worker.register_workflow(BlogGen())
+    worker.register_workflow(AgentCall())
+    # worker.register_workflow(AssisantFlow())
+    # worker.register_workflow(SystemBackendTask())
+
+    for graph in get_graphs():
+        builded_graph = await graph.build_graph()
+        graph_flow = await build_graph_flow(builded_graph)
+        worker.register_workflow(graph_flow())
+    await worker.async_start()
+
+    while True:
+        await asyncio.sleep(1)
