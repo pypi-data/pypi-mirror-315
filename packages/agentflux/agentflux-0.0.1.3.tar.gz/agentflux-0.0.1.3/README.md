b173cc5320ad60a45fb9de9f27b467614d279d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 16:04:23 +0000

    Add package agentflux/0.0.1.3/agentflux-0.0.1.3.tar.gz

---
+++ packages/agentflux/agentflux-0.0.1.3.tar.gz/agentflux-0.0.1.3/README.md
@@ -0,0 +1,14 @@
+# AgentFlux
+llm agent for function call
+
+
+# Build Package
+## Build to package
+```bash
+bash compile.sh
+```
+## Upload to pypi
+```bash
+bash pypi_upload.sh
+```
+
---
+++ packages/agentflux/agentflux-0.0.1.3.tar.gz/agentflux-0.0.1.3/agentflux/__init__.py
@@ -0,0 +1,5 @@
+from agentflux.agent_manager import AgentFilesManager,AgentManager,ToolManager
+from agentflux.qagent_manager import QAgentManager
+from agentflux.agent_manager import InfoManager
+from agentflux.manage.agent_create import AgentlDefine
+from agentflux.manage.tool_create import ToolDefine
