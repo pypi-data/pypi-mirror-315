823ddbc95c69cfc2544bd1e5d908d943b6e2beb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 14:04:07 +0000

    Add package promptquality/0.72.1/promptquality-0.72.1.tar.gz

---
+++ packages/promptquality/promptquality-0.72.1.tar.gz/promptquality-0.72.1/src/promptquality/utils/dependencies.py
@@ -0,0 +1,11 @@
+from importlib.util import find_spec
+
+
+def is_dependency_available(name: str) -> bool:
+    return find_spec(name) is not None
+
+
+is_langchain_available = is_dependency_available("langchain")
+is_numpy_available = is_dependency_available("numpy")
+is_pandas_available = is_dependency_available("pandas")
+is_pyarrow_available = is_dependency_available("pyarrow")
