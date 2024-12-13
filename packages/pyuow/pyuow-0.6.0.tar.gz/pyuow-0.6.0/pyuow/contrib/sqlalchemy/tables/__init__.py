9940da3039142ff443c9a27e7462261ca76b9671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 05:03:40 +0000

    Add package pyuow/0.6.0/pyuow-0.6.0.tar.gz

---
+++ packages/pyuow/pyuow-0.6.0.tar.gz/pyuow-0.6.0/pyuow/contrib/sqlalchemy/tables/__init__.py
@@ -0,0 +1,13 @@
+from .base import (
+    AuditedEntityTable,
+    BaseTable,
+    EntityTable,
+    VersionedEntityTable,
+)
+
+__all__ = (
+    "AuditedEntityTable",
+    "BaseTable",
+    "EntityTable",
+    "VersionedEntityTable",
+)
