c92eeffa22404cf84ec481fddc23fe084568914d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 04:04:12 +0000

    Add package tauth/2.5.2/tauth-2.5.2-py3-none-any.whl

---
+++ packages/tauth/tauth-2.5.2-py3-none-any.whl/tauth/resource_management/access/schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from redbaby.pyobjectid import PyObjectId
+
+from tauth.entities.schemas import EntityRefIn
+
+
+class ResourceAccessIn(BaseModel):
+    resource_id: PyObjectId
+    entity_ref: EntityRefIn
+
+
+class GrantIn(BaseModel):
+    resource_id: PyObjectId
+    entity_ref: EntityRefIn
+    permission_name: str
