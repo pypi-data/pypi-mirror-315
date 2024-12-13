0ca20855182d4554f98609105f8d611e0cf1eb33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 05:03:40 +0000

    Add package pyuow/0.6.0/pyuow-0.6.0-py3-none-any.whl

---
+++ packages/pyuow/pyuow-0.6.0-py3-none-any.whl/pyuow/datapoint/aio/impl.py
@@ -0,0 +1,59 @@
+import typing as t
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+from ...datapoint import (
+    BaseDataPointContainer,
+    BaseDataPointsDict,
+    BaseDataPointSpec,
+    DataPointIsNotProducedError,
+)
+from ...datapoint.aio import BaseDataPointConsumer, BaseDataPointProducer
+
+
+class ConsumesDataPoints(ABC):
+    @property
+    @abstractmethod
+    def _consumes(
+        self, *specs: BaseDataPointSpec[t.Any]
+    ) -> t.Set[BaseDataPointSpec[t.Any]]:
+        raise NotImplementedError
+
+    async def out_of(
+        self, consumer: BaseDataPointConsumer[t.Any]
+    ) -> BaseDataPointsDict:
+        consumed = await consumer.get(*self._consumes)
+        consumed_specs = set(consumed.keys())
+
+        if not self._consumes.issubset(consumed_specs):
+            raise DataPointIsNotProducedError(consumed_specs)
+
+        return t.cast(BaseDataPointsDict, consumed)
+
+
+class ProducesDataPoints(ABC):
+    @dataclass(frozen=True)
+    class ProducerProxy:
+        _producer: BaseDataPointProducer[t.Any]
+        _required_specs: t.Set[BaseDataPointSpec[t.Any]]
+
+        async def add(
+            self, *datapoints: BaseDataPointContainer[t.Any]
+        ) -> None:
+            actual_specs = {datapoint.spec for datapoint in datapoints}
+            missing_specs = self._required_specs - actual_specs
+
+            if len(missing_specs) > 0:
+                raise DataPointIsNotProducedError(missing_specs)
+
+            await self._producer.add(*datapoints)
+
+    @property
+    @abstractmethod
+    def _produces(
+        self, *specs: BaseDataPointSpec[t.Any]
+    ) -> t.Set[BaseDataPointSpec[t.Any]]:
+        raise NotImplementedError
+
+    def to(self, producer: BaseDataPointProducer[t.Any]) -> ProducerProxy:
+        return self.ProducerProxy(producer, self._produces)
---
+++ packages/pyuow/pyuow-0.6.0-py3-none-any.whl/pyuow/datapoint/impl.py
@@ -0,0 +1,58 @@
+import typing as t
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+from ..datapoint import (
+    BaseDataPointConsumer,
+    BaseDataPointContainer,
+    BaseDataPointProducer,
+    BaseDataPointsDict,
+    BaseDataPointSpec,
+    DataPointIsNotProducedError,
+)
+
+
+class ConsumesDataPoints(ABC):
+    @property
+    @abstractmethod
+    def _consumes(
+        self, *specs: BaseDataPointSpec[t.Any]
+    ) -> t.Set[BaseDataPointSpec[t.Any]]:
+        raise NotImplementedError
+
+    def out_of(
+        self, consumer: BaseDataPointConsumer[t.Any]
+    ) -> BaseDataPointsDict:
+        consumed = consumer.get(*self._consumes)
+        consumed_specs = set(consumed.keys())
+
+        if not self._consumes.issubset(consumed_specs):
+            raise DataPointIsNotProducedError(consumed_specs)
+
+        return t.cast(BaseDataPointsDict, consumed)
+
+
+class ProducesDataPoints(ABC):
+    @dataclass(frozen=True)
+    class ProducerProxy:
+        _producer: BaseDataPointProducer[t.Any]
+        _required_specs: t.Set[BaseDataPointSpec[t.Any]]
+
+        def add(self, *datapoints: BaseDataPointContainer[t.Any]) -> None:
+            actual_specs = {datapoint.spec for datapoint in datapoints}
+            missing = self._required_specs - actual_specs
+
+            if len(missing) > 0:
+                raise DataPointIsNotProducedError(missing)
+
+            self._producer.add(*datapoints)
+
+    @property
+    @abstractmethod
+    def _produces(
+        self, *specs: BaseDataPointSpec[t.Any]
+    ) -> t.Set[BaseDataPointSpec[t.Any]]:
+        raise NotImplementedError
+
+    def to(self, producer: BaseDataPointProducer[t.Any]) -> ProducerProxy:
+        return self.ProducerProxy(producer, self._produces)
