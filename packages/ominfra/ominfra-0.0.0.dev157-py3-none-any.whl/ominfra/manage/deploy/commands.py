36b771646ca53a4f135fe66ffb053771a6a30b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 14:04:22 +0000

    Add package ominfra/0.0.0.dev157/ominfra-0.0.0.dev157-py3-none-any.whl

---
+++ packages/ominfra/ominfra-0.0.0.dev157-py3-none-any.whl/ominfra/manage/deploy/commands.py
@@ -0,0 +1,24 @@
+# ruff: noqa: UP006 UP007
+import dataclasses as dc
+
+from omlish.lite.logs import log
+
+from ..commands.base import Command
+from ..commands.base import CommandExecutor
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class DeployCommand(Command['DeployCommand.Output']):
+    @dc.dataclass(frozen=True)
+    class Output(Command.Output):
+        pass
+
+
+class DeployCommandExecutor(CommandExecutor[DeployCommand, DeployCommand.Output]):
+    async def execute(self, cmd: DeployCommand) -> DeployCommand.Output:
+        log.info('Deploying!')
+
+        return DeployCommand.Output()
