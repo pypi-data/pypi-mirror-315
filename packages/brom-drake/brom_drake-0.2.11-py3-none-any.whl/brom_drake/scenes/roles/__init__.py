fb5493c7601a6422230e1096185a99a686d66299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 15:03:59 +0000

    Add package brom-drake/0.2.11/brom_drake-0.2.11-py3-none-any.whl

---
+++ packages/brom-drake/brom_drake-0.2.11-py3-none-any.whl/brom_drake/scenes/roles/__init__.py
@@ -0,0 +1,8 @@
+from brom_drake.scenes.roles.role import Role, RolePortAssignment
+from brom_drake.scenes.roles.motion_planners.kinematic import kKinematicMotionPlanner
+
+__all__ = [
+    "Role",
+    "kKinematicMotionPlanner",
+    "RolePortAssignment",
+]
