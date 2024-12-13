7b17ade86f0920420c5c322de2d52dd01cc75347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 14:03:49 +0000

    Add package ai-mock/0.2.7/ai_mock-0.2.7-py3-none-any.whl

---
+++ packages/ai-mock/ai_mock-0.2.7-py3-none-any.whl/mockai/dependencies.py
@@ -0,0 +1,19 @@
+import os
+from typing import Annotated
+
+import aiofiles
+from fastapi import Depends
+
+from mockai.models import PreDeterminedResponses
+
+
+async def read_response_file() -> PreDeterminedResponses | None:
+    if file := os.getenv("MOCKAI_RESPONSES"):
+        async with aiofiles.open(file, "r") as f:
+            contents = await f.read()
+        return PreDeterminedResponses.model_validate_json(contents)
+    else:
+        return None
+
+
+ResponseFile = Annotated[PreDeterminedResponses | None, Depends(read_response_file)]
