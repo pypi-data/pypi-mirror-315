1f2b860ad8a76768adb8df1ed39aaf18f7dcc9f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 16:04:07 +0000

    Add package phyjax2d/0.1.3/phyjax2d-0.1.3-py3-none-any.whl

---
+++ packages/phyjax2d/phyjax2d-0.1.3-py3-none-any.whl/phyjax2d/__init__.py
@@ -0,0 +1,30 @@
+from phyjax2d.impl import (
+    Capsule,
+    Circle,
+    Force,
+    Polygon,
+    Position,
+    Raycast,
+    Segment,
+    ShapeDict,
+    Space,
+    State,
+    StateDict,
+    Velocity,
+    VelocitySolver,
+    circle_raycast,
+    get_relative_angle,
+    segment_raycast,
+    step,
+)
+from phyjax2d.moderngl_vis import MglVisualizer
+from phyjax2d.utils import (
+    Color,
+    SpaceBuilder,
+    circle_overlap,
+    make_approx_circle,
+    make_square_segments,
+)
+from phyjax2d.vec2d import Vec2d
+
+__version__ = "0.1.3"
