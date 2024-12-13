39f9307a2e4f4f15fdbb03d655bdaedc9f97f955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 15:03:41 +0000

    Add package altikrity/0.1/altikrity-0.1.tar.gz

---
+++ packages/altikrity/altikrity-0.1.tar.gz/altikrity-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='altikrity',
+    version='0.1',
+    packages=find_packages(),
+    description='A custom encryption library with strong multi-layer encryption.',
+    author='Your Name',
+    author_email='your.email@example.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
