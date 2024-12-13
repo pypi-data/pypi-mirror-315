923f2556b0d8d62101794ca7a4eb0c701b29b5fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 15:03:35 +0000

    Add package JcFile/0.0.3/JcFile-0.0.3-py3-none-any.whl

---
+++ packages/JcFile/JcFile-0.0.3-py3-none-any.whl/JcFile/__init__.py
@@ -0,0 +1,20 @@
+__copyright__    = 'Copyright (C) 2024 JavaCommons Technologies'
+__version__      = '0.0.3'
+__license__      = 'MIT'
+__author__       = 'JavaCommons Technologies'
+__author_email__ = 'javacommmons@gmail.com'
+__url__          = 'https://github.com/javacommons/py-file'
+__all__ = ['JcFile']
+
+class JcFile:
+    def __init__(self):
+        pass
+    def read_all_text(self, path):
+        file = open(path, "rt")
+        content = file.read()
+        file.close()
+        return content
+    def write_all_text(self, path, content):
+        file = open(path, "wb")
+        file.write(content.replace("\r\n", "\n").replace("\r", "\n").encode())
+        file.close()
