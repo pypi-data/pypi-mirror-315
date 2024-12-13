5959a95e34a539d222be3a13a897f03d97f95049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 05:03:46 +0000

    Add package torrent-worker-coordinator/1.0.6/torrent_worker_coordinator-1.0.6.tar.gz

---
+++ packages/torrent-worker-coordinator/torrent_worker_coordinator-1.0.6.tar.gz/torrent_worker_coordinator-1.0.6/src/torrent_worker_coordinator/__init__.py
@@ -0,0 +1,34 @@
+from .app_schemas import InfoResponse, TorrentListResponse, TorrentResponse
+from .client import Client
+from .test.test_app import TestApp
+
+__version__ = "1.0.6"
+
+
+class Api:
+    @staticmethod
+    def client(
+        host: str,
+        port: int,
+        api_key: str,
+    ) -> Client:
+        client = Client(host=host, port=port, api_key=api_key)
+        return client
+
+
+class Test:
+    @staticmethod
+    def app(
+        api_key: str | None = None,
+    ) -> TestApp:
+        app = TestApp(api_key=api_key)
+        return app
+
+
+__all__ = [
+    "Api",
+    "Test",
+    "InfoResponse",
+    "TorrentListResponse",
+    "TorrentResponse",
+]
