c8ff64c0205d697eeeb74b5fd2b6a7daca9bb42d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 05:03:26 +0000

    Add package datafusion/43.1.0/datafusion-43.1.0.tar.gz

---
+++ packages/datafusion/datafusion-43.1.0.tar.gz/datafusion-43.1.0/src/errors.rs
@@ -0,0 +1,93 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use core::fmt;
+use std::error::Error;
+use std::fmt::Debug;
+
+use datafusion::arrow::error::ArrowError;
+use datafusion::error::DataFusionError as InnerDataFusionError;
+use prost::EncodeError;
+use pyo3::{exceptions::PyException, PyErr};
+
+pub type Result<T> = std::result::Result<T, DataFusionError>;
+
+#[derive(Debug)]
+pub enum DataFusionError {
+    ExecutionError(InnerDataFusionError),
+    ArrowError(ArrowError),
+    Common(String),
+    PythonError(PyErr),
+    EncodeError(EncodeError),
+}
+
+impl fmt::Display for DataFusionError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            DataFusionError::ExecutionError(e) => write!(f, "DataFusion error: {e:?}"),
+            DataFusionError::ArrowError(e) => write!(f, "Arrow error: {e:?}"),
+            DataFusionError::PythonError(e) => write!(f, "Python error {e:?}"),
+            DataFusionError::Common(e) => write!(f, "{e}"),
+            DataFusionError::EncodeError(e) => write!(f, "Failed to encode substrait plan: {e}"),
+        }
+    }
+}
+
+impl From<ArrowError> for DataFusionError {
+    fn from(err: ArrowError) -> DataFusionError {
+        DataFusionError::ArrowError(err)
+    }
+}
+
+impl From<InnerDataFusionError> for DataFusionError {
+    fn from(err: InnerDataFusionError) -> DataFusionError {
+        DataFusionError::ExecutionError(err)
+    }
+}
+
+impl From<PyErr> for DataFusionError {
+    fn from(err: PyErr) -> DataFusionError {
+        DataFusionError::PythonError(err)
+    }
+}
+
+impl From<DataFusionError> for PyErr {
+    fn from(err: DataFusionError) -> PyErr {
+        match err {
+            DataFusionError::PythonError(py_err) => py_err,
+            _ => PyException::new_err(err.to_string()),
+        }
+    }
+}
+
+impl Error for DataFusionError {}
+
+pub fn py_type_err(e: impl Debug) -> PyErr {
+    PyErr::new::<pyo3::exceptions::PyTypeError, _>(format!("{e:?}"))
+}
+
+pub fn py_runtime_err(e: impl Debug) -> PyErr {
+    PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!("{e:?}"))
+}
+
+pub fn py_datafusion_err(e: impl Debug) -> PyErr {
+    PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!("{e:?}"))
+}
+
+pub fn py_unsupported_variant_err(e: impl Debug) -> PyErr {
+    PyErr::new::<pyo3::exceptions::PyValueError, _>(format!("{e:?}"))
+}
---
+++ packages/datafusion/datafusion-43.1.0.tar.gz/datafusion-43.1.0/src/utils.rs
@@ -0,0 +1,60 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+use crate::errors::DataFusionError;
+use crate::TokioRuntime;
+use datafusion::logical_expr::Volatility;
+use pyo3::prelude::*;
+use std::future::Future;
+use std::sync::OnceLock;
+use tokio::runtime::Runtime;
+
+/// Utility to get the Tokio Runtime from Python
+#[inline]
+pub(crate) fn get_tokio_runtime() -> &'static TokioRuntime {
+    // NOTE: Other pyo3 python libraries have had issues with using tokio
+    // behind a forking app-server like `gunicorn`
+    // If we run into that problem, in the future we can look to `delta-rs`
+    // which adds a check in that disallows calls from a forked process
+    // https://github.com/delta-io/delta-rs/blob/87010461cfe01563d91a4b9cd6fa468e2ad5f283/python/src/utils.rs#L10-L31
+    static RUNTIME: OnceLock<TokioRuntime> = OnceLock::new();
+    RUNTIME.get_or_init(|| TokioRuntime(tokio::runtime::Runtime::new().unwrap()))
+}
+
+/// Utility to collect rust futures with GIL released
+pub fn wait_for_future<F>(py: Python, f: F) -> F::Output
+where
+    F: Future + Send,
+    F::Output: Send,
+{
+    let runtime: &Runtime = &get_tokio_runtime().0;
+    py.allow_threads(|| runtime.block_on(f))
+}
+
+pub(crate) fn parse_volatility(value: &str) -> Result<Volatility, DataFusionError> {
+    Ok(match value {
+        "immutable" => Volatility::Immutable,
+        "stable" => Volatility::Stable,
+        "volatile" => Volatility::Volatile,
+        value => {
+            return Err(DataFusionError::Common(format!(
+                "Unsupportad volatility type: `{value}`, supported \
+                 values are: immutable, stable and volatile."
+            )))
+        }
+    })
+}
