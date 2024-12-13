371f0f601b5ee835975cd719eb753775de40560c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 05:03:39 +0000

    Add package rats-apps/0.4.0.dev20241212213929/rats_apps-0.4.0.dev20241212213929-py3-none-any.whl

---
+++ packages/rats-apps/rats_apps-0.4.0.dev20241212213929-py3-none-any.whl/rats/apps/__init__.py
@@ -0,0 +1,72 @@
+"""
+Libraries to help create applications with a strong focus on composability and testability.
+
+Applications give you the ability to define a development experience to match your project's
+domain.
+"""
+
+from ._annotations import (
+    autoid,
+    autoid_factory_service,
+    autoid_service,
+    factory_service,
+    fallback_group,
+    fallback_service,
+    group,
+    service,
+)
+from ._composite_container import CompositeContainer
+from ._container import (
+    Container,
+    DuplicateServiceError,
+    GroupProvider,
+    Provider,
+    ServiceNotFoundError,
+    ServiceProvider,
+    container,
+)
+from ._executables import App, Executable
+from ._ids import ServiceId, T_ExecutableType, T_ServiceType
+from ._namespaces import ProviderNamespaces
+from ._plugin_container import PluginContainers
+from ._plugins import PluginRunner
+from ._runtimes import NullRuntime, Runtime
+from ._scoping import autoscope
+from ._simple_apps import AppServices, SimpleApplication, StandardRuntime
+from ._static_container import StaticContainer, StaticProvider
+
+__all__ = [
+    "App",
+    "CompositeContainer",
+    "Container",
+    "StaticContainer",
+    "StaticProvider",
+    "DuplicateServiceError",
+    "Executable",
+    "PluginContainers",
+    "ProviderNamespaces",
+    "ServiceProvider",
+    "Provider",
+    "GroupProvider",
+    "ServiceId",
+    "ServiceNotFoundError",
+    "autoid_service",
+    "autoscope",
+    "container",
+    "PluginRunner",
+    "fallback_group",
+    "fallback_service",
+    "group",
+    "autoid",
+    "service",
+    "T_ExecutableType",
+    "T_ServiceType",
+    "Runtime",
+    "NullRuntime",
+    "AppServices",
+    "StandardRuntime",
+    "StandardRuntime",
+    "SimpleApplication",
+    "factory_service",
+    "autoid_factory_service",
+]
