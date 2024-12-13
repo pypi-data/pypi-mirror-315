61c1be47d9798b26ab44bc45744fefcab9e132c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 05:03:27 +0000

    Add package deephaven-plugin-plotly-express/0.12.1/deephaven_plugin_plotly_express-0.12.1-py3-none-any.whl

---
+++ packages/deephaven-plugin-plotly-express/deephaven_plugin_plotly_express-0.12.1-py3-none-any.whl/deephaven/plot/express/_js/dist/PlotlyExpressChart.js
@@ -0,0 +1,35 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import { useEffect, useRef, useState } from 'react';
+import Plotly from 'plotly.js-dist-min';
+import { Chart } from '@deephaven/chart';
+import { useApi } from '@deephaven/jsapi-bootstrap';
+import PlotlyExpressChartModel from './PlotlyExpressChartModel.js';
+import { useHandleSceneTicks } from './useHandleSceneTicks.js';
+export function PlotlyExpressChart(props) {
+    const dh = useApi();
+    const { fetch } = props;
+    const containerRef = useRef(null);
+    const [model, setModel] = useState();
+    const [widgetRevision, setWidgetRevision] = useState(0); // Used to force a clean chart state on widget change
+    useEffect(() => {
+        let cancelled = false;
+        async function init() {
+            const widgetData = await fetch();
+            if (!cancelled) {
+                setModel(new PlotlyExpressChartModel(dh, widgetData, fetch));
+                setWidgetRevision(r => r + 1);
+            }
+        }
+        init();
+        return () => {
+            cancelled = true;
+        };
+    }, [dh, fetch]);
+    useHandleSceneTicks(model, containerRef.current);
+    return model ? (_jsx(Chart
+    // eslint-disable-next-line react/jsx-props-no-spreading, @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    , { containerRef: containerRef, model: model, Plotly: Plotly }, widgetRevision)) : null;
+}
+export default PlotlyExpressChart;
+//# sourceMappingURL=PlotlyExpressChart.js.map
