691ba3c5e5198801f753008b779cdf5f9babbd3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 16:04:21 +0000

    Add package gen-vm-image/0.0.6a1/gen_vm_image-0.0.6a1-py3-none-any.whl

---
+++ packages/gen-vm-image/gen_vm_image-0.0.6a1-py3-none-any.whl/gen_vm_image/utils/net.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2024  The gen-vm-image Project by the Science HPC Center at UCPH
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import requests
+import tqdm
+
+
+def download_file(url, output_path, chunk_size=8192, verbose=False):
+    with open(output_path, "wb") as _file:
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            total = int(r.headers.get("content-length", 0))
+
+            tqdm_params = {
+                "desc": url,
+                "total": total,
+                "miniters": 1,
+                "unit": "B",
+                "unit_scale": True,
+                "unit_divisor": 1024,
+                "disable": False,
+                "dynamic_ncols": True,
+            }
+            if not verbose:
+                tqdm_params["disable"] = True
+
+            with tqdm.tqdm(**tqdm_params) as pb:
+                for chunk in r.iter_content(chunk_size=chunk_size):
+                    pb.update(len(chunk))
+                    _file.write(chunk)
