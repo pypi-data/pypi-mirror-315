a073cd2bfaf0d2ff93c4d24d242333898f06659d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 14:03:47 +0000

    Add package documente_shared/0.1.43/documente_shared-0.1.43-py3-none-any.whl

---
+++ packages/documente_shared/documente_shared-0.1.43-py3-none-any.whl/documente_shared/domain/enums.py
@@ -0,0 +1,24 @@
+from documente_shared.domain.base_enum import BaseEnum
+
+
+class DocumentProcessStatus(BaseEnum):
+    PENDING = 'PENDING'
+    ENQUEUED = 'ENQUEUED'
+    PROCESSING = 'PROCESSING'
+    COMPLETED = 'COMPLETED'
+    FAILED = 'FAILED'
+    DELETED = 'DELETED'
+    CANCELLED = 'CANCELLED'
+
+
+class DocumentProcessCategory(BaseEnum):
+    CIRCULAR = 'CIRCULAR'
+
+
+class DocumentProcessSubCategory(BaseEnum):
+    # Circulares
+    CC_COMBINADA = 'CC_COMBINADA'
+    CC_NORMATIVA = 'CC_NORMATIVA'
+    CC_INFORMATIVA = 'CC_INFORMATIVA'
+    CC_RETENCION_SUSPENSION_REMISION = 'CC_RETENCION_SUSPENSION_REMISION'
+
