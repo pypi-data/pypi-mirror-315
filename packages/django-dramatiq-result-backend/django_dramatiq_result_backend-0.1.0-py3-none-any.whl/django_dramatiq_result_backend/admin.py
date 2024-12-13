2fdc2cd497bb932ae013507fbcb464b79bbf6ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 16:04:14 +0000

    Add package django-dramatiq-result-backend/0.1.0/django_dramatiq_result_backend-0.1.0-py3-none-any.whl

---
+++ packages/django-dramatiq-result-backend/django_dramatiq_result_backend-0.1.0-py3-none-any.whl/django_dramatiq_result_backend/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from .models import Result
+
+
+# Register your models here.
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    """Admin for the Result model."""
+
+    fields = (
+        "id",
+        "message_key",
+        "result_data",
+        "created_at",
+        "expiration_time",
+        "is_expired",
+    )
+    list_display = ("id", "message_key", "created_at", "expiration_time", "is_expired")
+    list_filter = ("is_expired",)
+    search_fields = ("message_key",)
