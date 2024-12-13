8a783268c8a88e3aca707f9f07c88ff16efb5395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 15:03:58 +0000

    Add package burstlink/0.3.1/burstlink-0.3.1.tar.gz

---
+++ packages/burstlink/burstlink-0.3.1.tar.gz/burstlink-0.3.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+long_description = Path("README.md").read_text(encoding="utf-8")
+
+setup(
+    name="burstlink",                  
+    version="0.3.1",                   
+    packages=find_packages(),            
+    python_requires=">=3.9",            
+    long_description=long_description,
+    long_description_content_type="text/markdown",  
+)
