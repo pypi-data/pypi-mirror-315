09a0d323ea3492a470028ecb32ca7e6f94da37b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 15:03:42 +0000

    Add package multiqc-dumpling/0.3/multiqc_dumpling-0.3.tar.gz

---
+++ packages/multiqc-dumpling/multiqc_dumpling-0.3.tar.gz/multiqc_dumpling-0.3/multiqc_dumpling/dumpling_intialization.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import importlib_metadata
+
+import logging
+
+from multiqc import config
+
+# Initialise the main MultiQC logger
+log = logging.getLogger("multiqc")
+
+__version__ = importlib_metadata.version("multiqc_dumpling")
+config.multiqc_dumpling_version = __version__
+
+
+def dumpling_plugin_execution_start():
+    """Code to intialize the dumpling MultiQC plugin.
+
+    This adds search patterns for counts outputs and parses
+    the snakemake config file to set some parameters of the target.
+    """
+    log.info("Running dumpling MultiQC Plugin v%s", config.multiqc_dumpling_version)
+
+    # Add to the search patterns used by modules
+
+    if "dumpling/counts" not in config.sp:
+        config.update_dict(
+            config.sp,
+            {
+                "dumpling/counts": {
+                    "fn": "*.csv",
+                    "contents_re": ".*mutation,length,hgvs.*",
+                    "num_lines": 2,
+                }
+            },
+        )
+    if "gatk/analyze_saturation_mutagenesis/refcoverage" not in config.sp:
+        config.update_dict(
+            config.sp,
+            {
+                "gatk/analyze_saturation_mutagenesis/refcoverage": {
+                    "fn": "*.refCoverage"
+                }
+            },
+        )
+    if "gatk/analyze_saturation_mutagenesis/coveragelengthcounts" not in config.sp:
+        config.update_dict(
+            config.sp,
+            {
+                "gatk/analyze_saturation_mutagenesis/coveragelengthcounts": {
+                    "fn": "*.coverageLengthCounts"
+                }
+            },
+        )
+
+    # Parse the config file to get the orf length from the string
+    # The orf is given in nucleotide coordinates in the form "XX-YY",
+    # where XX and YY are integers.
+
+    coords = config.multiqc_dumpling['orf']
+    start = int(coords[0])
+    end = int(coords[1])
+    config.orf_length = end - start + 1
+
+    # Read the designed variants file to get the number of designed variants
+    # The first line is a header, so subtract 1 from the total number of lines
+
+    with open(config.multiqc_dumpling['variants_file'], "r") as f:
+        config.n_variants = sum(1 for line in f) - 1
+
+    # Add the designed variants file to the ignore list
+    config.fn_ignore_files.extend([config.multiqc_dumpling['variants_file']])
