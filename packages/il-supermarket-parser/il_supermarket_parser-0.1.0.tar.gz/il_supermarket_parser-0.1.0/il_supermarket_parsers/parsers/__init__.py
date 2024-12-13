5f8e68051453c44fcbb5e2dc23a9f3ea19b214d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 16:04:36 +0000

    Add package il-supermarket-parser/0.1.0/il_supermarket_parser-0.1.0.tar.gz

---
+++ packages/il-supermarket-parser/il_supermarket_parser-0.1.0.tar.gz/il_supermarket_parser-0.1.0/il_supermarket_parsers/parsers/__init__.py
@@ -0,0 +1,12 @@
+from il_supermarket_parsers.engines.base import BaseFileConverter
+from .bareket import BareketFileConverter
+from .city_market import CityMarketGivatayim, CityMarketKiryatGat, CityMarketShops
+from .confix import CofixFileConverter
+from .mahsani_a_shuk import MahsaniAShukPromoFileConverter
+from .salach_dabach import SalachDabachFileConverter
+from .shufersal import ShufersalFileConverter
+from .super_pharm import SuperPharmFileConverter
+from .victory import VictoryFileConverter
+from .het_cohen import HetChoenFileConverter
+from .tiv_taam import TivTaamFileConverter
+from .other import *
