f4dd99b74abf0c7791f59c02469398124e7ec20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 16:04:36 +0000

    Add package altikrity/0.2/altikrity-0.2.tar.gz

---
+++ packages/altikrity/altikrity-0.2.tar.gz/altikrity-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='altikrity',  # اسم المكتبة
+    version='0.2',  # الإصدار
+    packages=find_packages(),  # حزمة المكتبة
+    install_requires=[  # المتطلبات
+        'zlib',  # إذا كنت تستخدم مكتبات أخرى، أضفها هنا
+    ],
+    author='Abdullah',  # اسم المؤلف
+    author_email='abdullah.alttikrity@gmail.com',  # البريد الإلكتروني
+    description='A powerful encryption library with multi-layer encryption techniques.',  # وصف المكتبة
+    long_description=open('README.md').read(),  # وصف مفصل من ملف README
+    long_description_content_type='text/markdown',
+    classifiers=[  # تصنيف المكتبة
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # الحد الأدنى للإصدار المدعوم من بايثون
+)
