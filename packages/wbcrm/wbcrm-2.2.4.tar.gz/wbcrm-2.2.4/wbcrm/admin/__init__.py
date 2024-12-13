7c8951bc74eafcde62c2b3090262df8ce278c796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 16:04:19 +0000

    Add package wbcrm/2.2.4/wbcrm-2.2.4.tar.gz

---
+++ packages/wbcrm/wbcrm-2.2.4.tar.gz/wbcrm-2.2.4/wbcrm/admin/__init__.py
@@ -0,0 +1,4 @@
+from .accounts import AccountRoleModelAdmin, AccountModelAdmin
+from .activities import ActivityAdmin, ActivityTypeAdmin
+from .groups import GroupModelAdmin
+from .products import ProductAdmin
