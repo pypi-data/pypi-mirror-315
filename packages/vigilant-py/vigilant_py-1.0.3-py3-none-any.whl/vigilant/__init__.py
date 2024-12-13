f0852f8dd8557c74563cbd66cc85a51645e7a8c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 03:18:42 +0000

    Add package vigilant-py/1.0.3/vigilant_py-1.0.3-py3-none-any.whl

---
+++ packages/vigilant-py/vigilant_py-1.0.3-py3-none-any.whl/vigilant/__init__.py
@@ -0,0 +1,5 @@
+from .logger import Logger, create_logger
+from .autocapture import AutocaptureLogger, create_autocapture_logger
+
+__all__ = ['Logger', 'create_logger',
+           'AutocaptureLogger', 'create_autocapture_logger']
