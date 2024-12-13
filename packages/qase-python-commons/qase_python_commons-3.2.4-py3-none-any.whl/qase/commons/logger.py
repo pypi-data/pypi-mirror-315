3eecb40eaddc5efe098a250adcc261051f9deb20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 16:04:23 +0000

    Add package qase-python-commons/3.2.4/qase_python_commons-3.2.4-py3-none-any.whl

---
+++ packages/qase-python-commons/qase_python_commons-3.2.4-py3-none-any.whl/qase/commons/logger.py
@@ -0,0 +1,32 @@
+import datetime
+
+import os
+
+
+class Logger:
+    def __init__(self, debug: bool = False, prefix: str = '', dir: str = os.path.join('.', 'logs')) -> None:
+        self.debug = debug
+        if self.debug:
+            filename = f'{prefix}_{self._get_timestamp()}.log'
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+            self.log_file = os.path.join(dir, f'{filename}')
+            with open(self.log_file, 'w', encoding='utf-8'):
+                pass
+
+    def log(self, message: str, level: str = 'info'):
+        time_str = self._get_timestamp("%H:%M:%S")
+        log = f"[Qase][{time_str}][{level}] {message}\n"
+        print(log)
+        if self.debug:
+            with open(self.log_file, 'a', encoding='utf-8') as f:
+                f.write(log)
+
+    def log_debug(self, message: str):
+        if self.debug:
+            self.log(message, 'debug')
+
+    @staticmethod
+    def _get_timestamp(fmt: str = "%Y%m%d_%H_%M_%S"):
+        now = datetime.datetime.now()
+        return now.strftime(fmt)
