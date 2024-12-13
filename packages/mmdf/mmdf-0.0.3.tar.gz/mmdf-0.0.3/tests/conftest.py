faa1e72927d3c20621e68ab90c9e3d634107e64b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 14:03:51 +0000

    Add package mmdf/0.0.3/mmdf-0.0.3.tar.gz

---
+++ packages/mmdf/mmdf-0.0.3.tar.gz/mmdf-0.0.3/tests/conftest.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture
+def test_data_directory() -> Path:
+    return Path(__file__).parent.parent / "test_data"
+
+
+@pytest.fixture
+def test_pdb_file(test_data_directory) -> Path:
+    return test_data_directory / "4v6x.cif"
+
+
+@pytest.fixture
+def test_output_file(test_data_directory) -> Path:
+    return test_data_directory / "test_output.pdb"
