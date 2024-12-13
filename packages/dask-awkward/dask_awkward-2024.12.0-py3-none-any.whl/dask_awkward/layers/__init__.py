2be58f457c21ceef40cab4fd46078829c6322d90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 05:03:37 +0000

    Add package dask-awkward/2024.12.0/dask_awkward-2024.12.0-py3-none-any.whl

---
+++ packages/dask-awkward/dask_awkward-2024.12.0-py3-none-any.whl/dask_awkward/layers/__init__.py
@@ -0,0 +1,21 @@
+from dask_awkward.layers.layers import (
+    AwkwardBlockwiseLayer,
+    AwkwardInputLayer,
+    AwkwardMaterializedLayer,
+    AwkwardTreeReductionLayer,
+    ImplementsIOFunction,
+    ImplementsProjection,
+    IOFunctionWithMocking,
+    io_func_implements_projection,
+)
+
+__all__ = (
+    "AwkwardInputLayer",
+    "AwkwardBlockwiseLayer",
+    "AwkwardMaterializedLayer",
+    "AwkwardTreeReductionLayer",
+    "ImplementsProjection",
+    "ImplementsIOFunction",
+    "IOFunctionWithMocking",
+    "io_func_implements_projection",
+)
