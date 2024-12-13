b6223a3ae1d135c53ad3e7b60008fd64d501a908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 04:04:00 +0000

    Add package morin/0.3.20/morin-0.3.20.tar.gz

---
+++ packages/morin/morin-0.3.20.tar.gz/morin-0.3.20/morin/__init__.py
@@ -0,0 +1,6 @@
+from .wb_by_date import WBbyDate
+from .yd_by_date import YDbyDate
+from .ozon_by_date import OZONbyDate
+from .market_by_date import MRKTbyDate
+from .getcourse_by_date import GCbyDate
+from .ym_by_date import YMbyDate
