4fc5e5b49339300dd2d2f3b0ec114f564de40d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 16:04:01 +0000

    Add package inspect-ai/0.3.51/inspect_ai-0.3.51-py3-none-any.whl

---
+++ packages/inspect-ai/inspect_ai-0.3.51-py3-none-any.whl/inspect_ai/_view/www/src/Types.mjs
@@ -0,0 +1,34 @@
+/**
+ * @typedef {Object} Capabilities
+ * @property {boolean} downloadFiles
+ * @property {boolean} webWorkers
+ */
+
+/**
+ * @typedef {Object} CurrentLog
+ * @property {string} name
+ * @property {import("./api/Types.mjs").EvalSummary} contents
+ * @property {string} raw
+ */
+
+/**
+ * @typedef {Object} Logs
+ * @property {string} log_dir
+ * @property {string[]} files
+ */
+
+/**
+ * @typedef {Object} ScoreLabel
+ * @property {string} name
+ * @property {string} scorer
+ */
+
+/**
+ * @typedef {Object} ScoreFilter
+ * @property {string} [value]
+ * @property {string} [type]
+ */
+
+/**
+ * @typedef {"none" | "single" | "many"} SampleMode
+ */
