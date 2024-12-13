c1c453ce4548b8679d58a8fb0cb2b0e50cb9b486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 15:04:02 +0000

    Add package emmetify/0.0.1rc2/emmetify-0.0.1rc2-py3-none-any.whl

---
+++ packages/emmetify/emmetify-0.0.1rc2-py3-none-any.whl/emmetify/__init__.py
@@ -0,0 +1,33 @@
+from emmetify.emmetifier import Emmetifier
+from emmetify.config import __all__ as config_all
+
+
+def emmetify_html(content, format="html", **options):
+    """Convenience function for quick conversions"""
+    emmetifier = Emmetifier(format=format, **options)
+    return emmetifier.emmetify(content)
+
+
+def emmetify_compact_html(content):
+    """Convenience function for quick HTML conversion with simplified tags and attributes"""
+    emmetifier = Emmetifier(
+        format="html",
+        config={
+            "html": {
+                "skip_tags": True,
+                "prioritize_attributes": True,
+                "simplify_classes": True,
+                "simplify_links": True,
+                "simplify_images": True,
+            }
+        },
+    )
+    return emmetifier.emmetify(content)
+
+
+__all__ = [
+    "Emmetifier",
+    "emmetify_html",
+    "emmetify_compact_html",
+    *config_all,
+]
---
+++ packages/emmetify/emmetify-0.0.1rc2-py3-none-any.whl/emmetify/config/__init__.py
@@ -0,0 +1,8 @@
+from emmetify.config.base_config import EmmetifierConfig
+from emmetify.config.html_config import HtmlConfig, HtmlAttributesPriority
+
+__all__ = [
+    "EmmetifierConfig",
+    "HtmlConfig",
+    "HtmlAttributesPriority"
+]
