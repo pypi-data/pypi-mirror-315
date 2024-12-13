1e3a339a8e43709f433b06e4495fafd17df2bdd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 04:04:11 +0000

    Add package Commify/1.4.2/Commify-1.4.2-py3-none-any.whl

---
+++ packages/Commify/Commify-1.4.2-py3-none-any.whl/commify/version.py
@@ -0,0 +1,26 @@
+try:
+    import requests
+except ImportError:
+    requests = None
+
+__version__ = "1.4.2"
+
+
+def get_pypi_version(packagename: str):
+    if requests is not None:
+        try:
+            response = requests.get(f"https://pypi.org/pypi/{packagename}/json").json()
+            return response["info"]["version"]
+        except requests.RequestException as e:
+            print(f"Error: Failed to get PyPI version. Detailed error: \n{e}")
+    else:
+        return 'Error: module "requests" is not installed.'
+    
+latest_version = get_pypi_version('Commify')
+
+def check_version():
+        try:
+            if __version__ != get_pypi_version('Commify'):
+                print(f'New Commify version available: {latest_version} (current: {__version__})! ‖ pip install -U Commify\n')
+        except Exception as e:
+            print(f'Error: Failed to check Commify version. Detailed error: \n{e}')
