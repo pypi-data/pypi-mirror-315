94b3c3ca2f0325ab813effe6d5c567a1f9a8766c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 16:04:32 +0000

    Add package ej2-streamlit-grids/28.1.33/ej2_streamlit_grids-28.1.33.tar.gz

---
+++ packages/ej2-streamlit-grids/ej2_streamlit_grids-28.1.33.tar.gz/ej2_streamlit_grids-28.1.33/ej2_streamlit_grids/frontend/build/precache-manifest.256d50cb2ef556ad9ff1e4cb73d16560.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53362e45efe8a5c11b0b2f2d220c864",
+    "url": "./index.html"
+  },
+  {
+    "revision": "69e9c2efda50a27f0654",
+    "url": "./static/js/2.91d1c49f.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.91d1c49f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03cb779166d6f3bc4592",
+    "url": "./static/js/main.7fa91135.chunk.js"
+  },
+  {
+    "revision": "7db74a2cdd82e5555bee",
+    "url": "./static/js/runtime-main.d4ad5a5b.js"
+  }
+]);
