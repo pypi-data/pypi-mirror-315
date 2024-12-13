7526f6ff589743727b06b624fb37294c0d0041bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 04:03:42 +0000

    Add package coldtype/0.11.0/coldtype-0.11.0.tar.gz

---
+++ packages/coldtype/coldtype-0.11.0.tar.gz/coldtype-0.11.0/coldtype/runon/mixins/SerializationMixin.py
@@ -0,0 +1,49 @@
+import pickle, json
+
+from pathlib import Path
+from fontTools.pens.recordingPen import RecordingPen
+
+from coldtype.geometry import Rect
+
+
+class SerializationMixin():
+    def pickle(self, dst):
+        dst.parent.mkdir(parents=True, exist_ok=True)
+        fh = open(str(dst), "wb")
+        
+        def prune(pen, state, data):
+            if state >= 0:
+                if hasattr(pen, "_stst"):
+                    pen._stst = None
+        
+        self.walk(prune)
+        pickle.dump(self, fh)
+        fh.close()
+        return self
+    
+    def Unpickle(self, src):
+        if isinstance(src, str):
+            src = Path(src)
+        return pickle.load(open(str(src.expanduser()), "rb"))
+    
+    def withJSONValue(self, path):
+        self._val.value = json.loads(Path(path)
+            .expanduser()
+            .read_text())
+        return self
+    
+    def withSVG(self, svg):
+        from fontTools.svgLib import SVGPath
+        svg = SVGPath.fromstring(svg)
+        rp = RecordingPen()
+        svg.draw(rp)
+        self._val.value = rp.value
+        return self
+
+    def withSVGFile(self, svg_file):
+        from fontTools.svgLib import SVGPath
+        svg = SVGPath.fromstring(svg_file.read_bytes())
+        rp = RecordingPen()
+        svg.draw(rp)
+        self._val.value = rp.value
+        return self
