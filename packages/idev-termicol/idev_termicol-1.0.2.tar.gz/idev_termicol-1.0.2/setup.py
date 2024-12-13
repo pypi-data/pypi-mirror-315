7e636ab38328157be3af229b7754549d4052ce2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 15:03:42 +0000

    Add package idev-termicol/1.0.2/idev_termicol-1.0.2.tar.gz

---
+++ packages/idev-termicol/idev_termicol-1.0.2.tar.gz/idev_termicol-1.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "idev-termicol",
+    version = "1.0.2",
+    author = "IrtsaDevelopment",
+    author_email = "irtsa.development@gmail.com",
+    description = "A python Simple python function aimed to handle printing of colored and / or decorated text to console / terminal.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/irtsa-dev/termicol",
+    project_urls = {
+        "Bug Tracker": "https://github.com/irtsa-dev/termicol/issues",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "idev-termicol"},
+    packages = ["termicol"],
+    python_requires = ">=3.6"
+)
