0f01ac2e3ae3d2d7bb52c04863b59201c3e9ae1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 15:04:25 +0000

    Add package kala-torch/1.1.7/kala_torch-1.1.7-py3-none-any.whl

---
+++ packages/kala-torch/kala_torch-1.1.7-py3-none-any.whl/KalaTorch/__init__.py
@@ -0,0 +1,13 @@
+"""
+KalaTorch: A PyTorch-based module for easy implementation of various AI models.
+
+Author: N V R K Sai Kamesh Sharma
+Version: 1.0.4
+"""
+
+from .kala_torch import KalaTorch
+
+__all__ = ["KalaTorch"]
+
+__version__ = "1.0.4"
+__author__ = "N V R K Sai Kamesh Sharma"
