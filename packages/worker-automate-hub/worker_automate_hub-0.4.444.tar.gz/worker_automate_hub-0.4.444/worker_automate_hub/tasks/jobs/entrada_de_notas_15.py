f5546c08f67ec80a13a0619ad6304d48aeff7acc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 05:03:36 +0000

    Add package worker-automate-hub/0.4.444/worker_automate_hub-0.4.444.tar.gz

---
+++ packages/worker-automate-hub/worker_automate_hub-0.4.444.tar.gz/worker_automate_hub-0.4.444/worker_automate_hub/tasks/jobs/entrada_de_notas_15.py
@@ -0,0 +1,434 @@
+import asyncio
+import getpass
+import os
+import warnings
+
+import pyautogui
+from pywinauto.keyboard import send_keys
+from PIL import Image, ImageEnhance
+import pytesseract
+from pywinauto.application import Application
+from pywinauto_recorder.player import set_combobox
+from rich.console import Console
+
+from worker_automate_hub.api.ahead_service import save_xml_to_downloads
+from worker_automate_hub.api.client import (
+    get_config_by_name,
+    get_status_nf_emsys,
+    sync_get_config_by_name,
+)
+from worker_automate_hub.config.settings import load_env_config
+from worker_automate_hub.models.dto.rpa_historico_request_dto import (
+    RpaHistoricoStatusEnum,
+    RpaRetornoProcessoDTO,
+)
+from worker_automate_hub.models.dto.rpa_processo_entrada_dto import (
+    RpaProcessoEntradaDTO,
+)
+from worker_automate_hub.utils.logger import logger
+from worker_automate_hub.utils.util import (
+    error_after_xml_imported,
+    error_before_persist_record,
+    import_nfe,
+    is_window_open,
+    is_window_open_by_class,
+    itens_not_found_supplier,
+    kill_process,
+    login_emsys,
+    set_variable,
+    type_text_into_field,
+    verify_nf_incuded,
+    warnings_after_xml_imported,
+    worker_sleep,
+)
+from worker_automate_hub.utils.utils_nfe_entrada import EMSys
+
+pyautogui.PAUSE = 0.5
+console = Console()
+
+emsys = EMSys()
+
+
+async def entrada_de_notas_15(task: RpaProcessoEntradaDTO) -> RpaRetornoProcessoDTO:
+    """
+    Processo que relazia entrada de notas no ERP EMSys(Linx).
+
+    """
+    try:
+        # Get config from BOF
+        config = await get_config_by_name("login_emsys")
+        console.print(task)
+
+        console.print(config)
+
+        # Seta config entrada na var nota para melhor entendimento
+        nota = task.configEntrada
+        multiplicador_timeout = int(float(task.sistemas[0].timeout))
+        set_variable("timeout_multiplicador", multiplicador_timeout)
+
+        # Abre um novo emsys
+        await kill_process("EMSys")
+
+        #Verifica se a nota ja foi lançada
+        console.print("\nVerifica se a nota ja foi lançada...")
+        nf_chave_acesso = int(nota.get("nfe"))
+        status_nf_emsys = await get_status_nf_emsys(nf_chave_acesso)
+        if status_nf_emsys.get("status") == "Lançada":
+            console.print("\nNota ja lançada, processo finalizado...", style="bold green")
+            return RpaRetornoProcessoDTO(
+                sucesso=False,
+                retorno="Nota já lançada",
+                status=RpaHistoricoStatusEnum.Descartado,
+            )
+        else:
+            console.print("\nNota não lançada, iniciando o processo...")
+
+        app = Application(backend="win32").start("C:\\Rezende\\EMSys3\\EMSys3.exe")
+        warnings.filterwarnings(
+            "ignore",
+            category=UserWarning,
+            message="32-bit application should be automated using 32-bit Python",
+        )
+        console.print("\nEMSys iniciando...", style="bold green")
+        return_login = await login_emsys(config.conConfiguracao, app, task)
+
+        if return_login.sucesso == True:
+            type_text_into_field(
+                "Nota Fiscal de Entrada", app["TFrmMenuPrincipal"]["Edit"], True, "50"
+            )
+            pyautogui.press("enter")
+            await worker_sleep(1)
+            pyautogui.press("enter")
+            console.print(
+                f"\nPesquisa: 'Nota Fiscal de Entrada' realizada com sucesso",
+                style="bold green",
+            )
+        else:
+            logger.info(f"\nError Message: {return_login.retorno}")
+            console.print(f"\nError Message: {return_login.retorno}", style="bold red")
+            return return_login
+
+        await worker_sleep(10)
+
+        # Procura campo documento
+        console.print("Navegando pela Janela de Nota Fiscal de Entrada...\n")
+        app = Application().connect(title="Nota Fiscal de Entrada")
+        main_window = app["Nota Fiscal de Entrada"]
+
+        console.print(
+            "Controles encontrados na janela 'Nota Fiscal de Entrada', navegando entre eles...\n"
+        )
+        panel_TNotebook = main_window.child_window(
+            class_name="TNotebook", found_index=0
+        )
+        panel_TPage = panel_TNotebook.child_window(class_name="TPage", found_index=0)
+        panel_TPageControl = panel_TPage.child_window(
+            class_name="TPageControl", found_index=0
+        )
+        panel_TTabSheet = panel_TPageControl.child_window(
+            class_name="TTabSheet", found_index=0
+        )
+        combo_box_tipo_documento = panel_TTabSheet.child_window(
+            class_name="TDBIComboBox", found_index=1
+        )
+
+        combo_box_tipo_documento.click()
+        console.print(
+            "Clique select box, Tipo de documento realizado com sucesso, selecionando o tipo de documento...\n"
+        )
+
+        await worker_sleep(4)
+
+        set_combobox("||List", "NOTA FISCAL DE ENTRADA ELETRONICA - DANFE")
+        console.print(
+            "Tipo de documento 'NOTA FISCAL DE ENTRADA ELETRONICA - DANFE', selecionado com sucesso...\n"
+        )
+
+        await worker_sleep(4)
+
+        # Clica em 'Importar-Nfe'
+        imported_nfe = await import_nfe()
+        if imported_nfe.sucesso == True:
+            console.log(imported_nfe.retorno, style="bold green")
+        else:
+            return RpaRetornoProcessoDTO(
+                sucesso=False,
+                retorno=imported_nfe.retorno,
+                status=RpaHistoricoStatusEnum.Falha,
+            )
+
+        await worker_sleep(10)
+
+        # Download XML
+        await save_xml_to_downloads(nota["nfe"])
+
+        # Permanece 'XML'
+        # Clica em  'OK' para selecionar
+        pyautogui.click(970, 666)
+        await worker_sleep(3)
+
+        # Click Downloads
+        await emsys.get_xml(nota["nfe"])
+        await worker_sleep(15)
+
+        # VERIFICANDO A EXISTENCIA DE WARNINGS
+        warning_pop_up = await is_window_open("Warning")
+        if warning_pop_up["IsOpened"] == True:
+            warning_work = await warnings_after_xml_imported()
+            if warning_work.sucesso == True:
+                console.log(warning_work.retorno, style="bold green")
+            else:
+                return RpaRetornoProcessoDTO(
+                    sucesso=False,
+                    retorno=warning_work.retorno,
+                    status=RpaHistoricoStatusEnum.Falha,
+                )
+
+        # VERIFICANDO A EXISTENCIA DE ERRO
+        erro_pop_up = await is_window_open("Erro")
+        if erro_pop_up["IsOpened"] == True:
+            error_work = await error_after_xml_imported()
+            return RpaRetornoProcessoDTO(
+                sucesso=error_work.sucesso,
+                retorno=error_work.retorno,
+                status=error_work.status,
+            )
+
+        # Deleta o xml
+        await emsys.delete_xml(nota.get("nfe"))
+        await worker_sleep(5)
+
+        app = Application().connect(
+            title="Informações para importação da Nota Fiscal Eletrônica"
+        )
+        main_window = app["Informações para importação da Nota Fiscal Eletrônica"]
+
+        combo_box_natureza_operacao = main_window.child_window(
+            class_name="TDBIComboBox", found_index=0
+        )
+        combo_box_natureza_operacao.click()
+        await worker_sleep(4)
+        set_combobox("||List", "1652-COMPRA DE MERCADORIAS- 1.652")
+        await worker_sleep(3)
+
+        # INTERAGINDO COM O CAMPO ALMOXARIFADO
+        filial_empresa_origem = nota.get("filialEmpresaOrigem")
+        valor_almoxarifado = filial_empresa_origem + "50"
+        pyautogui.press("tab")
+        pyautogui.write(valor_almoxarifado)
+        await worker_sleep(2)
+        pyautogui.press("tab")
+
+        await worker_sleep(10)
+        console.print("Clicando em OK... \n")
+
+        max_attempts = 3
+        i = 0
+        while i < max_attempts:
+            console.print("Clicando no botão de OK...\n")
+            try:
+                try:
+                    btn_ok = main_window.child_window(title="Ok")
+                    btn_ok.click()
+                except:
+                    btn_ok = main_window.child_window(title="&Ok")
+                    btn_ok.click()
+            except:
+                console.print("Não foi possivel clicar no Botão OK... \n")
+
+            await worker_sleep(1)
+            i += 1
+        
+        await worker_sleep(2)
+
+        try:
+            console.print(
+                "Verificando a existencia de POP-UP de Itens não localizados ou NCM ...\n"
+            )
+            itens_by_supplier = await is_window_open_by_class("TFrmAguarde", "TMessageForm")
+            if itens_by_supplier["IsOpened"] == True:
+                itens_by_supplier_work = await itens_not_found_supplier(nota.get("nfe"))
+                if itens_by_supplier_work.get("window") == "NCM":
+                    console.log(itens_by_supplier_work.get("retorno"), style="bold green")
+                else:
+                    return RpaRetornoProcessoDTO(
+                        sucesso=False,
+                        retorno=itens_by_supplier_work.get("retorno"),
+                        status=RpaHistoricoStatusEnum.Falha,
+                    )
+        except Exception as error:
+            return RpaRetornoProcessoDTO(
+                sucesso=False,
+                retorno=f"Falha ao verificar a existência de POP-UP de itens não localizados: {error}",
+                status=RpaHistoricoStatusEnum.Falha,
+            )
+
+        await worker_sleep(3)
+
+        console.print("Navegando pela Janela de Nota Fiscal de Entrada...\n")
+        app = Application().connect(class_name="TFrmNotaFiscalEntrada")
+        main_window = app["TFrmNotaFiscalEntrada"]
+
+        main_window.set_focus()
+
+        await emsys.verify_warning_and_error("Information", "&No")
+
+        await worker_sleep(10)
+        await emsys.select_tipo_cobranca()
+        await emsys.inserir_vencimento_e_valor(
+            nota.get("nomeFornecedor"),
+            nota.get("dataEmissao"),
+            nota.get("dataVencimento"),
+            nota.get("valorNota"),
+        )
+        await worker_sleep(8)
+        await emsys.incluir_registro()
+        await worker_sleep(5)
+
+        await emsys.verify_warning_and_error("Warning", "No")
+        await emsys.verify_warning_and_error("Warning", "&No")
+
+        try:
+            erro_pop_up = await is_window_open("Information")
+            if erro_pop_up["IsOpened"] == True:
+                error_work = await error_before_persist_record()
+                return RpaRetornoProcessoDTO(
+                    sucesso=error_work.sucesso,
+                    retorno=error_work.retorno,
+                    status=error_work.status,
+                )
+        except:
+            console.print("Erros não foram encontrados após incluir o registro")
+
+        # Verifica se a info 'Nota fiscal incluida' está na tela
+        await worker_sleep(6)
+        retorno = False
+        try:
+            app = Application().connect(class_name="TFrmNotaFiscalEntrada")
+            main_window = app["Information"]
+
+            main_window.set_focus()
+
+            console.print(f"Tentando clicar no Botão OK...\n")
+            btn_ok = main_window.child_window(class_name="TButton")
+
+            if btn_ok.exists():
+                btn_ok.click()
+                retorno = True
+            else:
+                console.print(f" botão OK Não enontrado")
+                retorno = await verify_nf_incuded()
+
+        except Exception as e:
+            try:
+                await worker_sleep(5)
+                await emsys.verify_warning_and_error("Aviso", "OK")
+                alterar_nop = await emsys.alterar_nop(nota["cfop"])
+
+                if alterar_nop:
+                    return alterar_nop
+            except Exception as error:
+                observacao = f"Erro Processo Entrada de Notas: {str(error)}"
+                logger.error(observacao)
+                console.print(observacao, style="bold red")
+                return RpaRetornoProcessoDTO(
+                    sucesso=False,
+                    retorno=observacao,
+                    status=RpaHistoricoStatusEnum.Falha,
+                )
+
+        try:
+            max_attempts = 60
+            i = 0
+
+            while i < max_attempts:
+                information_pop_up = await is_window_open("Information")
+                if information_pop_up["IsOpened"] == True:
+                    break
+                else:
+                    console.print(f"Aguardando confirmação de nota incluida...\n")
+                    await worker_sleep(5)
+                    i += 1
+
+            information_pop_up = await is_window_open("Information")
+            if information_pop_up["IsOpened"] == True:
+                app = Application().connect(class_name="TFrmNotaFiscalEntrada")
+                main_window = app["Information"]
+
+                main_window.set_focus()
+
+                console.print(f"Obtendo texto do Information...\n")
+                console.print(f"Tirando print da janela do Information para realização do OCR...\n")
+
+                window_rect = main_window.rectangle()
+                screenshot = pyautogui.screenshot(
+                    region=(
+                        window_rect.left,
+                        window_rect.top,
+                        window_rect.width(),
+                        window_rect.height(),
+                    )
+                )
+                username = getpass.getuser()
+                path_to_png = f"C:\\Users\\{username}\\Downloads\\information_popup_{nota.get("nfe")}.png"
+                screenshot.save(path_to_png)
+                console.print(f"Print salvo em {path_to_png}...\n")
+
+                console.print(
+                    f"Preparando a imagem para maior resolução e assertividade no OCR...\n"
+                )
+                image = Image.open(path_to_png)
+                image = image.convert("L")
+                enhancer = ImageEnhance.Contrast(image)
+                image = enhancer.enhance(2.0)
+                image.save(path_to_png)
+                console.print(f"Imagem preparada com sucesso...\n")
+                console.print(f"Realizando OCR...\n")
+                captured_text = pytesseract.image_to_string(Image.open(path_to_png))
+                console.print(
+                    f"Texto Full capturado {captured_text}...\n"
+                )
+                os.remove(path_to_png)
+                if 'nota fiscal inc' in captured_text.lower():
+                    console.print(f"Tentando clicar no Botão OK...\n")
+                    btn_ok = main_window.child_window(class_name="TButton")
+
+                    if btn_ok.exists():
+                        btn_ok.click()
+                        retorno = True
+                else:
+                    return RpaRetornoProcessoDTO(
+                        sucesso=False,
+                        retorno=f"Pop_up Informantion não mapeado para andamento do robô, mensagem {captured_text}",
+                        status=RpaHistoricoStatusEnum.Falha,
+                    )
+            else:
+                console.print(f"Aba Information não encontrada")
+                retorno = await verify_nf_incuded()
+
+        except Exception as e:
+            console.print(f"Erro ao conectar à janela Information: {e}\n")
+            return RpaRetornoProcessoDTO(
+                sucesso=False,
+                retorno=f"Erro em obter o retorno, Nota inserida com sucesso, erro {e}",
+                status=RpaHistoricoStatusEnum.Falha,
+            )
+
+        if retorno:
+            console.print("\nNota lançada com sucesso...", style="bold green")
+            await worker_sleep(6)
+            return RpaRetornoProcessoDTO(
+                sucesso=True,
+                retorno="Nota Lançada com sucesso!",
+                status=RpaHistoricoStatusEnum.Sucesso,
+            )
+
+    except Exception as ex:
+        observacao = f"Erro Processo Entrada de Notas: {str(ex)}"
+        logger.error(observacao)
+        console.print(observacao, style="bold red")
+        return RpaRetornoProcessoDTO(
+            sucesso=False,
+            retorno=observacao,
+            status=RpaHistoricoStatusEnum.Falha,
+        )
