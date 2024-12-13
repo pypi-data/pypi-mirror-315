0a643ac8e5afa3a2fcc5315be6c4c5d4d199a4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 14:04:24 +0000

    Add package omlish/0.0.0.dev157/omlish-0.0.0.dev157.tar.gz

---
+++ packages/omlish/omlish-0.0.0.dev157.tar.gz/omlish-0.0.0.dev157/omlish/lite/runtime.py
@@ -0,0 +1,17 @@
+import inspect
+import sys
+
+from .cached import cached_nullary
+
+
+@cached_nullary
+def is_debugger_attached() -> bool:
+    return any(frame[1].endswith('pydevd.py') for frame in inspect.stack())
+
+
+REQUIRED_PYTHON_VERSION = (3, 8)
+
+
+def check_runtime_version() -> None:
+    if sys.version_info < REQUIRED_PYTHON_VERSION:
+        raise OSError(f'Requires python {REQUIRED_PYTHON_VERSION}, got {sys.version_info} from {sys.executable}')  # noqa
