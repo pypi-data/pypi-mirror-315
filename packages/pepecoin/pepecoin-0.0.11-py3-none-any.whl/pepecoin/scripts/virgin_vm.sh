1760761599570863eea04b4c324ab564ab5d6a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 05:03:45 +0000

    Add package pepecoin/0.0.11/pepecoin-0.0.11-py3-none-any.whl

---
+++ packages/pepecoin/pepecoin-0.0.11-py3-none-any.whl/pepecoin/scripts/virgin_vm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get --assume-yes upgrade
+sudo apt-get --assume-yes install software-properties-common
+sudo apt-get --assume-yes install jq
+sudo apt-get --assume-yes install build-essential
+sudo apt-get --assume-yes install linux-headers-$(uname -r)
+wget https://repo.anaconda.com/miniconda/Miniconda3-py311_24.9.2-0-Linux-x86_64.sh
+chmod +x Miniconda3-py311_24.9.2-0-Linux-x86_64.sh
+./Miniconda3-py311_24.9.2-0-Linux-x86_64.sh -b -p $HOME/miniconda3
+~/miniconda3/bin/conda init bash
+source .bashrc
