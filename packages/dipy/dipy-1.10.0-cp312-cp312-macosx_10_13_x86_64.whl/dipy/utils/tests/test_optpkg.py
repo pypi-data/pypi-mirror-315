846aca0dcb54701d927d7b870381c4686e0fa4c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 04:04:12 +0000

    Add package dipy/1.10.0/dipy-1.10.0-cp312-cp312-macosx_10_13_x86_64.whl

---
+++ packages/dipy/dipy-1.10.0-cp312-cp312-macosx_10_13_x86_64.whl/dipy/utils/tests/test_optpkg.py
@@ -0,0 +1,24 @@
+import pytest
+
+from dipy.testing import assert_false, assert_true
+from dipy.utils.optpkg import optional_package
+from dipy.utils.tripwire import TripWireError
+
+
+def test_optional_package():
+    pkg, have_pkg, setup_module = optional_package("os")
+    assert_true(have_pkg)
+    assert_true(hasattr(pkg, "path"))
+
+    pkg, have_pkg, setup_module = optional_package("not_a_package")
+    assert_false(have_pkg)
+    with pytest.raises(TripWireError):
+        pkg.some_function()
+
+    pkg, have_pkg, setup_module = optional_package("dipy", min_version="10.0.0")
+    assert_false(have_pkg)
+    with pytest.raises(TripWireError):
+        pkg.some_function()
+
+    pkg, have_pkg, setup_module = optional_package("dipy", min_version="1.0.0")
+    assert_true(have_pkg)
