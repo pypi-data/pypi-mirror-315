82378cae272cae5101f9df10b861b6b1c6d07b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 04:04:11 +0000

    Add package monisha/0.0.104b3/monisha-0.0.104b3.tar.gz

---
+++ packages/monisha/monisha-0.0.104b3.tar.gz/monisha-0.0.104b3/Monisha/functions/function12.py
@@ -0,0 +1,5 @@
+class YKeys(object):
+
+    DATA01 = "%(title,fulltitle,alt_title)s%(season_number& |)s%(season_number&S|)s%(season_number|)02d%(episode_number&E|)s%(episode_number|)02d.%(ext)s"
+    DATA02 = "%(title,fulltitle,alt_title)s%(season_number& |)s%(season_number&S|)s%(season_number|)02d%(episode_number&E|)s%(episode_number|)02d%(height& |)s%(height|)s%(height&p|)s%(fps|)s%(fps&fps|)s%(tbr& |)s%(tbr|)d.%(ext)s"
+            
