805e58fb43c834980985a7aaf74953583415eb2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 04:04:03 +0000

    Add package bakerstreet/2.1.0/bakerstreet-2.1.0.tar.gz

---
+++ packages/bakerstreet/bakerstreet-2.1.0.tar.gz/bakerstreet-2.1.0/scripts/deploy.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#
+# deploy.sh
+# Copyright (C) 2015 dhilipsiva <dhilipsiva@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+export CURRENT_BRANCH
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+bumpversion patch
+git push origin "$CURRENT_BRANCH:$CURRENT_BRANCH"
+git push --tags
+rm -rf dist/
+python setup.py sdist
+python setup.py bdist_wheel
+twine upload dist/*
