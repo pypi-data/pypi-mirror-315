bdbf2fc86a819e5f3bf546d312e046a18e0a6749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 16:04:24 +0000

    Add package agentflux/0.0.1.4/agentflux-0.0.1.4-py3-none-any.whl

---
+++ packages/agentflux/agentflux-0.0.1.4-py3-none-any.whl/agentflux/__init__.py
@@ -0,0 +1,8 @@
+from agentflux.agent_manager import AgentFilesManager,AgentManager,ToolManager
+from agentflux.qagent_manager import QAgentManager
+from agentflux.agent_manager import InfoManager
+from agentflux.manage.agent_create import AgentlDefine
+from agentflux.manage.tool_create import ToolDefine
+
+from agentflux.core.llms.vllm import OpenAIVLLM,image_to_base64
+from agentflux.core.llms.llm import OpenAILLM,LLMRequestsOllama
