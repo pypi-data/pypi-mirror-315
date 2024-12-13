7aa729a87ca01c8d924d3e31571ee118205bb35a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-13 16:04:21 +0000

    Add package schemapack/2.0.0a5/schemapack-2.0.0a5-py3-none-any.whl

---
+++ packages/schemapack/schemapack-2.0.0a5-py3-none-any.whl/schemapack/_internals/validation/default.py
@@ -0,0 +1,58 @@
+# Copyright 2021 - 2024 Universität Tübingen, DKFZ, EMBL, and Universität zu Köln
+# for the German Human Genome-Phenome Archive (GHGA)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""Default Validation Plugin Registry"""
+
+from schemapack._internals.validation.base import (
+    ClassValidationPlugin,
+    GlobalValidationPlugin,
+    ResourceValidationPlugin,
+)
+from schemapack._internals.validation.plugins import (
+    ContentSchemaValidationPlugin,
+    ExpectedRootValidationPlugin,
+    MissingClassSlotValidationPlugin,
+    MissingMandatoryOriginValidationPlugin,
+    MissingMandatoryTargetValidationPlugin,
+    MissingRelationValidationPlugin,
+    MultipleTargetValidationPlugin,
+    TargetIdValidationPlugin,
+    TargetOverlapValidationPlugin,
+    UnexpectedRootValidationPlugin,
+    UnknownClassSlotValidationPlugin,
+    UnknownRelationValidationPlugin,
+    UnkownRootResourceValidationPlugin,
+)
+
+DEFAULT_GLOBAL_PLUGIN_REGISTRY: list[type[GlobalValidationPlugin]] = [
+    MissingClassSlotValidationPlugin,
+    UnknownClassSlotValidationPlugin,
+    ExpectedRootValidationPlugin,
+    UnkownRootResourceValidationPlugin,
+    UnexpectedRootValidationPlugin,
+]
+DEFAULT_CLASS_PLUGIN_REGISTRY: list[type[ClassValidationPlugin]] = [
+    TargetOverlapValidationPlugin,
+    MissingMandatoryOriginValidationPlugin,
+]
+DEFAULT_RESOURCE_PLUGIN_REGISTRY: list[type[ResourceValidationPlugin]] = [
+    ContentSchemaValidationPlugin,
+    TargetIdValidationPlugin,
+    MissingRelationValidationPlugin,
+    UnknownRelationValidationPlugin,
+    MultipleTargetValidationPlugin,
+    MissingMandatoryTargetValidationPlugin,
+]
