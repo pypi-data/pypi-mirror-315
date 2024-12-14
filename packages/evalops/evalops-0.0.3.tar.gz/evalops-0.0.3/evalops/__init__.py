611a6b9b57124c59e4a3e6b624369eb44bcde636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 15:04:16 +0000

    Add package evalops/0.0.3/evalops-0.0.3.tar.gz

---
+++ packages/evalops/evalops-0.0.3.tar.gz/evalops-0.0.3/evalops/__init__.py
@@ -0,0 +1,9 @@
+from evalops.main import StatisticalModelEvaluator
+from evalops.huggingface_loader import EvalDatasetLoader
+from evalops.wrapper import eval
+
+__all__ = [
+    "StatisticalModelEvaluator",
+    "EvalDatasetLoader",
+    "eval"
+]
