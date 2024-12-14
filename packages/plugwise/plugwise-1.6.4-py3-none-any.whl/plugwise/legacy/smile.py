4767ace0edbefb138c74762ec29e74b062a6c41b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 15:04:50 +0000

    Add package plugwise/1.6.4/plugwise-1.6.4-py3-none-any.whl

---
+++ packages/plugwise/plugwise-1.6.4-py3-none-any.whl/plugwise/legacy/smile.py
@@ -0,0 +1,314 @@
+"""Use of this source code is governed by the MIT license found in the LICENSE file.
+
+Plugwise backend module for Home Assistant Core - covering the legacy P1, Anna, and Stretch devices.
+"""
+
+from __future__ import annotations
+
+from collections.abc import Awaitable, Callable
+import datetime as dt
+from typing import Any
+
+from plugwise.constants import (
+    APPLIANCES,
+    DEFAULT_PORT,
+    DEFAULT_USERNAME,
+    DOMAIN_OBJECTS,
+    LOCATIONS,
+    LOGGER,
+    MODULES,
+    OFF,
+    REQUIRE_APPLIANCES,
+    RULES,
+    GatewayData,
+    GwEntityData,
+    PlugwiseData,
+    ThermoLoc,
+)
+from plugwise.exceptions import ConnectionFailedError, DataMissingError, PlugwiseError
+from plugwise.legacy.data import SmileLegacyData
+
+import aiohttp
+from munch import Munch
+from packaging.version import Version
+
+
+class SmileLegacyAPI(SmileLegacyData):
+    """The Plugwise SmileLegacyAPI class."""
+
+    # pylint: disable=too-many-instance-attributes, too-many-public-methods
+
+    def __init__(
+        self,
+        host: str,
+        password: str,
+        request: Callable[..., Awaitable[Any]],
+        websession: aiohttp.ClientSession,
+        _is_thermostat: bool,
+        _loc_data: dict[str, ThermoLoc],
+        _on_off_device: bool,
+        _opentherm_device: bool,
+        _stretch_v2: bool,
+        _target_smile: str,
+        smile_fw_version: Version | None,
+        smile_hostname: str,
+        smile_hw_version: str | None,
+        smile_mac_address: str | None,
+        smile_model: str,
+        smile_name: str,
+        smile_type: str,
+        smile_zigbee_mac_address: str | None,
+        port: int = DEFAULT_PORT,
+        username: str = DEFAULT_USERNAME,
+    ) -> None:
+        """Set the constructor for this class."""
+        self._cooling_present = False
+        self._is_thermostat = _is_thermostat
+        self._loc_data = _loc_data
+        self._on_off_device = _on_off_device
+        self._opentherm_device = _opentherm_device
+        self._stretch_v2 = _stretch_v2
+        self._target_smile = _target_smile
+        self.request = request
+        self.smile_fw_version = smile_fw_version
+        self.smile_hostname = smile_hostname
+        self.smile_hw_version = smile_hw_version
+        self.smile_mac_address = smile_mac_address
+        self.smile_model = smile_model
+        self.smile_name = smile_name
+        self.smile_type = smile_type
+        self.smile_zigbee_mac_address = smile_zigbee_mac_address
+        SmileLegacyData.__init__(self)
+
+        self._previous_day_number: str = "0"
+
+    async def full_xml_update(self) -> None:
+        """Perform a first fetch of all XML data, needed for initialization."""
+        self._domain_objects = await self.request(DOMAIN_OBJECTS)
+        self._locations = await self.request(LOCATIONS)
+        self._modules = await self.request(MODULES)
+        # P1 legacy has no appliances
+        if self.smile_type != "power":
+            self._appliances = await self.request(APPLIANCES)
+
+    def get_all_gateway_entities(self) -> None:
+        """Collect the gateway entities from the received raw XML-data.
+
+        Run this functions once to gather the initial device configuration,
+        then regularly run async_update() to refresh the device data.
+        """
+        # Gather all the devices and their initial data
+        self._all_appliances()
+
+        # Collect and add switching- and/or pump-group devices
+        if group_data := self._get_group_switches():
+            self.gw_entities.update(group_data)
+
+        # Collect the remaining data for all entities
+        self._all_entity_data()
+
+    async def async_update(self) -> PlugwiseData:
+        """Perform an incremental update for updating the various device states."""
+        # Perform a full update at day-change
+        day_number = dt.datetime.now().strftime("%w")
+        if (
+            day_number  # pylint: disable=consider-using-assignment-expr
+            != self._previous_day_number
+        ):
+            LOGGER.info(
+                "Performing daily full-update, reload the Plugwise integration when a single entity becomes unavailable."
+            )
+            self.gw_data: GatewayData = {}
+            self.gw_entities: dict[str, GwEntityData] = {}
+            try:
+                await self.full_xml_update()
+                self.get_all_gateway_entities()
+                # Detect failed data-retrieval
+                _ = self.gw_entities[self.gateway_id]["location"]
+            except KeyError as err:  # pragma: no cover
+                raise DataMissingError(
+                    "No (full) Plugwise legacy data received"
+                ) from err
+        # Otherwise perform an incremental update
+        else:
+            try:
+                self._domain_objects = await self.request(DOMAIN_OBJECTS)
+                match self._target_smile:
+                    case "smile_v2":
+                        self._modules = await self.request(MODULES)
+                    case self._target_smile if self._target_smile in REQUIRE_APPLIANCES:
+                        self._appliances = await self.request(APPLIANCES)
+
+                self._update_gw_entities()
+                # Detect failed data-retrieval
+                _ = self.gw_entities[self.gateway_id]["location"]
+            except KeyError as err:  # pragma: no cover
+                raise DataMissingError("No legacy Plugwise data received") from err
+
+        self._previous_day_number = day_number
+        return PlugwiseData(
+            devices=self.gw_entities,
+            gateway=self.gw_data,
+        )
+
+    ########################################################################################################
+    ###  API Set and HA Service-related Functions                                                        ###
+    ########################################################################################################
+
+    async def delete_notification(self) -> None:
+        """Set-function placeholder for legacy devices."""
+
+    async def reboot_gateway(self) -> None:
+        """Set-function placeholder for legacy devices."""
+
+    async def set_dhw_mode(self, mode: str) -> None:
+        """Set-function placeholder for legacy devices."""
+
+    async def set_gateway_mode(self, mode: str) -> None:
+        """Set-function placeholder for legacy devices."""
+
+    async def set_number(
+        self,
+        dev_id: str,
+        key: str,
+        temperature: float,
+    ) -> None:
+        """Set-function placeholder for legacy devices."""
+
+    async def set_offset(self, dev_id: str, offset: float) -> None:
+        """Set-function placeholder for legacy devices."""
+
+    async def set_preset(self, _: str, preset: str) -> None:
+        """Set the given Preset on the relevant Thermostat - from DOMAIN_OBJECTS."""
+        if (presets := self._presets()) is None:
+            raise PlugwiseError("Plugwise: no presets available.")  # pragma: no cover
+        if preset not in list(presets):
+            raise PlugwiseError("Plugwise: invalid preset.")
+
+        locator = f'rule/directives/when/then[@icon="{preset}"].../.../...'
+        rule = self._domain_objects.find(locator)
+        data = f'<rules><rule id="{rule.attrib["id"]}"><active>true</active></rule></rules>'
+
+        await self.call_request(RULES, method="put", data=data)
+
+    async def set_regulation_mode(self, mode: str) -> None:
+        """Set-function placeholder for legacy devices."""
+
+    async def set_select(
+        self, key: str, loc_id: str, option: str, state: str | None
+    ) -> None:
+        """Set the thermostat schedule option."""
+        # schedule name corresponds to select option
+        await self.set_schedule_state("dummy", state, option)
+
+    async def set_schedule_state(
+        self, _: str, state: str | None, name: str | None
+    ) -> None:
+        """Activate/deactivate the Schedule.
+
+        Determined from - DOMAIN_OBJECTS.
+        Used in HA Core to set the hvac_mode: in practice switch between schedule on - off.
+        """
+        if state not in ("on", "off"):
+            raise PlugwiseError("Plugwise: invalid schedule state.")
+
+        # Handle no schedule-name / Off-schedule provided
+        if name is None or name == OFF:
+            name = "Thermostat schedule"
+
+        schedule_rule_id: str | None = None
+        for rule in self._domain_objects.findall("rule"):
+            if rule.find("name").text == name:
+                schedule_rule_id = rule.attrib["id"]
+
+        if schedule_rule_id is None:
+            raise PlugwiseError(
+                "Plugwise: no schedule with this name available."
+            )  # pragma: no cover
+
+        new_state = "false"
+        if state == "on":
+            new_state = "true"
+
+        locator = f'.//*[@id="{schedule_rule_id}"]/template'
+        for rule in self._domain_objects.findall(locator):
+            template_id = rule.attrib["id"]
+
+        uri = f"{RULES};id={schedule_rule_id}"
+        data = (
+            "<rules><rule"
+            f' id="{schedule_rule_id}"><name><![CDATA[{name}]]></name><template'
+            f' id="{template_id}" /><active>{new_state}</active></rule></rules>'
+        )
+
+        await self.call_request(uri, method="put", data=data)
+
+    async def set_switch_state(
+        self, appl_id: str, members: list[str] | None, model: str, state: str
+    ) -> None:
+        """Set the given State of the relevant Switch."""
+        switch = Munch()
+        switch.actuator = "actuator_functionalities"
+        switch.func_type = "relay_functionality"
+        if self._stretch_v2:
+            switch.actuator = "actuators"
+            switch.func_type = "relay"
+        switch.func = "state"
+
+        if members is not None:
+            return await self._set_groupswitch_member_state(members, state, switch)
+
+        data = f"<{switch.func_type}><{switch.func}>{state}</{switch.func}></{switch.func_type}>"
+        uri = f"{APPLIANCES};id={appl_id}/{switch.func_type}"
+
+        if model == "relay":
+            locator = (
+                f'appliance[@id="{appl_id}"]/{switch.actuator}/{switch.func_type}/lock'
+            )
+            # Don't bother switching a relay when the corresponding lock-state is true
+            if self._appliances.find(locator).text == "true":
+                raise PlugwiseError("Plugwise: the locked Relay was not switched.")
+
+        await self.call_request(uri, method="put", data=data)
+
+    async def _set_groupswitch_member_state(
+        self, members: list[str], state: str, switch: Munch
+    ) -> None:
+        """Helper-function for set_switch_state().
+
+        Set the given State of the relevant Switch within a group of members.
+        """
+        for member in members:
+            uri = f"{APPLIANCES};id={member}/{switch.func_type}"
+            data = f"<{switch.func_type}><{switch.func}>{state}</{switch.func}></{switch.func_type}>"
+
+            await self.call_request(uri, method="put", data=data)
+
+    async def set_temperature(self, _: str, items: dict[str, float]) -> None:
+        """Set the given Temperature on the relevant Thermostat."""
+        setpoint: float | None = None
+        if "setpoint" in items:
+            setpoint = items["setpoint"]
+
+        if setpoint is None:
+            raise PlugwiseError(
+                "Plugwise: failed setting temperature: no valid input provided"
+            )  # pragma: no cover"
+
+        temperature = str(setpoint)
+        uri = self._thermostat_uri()
+        data = (
+            "<thermostat_functionality><setpoint>"
+            f"{temperature}</setpoint></thermostat_functionality>"
+        )
+
+        await self.call_request(uri, method="put", data=data)
+
+    async def call_request(self, uri: str, **kwargs: Any) -> None:
+        """ConnectionFailedError wrapper for calling request()."""
+        method: str = kwargs["method"]
+        data: str | None = kwargs.get("data")
+        try:
+            await self.request(uri, method=method, data=data)
+        except ConnectionFailedError as exc:
+            raise ConnectionFailedError from exc
---
+++ packages/plugwise/plugwise-1.6.4-py3-none-any.whl/plugwise/util.py
@@ -0,0 +1,248 @@
+"""Plugwise protocol helpers."""
+
+from __future__ import annotations
+
+import datetime as dt
+import re
+from typing import cast
+
+from plugwise.constants import (
+    ATTR_UNIT_OF_MEASUREMENT,
+    BINARY_SENSORS,
+    DATA,
+    ELECTRIC_POTENTIAL_VOLT,
+    ENERGY_KILO_WATT_HOUR,
+    HW_MODELS,
+    OBSOLETE_MEASUREMENTS,
+    PERCENTAGE,
+    POWER_WATT,
+    SENSORS,
+    SPECIAL_FORMAT,
+    SPECIALS,
+    SWITCHES,
+    TEMP_CELSIUS,
+    UOM,
+    BinarySensorType,
+    GwEntityData,
+    ModuleData,
+    SensorType,
+    SpecialType,
+    SwitchType,
+)
+
+from defusedxml import ElementTree as etree
+from munch import Munch
+
+
+def check_alternative_location(loc: Munch, legacy: bool) -> Munch:
+    """Helper-function for _power_data_peak_value()."""
+    if in_alternative_location(loc, legacy):
+        # Avoid double processing by skipping one peak-list option
+        if loc.peak_select == "nl_offpeak":
+            loc.found = False
+            return loc
+
+        loc.locator = f'./{loc.log_type}[type="{loc.measurement}"]/period/measurement'
+        if legacy:
+            loc.locator = (
+                f"./{loc.meas_list[0]}_{loc.log_type}/"
+                f'measurement[@directionality="{loc.meas_list[1]}"]'
+            )
+
+        if loc.logs.find(loc.locator) is None:
+            loc.found = False
+            return loc
+
+        return loc
+
+    loc.found = False
+    return loc
+
+
+def in_alternative_location(loc: Munch, legacy: bool) -> bool:
+    """Look for P1 gas_consumed or phase data (without tariff).
+
+    For legacy look for P1 legacy electricity_point_meter or gas_*_meter data.
+    """
+    present = "log" in loc.log_type and (
+        "gas" in loc.measurement or "phase" in loc.measurement
+    )
+    if legacy:
+        present = "meter" in loc.log_type and (
+            "point" in loc.log_type or "gas" in loc.measurement
+        )
+
+    return present
+
+
+def check_heater_central(xml: etree) -> str:
+    """Find the valid heater_central, helper-function for _appliance_info_finder().
+
+    Solution for Core Issue #104433,
+    for a system that has two heater_central appliances.
+    """
+    locator = "./appliance[type='heater_central']"
+    hc_count = 0
+    hc_list: list[dict[str, bool]] = []
+    for heater_central in xml.findall(locator):
+        hc_count += 1
+        hc_id: str = heater_central.attrib["id"]
+        has_actuators: bool = (
+            heater_central.find("actuator_functionalities/") is not None
+        )
+        # Filter for Plug/Circle/Stealth heater_central -- Pw-Beta Issue #739
+        if heater_central.find("name").text == "Central heating boiler":
+            hc_list.append({hc_id: has_actuators})
+
+    heater_central_id = list(hc_list[0].keys())[0]
+    if hc_count > 1:
+        for item in hc_list:  # pragma: no cover
+            for key, value in item.items():  # pragma: no cover
+                if value:  # pragma: no cover
+                    heater_central_id = key  # pragma: no cover
+                    # Stop when a valid id is found
+                    break  # pragma: no cover
+
+    return heater_central_id
+
+
+def check_model(name: str | None, vendor_name: str | None) -> str | None:
+    """Model checking before using version_to_model."""
+    if vendor_name == "Plugwise" and ((model := version_to_model(name)) != "Unknown"):
+        return model
+
+    if name is not None and "lumi.plug" in name:
+        return "Aqara Smart Plug"
+
+    return None
+
+
+def common_match_cases(
+    measurement: str,
+    attrs: DATA | UOM,
+    location: etree,
+    data: GwEntityData,
+) -> None:
+    """Helper-function for common match-case execution."""
+    value = location.text in ("on", "true")
+    match measurement:
+        case _ as measurement if measurement in BINARY_SENSORS:
+            bs_key = cast(BinarySensorType, measurement)
+            data["binary_sensors"][bs_key] = value
+        case _ as measurement if measurement in SENSORS:
+            s_key = cast(SensorType, measurement)
+            s_value = format_measure(
+                location.text, getattr(attrs, ATTR_UNIT_OF_MEASUREMENT)
+            )
+            data["sensors"][s_key] = s_value
+        case _ as measurement if measurement in SWITCHES:
+            sw_key = cast(SwitchType, measurement)
+            data["switches"][sw_key] = value
+        case _ as measurement if measurement in SPECIALS:
+            sp_key = cast(SpecialType, measurement)
+            data[sp_key] = value
+
+    if "battery" in data["sensors"]:
+        data["binary_sensors"]["low_battery"] = False
+
+
+def escape_illegal_xml_characters(xmldata: str) -> str:
+    """Replace illegal &-characters."""
+    return re.sub(r"&([^a-zA-Z#])", r"&amp;\1", xmldata)
+
+
+def format_measure(measure: str, unit: str) -> float | int:
+    """Format measure to correct type."""
+    result: float | int = 0
+    try:
+        result = int(measure)
+        if unit == TEMP_CELSIUS:
+            result = float(measure)
+    except ValueError:
+        float_measure = float(measure)
+        if unit == PERCENTAGE and 0 < float_measure <= 1:
+            return int(float_measure * 100)
+
+        if unit == ENERGY_KILO_WATT_HOUR:
+            float_measure = float_measure / 1000
+
+        if unit in SPECIAL_FORMAT:
+            result = float(f"{round(float_measure, 3):.3f}")
+        elif unit == ELECTRIC_POTENTIAL_VOLT:
+            result = float(f"{round(float_measure, 1):.1f}")
+        elif abs(float_measure) < 10:
+            result = float(f"{round(float_measure, 2):.2f}")
+        elif abs(float_measure) >= 10:
+            result = float(f"{round(float_measure, 1):.1f}")
+
+    return result
+
+
+def get_vendor_name(module: etree, model_data: ModuleData) -> ModuleData:
+    """Helper-function for _get_model_data()."""
+    if (vendor_name := module.find("vendor_name").text) is not None:
+        model_data["vendor_name"] = vendor_name
+        if "Plugwise" in vendor_name:
+            model_data["vendor_name"] = vendor_name.split(" ", 1)[0]
+
+    return model_data
+
+
+def power_data_local_format(
+    attrs: dict[str, str], key_string: str, val: str
+) -> float | int:
+    """Format power data."""
+    # Special formatting of P1_MEASUREMENT POWER_WATT values, do not move to util-format_measure() function!
+    if all(item in key_string for item in ("electricity", "cumulative")):
+        return format_measure(val, ENERGY_KILO_WATT_HOUR)
+    if (attrs_uom := getattr(attrs, ATTR_UNIT_OF_MEASUREMENT)) == POWER_WATT:
+        return int(round(float(val)))
+
+    return format_measure(val, attrs_uom)
+
+
+def remove_empty_platform_dicts(data: GwEntityData) -> None:
+    """Helper-function for removing any empty platform dicts."""
+    if not data["binary_sensors"]:
+        data.pop("binary_sensors")
+    if not data["sensors"]:
+        data.pop("sensors")
+    if not data["switches"]:
+        data.pop("switches")
+
+
+def return_valid(value: etree | None, default: etree) -> etree:
+    """Return default when value is None."""
+    return value if value is not None else default
+
+
+def skip_obsolete_measurements(xml: etree, measurement: str) -> bool:
+    """Skipping known obsolete measurements."""
+    locator = f".//logs/point_log[type='{measurement}']/updated_date"
+    if (
+        measurement in OBSOLETE_MEASUREMENTS
+        and (updated_date_key := xml.find(locator)) is not None
+    ):
+        updated_date = updated_date_key.text.split("T")[0]
+        date_1 = dt.datetime.strptime(updated_date, "%Y-%m-%d")
+        date_2 = dt.datetime.now()
+        return int((date_2 - date_1).days) > 7
+
+    return False
+
+
+# NOTE: this function version_to_model is shared between Smile and USB
+def version_to_model(version: str | None) -> str | None:
+    """Translate hardware_version to device type."""
+
+    if version is None:
+        return version
+
+    model = HW_MODELS.get(version)
+    if model is None:
+        model = HW_MODELS.get(version[4:10])
+    if model is None:
+        # Try again with reversed order
+        model = HW_MODELS.get(version[-2:] + version[-4:-2] + version[-6:-4])
+
+    return model if model is not None else "Unknown"
