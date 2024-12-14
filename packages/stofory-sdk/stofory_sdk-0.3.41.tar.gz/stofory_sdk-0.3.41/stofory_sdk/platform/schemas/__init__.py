171fc36835e77fc1bb87fa05a54c1fea2453ee38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 04:05:27 +0000

    Add package stofory-sdk/0.3.41/stofory_sdk-0.3.41.tar.gz

---
+++ packages/stofory-sdk/stofory_sdk-0.3.41.tar.gz/stofory_sdk-0.3.41/stofory_sdk/platform/schemas/__init__.py
@@ -0,0 +1,12 @@
+from .preset_schema import PlatformInfoResponse, LanguagesInfoResponse
+from .products_schema import CatalogProductsRequest, CatalogProductsRegion, CatalogProductsResponse, ProductInfoResponse
+
+__all__ = (
+    PlatformInfoResponse,
+    LanguagesInfoResponse,
+    CatalogProductsRequest,
+    CatalogProductsRegion,
+    CatalogProductsResponse,
+    ProductInfoResponse
+
+)
