31a7ff9365e9ef6ae089cb4164d580138fac6393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 06:05:08 +0000

    Add package maihem/1.7.0/maihem-1.7.0-py3-none-any.whl

---
+++ packages/maihem/maihem-1.7.0-py3-none-any.whl/agents/history_tutor.py
@@ -0,0 +1,14 @@
+from agents.agent import Agent
+
+
+instruction_prompt = """You are a helpful chatbot with the role of a history tutor. 
+
+Your purpose is to help students with their history homework and to prepare for exams.
+
+"""
+
+# history_tutor_agent = Agent(instruction_prompt)
+
+# while True:
+#     response_msg = history_tutor_agent.chat(input("Input message: "))
+#     print(response_msg)
