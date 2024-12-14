24e9b8dc669f97d5fdf98936f5fc12f05604a041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 15:04:49 +0000

    Add package makefilename/0.1.0/makefilename-0.1.0-py3-none-any.whl

---
+++ packages/makefilename/makefilename-0.1.0-py3-none-any.whl/makefilename/main.py
@@ -0,0 +1,30 @@
+def make_filename(
+    name: str,
+    extension: str = "",
+    max_length: int = 100,
+    ) -> str:
+    characters_to_remove = [
+        "\\", "/", ".", "'", "\"", "™", "`", "!", "?", "®", "©", "*", 
+        "<", ">", "(", ")", "[", "]", "{", "}", "#", "$", "%", "^", 
+        "€", "£", "¥", "§", "°", "¬", "¦", "´", "¨" ,"\0", ","
+    ]  
+    for character in characters_to_remove:
+        name = name.replace(character, "")
+
+    characters_to_replace_with_space = ["|", "~", "-", "–", "—", "―", ":", ";", "：", "+"]
+    for character in characters_to_replace_with_space:
+        name = name.replace(character, " ")
+  
+    name = name.replace("&", " and ")
+
+    name = '_'.join(name.split())
+
+    if len(name) > max_length:
+        name = name[:max_length]
+
+    if len(name) > 0 and name[-1] == "_":
+        name = name[:-1]
+
+    name = name + extension
+
+    return name
