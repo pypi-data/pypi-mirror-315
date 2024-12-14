260a26bb996864e5c0f5e49518fd741d1ed79886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 03:14:37 +0000

    Add package mag-tools/0.1.35/mag_tools-0.1.35.tar.gz

---
+++ packages/mag-tools/mag_tools-0.1.35.tar.gz/mag_tools-0.1.35/mag_tools/model/common/log_type.py
@@ -0,0 +1,8 @@
+from model.base_enum import BaseEnum
+
+
+class LogType(BaseEnum):
+    ROOT = ('root', '根')
+    FRAME = ('frame', '框架')
+    SERVICE = ('service', '服务')
+    PERFORMANCE = ('performance', '性能')
