b60f3043b23b701ed41c9e1a39ad25687808d411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 14:05:38 +0000

    Add package ominfra/0.0.0.dev160/ominfra-0.0.0.dev160-py3-none-any.whl

---
+++ packages/ominfra/ominfra-0.0.0.dev160-py3-none-any.whl/ominfra/manage/deploy/inject.py
@@ -0,0 +1,51 @@
+# ruff: noqa: UP006 UP007
+import os.path
+import typing as ta
+
+from omlish.lite.inject import InjectorBindingOrBindings
+from omlish.lite.inject import InjectorBindings
+from omlish.lite.inject import inj
+
+from ..commands.inject import bind_command
+from .apps import DeployAppManager
+from .atomics import DeployAtomicPathSwapping
+from .commands import DeployCommand
+from .commands import DeployCommandExecutor
+from .config import DeployConfig
+from .git import DeployGitManager
+from .interp import InterpCommand
+from .interp import InterpCommandExecutor
+from .tmp import DeployTmpManager
+from .types import DeployHome
+from .venvs import DeployVenvManager
+
+
+def bind_deploy(
+        *,
+        deploy_config: DeployConfig,
+) -> InjectorBindings:
+    lst: ta.List[InjectorBindingOrBindings] = [
+        inj.bind(deploy_config),
+
+        #
+
+        inj.bind(DeployAppManager, singleton=True),
+
+        inj.bind(DeployGitManager, singleton=True),
+
+        inj.bind(DeployTmpManager, singleton=True),
+        inj.bind(DeployAtomicPathSwapping, to_key=DeployTmpManager),
+
+        inj.bind(DeployVenvManager, singleton=True),
+
+        #
+
+        bind_command(DeployCommand, DeployCommandExecutor),
+        bind_command(InterpCommand, InterpCommandExecutor),
+    ]
+
+    if (dh := deploy_config.deploy_home) is not None:
+        dh = os.path.abspath(os.path.expanduser(dh))
+        lst.append(inj.bind(dh, key=DeployHome))
+
+    return inj.as_bindings(*lst)
---
+++ packages/ominfra/ominfra-0.0.0.dev160-py3-none-any.whl/ominfra/manage/deploy/specs.py
@@ -0,0 +1,56 @@
+# ruff: noqa: UP006 UP007
+import dataclasses as dc
+import hashlib
+import typing as ta
+
+from omlish.lite.cached import cached_nullary
+from omlish.lite.check import check
+
+from .types import DeployApp
+from .types import DeployKey
+from .types import DeployRev
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class DeployGitRepo:
+    host: ta.Optional[str] = None
+    username: ta.Optional[str] = None
+    path: ta.Optional[str] = None
+
+    def __post_init__(self) -> None:
+        check.not_in('..', check.non_empty_str(self.host))
+        check.not_in('.', check.non_empty_str(self.path))
+
+
+@dc.dataclass(frozen=True)
+class DeployGitCheckout:
+    repo: DeployGitRepo
+    rev: DeployRev
+
+    subtrees: ta.Optional[ta.Sequence[str]] = None
+
+    def __post_init__(self) -> None:
+        hash(self)
+        check.non_empty_str(self.rev)
+        if self.subtrees is not None:
+            for st in self.subtrees:
+                check.non_empty_str(st)
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class DeploySpec:
+    app: DeployApp
+    checkout: DeployGitCheckout
+
+    def __post_init__(self) -> None:
+        hash(self)
+
+    @cached_nullary
+    def key(self) -> DeployKey:
+        return DeployKey(hashlib.sha256(repr(self).encode('utf-8')).hexdigest()[:8])
