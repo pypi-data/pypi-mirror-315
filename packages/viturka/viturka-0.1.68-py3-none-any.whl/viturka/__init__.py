c74896d85377ca5ce11e0950988f7bf1c8e822b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 06:06:16 +0000

    Add package viturka/0.1.68/viturka-0.1.68-py3-none-any.whl

---
+++ packages/viturka/viturka-0.1.68-py3-none-any.whl/viturka/__init__.py
@@ -0,0 +1,4 @@
+from .FM_als import train_model, evaluate_model, find_similar_items
+from .client import ModelUploader
+from .collaborative_filtering import SVDPPModel
+from .utils import combine_dat_files_general
