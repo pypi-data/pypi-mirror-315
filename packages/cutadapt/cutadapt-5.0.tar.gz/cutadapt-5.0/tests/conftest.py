d355b3eedc8ce4a804e346818f74efc41ce34519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 14:04:29 +0000

    Add package cutadapt/5.0/cutadapt-5.0.tar.gz

---
+++ packages/cutadapt/cutadapt-5.0.tar.gz/cutadapt-5.0/tests/conftest.py
@@ -0,0 +1,28 @@
+import os
+
+import pytest
+from utils import assert_files_equal, datapath, cutpath
+from cutadapt.cli import main
+from cutadapt.report import Statistics
+
+
+@pytest.fixture(params=[1, 2])
+def cores(request):
+    return request.param
+
+
+@pytest.fixture
+def run(tmp_path):
+    def _run(params, expected, inpath) -> Statistics:
+        if type(params) is str:
+            params = params.split()
+        params += ["--json", os.fspath(tmp_path / "stats.cutadapt.json")]
+        tmp_fastaq = tmp_path / expected
+        params += ["-o", tmp_fastaq]
+        params += [datapath(inpath)]
+        stats = main([str(p) for p in params])
+        # TODO redirect standard output
+        assert_files_equal(cutpath(expected), tmp_fastaq)
+        return stats
+
+    return _run
