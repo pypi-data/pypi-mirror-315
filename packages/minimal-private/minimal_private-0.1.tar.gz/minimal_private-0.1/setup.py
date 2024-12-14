efa4df36b8809d4ca0efb9b724b0f662417046a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 04:04:04 +0000

    Add package minimal-private/0.1/minimal_private-0.1.tar.gz

---
+++ packages/minimal-private/minimal_private-0.1.tar.gz/minimal_private-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='minimal-private',
+      version='0.1',
+      #url='https://github.com/jfdev001/minimal-private',
+      license='MIT',
+      packages=['minimal_private'],
+      zip_safe=False)
