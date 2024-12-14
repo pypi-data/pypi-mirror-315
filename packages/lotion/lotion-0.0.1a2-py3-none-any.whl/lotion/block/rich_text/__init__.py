2a1d05f5fa610477c706451c8a3b4546b7d49cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 04:03:53 +0000

    Add package lotion/0.0.1a2/lotion-0.0.1a2-py3-none-any.whl

---
+++ packages/lotion/lotion-0.0.1a2-py3-none-any.whl/lotion/block/rich_text/__init__.py
@@ -0,0 +1,3 @@
+from .rich_text import RichText
+from .rich_text_element import RichTextElement
+from .rich_text_builder import RichTextBuilder
---
+++ packages/lotion/lotion-0.0.1a2-py3-none-any.whl/lotion/filter/condition/number_condition.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from enum import Enum
+
+from lotion.properties.property import Property
+
+from .condition import Condition
+
+
+class NumberConditionType(Enum):
+    EQUALS= "equals"
+
+@dataclass(frozen=True)
+class NumberCondition(Condition):
+    property_name: str
+    property_type: str
+    condition_type: NumberConditionType
+    value: str
+
+    @staticmethod
+    def equal(property: Property) -> "NumberCondition":
+        return NumberCondition(
+            property_name=property.name,
+            property_type=property.type,
+            condition_type=NumberConditionType.EQUALS,
+            value=property.value_for_filter(),
+        )
+
+    def __dict__(self) -> dict:
+        return {
+            "property": self.property_name,
+            self.property_type: {
+                self.condition_type.value: self.value,
+            },
+        }
