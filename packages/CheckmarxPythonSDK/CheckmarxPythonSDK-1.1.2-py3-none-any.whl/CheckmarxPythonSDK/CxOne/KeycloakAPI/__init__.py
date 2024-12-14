3e4e9c6fefe3dce29e78b7f38a502679c9dc408d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 14:04:31 +0000

    Add package CheckmarxPythonSDK/1.1.2/CheckmarxPythonSDK-1.1.2-py3-none-any.whl

---
+++ packages/CheckmarxPythonSDK/CheckmarxPythonSDK-1.1.2-py3-none-any.whl/CheckmarxPythonSDK/CxOne/KeycloakAPI/__init__.py
@@ -0,0 +1,24 @@
+from .RootAPI import get_realms
+from .UsersAPI import (
+    get_users,
+    create_a_new_user
+)
+
+from .GroupsAPI import (
+    get_group_hierarchy,
+    create_group_set,
+    get_group_by_name,
+    get_number_of_groups_in_a_realm,
+    get_group_by_id,
+    update_group_by_id,
+    delete_group_by_id,
+    create_subgroup,
+    get_group_permissions,
+    update_group_permissions,
+    get_group_members,
+    create_group,
+)
+
+from .RoleMapperAPI import (
+    get_role_mappings,
+)
