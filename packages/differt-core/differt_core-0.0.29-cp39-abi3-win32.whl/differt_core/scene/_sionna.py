e71e1e06f090bebb9d0bc52c001b53e20aae920f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 04:04:07 +0000

    Add package differt-core/0.0.29/differt_core-0.0.29-cp39-abi3-win32.whl

---
+++ packages/differt-core/differt_core-0.0.29-cp39-abi3-win32.whl/differt_core/scene/_sionna.py
@@ -0,0 +1,7 @@
+__all__ = ("Material", "Shape", "SionnaScene")
+
+from differt_core import _lowlevel
+
+Material = _lowlevel.scene.sionna.Material
+Shape = _lowlevel.scene.sionna.Shape
+SionnaScene = _lowlevel.scene.sionna.SionnaScene
