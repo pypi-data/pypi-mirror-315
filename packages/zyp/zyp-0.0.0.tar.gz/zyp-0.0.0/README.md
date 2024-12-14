2b2cbca2fee8859b692c028a7150d74eaf7fc3d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 14:04:40 +0000

    Add package zyp/0.0.0/zyp-0.0.0.tar.gz

---
+++ packages/zyp/zyp-0.0.0.tar.gz/zyp-0.0.0/README.md
@@ -0,0 +1,13 @@
+# zyp
+
+The fastest Python toolchain.
+
+## Installation
+
+```bash
+pip install zyp
+```
+
+## Usage
+
+TODO
---
+++ packages/zyp/zyp-0.0.0.tar.gz/zyp-0.0.0/src/zyp/cli.py
@@ -0,0 +1,20 @@
+"""CLI interface for zyp."""
+from __future__ import annotations
+
+import argparse
+
+from zyp import greet
+
+
+class CLIArgs:
+    name: str
+
+
+def cli(argv: list[str] | None = None) -> int:
+    """CLI interface."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name")
+    args = parser.parse_args(argv, namespace=CLIArgs)
+
+    print(greet(args.name))
+    return 0
