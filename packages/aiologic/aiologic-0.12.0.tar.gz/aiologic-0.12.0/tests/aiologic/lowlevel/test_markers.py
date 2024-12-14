94dfa4a5644b5787a9555b709fadb208c860f1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 15:04:16 +0000

    Add package aiologic/0.12.0/aiologic-0.12.0.tar.gz

---
+++ packages/aiologic/aiologic-0.12.0.tar.gz/aiologic-0.12.0/tests/aiologic/lowlevel/test_markers.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import pickle
+
+import pytest
+import aiologic.lowlevel
+
+
+class _TestMarker:
+    def test_base(self, /):
+        assert type(self.value)() is self.value  # singleton
+        assert repr(self.value) == self.name
+        assert not self.value
+
+    def test_attrs(self, /):
+        with pytest.raises(AttributeError):
+            self.value.nonexistent_attribute
+        with pytest.raises(AttributeError):
+            self.value.nonexistent_attribute = 42
+        with pytest.raises(AttributeError):
+            del self.value.nonexistent_attribute
+
+    def test_pickling(self, /):
+        assert pickle.loads(pickle.dumps(self.value)) is self.value
+
+    def test_inheritance(self, /):
+        with pytest.raises(TypeError):
+
+            class MarkerType(type(self.value)):
+                pass
+
+
+class TestNone(_TestMarker):
+    name = "None"
+    value = None
+
+
+class TestMissing(_TestMarker):  # like None
+    name = "MISSING"
+    value = aiologic.lowlevel.MISSING
