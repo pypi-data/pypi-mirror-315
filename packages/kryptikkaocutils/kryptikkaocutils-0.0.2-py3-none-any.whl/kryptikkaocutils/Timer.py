16d524bf9c8596b116c8a6370c5343342d894976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 03:14:53 +0000

    Add package kryptikkaocutils/0.0.2/kryptikkaocutils-0.0.2-py3-none-any.whl

---
+++ packages/kryptikkaocutils/kryptikkaocutils-0.0.2-py3-none-any.whl/kryptikkaocutils/Timer.py
@@ -0,0 +1,12 @@
+import time
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = func(*args, **kwargs)
+        t2 = time.time() - t1
+        print(f'{func.__name__} ran in {t2 * 1000:.6f} milliseconds')
+        return result
+
+    return wrapper
