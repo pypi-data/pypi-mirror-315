786b7f2cdd933bf2a5ffb93d7b2b2430d8f86f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 14:05:02 +0000

    Add package uv/0.5.9/uv-0.5.9.tar.gz

---
+++ packages/uv/uv-0.5.9.tar.gz/uv-0.5.9/crates/uv-resolver/src/pubgrub/priority.rs
@@ -0,0 +1,116 @@
+use std::cmp::Reverse;
+
+use pubgrub::Range;
+use rustc_hash::FxHashMap;
+
+use crate::fork_urls::ForkUrls;
+use uv_normalize::PackageName;
+use uv_pep440::Version;
+
+use crate::pubgrub::package::PubGrubPackage;
+use crate::pubgrub::PubGrubPackageInner;
+
+/// A prioritization map to guide the PubGrub resolution process.
+///
+/// During resolution, PubGrub needs to decide which package to consider next. The priorities
+/// encoded here are used to guide that decision.
+///
+/// Like `pip`, we prefer packages that are pinned to direct URLs over packages pinned to a single
+/// version over packages that are constrained in some way over packages that are unconstrained.
+///
+/// See: <https://github.com/pypa/pip/blob/ef78c129b1a966dbbbdb8ebfffc43723e89110d1/src/pip/_internal/resolution/resolvelib/provider.py#L120>
+#[derive(Clone, Debug, Default)]
+pub(crate) struct PubGrubPriorities(FxHashMap<PackageName, PubGrubPriority>);
+
+impl PubGrubPriorities {
+    /// Add a [`PubGrubPackage`] to the priority map.
+    pub(crate) fn insert(
+        &mut self,
+        package: &PubGrubPackage,
+        version: &Range<Version>,
+        urls: &ForkUrls,
+    ) {
+        let next = self.0.len();
+        // The root package and Python constraints have no explicit priority, the root package is
+        // always first and the Python version (range) is fixed.
+        let Some(name) = package.name_no_root() else {
+            return;
+        };
+
+        match self.0.entry(name.clone()) {
+            std::collections::hash_map::Entry::Occupied(mut entry) => {
+                // Preserve the original index.
+                let index = match entry.get() {
+                    PubGrubPriority::Unspecified(Reverse(index)) => *index,
+                    PubGrubPriority::Singleton(Reverse(index)) => *index,
+                    PubGrubPriority::DirectUrl(Reverse(index)) => *index,
+                    PubGrubPriority::Root => next,
+                };
+
+                // Compute the priority.
+                let priority = if urls.get(name).is_some() {
+                    PubGrubPriority::DirectUrl(Reverse(index))
+                } else if version.as_singleton().is_some() {
+                    PubGrubPriority::Singleton(Reverse(index))
+                } else {
+                    PubGrubPriority::Unspecified(Reverse(index))
+                };
+
+                // Take the maximum of the new and existing priorities.
+                if priority > *entry.get() {
+                    entry.insert(priority);
+                }
+            }
+            std::collections::hash_map::Entry::Vacant(entry) => {
+                // Compute the priority.
+                let priority = if urls.get(name).is_some() {
+                    PubGrubPriority::DirectUrl(Reverse(next))
+                } else if version.as_singleton().is_some() {
+                    PubGrubPriority::Singleton(Reverse(next))
+                } else {
+                    PubGrubPriority::Unspecified(Reverse(next))
+                };
+
+                // Insert the priority.
+                entry.insert(priority);
+            }
+        }
+    }
+
+    /// Return the [`PubGrubPriority`] of the given package, if it exists.
+    pub(crate) fn get(&self, package: &PubGrubPackage) -> Option<PubGrubPriority> {
+        match &**package {
+            PubGrubPackageInner::Root(_) => Some(PubGrubPriority::Root),
+            PubGrubPackageInner::Python(_) => Some(PubGrubPriority::Root),
+            PubGrubPackageInner::Marker { name, .. } => self.0.get(name).copied(),
+            PubGrubPackageInner::Extra { name, .. } => self.0.get(name).copied(),
+            PubGrubPackageInner::Dev { name, .. } => self.0.get(name).copied(),
+            PubGrubPackageInner::Package { name, .. } => self.0.get(name).copied(),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub(crate) enum PubGrubPriority {
+    /// The package has no specific priority.
+    ///
+    /// As such, its priority is based on the order in which the packages were added (FIFO), such
+    /// that the first package we visit is prioritized over subsequent packages.
+    ///
+    /// TODO(charlie): Prefer constrained over unconstrained packages, if they're at the same depth
+    /// in the dependency graph.
+    Unspecified(Reverse<usize>),
+
+    /// The version range is constrained to a single version (e.g., with the `==` operator).
+    Singleton(Reverse<usize>),
+
+    /// The package was specified via a direct URL.
+    ///
+    /// N.B.: URLs need to have priority over registry distributions for correctly matching registry
+    /// distributions to URLs, see [`PubGrubPackage::from_package`] an
+    /// [`crate::fork_urls::ForkUrls`].
+    DirectUrl(Reverse<usize>),
+
+    /// The package is the root package.
+    Root,
+}
---
+++ packages/uv/uv-0.5.9.tar.gz/uv-0.5.9/crates/uv-types/src/traits.rs
@@ -0,0 +1,183 @@
+use std::future::Future;
+use std::path::{Path, PathBuf};
+use uv_distribution_filename::DistFilename;
+
+use anyhow::Result;
+
+use uv_cache::Cache;
+use uv_configuration::{
+    BuildKind, BuildOptions, BuildOutput, ConfigSettings, LowerBound, SourceStrategy,
+};
+use uv_distribution_types::{
+    CachedDist, DependencyMetadata, IndexCapabilities, IndexLocations, InstalledDist, Resolution,
+    SourceDist,
+};
+use uv_git::GitResolver;
+use uv_pep508::PackageName;
+use uv_pypi_types::Requirement;
+use uv_python::{Interpreter, PythonEnvironment};
+
+///  Avoids cyclic crate dependencies between resolver, installer and builder.
+///
+/// To resolve the dependencies of a packages, we may need to build one or more source
+/// distributions. To building a source distribution, we need to create a virtual environment from
+/// the same base python as we use for the root resolution, resolve the build requirements
+/// (potentially which nested source distributions, recursing a level deeper), installing
+/// them and then build. The installer, the resolver and the source distribution builder are each in
+/// their own crate. To avoid circular crate dependencies, this type dispatches between the three
+/// crates with its three main methods ([`BuildContext::resolve`], [`BuildContext::install`] and
+/// [`BuildContext::setup_build`]).
+///
+/// The overall main crate structure looks like this:
+///
+/// ```text
+///                    ┌────────────────┐
+///                    │       uv       │
+///                    └───────▲────────┘
+///                            │
+///                            │
+///                    ┌───────┴────────┐
+///         ┌─────────►│  uv-dispatch   │◄─────────┐
+///         │          └───────▲────────┘          │
+///         │                  │                   │
+///         │                  │                   │
+/// ┌───────┴────────┐ ┌───────┴────────┐ ┌────────┴───────┐
+/// │  uv-resolver   │ │  uv-installer  │ │    uv-build    │
+/// └───────▲────────┘ └───────▲────────┘ └────────▲───────┘
+///         │                  │                   │
+///         └─────────────┐    │    ┌──────────────┘
+///                    ┌──┴────┴────┴───┐
+///                    │    uv-types   │
+///                    └────────────────┘
+/// ```
+///
+/// Put in a different way, the types here allow `uv-resolver` to depend on `uv-build` and
+/// `uv-build-frontend` to depend on `uv-resolver` without having actual crate dependencies between
+/// them.
+pub trait BuildContext {
+    type SourceDistBuilder: SourceBuildTrait;
+
+    /// Return a reference to the interpreter.
+    fn interpreter(&self) -> &Interpreter;
+
+    /// Return a reference to the cache.
+    fn cache(&self) -> &Cache;
+
+    /// Return a reference to the Git resolver.
+    fn git(&self) -> &GitResolver;
+
+    /// Return a reference to the discovered registry capabilities.
+    fn capabilities(&self) -> &IndexCapabilities;
+
+    /// Return a reference to any pre-defined static metadata.
+    fn dependency_metadata(&self) -> &DependencyMetadata;
+
+    /// Whether source distribution building or pre-built wheels is disabled.
+    ///
+    /// This [`BuildContext::setup_build`] calls will fail if builds are disabled.
+    /// This method exists to avoid fetching source distributions if we know we can't build them.
+    fn build_options(&self) -> &BuildOptions;
+
+    /// The [`ConfigSettings`] used to build distributions.
+    fn config_settings(&self) -> &ConfigSettings;
+
+    /// Whether to warn on missing lower bounds.
+    fn bounds(&self) -> LowerBound;
+
+    /// Whether to incorporate `tool.uv.sources` when resolving requirements.
+    fn sources(&self) -> SourceStrategy;
+
+    /// The index locations being searched.
+    fn locations(&self) -> &IndexLocations;
+
+    /// Resolve the given requirements into a ready-to-install set of package versions.
+    fn resolve<'a>(
+        &'a self,
+        requirements: &'a [Requirement],
+    ) -> impl Future<Output = Result<Resolution>> + 'a;
+
+    /// Install the given set of package versions into the virtual environment. The environment must
+    /// use the same base Python as [`BuildContext::interpreter`]
+    fn install<'a>(
+        &'a self,
+        resolution: &'a Resolution,
+        venv: &'a PythonEnvironment,
+    ) -> impl Future<Output = Result<Vec<CachedDist>>> + 'a;
+
+    /// Set up a source distribution build by installing the required dependencies. A wrapper for
+    /// `uv_build::SourceBuild::setup`.
+    ///
+    /// For PEP 517 builds, this calls `get_requires_for_build_wheel`.
+    ///
+    /// `version_id` is for error reporting only.
+    /// `dist` is for safety checks and may be null for editable builds.
+    fn setup_build<'a>(
+        &'a self,
+        source: &'a Path,
+        subdirectory: Option<&'a Path>,
+        install_path: &'a Path,
+        version_id: Option<&'a str>,
+        dist: Option<&'a SourceDist>,
+        sources: SourceStrategy,
+        build_kind: BuildKind,
+        build_output: BuildOutput,
+    ) -> impl Future<Output = Result<Self::SourceDistBuilder>> + 'a;
+
+    /// Build by calling directly into the uv build backend without PEP 517, if possible.
+    ///
+    /// Checks if the source tree uses uv as build backend. If not, it returns `Ok(None)`, otherwise
+    /// it builds and returns the name of the built file.
+    ///
+    /// `version_id` is for error reporting only.
+    fn direct_build<'a>(
+        &'a self,
+        source: &'a Path,
+        subdirectory: Option<&'a Path>,
+        output_dir: &'a Path,
+        build_kind: BuildKind,
+        version_id: Option<&'a str>,
+    ) -> impl Future<Output = Result<Option<DistFilename>>> + 'a;
+}
+
+/// A wrapper for `uv_build::SourceBuild` to avoid cyclical crate dependencies.
+///
+/// You can either call only `wheel()` to build the wheel directly, call only `metadata()` to get
+/// the metadata without performing the actual or first call `metadata()` and then `wheel()`.
+pub trait SourceBuildTrait {
+    /// A wrapper for `uv_build::SourceBuild::get_metadata_without_build`.
+    ///
+    /// For PEP 517 builds, this calls `prepare_metadata_for_build_wheel`
+    ///
+    /// Returns the metadata directory if we're having a PEP 517 build and the
+    /// `prepare_metadata_for_build_wheel` hook exists
+    fn metadata(&mut self) -> impl Future<Output = Result<Option<PathBuf>>>;
+
+    /// A wrapper for `uv_build::SourceBuild::build`.
+    ///
+    /// For PEP 517 builds, this calls `build_wheel`.
+    ///
+    /// Returns the filename of the built wheel inside the given `wheel_dir`. The filename is a
+    /// string and not a `WheelFilename` because the on disk filename might not be normalized in the
+    /// same way as uv would.
+    fn wheel<'a>(&'a self, wheel_dir: &'a Path) -> impl Future<Output = Result<String>> + 'a;
+}
+
+/// A wrapper for [`uv_installer::SitePackages`]
+pub trait InstalledPackagesProvider: Clone + Send + Sync + 'static {
+    fn iter(&self) -> impl Iterator<Item = &InstalledDist>;
+    fn get_packages(&self, name: &PackageName) -> Vec<&InstalledDist>;
+}
+
+/// An [`InstalledPackagesProvider`] with no packages in it.
+#[derive(Clone)]
+pub struct EmptyInstalledPackages;
+
+impl InstalledPackagesProvider for EmptyInstalledPackages {
+    fn get_packages(&self, _name: &PackageName) -> Vec<&InstalledDist> {
+        Vec::new()
+    }
+
+    fn iter(&self) -> impl Iterator<Item = &InstalledDist> {
+        std::iter::empty()
+    }
+}
