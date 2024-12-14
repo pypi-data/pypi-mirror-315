3082022b3977231b929328a348542d29219d082f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 06:05:30 +0000

    Add package dspy/2.6.0rc2/dspy-2.6.0rc2-py3-none-any.whl

---
+++ packages/dspy/dspy-2.6.0rc2-py3-none-any.whl/dspy/__init__.py
@@ -0,0 +1,38 @@
+from .predict import *
+from .primitives import *
+from .retrieve import *
+from .signatures import *
+
+import dspy.retrievers
+
+# Functional must be imported after primitives, predict and signatures
+from .functional import * # isort: skip
+from dspy.evaluate import Evaluate # isort: skip
+from dspy.clients import * # isort: skip
+from dspy.adapters import * # isort: skip
+from dspy.utils.logging_utils import configure_dspy_loggers, disable_logging, enable_logging
+from dspy.utils.asyncify import asyncify
+from dspy.utils.saving import load
+
+from dspy.dsp.utils.settings import settings
+
+configure_dspy_loggers(__name__)
+
+from dspy.dsp.colbertv2 import ColBERTv2
+# from dspy.dsp.you import You
+
+configure = settings.configure
+context = settings.context
+
+
+import dspy.teleprompt
+
+LabeledFewShot = dspy.teleprompt.LabeledFewShot
+BootstrapFewShot = dspy.teleprompt.BootstrapFewShot
+BootstrapFewShotWithRandomSearch = dspy.teleprompt.BootstrapFewShotWithRandomSearch
+BootstrapRS = dspy.teleprompt.BootstrapFewShotWithRandomSearch
+BootstrapFinetune = dspy.teleprompt.BootstrapFinetune
+BetterTogether = dspy.teleprompt.BetterTogether
+COPRO = dspy.teleprompt.COPRO
+MIPROv2 = dspy.teleprompt.MIPROv2
+Ensemble = dspy.teleprompt.Ensemble
