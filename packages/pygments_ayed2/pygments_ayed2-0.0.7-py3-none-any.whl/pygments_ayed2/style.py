5285507135bf5db8cd3abf075bd9eaeca423e0d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 06:05:31 +0000

    Add package pygments_ayed2/0.0.7/pygments_ayed2-0.0.7-py3-none-any.whl

---
+++ packages/pygments_ayed2/pygments_ayed2-0.0.7-py3-none-any.whl/pygments_ayed2/style.py
@@ -0,0 +1,28 @@
+from pygments.style import Style
+from pygments.token import Name, Punctuation, Token
+
+
+class Ayed2Style(Style):
+    name = "ayed2"
+
+    background_color = "#262220"
+    highlight_color = "#424032"
+
+    line_number_color = "#4e4e4e"
+    line_number_special_color = "#8f9494"
+
+    styles = {
+        Token.Keyword.Type:     'ansibrightblue',
+        Token.Keyword.Type.Definition: 'ansigreen',
+        Token.String:           'ansibrightblue',
+        Token.Number:           'ansibrightcyan',
+        Token.Operator:         'ansibrightred',
+        Token.Keyword:          'ansibrightgreen',
+        Token.Name:             'ansiwhite',
+        Token.Punctuation:      'ansicyan',
+        Punctuation.Assignment: 'ansibrightyellow',
+        Name.NamedLiteral:      'ansimagenta',
+        Name.Builtin:           'ansibrightmagenta',
+        Token.Keyword.Type.Enum: 'ansibrightred',
+    }
+
