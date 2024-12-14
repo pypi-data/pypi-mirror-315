c12831169f7c42e70ef3fe06540be45d518086e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 04:04:20 +0000

    Add package tilia/1.0.5/tilia-1.0.5.tar.gz

---
+++ packages/tilia/tilia-1.0.5.tar.gz/tilia-1.0.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+	name="tilia",
+	version="1.0.5",
+	author="ralves-e",
+	author_email="martins.ruiafonso@gmail.com",
+	description="One more layer for pyautogui. Utilities for SAP integration.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	project_urls={
+		"code": "https://github.com/rafonsomartins/tilia",
+		"Example of Usage": "https://github.com/rafonsomartins/job_rar/tree/main/Automate%20SAP%20Assessment%20Cycle"
+	},
+	packages=find_packages(),
+	install_requires=[
+		"pyautogui",
+		"pyperclip",
+		"pygetwindow"
+	],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent"
+	],
+	python_requires=">=3.6",
+)
