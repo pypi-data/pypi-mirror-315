279d4cf04504cf7b08497d72ebb337d885a8fe80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 05:04:03 +0000

    Add package duck-agents/0.1.4/duck_agents-0.1.4.tar.gz

---
+++ packages/duck-agents/duck_agents-0.1.4.tar.gz/duck_agents-0.1.4/src/duck_agents/llm/session_factory.py
@@ -0,0 +1,11 @@
+import logging
+
+from pydantic import BaseModel
+
+_logger = logging.getLogger(__name__)
+
+class SessionFactory:
+    def create_session(self, output_schema: BaseModel = None, temperature: float = 0):
+        _logger.warning(
+            "Trying to create a session with SessionFactory instance." 
+            "Subclass this class and provide your own implementation of create_session.")
