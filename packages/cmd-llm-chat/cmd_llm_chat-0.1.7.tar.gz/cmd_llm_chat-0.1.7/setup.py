565041210d7411f1693195e7cf0c0fbea308310a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 03:14:50 +0000

    Add package cmd-llm-chat/0.1.7/cmd_llm_chat-0.1.7.tar.gz

---
+++ packages/cmd-llm-chat/cmd_llm_chat-0.1.7.tar.gz/cmd_llm_chat-0.1.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cmd_llm_chat',
+    version='0.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+
+    entry_points={
+        'console_scripts': [
+            'chat=cmd_llm_chat.chat:main',
+        ],
+    },
+    install_requires=[
+        'colorama==0.4.6',
+        'matplotlib==3.7.1',
+        'openai~=1.55.3',
+        'prompt_toolkit~=3.0.38',
+        'rich==13.9.4',
+        'qianfan==0.4.12.1',
+        'pyyaml~=6.0',
+        'numpy~=1.24.3',
+        'pillow~=9.4.0',
+    ],
+)
