a99885bf70471c8893501ee0c6019b28aa417855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 04:05:21 +0000

    Add package Alaea/1.0.2/alaea-1.0.2.tar.gz

---
+++ packages/Alaea/alaea-1.0.2.tar.gz/alaea-1.0.2/README.md
@@ -0,0 +1,11 @@
+# This Package is a collection of Christmas related packages.
+## Authors : Christmas
+## Maintainer : Christmas
+
+To install this package, run:
+
+    pip install Alaea
+or
+
+    pip3 install Alaea
+
---
+++ packages/Alaea/alaea-1.0.2.tar.gz/alaea-1.0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="Alaea",
+    version="1.0.2",
+    author="Christmas",
+    author_email="273519355@qq.com",
+    description="Some simple tools for Christmas",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+    ],
+)
+# python3 setup.py sdist bdist_wheel
+# twine upload dist/*
