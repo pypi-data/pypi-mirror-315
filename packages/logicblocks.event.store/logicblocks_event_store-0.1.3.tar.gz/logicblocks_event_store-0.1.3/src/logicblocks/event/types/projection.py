ff5ec59ba496e75e2094a21bf35c291230f90789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 03:14:53 +0000

    Add package logicblocks.event.store/0.1.3/logicblocks_event_store-0.1.3.tar.gz

---
+++ packages/logicblocks.event.store/logicblocks_event_store-0.1.3.tar.gz/logicblocks_event_store-0.1.3/src/logicblocks/event/types/projection.py
@@ -0,0 +1,39 @@
+import json
+
+from collections.abc import Mapping
+from typing import Any
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Projection(object):
+    state: Mapping[str, Any]
+    position: int
+
+    def __init__(
+        self,
+        *,
+        state: Mapping[str, Any],
+        position: int,
+    ):
+        object.__setattr__(self, "state", state)
+        object.__setattr__(self, "position", position)
+
+    def json(self):
+        return json.dumps(
+            {
+                "state": self.state,
+                "position": self.position,
+            },
+            sort_keys=True,
+        )
+
+    def __repr__(self):
+        return (
+            f"Projection("
+            f"state={dict(self.state)}, "
+            f"position={self.position})"
+        )
+
+    def __hash__(self):
+        return hash(self.json())
