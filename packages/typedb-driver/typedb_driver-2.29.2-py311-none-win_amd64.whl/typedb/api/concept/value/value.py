18e669e957fe3499513b764d10cc0b114133efbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 05:04:14 +0000

    Add package typedb-driver/2.29.2/typedb_driver-2.29.2-py311-none-win_amd64.whl

---
+++ packages/typedb-driver/typedb_driver-2.29.2-py311-none-win_amd64.whl/typedb/api/concept/value/value.py
@@ -0,0 +1,300 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from datetime import datetime
+from enum import Enum
+from typing import Mapping, Union
+
+from typedb.native_driver_wrapper import Object, Boolean, Long, Double, String, DateTime
+
+from typedb.api.concept.concept import Concept
+from typedb.common.exception import TypeDBDriverException, UNEXPECTED_NATIVE_VALUE
+
+
+class Value(Concept, ABC):
+
+    @abstractmethod
+    def get_value_type(self) -> ValueType:
+        """
+        Retrieves the ``ValueType`` of this value concept.
+
+        :return:
+
+        Examples
+        --------
+        ::
+
+            value.get_value_type()
+        """
+        pass
+
+    @abstractmethod
+    def get(self) -> Union[bool, int, float, str, datetime]:
+        """
+        Retrieves the value which this value concept holds.
+
+        :return:
+
+        Examples
+        --------
+        ::
+
+            value.get()
+        """
+        pass
+
+    def is_value(self) -> bool:
+        """
+        Checks if the concept is a ``Value``.
+
+        :return:
+
+        Examples
+        --------
+        ::
+
+            value.is_value()
+        """
+        return True
+
+    def as_value(self) -> Value:
+        """
+        Casts the concept to ``Value``.
+
+        :return:
+
+        Examples
+        --------
+        ::
+
+            value.as_value()
+        """
+        return self
+
+    @abstractmethod
+    def is_boolean(self) -> bool:
+        """
+        Returns ``True`` if the value which this value concept holds is of type ``boolean``.
+        Otherwise, returns ``False``.
+
+        :return:
+
+        Examples
+        --------
+        ::
+
+            value.is_boolean()
+        """
+        pass
+
+    @abstractmethod
+    def is_long(self) -> bool:
+        """
+        Returns ``True`` if the value which this value concept holds is of type ``long``.
+        Otherwise, returns ``False``.
+
+        :return:
+
+        Examples
+        --------
+        ::
+
+            value.is_long()
+        """
+        pass
+
+    @abstractmethod
+    def is_double(self) -> bool:
+        """
+        Returns ``True`` if the value which this value concept holds is of type ``double``.
+        Otherwise, returns ``False``.
+
+        :return:
+
+        Examples
+        --------
+        ::
+
+            value.is_double()
+        """
+        pass
+
+    @abstractmethod
+    def is_string(self) -> bool:
+        """
+        Returns ``True`` if the value which this value concept holds is of type ``string``.
+        Otherwise, returns ``False``.
+
+        :return:
+
+        Examples
+        --------
+        ::
+
+            value.is_string()
+        """
+        pass
+
+    @abstractmethod
+    def is_datetime(self) -> bool:
+        """
+        Returns ``True`` if the value which this value concept holds is of type ``datetime``.
+        Otherwise, returns ``False``.
+
+        :return:
+
+        Examples
+        --------
+        ::
+
+            value.is_datetime()
+        """
+        pass
+
+    @abstractmethod
+    def as_boolean(self) -> bool:
+        """
+        Returns a ``boolean`` value of this value concept. If the value has
+        another type, raises an exception.
+
+        :return:
+
+        Examples
+        --------
+        ::
+
+            value.as_boolean()
+        """
+        pass
+
+    @abstractmethod
+    def as_long(self) -> int:
+        """
+        Returns a ``long`` value of this value concept. If the value has
+        another type, raises an exception.
+
+        :return:
+
+        Examples
+        --------
+        ::
+
+            value.as_long()
+        """
+        pass
+
+    @abstractmethod
+    def as_double(self) -> float:
+        """
+        Returns a ``double`` value of this value concept. If the value has
+        another type, raises an exception.
+
+        :return:
+
+        Examples
+        --------
+        ::
+
+            value.as_double()
+        """
+        pass
+
+    @abstractmethod
+    def as_string(self) -> str:
+        """
+        Returns a ``string`` value of this value concept. If the value has
+        another type, raises an exception.
+
+        :return:
+
+        Examples
+        --------
+        ::
+
+            value.as_string()
+        """
+        pass
+
+    @abstractmethod
+    def as_datetime(self) -> datetime:
+        """
+        Returns a timezone naive ``datetime`` value of this value concept. If the value has
+        another type, raises an exception.
+
+        :return:
+
+        Examples
+        --------
+        ::
+
+            value.as_datetime()
+        """
+        pass
+
+
+class _ValueType:
+
+    def __init__(self, is_writable: bool, is_keyable: bool, native_object):
+        self._is_writable = is_writable
+        self._is_keyable = is_keyable
+        self._native_object = native_object
+
+    @property
+    def native_object(self):
+        return self._native_object
+
+    def is_writable(self) -> bool:
+        return self._is_writable
+
+    def is_keyable(self) -> bool:
+        return self._is_keyable
+
+    def __repr__(self):
+        return f"_ValueType(is_writable={self._is_writable}, is_keyable={self._is_keyable}, {self._native_object})"
+
+
+class ValueType(Enum):
+    """ TypeQL value types for attributes and value concepts. """
+    OBJECT = _ValueType(False, False, Object)
+    BOOLEAN = _ValueType(True, False, Boolean)
+    LONG = _ValueType(True, True, Long)
+    DOUBLE = _ValueType(True, False, Double)
+    STRING = _ValueType(True, True, String)
+    DATETIME = _ValueType(True, True, DateTime)
+
+    @property
+    def native_object(self):
+        return self.value.native_object
+
+    def __str__(self):
+        return self.name.lower()
+
+    def __repr__(self):
+        return str(self)
+
+    @staticmethod
+    def of(value_type: Union[Object, Boolean, Long, Double, String, DateTime]) -> ValueType:
+        """
+        :meta private:
+        """
+        for type_ in ValueType:
+            if type_.native_object == value_type:
+                return type_
+        raise TypeDBDriverException(UNEXPECTED_NATIVE_VALUE)
---
+++ packages/typedb-driver/typedb_driver-2.29.2-py311-none-win_amd64.whl/typedb/connection/driver.py
@@ -0,0 +1,97 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from __future__ import annotations
+
+from typing import Optional, TYPE_CHECKING
+
+from typedb.native_driver_wrapper import connection_open_core, connection_open_cloud, connection_open_cloud_translated, \
+        connection_is_open, connection_force_close, Connection as NativeConnection, TypeDBDriverExceptionNative
+
+from typedb.api.connection.driver import TypeDBDriver
+from typedb.api.connection.options import TypeDBOptions
+from typedb.common.exception import TypeDBDriverException, DRIVER_CLOSED
+from typedb.common.native_wrapper import NativeWrapper
+from typedb.connection.database_manager import _DatabaseManager
+from typedb.connection.session import _Session
+from typedb.user.user_manager import _UserManager
+
+if TYPE_CHECKING:
+    from typedb.api.connection.credential import TypeDBCredential
+    from typedb.api.connection.session import SessionType
+    from typedb.api.user.user import UserManager, User
+
+
+class _Driver(TypeDBDriver, NativeWrapper[NativeConnection]):
+
+    def __init__(self, addresses: list[str] | dict[str], credential: Optional[TypeDBCredential] = None):
+        if credential:
+            try:
+                if isinstance(addresses, list):
+                    native_connection = connection_open_cloud(addresses, credential.native_object)
+                else:
+                    public_addresses = list(addresses.keys())
+                    private_addresses = [addresses[public] for public in public_addresses]
+                    native_connection = connection_open_cloud_translated(
+                            public_addresses, private_addresses, credential.native_object)
+            except TypeDBDriverExceptionNative as e:
+                raise TypeDBDriverException.of(e)
+        else:
+            try:
+                native_connection = connection_open_core(addresses[0])
+            except TypeDBDriverExceptionNative as e:
+                raise TypeDBDriverException.of(e)
+        super().__init__(native_connection)
+        self._database_manager = _DatabaseManager(native_connection)
+        self._user_manager = _UserManager(native_connection)
+
+    @property
+    def _native_object_not_owned_exception(self) -> TypeDBDriverException:
+        return TypeDBDriverException(DRIVER_CLOSED)
+
+    @property
+    def _native_connection(self) -> NativeConnection:
+        return self.native_object
+
+    def session(self, database_name: str, session_type: SessionType, options: TypeDBOptions = None) -> _Session:
+        return _Session(self.databases, database_name, session_type, options if options else TypeDBOptions())
+
+    def is_open(self) -> bool:
+        return connection_is_open(self._native_connection)
+
+    @property
+    def databases(self) -> _DatabaseManager:
+        return self._database_manager
+
+    @property
+    def users(self) -> UserManager:
+        return self._user_manager
+
+    def user(self) -> User:
+        return self._user_manager.get_current_user()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+        if exc_tb is not None:
+            return False
+
+    def close(self) -> None:
+        if self.is_open():
+            connection_force_close(self._native_connection)
