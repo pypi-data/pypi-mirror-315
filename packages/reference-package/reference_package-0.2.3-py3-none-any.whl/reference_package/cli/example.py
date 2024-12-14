5c6df92c3c904239d26272b4aed6324089492a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 14:05:52 +0000

    Add package reference-package/0.2.3/reference_package-0.2.3-py3-none-any.whl

---
+++ packages/reference-package/reference_package-0.2.3-py3-none-any.whl/reference_package/cli/example.py
@@ -0,0 +1,14 @@
+"""This is just an example."""
+
+import click
+from typeguard import typechecked
+
+from reference_package.lib import example
+
+
+@click.command()
+@click.option("--secs", type=int, required=False, default=1)
+@typechecked
+def main(secs: int = 1) -> None:
+    """Wait n seconds."""
+    example.wait_a_second(secs=secs)
