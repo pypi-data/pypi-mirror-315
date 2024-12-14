01642a382bfc249255893e3d5a8ed3619d133e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 15:04:48 +0000

    Add package pro-search/0.1.2/pro-search-0.1.2.tar.gz

---
+++ packages/pro-search/pro-search-0.1.2.tar.gz/pro-search-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pro-search",
+    version="0.1.2",
+    author="Pranav Kumar",
+    author_email="pranavkumarnair@gmail.com",
+    description="An advanced web search package combining AI and web scraping",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PraNavKumAr01/pro_search",
+    packages=find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+        "groq",
+    ],
+)
