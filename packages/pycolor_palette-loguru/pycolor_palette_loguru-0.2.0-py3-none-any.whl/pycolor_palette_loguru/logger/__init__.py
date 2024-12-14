fb586d87bba0c4aaa61de0a8be62b3291f51a80a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 03:14:37 +0000

    Add package pycolor_palette-loguru/0.2.0/pycolor_palette_loguru-0.2.0-py3-none-any.whl

---
+++ packages/pycolor_palette-loguru/pycolor_palette_loguru-0.2.0-py3-none-any.whl/pycolor_palette_loguru/logger/__init__.py
@@ -0,0 +1,9 @@
+from pycolor_palette_loguru.logger.logger import (
+	PyDBG_Obj,
+	benchmark,
+	debug_func,
+	set_default_theme,
+	setup_logger,
+)
+
+__all__ = (set_default_theme, PyDBG_Obj, debug_func, benchmark, setup_logger)
