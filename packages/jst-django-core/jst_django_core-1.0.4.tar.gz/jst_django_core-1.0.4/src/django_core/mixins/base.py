a4fbc54f726982e3f52c69670abe526c631884db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 15:04:17 +0000

    Add package jst-django-core/1.0.4/jst_django_core-1.0.4.tar.gz

---
+++ packages/jst-django-core/jst_django_core-1.0.4.tar.gz/jst_django_core-1.0.4/src/django_core/mixins/base.py
@@ -0,0 +1,23 @@
+from django.utils.translation import gettext as _
+
+
+class BaseViewSetMixin:
+    def finalize_response(self, request, response, *args, **kwargs):
+        if response.status_code >= 400:
+            response.data = {
+                "status": False,
+                "data": None,
+                "message": (
+                    response.data.get("detail", _("Xatolik yuz berdi"))
+                    if isinstance(response.data, dict)
+                    else _("Xatolik yuz berdi")
+                ),
+            }
+        else:
+            response.data = {
+                "status": True,
+                "data": response.data,
+                "message": _("Amal muvaffaqiyatli bajarildi"),
+            }
+
+        return super().finalize_response(request, response, *args, **kwargs)
