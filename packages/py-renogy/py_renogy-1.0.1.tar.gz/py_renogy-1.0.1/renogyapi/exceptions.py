3d1bbccd9067841c5561d7a28426dc0ab76471ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 05:04:21 +0000

    Add package py-renogy/1.0.1/py_renogy-1.0.1.tar.gz

---
+++ packages/py-renogy/py_renogy-1.0.1.tar.gz/py_renogy-1.0.1/renogyapi/exceptions.py
@@ -0,0 +1,21 @@
+"""Provide exceptions for renogyapi."""
+
+
+class UrlNotFound(Exception):
+    """Exception for NotFound."""
+
+
+class NotAuthorized(Exception):
+    """Exception for invalid API key."""
+
+
+class APIError(Exception):
+    """Exception for API errors."""
+
+
+class RateLimit(Exception):
+    """Exception for API errors."""
+
+
+class InvalidCall(Exception):
+    """Exception for missing device_id."""
