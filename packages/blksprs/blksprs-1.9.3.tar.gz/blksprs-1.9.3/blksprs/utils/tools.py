2a0ae903a03680b8de2cc5a9664c6cd4b006b5e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 04:04:27 +0000

    Add package blksprs/1.9.3/blksprs-1.9.3.tar.gz

---
+++ packages/blksprs/blksprs-1.9.3.tar.gz/blksprs-1.9.3/blksprs/utils/tools.py
@@ -0,0 +1,23 @@
+from torch import Tensor, Size
+
+
+def do_shape_blocksparse(x: Tensor):
+    if x.dim() == 3:
+        return x.contiguous(), x.size()
+
+    return x.reshape(-1, x.size(-2), x.size(-1)).contiguous(), x.size()
+
+
+def undo_shape_blocksparse(x: Tensor, shape: Size):
+    if x.shape[:-2] == shape[:-2]:
+        return x
+
+    return x.reshape((*shape[:-2], *x.shape[-2:]))
+
+
+def get_triton_block_size(sparsity_block_size: int, limit: int = 128):
+    return min(sparsity_block_size, limit)
+
+
+def stride(x: Tensor):
+    return x.view(x.shape).stride()
