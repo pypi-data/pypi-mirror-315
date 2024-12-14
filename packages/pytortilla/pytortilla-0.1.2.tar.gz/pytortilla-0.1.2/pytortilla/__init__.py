5dd27b66998aa7bc2b84f45cb7213583128f66ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 04:05:15 +0000

    Add package pytortilla/0.1.2/pytortilla-0.1.2.tar.gz

---
+++ packages/pytortilla/pytortilla-0.1.2.tar.gz/pytortilla-0.1.2/pytortilla/__init__.py
@@ -0,0 +1,5 @@
+import pytortilla.datamodel
+from pytortilla.compile.main import compile
+from pytortilla.create.main import create
+from pytortilla.edit.main import edit
+from pytortilla.load.main import load
