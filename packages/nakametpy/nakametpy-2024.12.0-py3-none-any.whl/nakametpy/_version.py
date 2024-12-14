d8af3acd29ac782fa73560bf8485f8f7c5439a98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 15:04:40 +0000

    Add package nakametpy/2024.12.0/nakametpy-2024.12.0-py3-none-any.whl

---
+++ packages/nakametpy/nakametpy-2024.12.0-py3-none-any.whl/nakametpy/_version.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021-2024, NakaMetPy Develoers.
+# Distributed under the terms of the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+
+def get_version():
+    return '2024.12.0'
