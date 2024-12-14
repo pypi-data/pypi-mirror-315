915cdcae80444ae8bf8e2d71ef2f378ebeda02b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 06:06:18 +0000

    Add package shelle/0.1.2/shelle-0.1.2-py3-none-any.whl

---
+++ packages/shelle/shelle-0.1.2-py3-none-any.whl/combot_installer/__init__.py
@@ -0,0 +1,9 @@
+"""
+Combot Installer
+
+A Python package that simplifies the installation process for Combot,
+a terminal assistant. The package automates downloading and running
+the installation script.
+"""
+__version__ = "0.1.0"
+__author__ = "Hkcode"
