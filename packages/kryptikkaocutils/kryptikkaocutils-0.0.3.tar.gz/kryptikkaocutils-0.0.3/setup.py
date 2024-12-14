79814a27281658740dcf94bdc86bb32bc13c14a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 03:14:56 +0000

    Add package kryptikkaocutils/0.0.3/kryptikkaocutils-0.0.3.tar.gz

---
+++ packages/kryptikkaocutils/kryptikkaocutils-0.0.3.tar.gz/kryptikkaocutils-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Basic aoc utils'
+LONG_DESCRIPTION = 'Library for basic Advent of Code Utilities'
+
+setup(
+    name="kryptikkaocutils",
+    version=VERSION,
+    author="Lars Junker",
+    author_email="larsjunker92@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    keywords=['python', 'aoc', 'aoc-utils'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
