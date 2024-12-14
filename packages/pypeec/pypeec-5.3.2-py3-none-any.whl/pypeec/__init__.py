bb9fae099a729fee92bb55f99035345b14444e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 06:07:10 +0000

    Add package pypeec/5.3.2/pypeec-5.3.2-py3-none-any.whl

---
+++ packages/pypeec/pypeec-5.3.2-py3-none-any.whl/pypeec/__init__.py
@@ -0,0 +1,18 @@
+import importlib.resources
+
+# set basic metadata
+__author__ = "Thomas Guillod"
+__copyright__ = "Thomas Guillod - Dartmouth College"
+__license__ = "Mozilla Public License Version 2.0"
+
+# get the version number
+try:
+    filename = importlib.resources.files("pypeec.data").joinpath("version.txt")
+    with filename.open("r") as fid:
+        __version__ = fid.read()
+except FileNotFoundError:
+    __version__ = 'x.x.x'
+
+# import the main method
+from pypeec.script import *
+from pypeec.main import *
