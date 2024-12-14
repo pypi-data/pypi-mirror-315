8075c2c42e72c681023a77b1de2131b810c08a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 14:05:05 +0000

    Add package pyrepohistory/0.0.2/pyrepohistory-0.0.2.tar.gz

---
+++ packages/pyrepohistory/pyrepohistory-0.0.2.tar.gz/pyrepohistory-0.0.2/src/pyrepohistory/__init__.py
@@ -0,0 +1,12 @@
+# __init__.py
+
+"""
+    repo_lifecycles is a small, lightweight data collection tool to support the longitudinal
+    analysis of software repositories. 
+"""
+
+from .main import repo_lifecycle
+
+__all__ = [
+    "repo_lifecycle"
+]
