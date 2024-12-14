814aac52b0294048cb52eb18e02b0058247d8249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 15:04:47 +0000

    Add package kuzu/0.7.1.dev30/kuzu-0.7.1.dev30.tar.gz

---
+++ packages/kuzu/kuzu-0.7.1.dev30.tar.gz/sdist/kuzu-source/src/function/table/call/current_setting.cpp
@@ -0,0 +1,57 @@
+#include "function/table/bind_input.h"
+#include "function/table/simple_table_functions.h"
+
+using namespace kuzu::common;
+using namespace kuzu::main;
+
+namespace kuzu {
+namespace function {
+
+struct CurrentSettingBindData final : public SimpleTableFuncBindData {
+    std::string result;
+
+    CurrentSettingBindData(std::string result, std::vector<LogicalType> returnTypes,
+        std::vector<std::string> returnColumnNames, offset_t maxOffset)
+        : SimpleTableFuncBindData{std::move(returnTypes), std::move(returnColumnNames), maxOffset},
+          result{std::move(result)} {}
+
+    std::unique_ptr<TableFuncBindData> copy() const override {
+        return std::make_unique<CurrentSettingBindData>(result, LogicalType::copy(columnTypes),
+            columnNames, maxOffset);
+    }
+};
+
+static common::offset_t tableFunc(TableFuncInput& data, TableFuncOutput& output) {
+    auto& dataChunk = output.dataChunk;
+    auto sharedState = data.sharedState->ptrCast<SimpleTableFuncSharedState>();
+    auto& outputVector = dataChunk.getValueVectorMutable(0);
+    if (!sharedState->getMorsel().hasMoreToOutput()) {
+        return 0;
+    }
+    auto currentSettingBindData = data.bindData->constPtrCast<CurrentSettingBindData>();
+    auto pos = dataChunk.state->getSelVector()[0];
+    outputVector.setValue(pos, currentSettingBindData->result);
+    return 1;
+}
+
+static std::unique_ptr<TableFuncBindData> bindFunc(ClientContext* context,
+    TableFuncBindInput* input) {
+    auto optionName = input->getLiteralVal<std::string>(0);
+    std::vector<std::string> columnNames;
+    std::vector<LogicalType> columnTypes;
+    columnNames.emplace_back(optionName);
+    columnTypes.push_back(LogicalType::STRING());
+    return std::make_unique<CurrentSettingBindData>(
+        context->getCurrentSetting(optionName).toString(), std::move(columnTypes),
+        std::move(columnNames), 1 /* one row result */);
+}
+
+function_set CurrentSettingFunction::getFunctionSet() {
+    function_set functionSet;
+    functionSet.push_back(std::make_unique<TableFunction>(name, tableFunc, bindFunc,
+        initSharedState, initEmptyLocalState, std::vector<LogicalTypeID>{LogicalTypeID::STRING}));
+    return functionSet;
+}
+
+} // namespace function
+} // namespace kuzu
---
+++ packages/kuzu/kuzu-0.7.1.dev30.tar.gz/sdist/kuzu-source/src/include/function/table/bind_data.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include "common/copier_config/reader_config.h"
+#include "common/types/types.h"
+#include "main/client_context.h"
+#include "storage/predicate/column_predicate.h"
+
+namespace kuzu {
+namespace common {
+class FileSystem;
+}
+
+namespace function {
+
+struct TableFuncBindData {
+    std::vector<common::LogicalType> columnTypes;
+    std::vector<std::string> columnNames;
+
+    // the last {numWarningDataColumns} columns are for temporary internal use
+    common::column_id_t numWarningDataColumns;
+
+    common::cardinality_t cardinality;
+
+    TableFuncBindData() : numWarningDataColumns{0}, cardinality(0) {}
+    TableFuncBindData(std::vector<common::LogicalType> columnTypes,
+        std::vector<std::string> columnNames)
+        : columnTypes{std::move(columnTypes)}, columnNames{std::move(columnNames)},
+          numWarningDataColumns(0), cardinality(0) {}
+    TableFuncBindData(std::vector<common::LogicalType> columnTypes,
+        std::vector<std::string> columnNames, common::column_id_t numWarningDataColumns,
+        common::cardinality_t cardinality)
+        : columnTypes{std::move(columnTypes)}, columnNames{std::move(columnNames)},
+          numWarningDataColumns{numWarningDataColumns}, cardinality(cardinality) {}
+    TableFuncBindData(const TableFuncBindData& other)
+        : columnTypes{common::LogicalType::copy(other.columnTypes)}, columnNames{other.columnNames},
+          numWarningDataColumns(other.numWarningDataColumns), cardinality(other.cardinality),
+          columnSkips{other.columnSkips}, columnPredicates{copyVector(other.columnPredicates)} {}
+    virtual ~TableFuncBindData() = default;
+
+    common::idx_t getNumColumns() const { return columnTypes.size(); }
+    void setColumnSkips(std::vector<bool> skips) { columnSkips = std::move(skips); }
+    KUZU_API std::vector<bool> getColumnSkips() const;
+
+    void setColumnPredicates(std::vector<storage::ColumnPredicateSet> predicates) {
+        columnPredicates = std::move(predicates);
+    }
+    const std::vector<storage::ColumnPredicateSet>& getColumnPredicates() const {
+        return columnPredicates;
+    }
+
+    virtual std::unique_ptr<TableFuncBindData> copy() const = 0;
+
+    template<class TARGET>
+    const TARGET* constPtrCast() const {
+        return common::ku_dynamic_cast<const TARGET*>(this);
+    }
+
+    template<class TARGET>
+    TARGET& cast() {
+        return *common::ku_dynamic_cast<TARGET*>(this);
+    }
+
+private:
+    std::vector<bool> columnSkips;
+    std::vector<storage::ColumnPredicateSet> columnPredicates;
+};
+
+struct ScanBindData : public TableFuncBindData {
+    common::ReaderConfig config;
+    main::ClientContext* context;
+
+    ScanBindData(std::vector<common::LogicalType> columnTypes, std::vector<std::string> columnNames,
+        common::ReaderConfig config, main::ClientContext* context,
+        common::row_idx_t estCardinality = 0, common::column_id_t numWarningDataColumns = 0)
+        : TableFuncBindData{std::move(columnTypes), std::move(columnNames), numWarningDataColumns,
+              estCardinality},
+          config{std::move(config)}, context{context} {}
+    ScanBindData(const ScanBindData& other)
+        : TableFuncBindData{other}, config{other.config.copy()}, context{other.context} {}
+
+    std::unique_ptr<TableFuncBindData> copy() const override {
+        return std::make_unique<ScanBindData>(*this);
+    }
+};
+
+} // namespace function
+} // namespace kuzu
