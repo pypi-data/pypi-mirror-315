386f3881ee852ad34c5722930bb949e4c3b55485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 03:14:28 +0000

    Add package telegramify-markdown/0.2.2/telegramify_markdown-0.2.2.tar.gz

---
+++ packages/telegramify-markdown/telegramify_markdown-0.2.2.tar.gz/telegramify_markdown-0.2.2/src/telegramify_markdown/customize.py
@@ -0,0 +1,24 @@
+class Symbol(object):
+    head_level_1 = "\N{PUSHPIN}"
+    # "📌"
+    head_level_2 = "\N{PENCIL}"
+    # "✏️"
+    head_level_3 = "\N{BOOKS}"
+    # "📚"
+    head_level_4 = "\N{BOOKMARK}"
+    # "🔖"
+    image = "\N{FRAME WITH PICTURE}"
+    # "🖼"
+    link = "\N{LINK SYMBOL}"
+    # "🔗"
+    task_completed = "\N{WHITE HEAVY CHECK MARK}"
+    # "✅"
+    task_uncompleted = "\N{BALLOT BOX WITH CHECK}"
+    # "☑️"
+
+
+markdown_symbol = Symbol()
+cite_expandable = True
+strict_markdown = True
+unescape_html = False
+latex_escape = False
