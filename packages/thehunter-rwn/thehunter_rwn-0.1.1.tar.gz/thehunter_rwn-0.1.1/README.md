30a04440ac0c4940da2f94ca32d94ee9b7d8947a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 15:04:50 +0000

    Add package thehunter-rwn/0.1.1/thehunter_rwn-0.1.1.tar.gz

---
+++ packages/thehunter-rwn/thehunter_rwn-0.1.1.tar.gz/thehunter_rwn-0.1.1/README.md
@@ -0,0 +1,6 @@
+# Typer CLI
+Automated web penetration testing tool CLI (TheHunter-rwn)
+Features:
+Passive scan: Whoisxml, theHarvester, Wappalyzer
+ACtive scan and vulnerability detection: OWASP ZAP and Nmap
+Exploitation: Metasploit, Sqlmap and Xsstrike
---
+++ packages/thehunter-rwn/thehunter_rwn-0.1.1.tar.gz/thehunter_rwn-0.1.1/thehunter_rwn/main.py
@@ -0,0 +1,15 @@
+from typer import Typer
+from thehunter_rwn.passive_scan import app as passive_scan_app
+from thehunter_rwn.active_scan import app as active_scan_app
+from thehunter_rwn.exploit import app as exploit_app
+
+
+app = Typer()
+
+# Register subcommands from passive_scan_commands.py
+app.add_typer(passive_scan_app, name="passive_scan")
+app.add_typer(active_scan_app, name="active_scan")
+app.add_typer(exploit_app, name="exploit")
+
+if __name__ == "__main__":
+    app()
