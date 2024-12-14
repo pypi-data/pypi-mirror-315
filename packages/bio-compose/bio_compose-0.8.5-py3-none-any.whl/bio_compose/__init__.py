818debc08f9f782ad16d19e97fbe320f91f8fd9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 05:03:53 +0000

    Add package bio-compose/0.8.5/bio_compose-0.8.5-py3-none-any.whl

---
+++ packages/bio-compose/bio_compose-0.8.5-py3-none-any.whl/bio_compose/__init__.py
@@ -0,0 +1,31 @@
+import os
+import toml
+
+from bio_compose.api import (
+    get_output,
+    get_compatible_verification_simulators,
+    get_biomodel_file,
+    get_biomodel_archive,
+    verify,
+    run_simulation,
+    visualize_observables
+)
+
+
+pyproject_file_path = os.path.join(os.path.dirname(__file__), '..', 'pyproject.toml')
+try:
+    __version__ = toml.load(pyproject_file_path)['tool']['poetry']['version']
+except:
+    __version__ = ' '
+
+
+__all__ = [
+    '__version__',
+    'get_output',
+    'get_compatible_verification_simulators',
+    'verify',
+    'run_simulation',
+    'visualize_observables',
+    'get_biomodel_file',
+    'get_biomodel_archive',
+]
