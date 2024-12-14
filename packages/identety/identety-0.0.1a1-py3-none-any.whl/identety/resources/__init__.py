be8c49a2cc31d2aa92188a40863e128d08587406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 15:04:49 +0000

    Add package identety/0.0.1a1/identety-0.0.1a1-py3-none-any.whl

---
+++ packages/identety/identety-0.0.1a1-py3-none-any.whl/identety/resources/__init__.py
@@ -0,0 +1,75 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .apps import (
+    AppsResource,
+    AsyncAppsResource,
+    AppsResourceWithRawResponse,
+    AsyncAppsResourceWithRawResponse,
+    AppsResourceWithStreamingResponse,
+    AsyncAppsResourceWithStreamingResponse,
+)
+from .orgs import (
+    OrgsResource,
+    AsyncOrgsResource,
+    OrgsResourceWithRawResponse,
+    AsyncOrgsResourceWithRawResponse,
+    OrgsResourceWithStreamingResponse,
+    AsyncOrgsResourceWithStreamingResponse,
+)
+from .roles import (
+    RolesResource,
+    AsyncRolesResource,
+    RolesResourceWithRawResponse,
+    AsyncRolesResourceWithRawResponse,
+    RolesResourceWithStreamingResponse,
+    AsyncRolesResourceWithStreamingResponse,
+)
+from .users import (
+    UsersResource,
+    AsyncUsersResource,
+    UsersResourceWithRawResponse,
+    AsyncUsersResourceWithRawResponse,
+    UsersResourceWithStreamingResponse,
+    AsyncUsersResourceWithStreamingResponse,
+)
+from .clients import (
+    ClientsResource,
+    AsyncClientsResource,
+    ClientsResourceWithRawResponse,
+    AsyncClientsResourceWithRawResponse,
+    ClientsResourceWithStreamingResponse,
+    AsyncClientsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "AppsResource",
+    "AsyncAppsResource",
+    "AppsResourceWithRawResponse",
+    "AsyncAppsResourceWithRawResponse",
+    "AppsResourceWithStreamingResponse",
+    "AsyncAppsResourceWithStreamingResponse",
+    "ClientsResource",
+    "AsyncClientsResource",
+    "ClientsResourceWithRawResponse",
+    "AsyncClientsResourceWithRawResponse",
+    "ClientsResourceWithStreamingResponse",
+    "AsyncClientsResourceWithStreamingResponse",
+    "UsersResource",
+    "AsyncUsersResource",
+    "UsersResourceWithRawResponse",
+    "AsyncUsersResourceWithRawResponse",
+    "UsersResourceWithStreamingResponse",
+    "AsyncUsersResourceWithStreamingResponse",
+    "OrgsResource",
+    "AsyncOrgsResource",
+    "OrgsResourceWithRawResponse",
+    "AsyncOrgsResourceWithRawResponse",
+    "OrgsResourceWithStreamingResponse",
+    "AsyncOrgsResourceWithStreamingResponse",
+    "RolesResource",
+    "AsyncRolesResource",
+    "RolesResourceWithRawResponse",
+    "AsyncRolesResourceWithRawResponse",
+    "RolesResourceWithStreamingResponse",
+    "AsyncRolesResourceWithStreamingResponse",
+]
---
+++ packages/identety/identety-0.0.1a1-py3-none-any.whl/identety/resources/apps.py
@@ -0,0 +1,134 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+import httpx
+
+from .._types import NOT_GIVEN, Body, Query, Headers, NoneType, NotGiven
+from .._compat import cached_property
+from .._resource import SyncAPIResource, AsyncAPIResource
+from .._response import (
+    to_raw_response_wrapper,
+    to_streamed_response_wrapper,
+    async_to_raw_response_wrapper,
+    async_to_streamed_response_wrapper,
+)
+from .._base_client import make_request_options
+
+__all__ = ["AppsResource", "AsyncAppsResource"]
+
+
+class AppsResource(SyncAPIResource):
+    @cached_property
+    def with_raw_response(self) -> AppsResourceWithRawResponse:
+        """
+        This property can be used as a prefix for any HTTP method call to return the
+        the raw response object instead of the parsed content.
+
+        For more information, see https://www.github.com/identety/identety-python-sdk#accessing-raw-response-data-eg-headers
+        """
+        return AppsResourceWithRawResponse(self)
+
+    @cached_property
+    def with_streaming_response(self) -> AppsResourceWithStreamingResponse:
+        """
+        An alternative to `.with_raw_response` that doesn't eagerly read the response body.
+
+        For more information, see https://www.github.com/identety/identety-python-sdk#with_streaming_response
+        """
+        return AppsResourceWithStreamingResponse(self)
+
+    def retrieve(
+        self,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> None:
+        extra_headers = {"Accept": "*/*", **(extra_headers or {})}
+        return self._get(
+            "/",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=NoneType,
+        )
+
+
+class AsyncAppsResource(AsyncAPIResource):
+    @cached_property
+    def with_raw_response(self) -> AsyncAppsResourceWithRawResponse:
+        """
+        This property can be used as a prefix for any HTTP method call to return the
+        the raw response object instead of the parsed content.
+
+        For more information, see https://www.github.com/identety/identety-python-sdk#accessing-raw-response-data-eg-headers
+        """
+        return AsyncAppsResourceWithRawResponse(self)
+
+    @cached_property
+    def with_streaming_response(self) -> AsyncAppsResourceWithStreamingResponse:
+        """
+        An alternative to `.with_raw_response` that doesn't eagerly read the response body.
+
+        For more information, see https://www.github.com/identety/identety-python-sdk#with_streaming_response
+        """
+        return AsyncAppsResourceWithStreamingResponse(self)
+
+    async def retrieve(
+        self,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
+    ) -> None:
+        extra_headers = {"Accept": "*/*", **(extra_headers or {})}
+        return await self._get(
+            "/",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=NoneType,
+        )
+
+
+class AppsResourceWithRawResponse:
+    def __init__(self, apps: AppsResource) -> None:
+        self._apps = apps
+
+        self.retrieve = to_raw_response_wrapper(
+            apps.retrieve,
+        )
+
+
+class AsyncAppsResourceWithRawResponse:
+    def __init__(self, apps: AsyncAppsResource) -> None:
+        self._apps = apps
+
+        self.retrieve = async_to_raw_response_wrapper(
+            apps.retrieve,
+        )
+
+
+class AppsResourceWithStreamingResponse:
+    def __init__(self, apps: AppsResource) -> None:
+        self._apps = apps
+
+        self.retrieve = to_streamed_response_wrapper(
+            apps.retrieve,
+        )
+
+
+class AsyncAppsResourceWithStreamingResponse:
+    def __init__(self, apps: AsyncAppsResource) -> None:
+        self._apps = apps
+
+        self.retrieve = async_to_streamed_response_wrapper(
+            apps.retrieve,
+        )
