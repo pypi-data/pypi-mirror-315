268843406adabd0679ca9b38e197a2cce2548065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 14:04:53 +0000

    Add package ipalab-config/0.4.1/ipalab_config-0.4.1.tar.gz

---
+++ packages/ipalab-config/ipalab_config-0.4.1.tar.gz/ipalab_config-0.4.1/ipalab_config/utils.py
@@ -0,0 +1,33 @@
+"""ipalab_config utility functions."""
+
+import sys
+import socket
+
+
+def die(msg, err=1):
+    """Display message to stderr stream and exit program with error."""
+    print(msg, file=sys.stderr)
+    sys.exit(err)
+
+
+def ensure_fqdn(hostname, domain):
+    """Ensure hostame is a FQDN."""
+    return hostname if "." in hostname else f"{hostname}.{domain}"
+
+
+def get_hostname(config, name, domain):
+    """Ensure hostname from config is FQDN."""
+    hostname = config.get("hostname", name)
+    return ensure_fqdn(hostname, domain)
+
+
+def is_ip_address(addr):
+    """Check if a given string represents an IP address."""
+    try:
+        socket.inet_pton(socket.AF_INET, addr)
+    except OSError:
+        try:
+            socket.inet_pton(socket.AF_INET6, addr)
+        except OSError:
+            return False
+    return True
