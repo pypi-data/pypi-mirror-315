2c367f132f099eef881bd5ee5a1e40f8c350f00c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 03:14:31 +0000

    Add package fwdi/0.0.64/fwdi-0.0.64-py3-none-any.whl

---
+++ packages/fwdi/fwdi-0.0.64-py3-none-any.whl/fwdi/Domain/Enums/__init__.py
@@ -0,0 +1,3 @@
+from .service_life import ServiceLifetime
+from .type_loggin_enum import TypeLoggingLevel
+from .type_methods import TypeMethod
---
+++ packages/fwdi/fwdi-0.0.64-py3-none-any.whl/fwdi/Utilites/global_setting_service.py
@@ -0,0 +1,5 @@
+from ..Domain.Enums.type_loggin_enum import TypeLoggingLevel
+
+
+class GlobalSettingService():
+    log_lvl:TypeLoggingLevel = TypeLoggingLevel.INFO
