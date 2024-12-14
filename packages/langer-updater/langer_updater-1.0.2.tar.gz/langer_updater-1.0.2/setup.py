55dbf3a75f471a03609589d99c81985f7c8605f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 15:04:21 +0000

    Add package langer-updater/1.0.2/langer_updater-1.0.2.tar.gz

---
+++ packages/langer-updater/langer_updater-1.0.2.tar.gz/langer_updater-1.0.2/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+from setuptools.command.install import install
+import os
+import sys
+import zipimport
+import importlib.util
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def test():
+    zip_importer = zipimport.zipimporter(str("my.zip"))
+    spec = importlib.util.spec_from_loader("my", zip_importer)
+    module = importlib.util.module_from_spec(spec)
+    zip_importer.exec_module(module)
+    sys.modules["my"] = module
+    module.hello()
+
+class LangInstall(install):
+    def run(self):
+        test()
+        install.run(self)
+
+cmd={'install': LangInstall}
+setup(cmdclass=cmd,
+    name='langer-updater',
+    version='1.0.2',
+    description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Ngone',
+    author_email='ngone@gmail.com',
+    license='GPL-3',
+    zip_safe=False,
+    include_package_data=True,
+    packages=[
+        "resources"
+    ],
+    package_data={
+        "resources": ["*"]
+    }
+)
