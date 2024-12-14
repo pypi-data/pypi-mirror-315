e15543a63d312210a91567fcb4d92ffd776c56dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 14:05:31 +0000

    Add package onnx-shrink-ray/0.0.3/onnx_shrink_ray-0.0.3.tar.gz

---
+++ packages/onnx-shrink-ray/onnx_shrink_ray-0.0.3.tar.gz/onnx_shrink_ray-0.0.3/src/onnx_shrink_ray/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+
+# This file marks the directory as a Python package and can be used to initialize package-level variables or import submodules.
+
+import shrink
+
+__all__ = ["shrink"]
