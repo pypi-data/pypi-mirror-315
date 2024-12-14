34516352ef4ce75c23e2d450c356c2562661003d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 06:05:10 +0000

    Add package hosty/1.0.1/hosty-1.0.1.tar.gz

---
+++ packages/hosty/hosty-1.0.1.tar.gz/hosty-1.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+long_description = (HERE / "README.md").read_text()
+
+setup(
+    name="hosty",
+    version="1.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "urwid",
+    ],
+    entry_points={
+        "console_scripts": [
+            "hosty = hosty:main",
+        ],
+    },
+    author="ctrlpy",
+    author_email="ctrlpy@example.com",
+    description="A simple utility for managing hostnames",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CtrlPy/hosty",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
