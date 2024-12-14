d47c63cf9773daa0351b1fea31e7857aa26e39ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 03:14:22 +0000

    Add package megaparse/0.0.50/megaparse-0.0.50.tar.gz

---
+++ packages/megaparse/megaparse-0.0.50.tar.gz/megaparse-0.0.50/src/megaparse/parser/base.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import IO
+
+from megaparse_sdk.schema.extensions import FileExtension
+
+
+class BaseParser(ABC):
+    """Mother Class for all the parsers [Unstructured, LlamaParse, MegaParseVision]"""
+
+    @abstractmethod
+    async def convert(
+        self,
+        file_path: str | Path | None = None,
+        file: IO[bytes] | None = None,
+        file_extension: FileExtension | None = None,
+        **kwargs,
+    ) -> str:
+        """
+        Convert the given file to a specific format.
+
+        Args:
+            file_path (str | Path): The path to the file to be converted.
+            **kwargs: Additional keyword arguments for the conversion process.
+
+        Returns:
+            str: The result of the conversion process.
+
+        Raises:
+            NotImplementedError: If the method is not implemented by a subclass.
+        """
+        raise NotImplementedError("Subclasses should implement this method")
