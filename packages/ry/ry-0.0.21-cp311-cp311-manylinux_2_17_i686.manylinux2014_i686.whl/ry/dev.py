df3f92539ac6cb64a3b31e29927656cfc54b5a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 14:05:07 +0000

    Add package ry/0.0.21/ry-0.0.21-cp311-cp311-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/ry/ry-0.0.21-cp311-cp311-manylinux_2_17_i686.manylinux2014_i686.whl/ry/dev.py
@@ -0,0 +1,20 @@
+"""dev entry point"""
+
+from __future__ import annotations
+
+from ry import ryo3
+
+# noinspection PyUnresolvedReferences
+from ry.ryo3 import *  # noqa: F403
+
+__version__ = ryo3.__version__
+__build_profile__ = ryo3.__build_profile__
+__build_timestamp__ = ryo3.__build_timestamp__
+if hasattr(ryo3, "__all__"):
+    __all__ = ryo3.__all__
+
+# assign all things in ry03 to this module
+
+for _k in dir(ryo3):
+    if not _k.startswith("_"):
+        globals()[_k] = getattr(ryo3, _k)
