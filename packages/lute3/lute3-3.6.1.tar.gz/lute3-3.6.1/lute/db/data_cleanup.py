1c5287be6e66bfcd7b670ef3665c814b2244dbf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 06:05:49 +0000

    Add package lute3/3.6.1/lute3-3.6.1.tar.gz

---
+++ packages/lute3/lute3-3.6.1.tar.gz/lute3-3.6.1/lute/db/data_cleanup.py
@@ -0,0 +1,39 @@
+"""
+Data cleanup routines.
+
+Sometimes required as data management changes.
+These cleanup routines will be called by the app_factory.
+"""
+
+from lute.models.book import Text
+
+
+def _set_texts_word_count(session):
+    """
+    texts.TxWordCount should be set for all texts.
+
+    Fixing a design error: the counts should have been stored here,
+    instead of only in books.BkWordCount.
+
+    Ref https://github.com/jzohrab/lute-v3/issues/95
+    """
+    calc_counts = session.query(Text).filter(Text.word_count.is_(None)).all()
+
+    # Don't recalc with invalid parsers!!!!
+    recalc = [t for t in calc_counts if t.book.language.is_supported]
+
+    if len(recalc) == 0:
+        # Nothing to calculate, quit.
+        return
+
+    for t in recalc:
+        pt = t.book.language.get_parsed_tokens(t.text)
+        words = [w for w in pt if w.is_word]
+        t.word_count = len(words)
+        session.add(t)
+    session.commit()
+
+
+def clean_data(session):
+    "Clean all data as required."
+    _set_texts_word_count(session)
