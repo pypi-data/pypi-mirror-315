992617f42f2c5983f2e257802a7dc027f388e1a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 05:04:12 +0000

    Add package omni-authify/0.1.4/omni-authify-0.1.4.tar.gz

---
+++ packages/omni-authify/omni-authify-0.1.4.tar.gz/omni-authify-0.1.4/omni_authify/providers/__init__.py
@@ -0,0 +1,11 @@
+from .base import *
+from .facebook import *
+from .github import *
+
+# Commented out providers not yet implemented
+# from .github import *
+# from .google import *
+# from .instagram import *
+# from .linkedin import *
+# from .telegram import *
+# from .twitter import *
