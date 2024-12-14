c6931c7756423bb3dbb6d99a9018cf0e2705280f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 14:04:49 +0000

    Add package pytokens/0.0.0/pytokens-0.0.0-py3-none-any.whl

---
+++ packages/pytokens/pytokens-0.0.0-py3-none-any.whl/pytokens/__init__.py
@@ -0,0 +1,7 @@
+"""pytokens - A Fast, spec compliant Python 3.12+ tokenizer that runs on older Pythons."""
+from __future__ import annotations
+
+
+def greet(name: str) -> str:
+    """Returns a greeting."""
+    return f"Hello {name}!"
