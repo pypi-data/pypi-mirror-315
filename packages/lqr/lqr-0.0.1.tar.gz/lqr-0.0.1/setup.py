b733631bf0c0a63249e342c5634603a23a2debb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 05:03:54 +0000

    Add package lqr/0.0.1/lqr-0.0.1.tar.gz

---
+++ packages/lqr/lqr-0.0.1.tar.gz/lqr-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='lqr',
+    version='0.0.1',
+    py_modules=['lqr'],
+    # description='',
+    author='Max M. Sun',
+    author_email='maxxmsun@gmail.com',
+    # url='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+    ],
+    python_requires='>=3.8',
+)
