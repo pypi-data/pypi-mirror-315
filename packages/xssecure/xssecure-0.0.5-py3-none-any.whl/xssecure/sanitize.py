81952149baf06a92dbbbb0885565338c3e8cfc7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 06:07:28 +0000

    Add package xssecure/0.0.5/xssecure-0.0.5-py3-none-any.whl

---
+++ packages/xssecure/xssecure-0.0.5-py3-none-any.whl/xssecure/sanitize.py
@@ -0,0 +1,32 @@
+import os
+
+from bs4 import BeautifulSoup
+
+
+def clean_html(html_string: str) -> str:
+    base_path = os.path.dirname(__file__)
+
+    def remove_malicious_tags_and_attributes(html: str) -> str:
+        soup = BeautifulSoup(html, "html.parser")
+        with open(os.path.join(base_path, 'whitelist_tags.txt')) as f:
+            whitelist_tags = f.readlines()
+
+        for tag in soup(whitelist_tags):
+            tag.decompose()
+
+        with open(os.path.join(base_path, 'whitelist_attributes.txt')) as f:
+            whitelist_attributes = f.readlines()
+        for tag in soup.find_all(True):
+            for attribute in whitelist_attributes:
+                if attribute in tag.attrs:
+                    del tag.attrs[attribute]
+
+        return str(soup)
+
+    cleaned_html = html_string
+    previous_html = ""
+
+    while cleaned_html != previous_html:
+        previous_html = cleaned_html
+        cleaned_html = remove_malicious_tags_and_attributes(cleaned_html)
+    return cleaned_html
