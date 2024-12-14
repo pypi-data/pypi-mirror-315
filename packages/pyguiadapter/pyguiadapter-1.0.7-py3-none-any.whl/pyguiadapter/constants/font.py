22006780e745c8f668760cb254c74628213bb4ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 04:04:16 +0000

    Add package pyguiadapter/1.0.7/pyguiadapter-1.0.7-py3-none-any.whl

---
+++ packages/pyguiadapter/pyguiadapter-1.0.7-py3-none-any.whl/pyguiadapter/constants/font.py
@@ -0,0 +1,2 @@
+FONT_FAMILY = "Consolas, monospace, Monaco, Source Code Pro, Inter, Arial, sans-serif;"
+FONT_SIZE_CODE_TEXT = 16
---
+++ packages/pyguiadapter/pyguiadapter-1.0.7-py3-none-any.whl/pyguiadapter/widgets/extend/__init__.py
@@ -0,0 +1,95 @@
+from .choicebox import ChoiceBox, ChoiceBoxConfig
+from .multichoice import MultiChoiceBox, MultiChoiceBoxConfig
+from .slider import Slider, SliderConfig
+from .dial import Dial, DialConfig
+from .colorpicker import (
+    ColorType,
+    ColorPicker,
+    ColorPickerConfig,
+    ColorTuplePicker,
+    ColorTuplePickerConfig,
+    ColorHexPickerConfig,
+    ColorHexPicker,
+)
+from .keysequenceedit import KeySequenceEdit, KeySequenceEditConfig, KeySequenceFormat
+from .stringlist import StringListEdit, StringListEditConfig
+from .plaindict import PlainDictEdit, PlainDictEditConfig
+from .fileselect import (
+    FileSelectConfig,
+    FileSelect,
+    MultiFileSelectConfig,
+    MultiFileSelect,
+)
+from .dirselect import DirSelectConfig, DirSelect
+from .intedit import IntLineEditConfig, IntLineEdit
+from .floatedit import FloatLineEditConfig, FloatLineEdit
+from .jsonedit import JsonEditConfig, JsonEdit
+from .textedit import TextEdit, TextEditConfig
+from .pathlist import (
+    PathListEdit,
+    PathListEditConfig,
+    FileListEdit,
+    DirectoryListEdit,
+    PathEditDialogConfig,
+    FileListEditConfig,
+    DirectoryListEditConfig,
+)
+from .fontselect import FontSelect, FontSelectConfig
+from .quantitybox import (
+    IntQuantityBox,
+    IntQuantityBoxConfig,
+    FloatQuantityBox,
+    FloatQuantityBoxConfig,
+)
+
+
+__all__ = [
+    "IntLineEdit",
+    "IntLineEditConfig",
+    "FloatLineEditConfig",
+    "FloatLineEdit",
+    "JsonEditConfig",
+    "JsonEdit",
+    "TextEdit",
+    "TextEditConfig",
+    "MultiChoiceBoxConfig",
+    "MultiChoiceBox",
+    "ChoiceBox",
+    "ChoiceBoxConfig",
+    "Slider",
+    "SliderConfig",
+    "Dial",
+    "DialConfig",
+    "ColorPicker",
+    "ColorTuplePicker",
+    "ColorHexPicker",
+    "ColorPickerConfig",
+    "ColorTuplePickerConfig",
+    "ColorHexPickerConfig",
+    "KeySequenceEditConfig",
+    "KeySequenceEdit",
+    "KeySequenceFormat",
+    "StringListEdit",
+    "StringListEditConfig",
+    "PlainDictEdit",
+    "PlainDictEditConfig",
+    "FileSelect",
+    "FileSelectConfig",
+    "DirSelect",
+    "DirSelectConfig",
+    "MultiFileSelect",
+    "MultiFileSelectConfig",
+    "PathListEdit",
+    "FileListEdit",
+    "DirectoryListEdit",
+    "PathListEditConfig",
+    "PathEditDialogConfig",
+    "FileListEditConfig",
+    "DirectoryListEditConfig",
+    "FontSelect",
+    "FontSelectConfig",
+    "IntQuantityBox",
+    "IntQuantityBoxConfig",
+    "FloatQuantityBox",
+    "FloatQuantityBoxConfig",
+]
