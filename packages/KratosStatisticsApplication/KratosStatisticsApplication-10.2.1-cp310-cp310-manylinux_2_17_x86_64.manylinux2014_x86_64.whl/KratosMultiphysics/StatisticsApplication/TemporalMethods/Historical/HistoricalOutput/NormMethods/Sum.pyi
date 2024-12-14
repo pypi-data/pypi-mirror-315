f7e26b3729783897de535ed6a060c0399ab79391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-14 03:14:45 +0000

    Add package KratosStatisticsApplication/10.2.1/KratosStatisticsApplication-10.2.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/KratosStatisticsApplication/KratosStatisticsApplication-10.2.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/KratosMultiphysics/StatisticsApplication/TemporalMethods/Historical/HistoricalOutput/NormMethods/Sum.pyi
@@ -0,0 +1,14 @@
+import Kratos
+import KratosStatisticsApplication.TemporalMethods
+
+class Array(KratosStatisticsApplication.TemporalMethods.TemporalMethod):
+    def __init__(self, arg0: Kratos.ModelPart, arg1: str, arg2: Kratos.Array1DVariable3, arg3: int, arg4: Kratos.DoubleVariable) -> None: ...
+
+class Double(KratosStatisticsApplication.TemporalMethods.TemporalMethod):
+    def __init__(self, arg0: Kratos.ModelPart, arg1: str, arg2: Kratos.DoubleVariable, arg3: int, arg4: Kratos.DoubleVariable) -> None: ...
+
+class Matrix(KratosStatisticsApplication.TemporalMethods.TemporalMethod):
+    def __init__(self, arg0: Kratos.ModelPart, arg1: str, arg2: Kratos.MatrixVariable, arg3: int, arg4: Kratos.DoubleVariable) -> None: ...
+
+class Vector(KratosStatisticsApplication.TemporalMethods.TemporalMethod):
+    def __init__(self, arg0: Kratos.ModelPart, arg1: str, arg2: Kratos.VectorVariable, arg3: int, arg4: Kratos.DoubleVariable) -> None: ...
