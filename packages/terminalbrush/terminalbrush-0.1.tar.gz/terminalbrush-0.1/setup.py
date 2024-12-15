f8b4b7c07d1480742cc895a408c4f36531b357eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 03:20:14 +0000

    Add package terminalbrush/0.1/terminalbrush-0.1.tar.gz

---
+++ packages/terminalbrush/terminalbrush-0.1.tar.gz/terminalbrush-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='terminalbrush',  # The name of your module
+    version='0.1',    # Initial version
+    packages=find_packages(),
+    install_requires=[
+       'requests'
+    ]
+)
