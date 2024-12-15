bd8b4f1b0847e8c84ee8df93fcaa8ea171d15be1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 15:04:41 +0000

    Add package telegramify-markdown/0.3.0/telegramify_markdown-0.3.0-py3-none-any.whl

---
+++ packages/telegramify-markdown/telegramify_markdown-0.3.0-py3-none-any.whl/telegramify_markdown/type.py
@@ -0,0 +1,52 @@
+from abc import ABCMeta
+from enum import Enum
+from typing import Tuple, List, Any, Union
+
+from pydantic import dataclasses
+
+TaskType = Tuple[str, List[Tuple[Any, Any]]]
+SentType = List[Union["Text", "File", "Photo"]]
+
+
+class ContentTypes(Enum):
+    TEXT = "text"
+    FILE = "file"
+    PHOTO = "photo"
+
+
+class RenderedContent(object, metaclass=ABCMeta):
+    """
+    The rendered content.
+
+    - content: str
+    - content_type: ContentTypes
+    """
+    content_type: ContentTypes
+
+
+@dataclasses.dataclass
+class Text(RenderedContent):
+    content: str
+    content_type: ContentTypes = ContentTypes.TEXT
+
+
+@dataclasses.dataclass
+class File(RenderedContent):
+    file_name: str
+    file_data: bytes
+
+    caption: str = ""
+    """Please use render_lines_func to render the content."""
+
+    content_type: ContentTypes = ContentTypes.FILE
+
+
+@dataclasses.dataclass
+class Photo(RenderedContent):
+    file_name: str
+    file_data: bytes
+
+    caption: str = ""
+    """Please use render_lines_func to render the content."""
+
+    content_type: ContentTypes = ContentTypes.PHOTO
