af337012fd3318f95e4edd4357072cbd5bbdec31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 04:04:00 +0000

    Add package sy-ofb-python-sdk/0.2.0/sy_ofb_python_sdk-0.2.0-py3-none-any.whl

---
+++ packages/sy-ofb-python-sdk/sy_ofb_python_sdk-0.2.0-py3-none-any.whl/example.py
@@ -0,0 +1,32 @@
+from ofb_python_sdk import Client
+
+client_id = "your client id"
+client_secret = "your client secret"
+refresh_token = "your refresh token"
+redirect_uri = "your redirect uri"
+
+app = Client(client_id, client_secret, refresh_token, redirect_uri)
+
+# Upload a file
+app.upload_file("Hello, World!".encode("utf-8"), "/test.txt")
+
+# Upload a big file
+app.upload_big_file("Hello, World!".encode("utf-8"), "/testbig.txt")
+
+# create a folder
+app.create_folder("/test")
+
+# download a file
+file = app.download_file("/test.txt")
+print(file.decode("utf-8"))
+
+# move a file to recycle bin
+app.delete_file("/test.txt")
+
+# get a temp link
+temp_link = app.get_temp_link("/testbig.txt")
+print(temp_link)
+
+# get children of a folder
+children = app.get_children("/test")
+print(children)
