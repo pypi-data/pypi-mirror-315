c9af45246209297fb1a0463ef029d2bbbbe7ac7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 04:04:13 +0000

    Add package origottlieb/0.0.1/origottlieb-0.0.1.tar.gz

---
+++ packages/origottlieb/origottlieb-0.0.1.tar.gz/origottlieb-0.0.1/setup.py
@@ -0,0 +1,20 @@
+
+
+import setuptools
+
+setuptools.setup(
+    name="origottlieb",
+    version="0.0.1",
+    author="ItayGo",
+    author_email="itaygonen797@example.com",
+    description="The beautiest",
+    long_description="The beautiest",
+    url="https://github.com/MyUser/MyProject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)            
