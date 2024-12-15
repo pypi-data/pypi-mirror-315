75a83177ee12bd507e7d49d7ff22d6d90ced5db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 15:04:37 +0000

    Add package istalktrance/0.4/istalktrance-0.4.tar.gz

---
+++ packages/istalktrance/istalktrance-0.4.tar.gz/istalktrance-0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="istalktrance",
+    version="0.4",
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+        'numpy>=1.11.1'
+    ],
+)
