e09e1a7c6aaf51a5c2fa65322c7641ed8859790c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 04:04:11 +0000

    Add package aiida-pythonjob/0.1.5/aiida_pythonjob-0.1.5-py3-none-any.whl

---
+++ packages/aiida-pythonjob/aiida_pythonjob-0.1.5-py3-none-any.whl/aiida_pythonjob/data/data_with_value.py
@@ -0,0 +1,13 @@
+from aiida import orm
+
+
+class Dict(orm.Dict):
+    @property
+    def value(self):
+        return self.get_dict()
+
+
+class List(orm.List):
+    @property
+    def value(self):
+        return self.get_list()
