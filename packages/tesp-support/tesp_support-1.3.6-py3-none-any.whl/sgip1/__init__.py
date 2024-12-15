0bc68cd576e31f94cc3ea4cfd066a842a57c820e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 14:04:11 +0000

    Add package tesp-support/1.3.6/tesp_support-1.3.6-py3-none-any.whl

---
+++ packages/tesp-support/tesp_support-1.3.6-py3-none-any.whl/sgip1/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2017-2024 Battelle Memorial Institute
+# See LICENSE file at https://github.com/pnnl/tesp
+# file: __init__.py
+""" Transactive Energy Simulation Platform (TESP)
+Contains the python files for the SGIP1 analysis
+Plotting all SGIP1 variants on the same graph
+"""
---
+++ packages/tesp-support/tesp_support-1.3.6-py3-none-any.whl/tesp_support/matpower/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2017-2024 Battelle Memorial Institute
+# See LICENSE file at https://github.com/pnnl/tesp
+# file: __init__.py
+"""Support files for using MATPOWER - also see instructions on
+readthedocs for building the FNCS wrapper around the MATLAB run-time
+"""
