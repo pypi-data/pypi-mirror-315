24adbabce24715a135283c8734c1df47683ce08c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 03:22:01 +0000

    Add package ChinnuAi/1.2.5/ChinnuAi-1.2.5-py3-none-any.whl

---
+++ packages/ChinnuAi/ChinnuAi-1.2.5-py3-none-any.whl/ChinnuAi/__init__.py
@@ -0,0 +1,12 @@
+from .KalaQuantum import (
+    QuantumGates,
+    QuantumState,
+    QuantumTrainer,
+    ChinnuAI,
+    initialize_chinnu_ai,
+    train_chinnu_ai,
+    chat_with_chinnu_ai
+
+)
+
+__version__ = "1.2.5"
