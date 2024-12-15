b52b10588b2a5f9324b56ccb0bce4ee2ae2b83b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 03:22:00 +0000

    Add package django-azure-auth/2.2.0/django_azure_auth-2.2.0.tar.gz

---
+++ packages/django-azure-auth/django_azure_auth-2.2.0.tar.gz/django_azure_auth-2.2.0/azure_auth/middleware.py
@@ -0,0 +1,36 @@
+from urllib.parse import urlparse
+
+from django.conf import settings
+from django.http import HttpRequest
+from django.shortcuts import redirect
+from django.urls import reverse
+
+from .handlers import AuthHandler
+
+
+class AzureMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request: HttpRequest):
+        public_views = ["azure_auth:login", "azure_auth:logout", "azure_auth:callback"]
+        public_views.extend(settings.AZURE_AUTH.get("PUBLIC_URLS", []))
+        public_urls = [reverse(view_name) for view_name in public_views]
+        public_paths = settings.AZURE_AUTH.get(
+            "PUBLIC_PATHS", []
+        )  # added to resolve paths
+
+        if request.path_info in public_urls:
+            return self.get_response(request)
+
+        # Added to resolve paths that can't be reversed
+        for path in public_paths:
+            if request.path_info.startswith(path):
+                return self.get_response(request)
+
+        if AuthHandler(request).user_is_authenticated:
+            return self.get_response(request)
+
+        return redirect(
+            f"{reverse('azure_auth:login')}?next={urlparse(request.path).path}"
+        )
