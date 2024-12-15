6d355146ed1f97cf016c1b416c1a64db53264b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 03:20:23 +0000

    Add package adminui2/1.5.5/adminui2-1.5.5.tar.gz

---
+++ packages/adminui2/adminui2-1.5.5.tar.gz/adminui2-1.5.5/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open("./README_PIP.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="adminui2", # Replace with your own username
+    version="1.5.5",
+    author="Yashar (XemonDev)",
+    author_email="XemonDev@gmail.com",
+    description="Write professional web interface with Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/XemonDev/python-adminui",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[      
+        'aioflask==0.4.0',
+        'asgiref==3.5.2',
+        'click==8.1.3',
+        'flask==3.1.0',
+        'greenlet==3.1.1',
+        'greenletio==0.9.0',
+        'gunicorn==20.1.0',
+        'h11==0.13.0',
+        'importlib-metadata==4.12.0',
+        'itsdangerous==2.1.2',
+        'Jinja2==3.1.2',
+        'MarkupSafe==2.1.1',
+        'PyJWT==2.3.0',
+        'python-multipart==0.0.5',
+        'six==1.16.0',
+        'uvicorn==0.18.0',
+        'Werkzeug==3.1.3',
+        'zipp==3.8.1'
+    ],
+    include_package_data = True,
+    python_requires='>=3.6',
+)
