8156122513b78473204673b73f2e2df960f95460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 03:20:40 +0000

    Add package Shelley/0.1/Shelley-0.1-py3-none-any.whl

---
+++ packages/Shelley/Shelley-0.1-py3-none-any.whl/shelley/__main__.py
@@ -0,0 +1,11 @@
+from .installer import run_install_script
+
+def main():
+    """
+    Entry point for running the installer manually.
+    """
+    print("Running the LingoShell installer...")
+    run_install_script()
+
+if __name__ == "__main__":
+    main()
