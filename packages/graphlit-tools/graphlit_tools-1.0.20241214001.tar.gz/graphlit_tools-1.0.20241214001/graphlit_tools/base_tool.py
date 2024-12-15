23b26c6927fa7c51742e44d235ce230b09c154b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 04:04:25 +0000

    Add package graphlit-tools/1.0.20241214001/graphlit_tools-1.0.20241214001.tar.gz

---
+++ packages/graphlit-tools/graphlit_tools-1.0.20241214001.tar.gz/graphlit_tools-1.0.20241214001/graphlit_tools/base_tool.py
@@ -0,0 +1,93 @@
+from abc import abstractmethod
+from typing import Any, Type
+from pydantic import BaseModel
+
+class BaseTool(BaseModel):
+    """
+    Abstract base class for tools.
+    
+    Attributes:
+        name (str): The name of the tool.
+        description (str): A short description of the tool's functionality.
+        args_schema (Type[BaseModel]): The schema for arguments expected by the tool.
+    """
+    name: str
+    description: str
+    args_schema: Type[BaseModel]
+
+    def run(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Any:
+        """
+        Public method to execute the tool. Delegates to the abstract _run method.
+
+        Args:
+            *args (Any): Positional arguments passed to the tool.
+            **kwargs (Any): Keyword arguments passed to the tool.
+
+        Returns:
+            Any: The result of executing the tool.
+        """
+        return self._run(*args, **kwargs)
+
+    @abstractmethod
+    def _run(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Any:
+        """
+        Abstract method to define the tool's behavior.
+
+        Subclasses must implement this method to provide the actual logic.
+
+        Args:
+            *args (Any): Positional arguments passed to the tool.
+            **kwargs (Any): Keyword arguments passed to the tool.
+
+        Returns:
+            Any: The result of executing the tool.
+        """
+
+    async def arun(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Any:
+        """
+        Public async method to execute the tool. Delegates to the abstract _arun method.
+
+        Args:
+            *args (Any): Positional arguments passed to the tool.
+            **kwargs (Any): Keyword arguments passed to the tool.
+
+        Returns:
+            Any: The result of executing the tool asynchronously.
+        """
+        return await self._arun(*args, **kwargs)
+
+    @abstractmethod
+    async def _arun(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Any:
+        """
+        Abstract async method to define the tool's asynchronous behavior.
+
+        Subclasses must implement this method to provide the actual async logic.
+
+        Args:
+            *args (Any): Positional arguments passed to the tool.
+            **kwargs (Any): Keyword arguments passed to the tool.
+
+        Returns:
+            Any: The result of executing the tool asynchronously.
+        """
+
+    @property
+    def args(self) -> dict:
+        """Get the tool's arguments JSON schema."""
+        return self.args_schema.model_json_schema()["properties"]
