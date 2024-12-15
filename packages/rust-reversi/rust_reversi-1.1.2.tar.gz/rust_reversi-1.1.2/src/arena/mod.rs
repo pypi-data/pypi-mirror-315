3d7ade2a151c91664113f25b6c947a15191f8a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 15:04:17 +0000

    Add package rust-reversi/1.1.2/rust_reversi-1.1.2.tar.gz

---
+++ packages/rust-reversi/rust_reversi-1.1.2.tar.gz/rust_reversi-1.1.2/src/arena/mod.rs
@@ -0,0 +1,36 @@
+use pyo3::{exceptions::PyValueError, prelude::*};
+
+mod core;
+use core::{Arena as RustArena, ArenaError};
+
+#[pyclass]
+pub struct Arena {
+    inner: RustArena,
+}
+
+#[pymethods]
+impl Arena {
+    #[new]
+    fn new(command1: Vec<String>, command2: Vec<String>) -> Self {
+        Arena {
+            inner: RustArena::new(command1, command2),
+        }
+    }
+
+    fn play_n(&mut self, n: usize) -> PyResult<()> {
+        match self.inner.play_n(n) {
+            Ok(_) => Ok(()),
+            Err(ArenaError::EngineStartError) => Err(PyValueError::new_err("Engine start error")),
+            Err(ArenaError::GameNumberInvalid) => Err(PyValueError::new_err("Game count must be even")),
+            Err(ArenaError::GameError(s)) => Err(PyValueError::new_err(format!("Game error: {:?}", s))),
+        }
+    }
+
+    fn get_stats(&self) -> (usize, usize, usize) {
+        self.inner.get_stats()
+    }
+
+    fn get_pieces(&self) -> (usize, usize) {
+        self.inner.get_pieces()
+    }
+}
