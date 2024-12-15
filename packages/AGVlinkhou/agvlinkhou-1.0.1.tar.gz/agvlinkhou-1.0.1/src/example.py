6ab4de45f8266a032c5d9ce6108f048fa0e23e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 15:04:39 +0000

    Add package AGVlinkhou/1.0.1/agvlinkhou-1.0.1.tar.gz

---
+++ packages/AGVlinkhou/agvlinkhou-1.0.1.tar.gz/agvlinkhou-1.0.1/src/example.py
@@ -0,0 +1,9 @@
+from linkhouWebSocketClient import LinkHouApi
+import time
+
+amr = LinkHouApi("ws://192.168.16.216:6001")
+#print(amr.GetState(id = 5))
+#amr.CreateTask()
+#amr.CancelTask()
+#amr.close()
+
