a2e4e83b79e22ed02932591acd97ca227d52c285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 04:04:18 +0000

    Add package aiml-dsce/1.0.0/aiml_dsce-1.0.0-py3-none-any.whl

---
+++ packages/aiml-dsce/aiml_dsce-1.0.0-py3-none-any.whl/aiml/__init__.py
@@ -0,0 +1,12 @@
+from .programs import (
+    program_1,
+    program_2,
+    program_3,
+    program_4,
+    program_5,
+    program_6,
+    program_7,
+    program_8,
+    program_9,
+    program_10,
+)
