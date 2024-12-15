98142028c3d72680e87f421c4731d7c2c9d4d3b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 04:05:50 +0000

    Add package ptCrypt/0.0.12/ptcrypt-0.0.12.tar.gz

---
+++ packages/ptCrypt/ptcrypt-0.0.12.tar.gz/ptcrypt-0.0.12/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+    name = 'ptCrypt',
+    packages = find_packages(),
+    version = '0.0.12',
+    description = 'Cryptographic and cryptography related algorithms implemented with pure Python',
+    author = 'awawa',
+    author_email = 'mail.0awawa0@gmail.com',
+    url = 'https://github.com/0awawa0/ptCrypt',
+    download_url = 'https://github.com/0awawa0/ptCrypt/archive/refs/tags/0.0.10.tar.gz',
+    keywords = ['Cryptography'],
+    classifiers = [],
+)
