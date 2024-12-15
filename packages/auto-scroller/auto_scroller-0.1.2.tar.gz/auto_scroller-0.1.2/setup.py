35257dc866b0cf11860698e71518901800567981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 15:04:42 +0000

    Add package auto-scroller/0.1.2/auto_scroller-0.1.2.tar.gz

---
+++ packages/auto-scroller/auto_scroller-0.1.2.tar.gz/auto_scroller-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="auto_scroller",
+    version="0.1.2",
+    author="Neel Popat",
+    author_email="neelpopat242@gmail.com",
+    description="A package for detecting hand gestures to control scrolling.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/neelpopat242/auto-scroller",  # Update with your repo URL
+    packages=find_packages(),
+    install_requires=open("requirements.txt").readlines(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
