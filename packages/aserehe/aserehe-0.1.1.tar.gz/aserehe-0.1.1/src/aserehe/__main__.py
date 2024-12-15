e42afdffa4275733f7e51b298fe7532d6553a84a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 14:04:11 +0000

    Add package aserehe/0.1.1/aserehe-0.1.1.tar.gz

---
+++ packages/aserehe/aserehe-0.1.1.tar.gz/aserehe-0.1.1/src/aserehe/__main__.py
@@ -0,0 +1,51 @@
+import typer
+from typing_extensions import Annotated
+
+from aserehe._check import parse_git_history, ConventionalCommit
+from aserehe._version import get_current_version, get_next_version
+
+app = typer.Typer()
+
+
+@app.command()
+def check(from_stdin: bool = typer.Option(False, "--from-stdin")) -> None:
+    if from_stdin:
+        stdin = typer.get_text_stream("stdin")
+        ConventionalCommit.from_message(stdin.read())
+    else:
+        list(parse_git_history())
+
+
+@app.command()
+def version(
+    next: Annotated[
+        bool,
+        typer.Option(
+            "--next",
+            is_flag=True,
+            help="Whether to print the next semantic version instead of the current",
+        ),
+    ] = False,
+) -> None:
+    """
+    Print the current or next version. A current version is printed unless --next option
+    is passed in which case the next semantic version is printed.
+
+    The current version is inferred from the latest git tag in the current and parent
+    commits.
+
+    The next semantic version is inferred from the conventional commits since the commit
+    tagged with the current version.
+    E.g. if the current version is 1.0.0 and there is a descendant conventional commit
+    with a breaking change, the next version will be 2.0.0.
+    """
+    version_to_print = None
+    if next:
+        version_to_print = get_next_version()
+    else:
+        version_to_print = get_current_version()
+    typer.echo(version_to_print)
+
+
+if __name__ == "__main__":
+    app()
