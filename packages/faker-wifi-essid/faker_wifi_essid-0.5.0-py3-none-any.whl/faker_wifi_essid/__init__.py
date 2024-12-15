3ecb05235371d8144e3059f2cc18884f4d36c1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 03:21:16 +0000

    Add package faker-wifi-essid/0.5.0/faker_wifi_essid-0.5.0-py3-none-any.whl

---
+++ packages/faker-wifi-essid/faker_wifi_essid-0.5.0-py3-none-any.whl/faker_wifi_essid/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Faker Wi-Fi ESSID provider.
+"""
+
+from .wifi_essid import WifiESSID
+
+__version__ = "0.5.0"
+
+__all__ = [
+    "WifiESSID",
+]
---
+++ packages/faker-wifi-essid/faker_wifi_essid-0.5.0-py3-none-any.whl/faker_wifi_essid/wifi_essid.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+"""
+A Faker provider for Wi-Fi ESSIDs.
+"""
+
+from collections.abc import Callable
+
+from faker.providers import BaseProvider
+
+from .common_essids import COMMON_ESSIDS
+
+
+class WifiESSID(BaseProvider):
+    """
+    A Faker provider for Wi-Fi ESSIDs.
+    """
+
+    def common_essid(self) -> str:
+        """
+        Returns a random ESSID from a list of the most commonly used ones.
+
+        See https://wigle.net/stats#ssidstats.
+        """
+
+        return self.random_element(COMMON_ESSIDS)
+
+    def upc_default_essid(self) -> str:
+        """
+        Generates a random ESSID similar to the default ones used by UPC.
+
+        https://deadcode.me/blog/2016/07/01/UPC-UBEE-EVW3226-WPA2-Reversing.html.
+        """
+
+        return "UPC" + str(self.random_number(7, True))
+
+    def bbox_default_essid(self) -> str:
+        """
+        Generates a random ESSID similar to the default ones used by Bouygues
+        Telecom's Bbox.
+        """
+
+        return self.hexify("Bbox-^^^^^^", upper=True)
+
+    # List of the different ESSID generators.
+    _essid_generators: list[Callable] = [
+        bbox_default_essid,
+        common_essid,
+        upc_default_essid,
+    ]
+
+    def wifi_essid(self) -> str:
+        """
+        Returns a random fake Wi-Fi essid.
+        """
+
+        return self.random_element(self._essid_generators)(self)
