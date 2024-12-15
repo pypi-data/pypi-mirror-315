bdd3e04983414df86267538413d5120b6ecdfe33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 04:04:55 +0000

    Add package download-boss/0.0.7/download_boss-0.0.7.tar.gz

---
+++ packages/download-boss/download_boss-0.0.7.tar.gz/download_boss-0.0.7/src/download_boss/auth/BasicAuthGenerator.py
@@ -0,0 +1,33 @@
+import logging
+from requests.auth import HTTPBasicAuth
+
+from .AbstractAuthGenerator import AbstractAuthGenerator
+
+class BasicAuthGenerator(AbstractAuthGenerator):
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+        self.auth = None
+
+    """
+    Return HTTPBasicAuth with current credentials
+    """
+    def get(self):
+        if self.auth is None:
+            self.auth = self._auth()
+
+        return self.auth
+
+    """
+    Recreate HTTPBasicAuth and store it
+    """
+    def refresh(self):
+        self.auth = self._auth()
+
+    """
+    Create HTTPBasicAuth
+    """
+    def _auth(self):
+        return HTTPBasicAuth(self.username, self.password)
