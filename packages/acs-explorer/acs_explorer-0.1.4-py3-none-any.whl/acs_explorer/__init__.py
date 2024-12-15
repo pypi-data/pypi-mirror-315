3d66287812fa823f0ebb5bbb288f48a2be9c3399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 03:20:23 +0000

    Add package acs-explorer/0.1.4/acs_explorer-0.1.4-py3-none-any.whl

---
+++ packages/acs-explorer/acs_explorer-0.1.4-py3-none-any.whl/acs_explorer/__init__.py
@@ -0,0 +1,19 @@
+# read version from installed package
+from importlib.metadata import version
+
+# __init__.py in the acs_explorer package
+
+from acs_explorer import acsexplorer_get_geo_info, acsexplorer_get_data, acsexplorer_topic_search, acsexplorer_topic_search_shortlist, acsexplorer_pipeline_by_location, acsexplorer_pipeline_by_keyword, acsexplorer_analyze_trends, acsexplorer_generate_report
+
+__all__ = [
+    "acsexplorer_get_geo_info",
+    "acsexplorer_topic_search",
+    "acsexplorer_topic_search_shortlist",
+    "acsexplorer_get_data",
+    "acsexplorer_pipeline_by_location",
+    "acsexplorer_pipeline_by_keyword",
+    "acsexplorer_analyze_trends",
+    "acsexplorer_generate_report"
+]
+
+__version__ = version("acs_explorer")
