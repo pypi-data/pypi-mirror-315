bea653142624dd15b683ed8fd672df0b7c5f6cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 03:20:07 +0000

    Add package revit-meows/0.1.0/revit_meows-0.1.0.tar.gz

---
+++ packages/revit-meows/revit_meows-0.1.0.tar.gz/revit_meows-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="revit_meows",
+    version="0.1.0",
+    author="chuongmep",
+    author_email="chuongpqvn@gmail.com",
+    description="A tool for extracting data from Revit ACC",
+    long_description=open("Readme.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/chuongmep/revit-meows",
+    packages=find_packages(exclude=["revit*", "aps*",'revit_meows*,aps-toolkit']),
+    include_package_data=True,
+    install_requires=[
+        "pandas>=2.1.4",
+        "numpy>=1.26.0",
+        "requests>=2.0",
+        "aps-toolkit"
+    ],
+    python_requires=">=3.9",
+    package_dir={"" : "."},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": [
+            # Define command-line scripts here if needed
+        ],
+    },
+)
