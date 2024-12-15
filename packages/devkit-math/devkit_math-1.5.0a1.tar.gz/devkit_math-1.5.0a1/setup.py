76893d384dcbbab4b62409607c68fef8245339aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 14:04:22 +0000

    Add package devkit-math/1.5.0a1/devkit_math-1.5.0a1.tar.gz

---
+++ packages/devkit-math/devkit_math-1.5.0a1.tar.gz/devkit_math-1.5.0a1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages, Extension
+prime = Extension(
+    name = "prime",
+    sources = ["prime.cpp"]
+)
+setup(
+    name = "devkit-math",
+    version = "1.5.0a1",
+    author = "Pemrilect",
+    author_email = "retres243@outlook.com",
+    license = "MIT",
+    packages = find_packages(),
+    ext_modules = [prime],
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: C++",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.12"
+    ]
+)
