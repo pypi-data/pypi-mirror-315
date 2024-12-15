b95a5bcc19fe70ce4ddedb53a6a3e44f9d16831e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 03:20:28 +0000

    Add package fudstop/5.9.2/fudstop-5.9.2.tar.gz

---
+++ packages/fudstop/fudstop-5.9.2.tar.gz/fudstop-5.9.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+# Read requirements.txt and remove any comments
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+    requirements = [r.strip() for r in requirements if not r.startswith('#')]
+
+setup(
+    name='fudstop',
+    version='5.9.2',
+    packages=find_packages(),
+    install_requires=requirements,
+)
