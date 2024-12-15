999d53dd101591c2f398f8f9f74ab678d1ef0b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 04:04:21 +0000

    Add package nomad1/0.5.0/nomad1-0.5.0.tar.gz

---
+++ packages/nomad1/nomad1-0.5.0.tar.gz/nomad1-0.5.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="nomad1",
+      version="0.5.0",
+      description="This code with nomad package",
+      long_description="This is a very very long description",
+      author="Vinit",
+      package=['nomad1'],
+      install_requires=[])
