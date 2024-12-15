a7fe89980f471a24e0f4500c456de32cd60879ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 03:20:15 +0000

    Add package deps-rocker/0.6.0/deps_rocker-0.6.0.tar.gz

---
+++ packages/deps-rocker/deps_rocker-0.6.0.tar.gz/deps_rocker-0.6.0/deps_rocker/fzf.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from deps_rocker.simple_rocker_extension import SimpleRockerExtension
+
+
+class Fzf(SimpleRockerExtension):
+    """Adds fzf autocomplete to your container"""
+
+    name = "fzf"
+
+    def invoke_after(self, cliargs) -> set:
+        return {"user"}
+
+    def required(self, cliargs):
+        return {"git","git_clone","curl"}
