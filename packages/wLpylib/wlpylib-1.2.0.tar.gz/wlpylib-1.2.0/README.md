e1bcbcb6ab2e8c650cb41b7580c6b49f533ae867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 04:03:58 +0000

    Add package wLpylib/1.2.0/wlpylib-1.2.0.tar.gz

---
+++ packages/wLpylib/wlpylib-1.2.0.tar.gz/wlpylib-1.2.0/README.md
@@ -0,0 +1,8 @@
+# wL
+
+**wL** is a *markup metalanguage* designed to store arbitrary data in a format that is both human-readable and machine-readable.
+
+The design goals of wL emphasize elementary simplicity, generality, stability, and usability across all programming language.
+Plus, wL format does not contain no data specialisation or typing, for a linear read.
+
+Consider checking the [GitHub rep](https://github.com/WWX-Team/wL/) to get a full documentation.
---
+++ packages/wLpylib/wlpylib-1.2.0.tar.gz/wlpylib-1.2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name            = "wLpylib",
+    fullname        = "wL lib for python",
+    version         = "1.2.0",
+    packages        = ["wLpylib"],
+    url             = "https://github.com/WWX-Team/wL/",
+    classifiers     = [ "Programming Language :: Python :: 3",
+                        "License :: CC-BY-SA-4.0",
+                        "Operating System :: OS Independent" ],
+    python_requires = ">=3")
