15cd8d2ec656255e3320bdc8f44f5551ab6fa41c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 03:20:28 +0000

    Add package Subplot2Grid/0.0.1/Subplot2Grid-0.0.1-py3-none-any.whl

---
+++ packages/Subplot2Grid/Subplot2Grid-0.0.1-py3-none-any.whl/Subplot2Grid/__init__.py
@@ -0,0 +1,3 @@
+from .Subplot2Grid import Subplot2Grid
+
+__version__ = Subplot2Grid.version
---
+++ packages/Subplot2Grid/Subplot2Grid-0.0.1-py3-none-any.whl/Subplot2Grid/__main__.py
@@ -0,0 +1,15 @@
+"""
+Command example:
+python -m Subplot2Grid
+
+"""
+
+import tkinter as tk
+from .Subplot2Grid import Subplot2Grid
+
+root = tk.Tk()
+app = Subplot2Grid(root)
+root.mainloop()
+
+
+
