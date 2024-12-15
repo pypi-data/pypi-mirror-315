97954137336520deabde47049972ea2fae54cbc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 15:04:37 +0000

    Add package istalktrance/0.4/istalktrance-0.4-py3-none-any.whl

---
+++ packages/istalktrance/istalktrance-0.4-py3-none-any.whl/istalktrance/__init__.py
@@ -0,0 +1,2 @@
+from .main import bigdata
+from .main import Sang
---
+++ packages/istalktrance/istalktrance-0.4-py3-none-any.whl/istalktrance/main.py
@@ -0,0 +1,6 @@
+def bigdata():
+    return "Hello from my library!"
+
+class Sang:
+    def run_model(self):
+        return "A method in my library"
