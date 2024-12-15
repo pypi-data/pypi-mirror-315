bd895008ee9c84a9761cea2e38a4f9844113172c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 15:04:37 +0000

    Add package wowsnake/98.666.1333/wowsnake-98.666.1333.tar.gz

---
+++ packages/wowsnake/wowsnake-98.666.1333.tar.gz/wowsnake-98.666.1333/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '98.666.1333'
+DESCRIPTION = 'Stupid gamedeveloper games example'
+
+# Setting up
+setup(
+    name="wowsnake",
+    version=VERSION,
+    author="dclxviclan (dclxviclan)",
+    author_email="<dclxviclan@gmail.com>",
+    description="👁👄👁💬 import💢🚸🧞‍♀️/x  and play 🎃👻🍬",
+    packages=find_packages(),
+    install_requires=['colorama'],
+    keywords=['python', 'game', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
