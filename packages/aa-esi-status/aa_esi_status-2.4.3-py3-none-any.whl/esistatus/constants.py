d09eb71ff0a1b480b1db066fd43a2f19c0a48763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 03:21:44 +0000

    Add package aa-esi-status/2.4.3/aa_esi_status-2.4.3-py3-none-any.whl

---
+++ packages/aa-esi-status/aa_esi_status-2.4.3-py3-none-any.whl/esistatus/constants.py
@@ -0,0 +1,16 @@
+"""
+Constants used in this app
+"""
+
+# Django
+from django.utils.text import slugify
+
+# AA ESI Status
+from esistatus import __version__
+
+VERBOSE_NAME = "AA ESI Status for Alliance Auth"
+
+slugified_name: str = slugify(value=VERBOSE_NAME, allow_unicode=True)
+github_url: str = "https://github.com/ppfeufer/aa-esi-status"
+
+USER_AGENT = f"{slugified_name} v{__version__} {github_url}"
