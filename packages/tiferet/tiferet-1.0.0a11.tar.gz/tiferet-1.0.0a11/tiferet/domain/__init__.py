6352743817329226493dbcee4f8ea0a2cb39fa5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 14:04:05 +0000

    Add package tiferet/1.0.0a11/tiferet-1.0.0a11.tar.gz

---
+++ packages/tiferet/tiferet-1.0.0a11.tar.gz/tiferet-1.0.0a11/tiferet/domain/__init__.py
@@ -0,0 +1,5 @@
+from .core import *
+from .app import AppInterface, AppDependency, AppRepositoryConfiguration
+from .container import ContainerAttribute, ContainerDependency
+from .error import Error
+from .feature import Feature
