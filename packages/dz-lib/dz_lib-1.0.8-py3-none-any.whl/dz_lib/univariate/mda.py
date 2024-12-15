cc90e012a408b19ec635ad66fb1b03ddc67a6006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 14:04:10 +0000

    Add package dz-lib/1.0.8/dz_lib-1.0.8-py3-none-any.whl

---
+++ packages/dz-lib/dz_lib-1.0.8-py3-none-any.whl/dz_lib/univariate/mda.py
@@ -0,0 +1,37 @@
+from dz_lib.univariate import distributions, data
+
+# youngest single grain
+def ysg(sample: data.Sample):
+    pass
+
+# youngest graphical peak
+def ygp(distribution: distributions.Distribution):
+    pass
+
+# youngest gaussian fit of pdp
+def ygf(distribution: distributions.Distribution):
+    pass
+
+# Youngest grain cluster at 1s
+def ygc1s(sample: data.Sample):
+    pass
+
+# Youngest grain cluster at 2s
+def ygc2s(sample: data.Sample):
+    pass
+
+# Youngest 3 Zircons (method 1)
+def y3zo(sample: data.Sample):
+    pass
+
+# Youngest 3 Zircons (method 2)
+def y3za(sample: data.Sample):
+    pass
+
+# Tau Method
+def tau_method(sample: data.Sample):
+    pass
+
+# Youngest Statistical Population
+def ysp(sample: data.Sample):
+    pass
