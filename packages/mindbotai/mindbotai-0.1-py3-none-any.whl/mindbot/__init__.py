c6a52a81aa151afbf3f9046355b0dcf630dba374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 04:04:26 +0000

    Add package mindbotai/0.1/mindbotai-0.1-py3-none-any.whl

---
+++ packages/mindbotai/mindbotai-0.1-py3-none-any.whl/mindbot/__init__.py
@@ -0,0 +1,10 @@
+# mindbot/__init__.py
+
+from .ai_functions import (
+    interact_with_ai,
+    generate_image_prompt_content,
+    upload_video,
+    summarize_video,
+    ask_specific_question,
+    transcribe_and_describe
+)
