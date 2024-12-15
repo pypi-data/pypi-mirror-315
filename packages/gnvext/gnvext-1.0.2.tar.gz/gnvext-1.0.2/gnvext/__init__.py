195294b3069502c30ee4ff7c2ced24a19a3d20cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-15 03:20:40 +0000

    Add package gnvext/1.0.2/gnvext-1.0.2.tar.gz

---
+++ packages/gnvext/gnvext-1.0.2.tar.gz/gnvext-1.0.2/gnvext/__init__.py
@@ -0,0 +1,18 @@
+"""
+============================================================
+                         gnvext
+
+Wrapper for converting environment variables to python types
+============================================================
+
+:copyright: (c) 2024-present DouleLove
+:license: MIT, see LICENCE file for more information
+"""
+
+__author__ = 'DouleLove'
+__copyright__ = 'Copyright 2024-present DouleLove'
+__license__ = 'MIT'
+__title__ = 'gnvext'
+__version__ = '1.0.2'
+
+from .converters.converters import *
