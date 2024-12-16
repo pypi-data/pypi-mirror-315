a1fbd8e54e214cc4993f2ea2692f06501228c8e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 15:05:01 +0000

    Add package uiform/1.0.0/uiform-1.0.0.tar.gz

---
+++ packages/uiform/uiform-1.0.0.tar.gz/uiform-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="uiform",
+    version="1.0.0",
+    description="Temporary reservation for uiform",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
