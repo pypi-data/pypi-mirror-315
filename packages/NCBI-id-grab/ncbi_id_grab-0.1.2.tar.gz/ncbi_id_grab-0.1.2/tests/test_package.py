22e0999e4ec8add99ea6a029c22e708548408840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 15:05:15 +0000

    Add package NCBI-id-grab/0.1.2/ncbi_id_grab-0.1.2.tar.gz

---
+++ packages/NCBI-id-grab/ncbi_id_grab-0.1.2.tar.gz/ncbi_id_grab-0.1.2/tests/test_package.py
@@ -0,0 +1,15 @@
+# import sys
+# import os
+
+# # Add src to the system path
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+# # Now import the package
+
+
+from NCBI_ID_grab import IDfind
+
+
+bacteria = ['Eubacterium_sp.', 'Ruminococcaceae_', 'Blautia_', 'Lactiplantibacillus_plantarum']
+dict_1 = IDfind.NCBI_ID(bacteria)
+print(dict_1)
