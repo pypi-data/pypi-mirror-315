5581344bd991472a5ffde724d28ac287a6e75bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 05:05:18 +0000

    Add package statsy/0.1/statsy-0.1.tar.gz

---
+++ packages/statsy/statsy-0.1.tar.gz/statsy-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='statsy',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        #'numpy>=1.11.1'
+    ],
+    python_requires='>=3.6',
+)
