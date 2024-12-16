04a32d3a2baadb3ad335daf840944f8874f22637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 14:05:17 +0000

    Add package basics-operations-jm/0.1.1/basics_operations_jm-0.1.1.tar.gz

---
+++ packages/basics-operations-jm/basics_operations_jm-0.1.1.tar.gz/basics_operations_jm-0.1.1/README.md
@@ -0,0 +1,9 @@
+# Minha Biblioteca
+
+Esta é uma biblioteca de exemplo que faz somas!
+
+## Instalação
+
+```bash
+pip install minha_biblioteca
+# operations
---
+++ packages/basics-operations-jm/basics_operations_jm-0.1.1.tar.gz/basics_operations_jm-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="basics_operations_jm",  # Nome da biblioteca
+    version="0.1.1",  # Versão
+    packages=find_packages(),  # Descobre pacotes automaticamente
+    install_requires=[],  # Dependências, se houver
+    author="João Marcos",
+    author_email="joaomarcospaulini@gmail.com",
+    description="Uma biblioteca para operações básicas",
+    url="https://github.com/JoaoMarcos91/operations",  # Opcional
+)
