e2e789feef392c5a1f5985b44edd6d900792f582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 04:07:35 +0000

    Add package rmwatchlater/0.4.0/rmwatchlater-0.4.0-py3-none-any.whl

---
+++ packages/rmwatchlater/rmwatchlater-0.4.0-py3-none-any.whl/rmwatchlater/__init__.py
@@ -0,0 +1,26 @@
+import pyautogui as gui
+import webbrowser
+import time
+from mylocale.TR import tr
+
+
+def main():
+    url = "https://www.youtube.com/playlist?list=WL"
+    trfile = "rmwatcher/localisation.csv"
+    promt = input(
+        "Are you logged into your Google Account? ([{yes}]es/[{no}]o)".format(
+            yes="y", no="n"
+        )
+    )
+    webbrowser.open(url)
+    try:
+        if promt == tr(csv_file=trfile, target_key="YES"):
+            while True:
+                gui.click("/rmwatchlater/imgs/menue.png")
+
+    except:
+        pass
+
+
+if __name__ == "__main__":
+    main()
