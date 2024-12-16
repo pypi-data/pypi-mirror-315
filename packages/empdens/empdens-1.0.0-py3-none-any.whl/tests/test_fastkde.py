193399a4dc28caa75c9b9522e0f2ae28f824b47e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 04:08:13 +0000

    Add package empdens/1.0.0/empdens-1.0.0-py3-none-any.whl

---
+++ packages/empdens/empdens-1.0.0-py3-none-any.whl/tests/test_fastkde.py
@@ -0,0 +1,11 @@
+import pytest
+from scipy import stats
+
+fastkde = pytest.importorskip("fastkde")
+from fastkde import fastKDE
+
+
+def test_fastkde_runs():
+    gauss = stats.norm(-2, 4)
+    data = gauss.rvs(size=100)
+    _ = fastKDE.pdf(data)
