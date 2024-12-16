af21893e9128ec7f7c6d689bb5f396d633eab1b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 14:05:43 +0000

    Add package paravastu/1.9.1/paravastu-1.9.1-py3-none-any.whl

---
+++ packages/paravastu/paravastu-1.9.1-py3-none-any.whl/paravastu/circular_dichroism.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+
+class circular_dichroism:
+    def __init__(self, filenames):
+        self.data = []
+        for filename in filenames:
+            df = pd.read_csv(filename, header=None)
+            x = df.iloc[:, 0].values
+            y = df.iloc[:, 1].values
+            self.data.append(np.column_stack((x, y)))
+
+    def find_min(self):
+        minima = []
+        for arr in self.data:
+            min_idx = np.argmin(arr[:, 1])  # Find the index of the minimum y-value
+            minima.append(tuple(arr[min_idx]))
+        return minima
+
+    def find_max(self):
+        maxima = []
+        for arr in self.data:
+            max_idx = np.argmax(arr[:, 1])  # Find the index of the maximum y-value
+            maxima.append(tuple(arr[max_idx]))
+        return maxima
+        
+    def author():
+        print("This class was developed by Anish Kelam")
