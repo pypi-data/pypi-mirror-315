e7db14224560d86d35e55374ea6146e08e2b191c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 04:06:37 +0000

    Add package adaletCleaningV2/0.1.0/adaletcleaningv2-0.1.0.tar.gz

---
+++ packages/adaletCleaningV2/adaletcleaningv2-0.1.0.tar.gz/adaletcleaningv2-0.1.0/adaletCleaningV2/myfunctions.py
@@ -0,0 +1,287 @@
+import re
+#import string
+import json
+from adaletCleaningV2 import utils
+
+
+
+def convertTRChars(text):
+
+    with open("resources/trCharToReplace.json", "r", encoding="utf-8") as file:
+        TRCHARTOREPLACE = json.load(file)
+
+    try:
+        if isinstance(text, str):
+            # Iterate over all key-value pairs in dictionary
+            for key, value in TRCHARTOREPLACE.items():
+                # Replace key character with value character in string
+                text = text.replace(key, value)
+    except Exception as e:
+        print("Error: ", e)
+    
+    return text
+ 
+
+def lowercase(text):
+
+    try:
+        if isinstance(text, str):
+            text = utils.turkish_lower(text)
+         
+    except Exception as e:
+        print("Error: ", e)
+
+    return text
+
+
+def removeCityDistrictNames(text):
+    
+    _, _, cityDistrictNames = utils.getCityDistrictNames()  
+        
+    try:
+        if isinstance(text, str):
+            text = utils.turkish_lower(text)
+            text = ' '.join(word for word in text.split() if word not in cityDistrictNames)
+    except Exception as e:
+        print("Error: ", e)
+
+    return text
+
+
+def removePunctuations(text):
+    # string.punctuations characters are : !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+    #PUNCTUATIONS = string.punctuation
+    with open("resources/punctuations.txt", "r", encoding="utf-8") as file:
+        PUNCTUATIONS = file.read()
+
+    try:
+        if isinstance(text, str):
+            text = text.translate(str.maketrans(PUNCTUATIONS, ' ' * len(PUNCTUATIONS)))
+    except Exception as e:
+        print("Error: ", e)
+
+    return text
+
+
+def keepCharacters(text, keepNumber):
+
+    with open("resources/cleaningParams.txt", "r", encoding="utf-8") as file:
+        partOfFile = file.read()
+
+        charactersToKeepStart = partOfFile.find("<CHARACTERS_TO_KEEP>") + len("<CHARACTERS_TO_KEEP>")
+        charactersToKeepEnd = partOfFile.find("</CHARACTERS_TO_KEEP>")
+        charactersAndNumbersToKeepStart = partOfFile.find("<CHARACTERSANDNUMBERS_TO_KEEP>") + len("<CHARACTERSANDNUMBERS_TO_KEEP>")
+        charactersAndNumbersToKeepEnd = partOfFile.find("</CHARACTERSANDNUMBERS_TO_KEEP>")
+
+        characterTokens = partOfFile[charactersToKeepStart:charactersToKeepEnd]
+        CHARACTERSTOKEEP = re.compile(r'[{}]'.format(characterTokens))
+        characterAndNumbersTokens = partOfFile[charactersAndNumbersToKeepStart:charactersAndNumbersToKeepEnd]
+        CHARACTERSANDNUMBERSTOKEEP = re.compile(r'[{}]'.format(characterAndNumbersTokens))
+
+    try:
+        if isinstance(text, str):
+            if keepNumber:
+                pattern = re.compile(CHARACTERSANDNUMBERSTOKEEP)
+            else:
+                pattern = re.compile(CHARACTERSTOKEEP)
+            text = re.sub(pattern, ' ', text)
+    except Exception as e:
+        print("Error: ", e)
+
+    return text
+
+
+def removeSpecialCharacters(text):
+
+    with open("resources/cleaningParams.txt", "r", encoding="utf-8") as file:
+        partOfFile = file.read()
+        specialCharactersStart = partOfFile.find("<SPECIAL_CHARACTERS>") + len("<SPECIAL_CHARACTERS>")
+        specialCharactersEnd = partOfFile.find("</SPECIAL_CHARACTERS>")
+        specialCharacters = partOfFile[specialCharactersStart:specialCharactersEnd]
+        SPECIALCHARACTERS = re.compile(r'[{}]'.format(specialCharacters))
+
+    try:
+        if isinstance(text, str):
+            pattern = re.compile(SPECIALCHARACTERS)
+            text = re.sub(pattern, ' ', text)
+    except Exception as e:
+        print("Error: ", e)
+
+    return text
+
+
+def removeStopwords(text):
+
+    with open(("resources/stopwords.txt"), "r", encoding="utf-8") as file:
+        STOPWORDS = file.read().split("\n")
+
+    try:
+        if isinstance(text, str):
+            text = utils.turkish_lower(text)
+            text = ' '.join(word for word in text.split() if word not in STOPWORDS)
+    except Exception as e:
+        print("Error: ", e)
+
+    return text
+        
+
+def removeSingleCharacters(text):
+
+    try:
+        if isinstance(text, str):
+            text = ' '.join([w for w in text.split() if len(w) > 1])
+    except Exception as e:
+        print("Error: ", e)
+
+    return text
+
+
+def removeUnusedWords(text):
+
+    with open("resources/adaletUnusedWords.txt", "r", encoding="utf-8") as file:
+        ADALETUNUSEDWORDS = file.read().split("\n")
+
+    try:
+        if isinstance(text, str):
+            text = utils.turkish_lower(text)
+            text = ' '.join(word for word in text.split() if word not in ADALETUNUSEDWORDS)
+    except Exception as e:
+        print("Error: ", e)
+
+    return text
+
+
+def removeCommonWords(text):
+
+    with open("resources/adaletCommonWords.txt", "r", encoding="utf-8") as file:
+        ADALETCOMMONWORDS = file.read().split("\n")
+
+    try:
+        if isinstance(text, str):
+            text = utils.turkish_lower(text)
+            encodeText, indices = utils.encodeTextToArray(text, ADALETCOMMONWORDS, fullTextSearch=True)
+            resolvedText = text.split(' ')
+            if len(resolvedText):
+                resolvedText = utils.removeElements(resolvedText, indices)
+                text = ' '.join(resolvedText)
+    except Exception as e:
+        print("Error: ", e)
+
+    return text
+
+
+def removeConsecutiveConsonants(text):
+
+    try:
+        if isinstance(text, str):
+            result = []
+            for word in text.strip().split(" "):
+                if isinstance(word, str):
+                    # if there are more than 3 consecutive consonants for a text
+                    if len(utils.consonantConsecutiveList(word, 3)) == 0:
+                        result.append(word)
+            if len(result) > 0:
+                text = ' '.join(result)
+    except Exception as e:
+        print("Error: ", e)
+
+    return text
+
+
+def removeConsecutiveVowels(text):
+    try:
+        if isinstance(text, str):
+            result = []
+            for word in text.strip().split(" "):
+                if isinstance(word, str):
+                    # if there are more than 2 consecutive vowels for a text
+                    if len(utils.vowelConsecutiveList(word, 2)) == 0:
+                        result.append(word)
+            if len(result) > 0:
+                text = ' '.join(result)
+    except Exception as e:
+        print("Error: ", e)
+
+    return text
+
+
+def cleanSpaces(text):
+
+    try:
+        # return str(rawText).replace("\'", "").replace('"', "").replace("\t", "").replace("\n", "")
+        if isinstance(text, str):
+            #text = re.sub(r'\s+', ' ', text).strip()
+            text = re.sub(r'\s+', ' ', text).replace("'", " ").replace('"', " ").strip()
+    except Exception as e:
+        print("Error: ", e)
+
+    return text
+
+
+def replaceAbbreviations(text):
+
+    with open("resources/abbreviations.json", "r", encoding="utf-8") as file:
+        ABBREVIATIONS = json.load(file)
+    try:
+        if isinstance(text, str): 
+            text = utils.turkish_lower(text) 
+            for abbr, full_form in ABBREVIATIONS.items():
+                text = re.sub(r'\b{}\b'.format(re.escape(abbr)), full_form, text)
+         
+    except Exception as e:
+        print("Error: ", e)
+
+    return text
+
+
+#: if requested allowCharacters params, then merge all unwanted punctuations and finally remove allowedchars from them
+def allowOnlySpecificCharacters(text, isSpecialCharacters):
+
+    with open("resources/cleaningParams.txt", "r", encoding="utf-8") as file:
+        partOfFile = file.read()
+        specialCharactersStart = partOfFile.find("<SPECIAL_CHARACTERS>") + len("<SPECIAL_CHARACTERS>")
+        specialCharactersEnd = partOfFile.find("</SPECIAL_CHARACTERS>")
+        specialCharacters = partOfFile[specialCharactersStart:specialCharactersEnd]
+        SPECIALCHARACTERS = re.compile(r'[{}]'.format(specialCharacters))
+    with open("resources/punctuations.txt", "r") as file:
+        PUNCTUATIONS = file.read()
+    with open("resources/allowedChars.txt", "r") as file:
+        ALLOWEDCHARS = file.read()
+
+    try:
+        if isinstance(text, str):
+            charactersForReplace = PUNCTUATIONS
+            if isSpecialCharacters:
+                charactersForReplace += SPECIALCHARACTERS.pattern
+            if len(ALLOWEDCHARS.strip()) > 0:
+                uniqueCharacters = ''.join(set(charactersForReplace) - set(ALLOWEDCHARS))
+            else:
+                uniqueCharacters = ''.join(set(charactersForReplace))
+            if isinstance(text, str):
+                text = text.translate(str.maketrans(uniqueCharacters, ' ' * len(uniqueCharacters)))
+    except Exception as e:
+        print("Error: ", e)
+
+    return text
+
+
+
+def text_normalizer(text):
+    text = cleanSpaces(text)
+    text = lowercase(text)
+    text = removePunctuations(text)
+    text = removeSpecialCharacters(text)
+    text = removeCommonWords(text)
+    text = removeCityDistrictNames(text)
+    text = removeStopwords(text)
+    text = removeUnusedWords(text)
+    text = convertTRChars(text)
+    text = keepCharacters(text, False)
+    text = replaceAbbreviations(text)
+    text = removeSingleCharacters(text)
+    text = removeConsecutiveConsonants(text)
+    text = removeConsecutiveVowels(text)
+    
+    return text
+
+
---
+++ packages/adaletCleaningV2/adaletcleaningv2-0.1.0.tar.gz/adaletcleaningv2-0.1.0/tests/test_myfunctions.py
@@ -0,0 +1,129 @@
+from adaletCleaningV2 import myfunctions
+
+
+
+def test_convertTRChars():
+    text = "İstanbul güzel bir şehir"
+    expected = "Istanbul guzel bir sehir"
+    print(myfunctions.convertTRChars(text))
+    assert myfunctions.convertTRChars(text) == expected
+
+def test_lowercase():
+    text = "T.C. KÜÇÜK HARFLERE ÇEVİR"
+    expected = " tc  küçük harflere çevir"
+    print(myfunctions.lowercase(text))
+    assert myfunctions.lowercase(text) == expected
+
+def test_removeCityDistrictNames():
+
+    text = "İstanbul Kadıköy güzel bir yer"
+    expected = "güzel bir yer"
+    print(myfunctions.removeCityDistrictNames(text))
+    assert myfunctions.removeCityDistrictNames(text) == expected
+
+def test_removePunctuations():
+
+    text = "Merhaba, dünya! Nasıl?"
+    expected = "Merhaba  dünya  Nasıl "
+    print(myfunctions.removePunctuations(text))
+    assert myfunctions.removePunctuations(text) == expected
+
+def test_keepCharacters():
+    text = "abc&%123 xyz"
+    expected = "abc      xyz"
+    print(myfunctions.keepCharacters(text, False))
+    print(myfunctions.keepCharacters(text, True))
+    assert myfunctions.keepCharacters(text, keepNumber=False) == expected
+    assert myfunctions.keepCharacters(text, True) == "abc  123 xyz"
+
+def test_removeSpecialCharacters():
+ 
+    text = "«test»—data”"
+    expected = " test  data "
+    print(myfunctions.removeSpecialCharacters(text))
+    assert myfunctions.removeSpecialCharacters(text) == expected
+
+def test_removeStopwords():
+
+    text = "Bu test   cümlesi ile deneme"
+    expected = "test cümlesi deneme"
+    print(myfunctions.removeStopwords(text))
+    assert myfunctions.removeStopwords(text) == expected
+
+def test_removeSingleCharacters():
+    text = "a test cümlesi b c d"
+    expected = "test cümlesi"
+    print(myfunctions.removeSingleCharacters(text))
+    assert myfunctions.removeSingleCharacters(text) == expected
+
+def test_removeUnusedWords():
+
+    text = "imzalıdır bu örnek bir metindir"
+    expected = "bu örnek bir metindir"
+    print(myfunctions.removeUnusedWords(text))
+    assert myfunctions.removeUnusedWords(text) == expected
+
+def test_removeCommonWords():
+
+    text = "Bu Ticaret hukuk test"
+    expected = "bu test"
+    print(myfunctions.removeCommonWords(text))
+    assert myfunctions.removeCommonWords(text) == expected
+
+def test_removeConsecutiveConsonants():
+    text = "bcdc kfg güzel"
+    expected = "kfg güzel"
+    print(myfunctions.removeConsecutiveConsonants(text))
+    assert myfunctions.removeConsecutiveConsonants(text) == expected
+
+def test_removeConsecutiveVowels():
+    text = "aai aa güzel"
+    expected = "aa güzel"
+    print(myfunctions.removeConsecutiveVowels(text))
+    assert myfunctions.removeConsecutiveVowels(text) == expected
+
+def test_cleanSpaces():
+    text = "  Bu  bir    test.  "
+    expected = "Bu bir test."
+    print(myfunctions.cleanSpaces(text))
+    assert myfunctions.cleanSpaces(text) == expected
+
+def test_replaceAbbreviations():
+
+    text = "Av. Ahmet ve inş"
+    expected = "avukat. ahmet ve inşaat"
+    print(myfunctions.replaceAbbreviations(text))
+    assert myfunctions.replaceAbbreviations(text) == expected
+
+def test_allowOnlySpecificCharacters():
+    
+    text = "abc@!10 x.yz"
+    expected = "abc  10 x.yz"
+    print(myfunctions.allowOnlySpecificCharacters(text, True))
+    assert myfunctions.allowOnlySpecificCharacters(text, isSpecialCharacters=True) == expected
+
+def test_text_normalizer():
+
+    text = "T.C. Av. Mehmet Kadıköy'den10 İstanbul'a geçti."
+    expected = "tc avukat mehmet den gecti"
+    print(myfunctions.text_normalizer(text))
+    assert myfunctions.text_normalizer(text) == expected
+
+if __name__ == "__main__":
+    test_convertTRChars()
+    test_lowercase()
+    test_removeCityDistrictNames()
+    test_removePunctuations()
+    test_keepCharacters()
+    test_removeSpecialCharacters()
+    test_removeStopwords()
+    test_removeSingleCharacters()
+    test_removeUnusedWords()
+    test_removeCommonWords()
+    test_removeConsecutiveConsonants()
+    test_removeConsecutiveVowels()
+    test_cleanSpaces()
+    test_replaceAbbreviations()
+    test_allowOnlySpecificCharacters()
+    test_text_normalizer()
+    print("Tüm testler başarıyla geçti!")
