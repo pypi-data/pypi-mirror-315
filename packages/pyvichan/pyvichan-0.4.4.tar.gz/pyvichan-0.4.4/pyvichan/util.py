45d0a0ed7af59e4486d98f7297769e13bd2c679a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 04:08:28 +0000

    Add package pyvichan/0.4.4/pyvichan-0.4.4.tar.gz

---
+++ packages/pyvichan/pyvichan-0.4.4.tar.gz/pyvichan-0.4.4/pyvichan/util.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Utility functions."""
+
+import re
+
+# HTMLParser was deprecated since 3.4
+try:
+    from html import unescape
+except ImportError:
+    try:
+        from html.parser import HTMLParser
+    except ImportError:
+        from HTMLParser import HTMLParser
+    _parser = HTMLParser()
+    unescape = _parser.unescape
+
+
+def clean_comment_body(body):
+    """Returns given comment HTML as plaintext.
+
+    Converts all HTML tags and entities within 4chan comments
+    into human-readable text equivalents.
+    """
+    body = unescape(body)
+    body = re.sub(r'<a [^>]+>(.+?)</a>', r'\1', body)
+    body = body.replace('<br>', '\n')
+    body = re.sub(r'<.+?>', '', body)
+    body = body.strip()
+    return body
---
+++ packages/pyvichan/pyvichan-0.4.4.tar.gz/pyvichan-0.4.4/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""8chan/vichan Python Library.
+
+pyvichan is a Python library that gives access to the 8chan/vichan API
+and an object-oriented way to browse and get board and thread
+information quickly and easily.
+
+This program is free software. It comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What The Fuck You Want
+To Public License, Version 2, as published by Sam Hocevar. See
+the LICENSE file for more details.
+"""
+
+from setuptools import setup
+
+setup(
+    name='pyvichan',
+    version='0.4.4',
+    description=("Python vichan API Wrapper. Based on BASC-py4chan by the Bibliotheca Anonoma"),
+    long_description=open('README.rst').read(),
+    license=open('LICENSE').read(),
+    author='Antonizoon Overtwater',
+    author_email='antonizoon@bibanon.org',
+    url='http://github.com/bibanon/pyvichan',
+    packages=['pyvichan'],
+    package_dir={
+        'pyvichan': 'pyvichan',
+    },
+    package_data={'': ['README.rst', 'LICENSE']},
+    install_requires=['requests >= 1.0.0'],
+    keywords='8chan api vichan',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ]
+)
