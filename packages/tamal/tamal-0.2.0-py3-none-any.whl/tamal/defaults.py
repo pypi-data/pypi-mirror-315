5e3f62e743f63490312a5744045e25ff1ff0211f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 05:05:47 +0000

    Add package tamal/0.2.0/tamal-0.2.0-py3-none-any.whl

---
+++ packages/tamal/tamal-0.2.0-py3-none-any.whl/tamal/defaults.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "DEFAULT_HYPHEN",
+    "DEFAULT_HYPHENS",
+    "DEFAULT_PARAGRAPH",
+    "DEFAULT_SOFT_HYPHEN",
+    "DEFAULT_WHITESPACES",
+]
+
+DEFAULT_HYPHEN = "-"  # dash
+DEFAULT_HYPHENS = {"-", "–", "—"}  # dash, en-dash, em-dash
+DEFAULT_PARAGRAPH = "\n\n"
+DEFAULT_SOFT_HYPHEN = "·"
+DEFAULT_WHITESPACES = {" ", "\t", "\n"}
