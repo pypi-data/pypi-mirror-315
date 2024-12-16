f08c344a97b19762805df99ba0f072d68d7d4916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 03:20:14 +0000

    Add package dropletevaporationsimulation/1.1.21/dropletevaporationsimulation-1.1.21.tar.gz

---
+++ packages/dropletevaporationsimulation/dropletevaporationsimulation-1.1.21.tar.gz/dropletevaporationsimulation-1.1.21/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dropletevaporationsimulation",
+    version="1.1.21",
+    author="Guillaume Bailly",
+    author_email="guillaume.bailly@estaca.eu",
+    description="This project simulates the evaporation and dynamics of an n-Decane droplet in a heated airflow.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/guillaume-bailly/DropletEvaporationSimulation",
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=1.21.0",
+        "matplotlib>=3.4.0",
+        "scipy>=1.7.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "des_run_simulation = main:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",  # Minimum Python version
+)
