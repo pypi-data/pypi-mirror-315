262767d386bf17c3ffe88127ec1fc200322e9c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 05:05:25 +0000

    Add package pianosdk/16.354.1/pianosdk-16.354.1-py3-none-any.whl

---
+++ packages/pianosdk/pianosdk-16.354.1-py3-none-any.whl/pianosdk/publisher/models/experience_version.py
@@ -0,0 +1,14 @@
+from datetime import date, datetime
+from pydantic.main import BaseModel
+from typing import Optional
+
+
+class ExperienceVersion(BaseModel):
+    experience_id: Optional[str] = None
+    version: Optional[int] = None
+    committed: Optional[bool] = None
+    committed_date: Optional[datetime] = None
+    revision_notes: Optional[str] = None
+
+
+ExperienceVersion.model_rebuild()
