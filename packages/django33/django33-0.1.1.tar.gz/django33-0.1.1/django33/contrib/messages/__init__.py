c0a2e3a49221729d73477654f5facc31b06d766d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 15:04:58 +0000

    Add package django33/0.1.1/django33-0.1.1.tar.gz

---
+++ packages/django33/django33-0.1.1.tar.gz/django33-0.1.1/django33/contrib/messages/__init__.py
@@ -0,0 +1,3 @@
+from django33.contrib.messages.api import *  # NOQA
+from django33.contrib.messages.constants import *  # NOQA
+from django33.contrib.messages.storage.base import Message  # NOQA
---
+++ packages/django33/django33-0.1.1.tar.gz/django33-0.1.1/django33/contrib/staticfiles/urls.py
@@ -0,0 +1,19 @@
+from django33.conf import settings
+from django33.conf.urls.static import static
+from django33.contrib.staticfiles.views import serve
+
+urlpatterns = []
+
+
+def staticfiles_urlpatterns(prefix=None):
+    """
+    Helper function to return a URL pattern for serving static files.
+    """
+    if prefix is None:
+        prefix = settings.STATIC_URL
+    return static(prefix, view=serve)
+
+
+# Only append if urlpatterns are empty
+if settings.DEBUG and not urlpatterns:
+    urlpatterns += staticfiles_urlpatterns()
