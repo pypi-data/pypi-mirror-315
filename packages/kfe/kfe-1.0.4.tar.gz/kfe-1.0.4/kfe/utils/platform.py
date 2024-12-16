4401119b8e766544d580f17095ca8bd33929c2a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 05:05:08 +0000

    Add package kfe/1.0.4/kfe-1.0.4.tar.gz

---
+++ packages/kfe/kfe-1.0.4.tar.gz/kfe-1.0.4/kfe/utils/platform.py
@@ -0,0 +1,16 @@
+import os
+import sys
+from typing import Optional
+
+
+def is_linux() -> bool:
+    return sys.platform == 'linux'
+
+def is_mac_os() -> bool:
+    return sys.platform == 'darwin'
+
+def is_windows() -> bool:
+    return sys.platform == 'win32'
+
+def get_home_dir_path() -> Optional[str]:
+    return os.getenv('USERPROFILE') if is_windows() else os.getenv('HOME')
