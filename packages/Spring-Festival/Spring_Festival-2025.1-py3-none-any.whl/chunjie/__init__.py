8f0d7ec12f19c8ad4c97218566465ac5061cdd21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 03:20:06 +0000

    Add package Spring-Festival/2025.1/Spring_Festival-2025.1-py3-none-any.whl

---
+++ packages/Spring-Festival/Spring_Festival-2025.1-py3-none-any.whl/chunjie/__init__.py
@@ -0,0 +1,15 @@
+from .logo import *
+
+__version__ = "2025.01"
+
+__doc__ = '''
+2025春节彩蛋
+
+从 https://www.bilibili.com/video/BV12PqhYwEe3 的评论区得到的灵感
+
+纯属整活彩蛋，欢迎pr [https://github.com/QJZX/Spring_Festival] 补充内容【doge】
+
+
+'''
+
+print_simple_sologon()
