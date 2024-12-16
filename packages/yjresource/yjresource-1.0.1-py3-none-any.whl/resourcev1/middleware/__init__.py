84af70aca77786479f5fef6a231c90f26a5a28a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 04:06:42 +0000

    Add package yjresource/1.0.1/yjresource-1.0.1-py3-none-any.whl

---
+++ packages/yjresource/yjresource-1.0.1-py3-none-any.whl/resourcev1/middleware/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+ Copyright (c) 2024 Baidu, Inc.
+# All rights reserved.
+#
+# File    : middleware
+# Author  : zhoubohan
+# Date    : 2024/12/11
+# Time    : 10:51
+# Description :
+"""
+from .middleware import *
