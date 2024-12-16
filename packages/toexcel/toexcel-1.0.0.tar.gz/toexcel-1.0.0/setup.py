522fe90931f1f817656f5c8ba615ee74251fcc85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 15:05:26 +0000

    Add package toexcel/1.0.0/toexcel-1.0.0.tar.gz

---
+++ packages/toexcel/toexcel-1.0.0.tar.gz/toexcel-1.0.0/setup.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    此脚本用于 py 打包和分发工具 `setuptools`。
+
+    主要用来定义如何【安装】和【分发】此 py 项目。
+
+    (
+        `setuptools` 是 py 的一个库，它旨在简化打包和分发 py 项目的过程。
+
+        `setuptools` 库官方文档地址：
+        【https://setuptools.pypa.io/en/latest/】
+
+        `setuptools` 提供一系列命令和工具，使开发者能够轻松创建可重用的 py 包，
+        并管理它们的依赖关系、版本、发布等。
+
+        `setuptools` 是 `distutils` 的增强版，它在原有基础上增加许多新特性和功能。
+
+        它由 `PEP 517` 和 `PEP 518` 标准支持，该标准定义 py 项目的构建要求。
+
+            (
+                `PEP 517` 官方文档地址：【https://peps.python.org/pep-0517/】
+                `PEP 518` 官方文档地址：【https://www.python.org/dev/peps/pep-0518/】
+            )
+
+        随着时间推移，`setuptools` 已经成为 py 包创建和管理的事实标准。
+
+        `setuptools` 库主要功能：
+            - 安装包：允许用户安装和卸载 py 包。
+
+            - 依赖关系管理：自动处理包依赖项，并确保在安装包时自动下载需要的依赖包。
+
+            - 打包和分发：提供工具来轻松打包 py 代码，并将其分发给其他用户。
+              这通常是通过创建 `.egg` 或 `.wheel` 文件完成的。
+
+            - 版本管理：确保可以为软件包指定版本号以及在需要时指定最小或确切的依赖版本。
+
+            - 扩展构建：针对一些需要编译 C 语言扩展等复杂情况提供支持。
+
+            - 测试支持：集成设置测试套件并运行测试用例的能力。
+
+            - 生成脚本：允许自动创建可以调用软件包中函数的命令行脚本。
+
+            - 声明性配置：使用 `setup.cfg` 或 `pyproject.toml` 文件对项目进行配置，
+              而不是全部通过编写 `setup.py` 脚本来完成。
+
+        使用 `setuptools` 创建一个新项目一般会涉及以下步骤：
+            - 创建 `setup.py` 文件，在其中定义项目信息
+              （比如项目名、版本、描述等）以及任何相关依赖项。
+
+            - 可选地使用其他配置文件如 `setup.cfg` 或
+              `pyproject.toml` 来进一步细化设置（例如定义元数据或选项）。
+
+            - 使用 `setuptools` 提供的命令
+              （如 `python setup.py sdist bdist_wheel`）来打包项目。
+
+        随着 py 包管理器 pip 的成熟以及 `wheel` 格式的广泛接受，
+        现代 py 打包流程往往更倾向于使用这些工具而非直接操作 `setuptools`。
+
+        不过，`setuptools` 仍然是整个生态系统中一个非常重要且不可或缺的组成部分。
+    )
+
+    注意：本文档中出现的 "py"，如无特殊说明，则指代 "Python"。
+"""
+import os
+from setuptools import setup, find_packages
+
+
+def read_requirements():
+    """
+        读取【项目根目录】下的 `requirements.txt` 文件
+        并提取所有依赖项。
+
+        如果文件存在，则打开并按行读取，
+        去除每行末尾的空白字符后返回一个列表；
+        如果文件不存在，则返回一个空列表。
+
+    :return:
+    """
+    requirements_path = "requirements.txt"
+    if not os.path.exists(requirements_path):
+        return []
+    with open(
+            file=requirements_path,
+            mode="r",
+            encoding="utf-8"
+    ) as reqs_file:
+        reqs = [
+            req.strip()
+            for req in reqs_file.readlines()
+        ]
+        return reqs
+
+
+def read_long_description():
+    """
+        读取【项目根目录】下的 `README.rst` 文件
+        并提取完整的长描述文本。
+
+        如果文件存在，则打开并读取文件内容，
+        作为长描述文本返回；
+        如果文件不存在，则返回一个空字符串。
+
+    :return:
+    """
+    readme_path = "README.rst"
+    if not os.path.exists(readme_path):
+        return ""
+    with open(
+            file=readme_path,
+            mode="r",
+            encoding="utf-8"
+    ) as desc_file:
+        long_description = desc_file.read()
+        return long_description
+
+
+setup(
+    # 指定包的【名称】
+    # 在 `PyPI` 或其他索引服务器上应该是唯一的
+    name="toexcel",
+    # 给出包的【简短描述】
+    description="多场景数据转换至 Excel 表格工具。",
+    # 提供一个【详细描述】
+    long_description=read_long_description(),
+    # 指定包描述的【内容类型】
+    # 指出包的长描述（long description）是
+    # 用 `reStructuredText` 格式编写的
+    long_description_content_type="text/x-rst",
+    # 指定项目的【主页链接】
+    # 通常是指向项目的【官方仓库】或者专门为该项目建立的【网站】
+    url="https://github.com/gary714/toexecl",
+    # 声明项目使用的【许可证】
+    # "MIT" 表示该软件包采用 `MIT` 许可证
+    # 这是一种宽松的【开源许可证】
+    # 允许人们自由地使用、复制、修改和发布软件
+    license="MIT",
+    # 指定软件包的【作者名称】
+    author="gary",
+    # 指定作者的【电子邮件地址】
+    author_email="mepmb@sina.com",
+    # 用于分类和标记包
+    # `PyPI` 使用它们来组织和搜索包
+    classifiers=[
+        # 表明软件包采用一个被开源倡议
+        # （Open Source Initiative, OSI）认可的 `MIT` 许可证
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9"
+    ],
+    # 用于定义一些与包相关联的关键字
+    # 以便用户在 `PyPI` 上搜索时能找到这个软件包
+    keywords="Execl",
+    # 允许提供额外的链接信息
+    # 这些链接将会显示在 `PyPI` 的项目页面上
+    # 提供给用户额外资源如文档、源代码等参考资料
+    project_urls={
+        # 项目主页
+        "Homepage": "https://github.com/gary714/toexecl",
+        # 标明有额外文档资源可以访问
+        "Documentation": "https://github.com/gary714/toexecl",
+        # 显示源代码所在位置
+        "Source": "https://github.com/gary714/toexecl"
+    },
+    # 明确列出哪些【平台】适用这个软件包
+    # 以便用户和自动化工具知道【兼容性信息】
+    platforms=["Linux", "Windows"],
+    # 定义映射字典
+    # 告诉 `setuptools` 在哪里找到
+    # 【源代码文件】来构建 py 包
+    # 字典中的【键】是【包名称】，【空字符】串键表示【根包】
+    # 【值】是相应【源代码所在目录路径】
+    # "." 表示【当前目录】
+    package_dir={"": "."},
+    # 使用 `setuptools` 提供的 `find_packages()` 函数
+    # 自动查找并包含所有应该被打包为部分安装包的子目录
+    # （通常是那些含有 `__init__.py` 文件的目录）
+    packages=find_packages(),
+    # 直接指定【版本号】
+    version="1.0.0",
+    # 配置涉及到 `setuptools_scm` 插件
+    # 它使用源码管理（SCM）系统（如 git）来发现项目版本
+    use_scm_version={
+        "relative_to": __file__,
+        "local_scheme": "no-local-version"
+    },
+    # 列出在运行时需要满足的【依赖项】
+    # 确保在安装此软件之前这些依赖项也将被安装
+    install_requires=read_requirements()
+)
---
+++ packages/toexcel/toexcel-1.0.0.tar.gz/toexcel-1.0.0/toexcel/pd2xl.py
@@ -0,0 +1,264 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    pd 的 `to_excel()` 函数允许将 df 对象导出到一个 Excel 文件中。
+
+    这个功能是 pd 库中的一部分，使用起来非常方便，
+    尤其是在处理数据分析和数据科学任务时。
+
+    以下是一些 `to_excel()` 函数的基本用法和参数：
+        - 基本用法:
+              `df.to_excel("output.xlsx")`
+          这会将 df 保存到名为 "output.xlsx" 的 Excel 文件中。
+
+        - 指定工作表(sheet)名称:
+              `df.to_excel("output.xlsx", sheet_name="Sheet1")`
+          通过 `sheet_name` 参数可以指定工作表的名称，默认为 "Sheet1"。
+
+        - 设置没有索引:
+              `df.to_excel("output.xlsx", index=False)`
+          如果不想将 df 的索引保存到 Excel 文件中，
+          可以使用 `index=False` 参数。
+
+        - 设置列标题:
+              `df.to_excel("output.xlsx", header=True)`
+          默认情况下 `header=True`，
+          它会将列名作为 Excel 文件中的标题行。
+          如果不需要，可以设置为 False。
+
+    pd 的 `to_excel()` 函数是 df 对象的一个非常有用的方法，
+    它允许将 df 数据直接写入到 Excel 文件中。
+
+    这个功能是通过依赖 `openpyxl` 或者 `xlsxwriter` 等库实现的，
+    为 py 中数据分析提供了极大的便利。
+
+    以下是 `to_excel()` 函数的一些关键特性和参数：
+        - `excel_writer`:
+          指定输出的 Excel 文件的路径或者现有的 ExcelWriter 对象
+          （例如，使用 `pd.ExcelWriter()` 创建）。
+          当需要将多个 df 写入同一个文件的不同工作表时，
+          可以使用 ExcelWriter。
+
+        - `sheet_name`: 字符串类型，默认为 "Sheet1"。
+          指定要写入数据的工作表名称。
+
+        - `na_rep`: 字符串类型，默认为空字符串。
+          在写入 Excel 时用来替换缺失值（NaN）。
+
+        - `float_format`: 字符串类型，默认为 None。控制浮点数的格式。
+
+        - `columns`: 序列类型，默认为 None。如果指定，则只输出这些列。
+
+        - `header`: 布尔值或字符串列表，默认为 True。
+          如果传递了字符串列表，则假设它们是列名；
+          如果 `header` 是 `False`，则在输出文件中省略列名。
+
+        - `index`: 布尔值，默认为 True。决定是否写入索引（行标签）。
+          如果设置为 False，则不会在 Excel 文件中显示 df 索引。
+
+        - `index_label`: 字符串或序列，默认为 None。
+          如果指定并且 index 为 True，则使用这个参数作为索引列的标签。
+
+        - `startrow` 和 `startcol`:
+          分别指定开始写数据时起始行和起始列（从 0 开始计数）。
+
+        - `engine`: 字符串类型，指定用于写文件的引擎，
+          比如 "openpyxl" 或 "xlsxwriter" 等。
+
+        - `merge_cells`: 布尔值，默认为 True。
+          决定是否合并属于 MultiIndex index 或 columns
+          （即具有多级索引）中相同类别下各单元格。
+
+        - `encoding`: 已废弃参数，在新版本 pd 中不再使用，
+          因 py 默认已经是 utf-8 编码。
+
+        - `infs_rep`：字符串类型，默认为空字符串。
+          定义当数据框中存在无穷大值时将其替换成该参数所表示的字符。
+
+        - `mode`：只在使用 `ExcelWriter` 对象时可用；
+          设置模式，例如追加到现有文件而非覆盖等模式选项
+          （例如 "a" 表示追加模式）。
+
+    请注意，在执行 `to_excel()` 之前需要确保
+    已安装 `openpyxl` 或者 `xlsxwriter` 库，
+    因为 pd 需要这些库来处理 xlsx 文件格式。
+
+    通过使用不同参数可以很灵活地控制输出内容和格式，
+    并且可以很容易地集成到自动化数据处理流程中去。
+
+    注意：本脚本中出现的 "py"，如无特殊说明，则指代 "Python"。
+         本脚本中出现的 "pd"，如无特殊说明，则指代 "Pandas"。
+         本脚本中出现的 "df"，如无特殊说明，则指代 "DataFrame"。
+         本脚本中出现的 "np"，如无特殊说明，则指代 "Numpy"。
+"""
+import sys
+from pathlib import Path
+import numpy as np
+import pandas as pd
+
+EXCEL_WRITER_ENGINE = "openpyxl"
+
+# `__all__` 是一个特殊的列表
+# 它定义当从模块执行 `from module import *` 时应该导入哪些属性
+# 如果定义了 `__all__`，只有在这个列表中的属性才会被导入
+# 如果没有定义 `__all__`，那么默认导入模块中不以下划线开头的所有属性
+__all__ = [
+    "pandas2excel"
+]
+
+# 将当前运行的 py 文件所在的上两级目录加入到 py 的【系统路径】中
+# 使得在这个【根目录】下的【模块】和【包】可以被当前文件所引用
+current_file_path = Path(__file__).absolute()
+# 移动到上两级目录以获取【根路径】
+root_path = current_file_path.parent.parent
+# 将【根路径】作为【系统路径】加入 `sys.path`
+sys.path.append(str(root_path))
+
+
+def __compute_column_widths(df: pd.DataFrame) -> np.ndarray:
+    """
+         根据 df 中的数据计算推荐的列宽。
+
+        该函数会遍历 df 的每一列，包括表头和数据单元格，
+        并计算出最适合每列内容显示的宽度。
+
+        为了更准确地反映所需的宽度，
+        这里采用了 UTF-8 编码长度作为宽度的估算值。
+
+    :param df: pd.df - 需要计算列宽的 df 对象。
+    :return: np.ndarray -
+    包含每列推荐宽度（按 UTF-8 编码长度）的 np 数组。
+    """
+    widths = np.max(
+        [
+            df.columns.to_series().apply(
+                lambda x: len(x.encode("utf-8"))
+            ).values,
+            df.astype(str).applymap(
+                lambda x: len(x.encode("utf-8"))
+            ).agg(max).values
+        ],
+        axis=0
+    )
+    return widths
+
+
+def __set_openpyxl_column_widths(
+        widths: np.ndarray,
+        writer: pd.ExcelWriter,
+        sheet_name: str) -> None:
+    """
+        根据提供的列宽数组，设置指定 Excel 工作表的列宽。
+
+        使用 openpyxl 库设置 pd ExcelWriter 对象中特定工作表的列宽。
+
+        每个列宽将会增加 2 个单位以获得更好的可读性。
+
+        注意：此函数不会保存工作簿；
+        调用者应单独调用 writer.save() 方法来保存更改。
+
+    :param widths: numpy 数组，包含要设置的每列的宽度。
+    :param writer: pd ExcelWriter 对象，用于写入 Excel 文件。
+    :param sheet_name: 字符串，指定要设置列宽的工作表名称。
+    :return: 无返回值。
+    """
+    from openpyxl.utils import get_column_letter
+
+    for i, width in enumerate(widths, start=1):
+        writer.sheets[sheet_name].column_dimensions[
+            get_column_letter(i)
+        ].width = width + 2
+
+
+def __set_xlsxwriter_column_widths(
+        widths: np.ndarray,
+        writer: pd.ExcelWriter,
+        sheet_name: str) -> None:
+    """
+        设置使用 xlsxwriter 引擎的 Excel 工作表中各列的宽度。
+
+        根据提供的列宽数组 `widths` 来调整指定
+        工作表 `sheet_name` 中各列的宽度。
+
+        此函数专门为使用 xlsxwriter 引擎的 pd.ExcelWriter 编写，
+        不适用于其他引擎。
+
+        注意：实际设置的列宽会在指定宽度基础上增加 2 个单位，
+        以便留出一些额外空间。
+
+    :param widths: np 数组，包含希望设置的每一列的宽度。
+    :param writer: pd 的 ExcelWriter 实例，
+    必须已经使用 xlsxwriter 引擎初始化。
+    :param sheet_name: 字符串，表示要调整列宽的工作表名称。
+    :return: 无返回值（None）。
+    """
+    worksheet = writer.sheets.get(sheet_name)
+    for i, width in enumerate(widths, start=1):
+        worksheet.set_column(i - 1, i - 1, width + 2)
+
+
+column_widths_setters = {
+    "xlsxwriter": __set_xlsxwriter_column_widths,
+    "openpyxl": __set_openpyxl_column_widths,
+}
+
+
+def pandas2excel(
+        df: pd.DataFrame,
+        filename: str,
+        sheet_name: str = "Sheet1",
+        engine: str = EXCEL_WRITER_ENGINE,
+        index: bool = False,
+        header: bool = True,
+        **kwargs) -> None:
+    """
+        将 pd df 导出为 Excel 文件，并自动调整列宽。
+
+        此函数会计算 df 每列内容的最大字节长度，
+        以此估计并设置 Excel 单元格的列宽，
+        确保内容在 Execl 表格中能够优雅地展示而不会出现截断。
+
+    :param df: 需要导出的 pd df 对象。
+    :param filename:
+    导出的 Excel 文件名，包含路径和文件扩展名。
+    :param sheet_name:
+    Excel 文件中工作表的名称，默认为 "Sheet1"。
+    :param engine: 写入 Excel 文件时使用的引擎，
+    如 "openpyxl" 或 "xlsxwriter"。
+    :param index: 是否将行索引输出到 Excel 文件，
+    默认为 False，不输出行索引。
+    :param header: 是否将列名作为头部输出到 Excel 文件，
+    默认为 True，输出头部。
+    :return: 无返回值。函数执行后，
+    输入的 pd.DataFrame 数据会被写入指定路径的 Excel 文件中。
+    """
+    # 计算数据框各列内容所需的最大列宽
+    widths = __compute_column_widths(df)
+    # 创建一个用于写入 Execl 的上下文环境
+    # 并确保文件正确保存关闭
+    with pd.ExcelWriter(
+            filename,
+            engine=engine
+    ) as writer:
+        # 将数据框导出至 Execl
+        # 指定工作表名称并排除索引
+        df.to_excel(
+            excel_writer=writer,
+            sheet_name=sheet_name,
+            index=index,
+            header=header,
+            # 解包额外参数
+            **kwargs
+        )
+        if engine in column_widths_setters:
+            set_column_widths_func = \
+                column_widths_setters.get(engine)
+            set_column_widths_func(
+                widths=widths,
+                writer=writer,
+                sheet_name=sheet_name
+            )
+        else:
+            raise ValueError(
+                f"不支持的引擎类型 \"{engine}\"。"
+            )
