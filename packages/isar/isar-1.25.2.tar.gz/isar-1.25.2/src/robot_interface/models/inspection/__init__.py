73ee26ffea208e85dd37107377cd0def89b38d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 15:05:02 +0000

    Add package isar/1.25.2/isar-1.25.2.tar.gz

---
+++ packages/isar/isar-1.25.2.tar.gz/isar-1.25.2/src/robot_interface/models/inspection/__init__.py
@@ -0,0 +1,13 @@
+from .inspection import (
+    Audio,
+    Image,
+    ImageMetadata,
+    Inspection,
+    InspectionMetadata,
+    ThermalImage,
+    ThermalImageMetadata,
+    ThermalVideo,
+    ThermalVideoMetadata,
+    Video,
+    VideoMetadata,
+)
