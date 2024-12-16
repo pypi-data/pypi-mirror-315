ca21030876dc0cdada7bc74df8c501eac7d01165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 14:05:15 +0000

    Add package mafm/0.0.10/mafm-0.0.10-py3-none-any.whl

---
+++ packages/mafm/mafm-0.0.10-py3-none-any.whl/mafm/wrappers/__init__.py
@@ -0,0 +1,7 @@
+"""Initailize the wrappers module."""
+
+from mafm.wrappers.abf import run_abf
+from mafm.wrappers.finemap import run_finemap
+from mafm.wrappers.susie import run_susie
+from mafm.wrappers.carma import run_carma
+from mafm.wrappers.RSparsePro import run_rsparsepro
