56cc73107d15a3f9744249218e84072b91dea49e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 04:04:56 +0000

    Add package test-evilaliv3/0.0.3/test_evilaliv3-0.0.3.tar.gz

---
+++ packages/test-evilaliv3/test_evilaliv3-0.0.3.tar.gz/test_evilaliv3-0.0.3/README.md
@@ -0,0 +1,17 @@
+globaleaks-ephemeral-fs
+=======================
+Ephemeral encrypted filesystem built on top of FUSE and ChaCha20
+
+Installation
+------------
+pip install globaleaks-ephemeral-fs
+
+In Python
+---------
+from globaleaks_ephemeral_fs import EphemeralFS
+
+
+Command line use
+----------------
+globaleaks-ephemeral-fs <mountpoint>
+
---
+++ packages/test-evilaliv3/test_evilaliv3-0.0.3.tar.gz/test_evilaliv3-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="test-evilaliv3",  # Your package name
+    version="0.0.3",  # Version number
+    description="Ephemeral file system with secure temporary file handling",
+    author="Giovanni Pellerano",
+    author_email="giovanni.pellerano@globaleaks.org",
+    license="AGPL-3.0-or-later",
+    install_requires=["cryptography", "fusepy"],
+    test_suite="tests",  # The directory for your tests
+    tests_require=[
+        "unittest",  # If you’re using unittest, this is the dependency
+    ],
+    entry_points={
+        "console_scripts": [
+            "globaleaks-ephemeral-fs=globaleaks_ephemeral_fs.mount_ephemeral_fs:main",  # If you have a main entry point in the mount module
+        ],
+    },
+    python_requires='>=3.6',  # Minimum Python version required,
+    project_urls={
+        'Homepage': 'https://github.com/globaleaks/globaleaks_ephemeral_fs',
+        'Issues': 'https://github.com/globaleaks/globaleaks_ephemeral_fs'
+    },
+)
