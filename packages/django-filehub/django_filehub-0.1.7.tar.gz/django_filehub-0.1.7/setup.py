8156823a5e929cedcbfc6d4bfbc43765f2306c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 03:19:45 +0000

    Add package django-filehub/0.1.7/django_filehub-0.1.7.tar.gz

---
+++ packages/django-filehub/django_filehub-0.1.7.tar.gz/django_filehub-0.1.7/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django_filehub",
+    version="0.1.7",
+    author="Suresh Chand",
+    author_email="scthakuri12a@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "django",
+        "requests"
+    ],
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    include_package_data=True,
+    package_data={
+        "django_filehub": [
+            "templates/*",
+            "static/*",
+        ],
+    },
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/scthakuri/django-filehub/issues",
+        "Source": "https://github.com/scthakuri/django-filehub",
+    },
+)
