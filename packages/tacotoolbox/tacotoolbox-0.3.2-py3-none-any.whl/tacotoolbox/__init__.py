1345cbbc205bb0814c0f623eb98a2711b9e4f8c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 03:19:59 +0000

    Add package tacotoolbox/0.3.2/tacotoolbox-0.3.2-py3-none-any.whl

---
+++ packages/tacotoolbox/tacotoolbox-0.3.2-py3-none-any.whl/tacotoolbox/__init__.py
@@ -0,0 +1,7 @@
+from tacotoolbox import datamodel
+from tacotoolbox.create import create
+from tacotoolbox import edit
+import pytortilla as tortilla
+
+__all__ = ["datamodel", "create", "tortilla", "edit", "tortilla2taco", "taco2tortilla"]
+__version__ = "0.3.2"
