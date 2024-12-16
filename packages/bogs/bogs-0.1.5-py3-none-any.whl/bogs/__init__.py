acee55516d5673c22f5294119669728cc9020cd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 04:07:57 +0000

    Add package bogs/0.1.5/bogs-0.1.5-py3-none-any.whl

---
+++ packages/bogs/bogs-0.1.5-py3-none-any.whl/bogs/__init__.py
@@ -0,0 +1,19 @@
+"""Top-level package for TCRA."""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import scipy
+import folium
+from bogs import hazard
+from bogs import vulnerability
+from bogs import damageprob
+from bogs import loss
+from bogs import recovery
+from bogs import plot
+from bogs import functionality
+from bogs import social
+
+__author__ = """Ram Krishna Mazumder"""
+__email__ = 'swaranjitroy08@gmail.com'
+__version__ = '0.1'
+
