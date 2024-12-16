1c055963fbc27ff12da5826ca388fe7f3d396ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 05:05:50 +0000

    Add package vellum-ai/0.11.4/vellum_ai-0.11.4-py3-none-any.whl

---
+++ packages/vellum-ai/vellum_ai-0.11.4-py3-none-any.whl/vellum/workflows/nodes/displayable/code_execution_node/utils.py
@@ -0,0 +1,22 @@
+import os
+from typing import Union
+
+
+def get_project_root() -> str:
+    current_dir = os.getcwd()
+    while current_dir != "/":
+        if ".git" in os.listdir(current_dir):
+            return current_dir
+        current_dir = os.path.dirname(current_dir)
+    raise FileNotFoundError("Project root not found.")
+
+
+def read_file_from_path(filepath: str) -> Union[str, None]:
+    project_root = get_project_root()
+    relative_filepath = os.path.join(project_root, filepath)
+
+    if not os.path.exists(relative_filepath):
+        return None
+
+    with open(relative_filepath) as file:
+        return file.read()
