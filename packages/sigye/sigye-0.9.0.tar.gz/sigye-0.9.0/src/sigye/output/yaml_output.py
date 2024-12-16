71b94dadef709d52b6829f8f9c9122c4c52dba6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 04:06:47 +0000

    Add package sigye/0.9.0/sigye-0.9.0.tar.gz

---
+++ packages/sigye/sigye-0.9.0.tar.gz/sigye-0.9.0/src/sigye/output/yaml_output.py
@@ -0,0 +1,14 @@
+import ryaml
+
+from ..models import TimeEntry
+from .output import OutputFormatter
+
+
+class YamlOutput(OutputFormatter):
+    def single_entry_output(self, entry: TimeEntry | None) -> None:
+        if entry is None:
+            return
+        print(ryaml.dumps(entry.model_dump(mode="json")))
+
+    def multiple_entries_output(self, entries: list[TimeEntry]) -> None:
+        print(ryaml.dumps({"entries": [entry.model_dump(mode="json") for entry in entries]}))
