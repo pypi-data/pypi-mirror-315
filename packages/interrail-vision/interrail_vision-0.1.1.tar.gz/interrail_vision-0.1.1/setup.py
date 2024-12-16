bbf811b4765313373c7b08dc0228765398eaf2a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 15:05:26 +0000

    Add package interrail-vision/0.1.1/interrail_vision-0.1.1.tar.gz

---
+++ packages/interrail-vision/interrail_vision-0.1.1.tar.gz/interrail_vision-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="interrail_vision",
+    version="0.1.1",
+    description="A package for reading images and PDFs via AI-powered vision",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="ShakhCo",
+    author_email="shakhzodbeksharipov@outlook.com",
+    url="https://github.com/Shakh7/",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "pillow",
+        "pymupdf",
+        "openai",
+    ],
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
