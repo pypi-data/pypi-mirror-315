43c9c8f8fd4d4a03cfb83423a6af57a55d093c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 14:05:44 +0000

    Add package openpo/0.5.13/openpo-0.5.13.tar.gz

---
+++ packages/openpo/openpo-0.5.13.tar.gz/openpo-0.5.13/openpo/resources/provider/openai.py
@@ -0,0 +1,82 @@
+import os
+from typing import Any, Dict, List, Optional, Union
+
+import pandas as pd
+from openai import OpenAI as OpenAIClient
+from openai import OpenAIError
+from pydantic import BaseModel
+
+from openpo.internal import prompt as prompt_lib
+from openpo.internal.error import AuthenticationError, ProviderError
+
+from .base import LLMProvider
+
+
+class AnnotateModel(BaseModel):
+    q_index: int
+    rank: List[int]
+    preferred_score: float
+    rejected_score: float
+    reason: str
+
+
+class Response(BaseModel):
+    evaluation: List[AnnotateModel]
+
+
+class OpenAI(LLMProvider):
+    def __init__(self, api_key: str):
+        if not api_key:
+            raise AuthenticationError("OpenAI")
+        try:
+            self.client = OpenAIClient(api_key=api_key)
+        except Exception as e:
+            raise AuthenticationError(
+                "OpenAI", message=f"Failed to initialize OpenAI client: {str(e)}"
+            )
+
+    def generate(
+        self,
+        model: str,
+        questions: List[str],
+        responses: List[List],
+        prompt: Optional[str] = None,
+    ):
+        messages = [
+            {
+                "role": "system",
+                "content": prompt if prompt else prompt_lib.EVALUATION_PROMPT,
+            },
+            {
+                "role": "user",
+                "content": prompt_lib.EVALUATION_QUERY.format(questions, responses),
+            },
+        ]
+
+        try:
+            res = self.client.beta.chat.completions.parse(
+                model=model,
+                messages=messages,
+                response_format=Response,
+                max_tokens=8192,
+            )
+
+            return res
+        except OpenAIError as e:
+            if "authentication" in str(e).lower():
+                raise AuthenticationError(
+                    "OpenAI",
+                    message=str(e),
+                    status_code=e.status_code if hasattr(e, "status_code") else None,
+                    response=e.response if hasattr(e, "response") else None,
+                )
+            raise ProviderError(
+                "OpenAI",
+                message=str(e),
+                status_code=e.status_code if hasattr(e, "status_code") else None,
+                response=e.response if hasattr(e, "response") else None,
+            )
+        except Exception as e:
+            raise ProviderError(
+                "OpenAI", message=f"Request to OpenAI model failed: {str(e)}"
+            )
