8c229781f89eea9ec6e44135f05b6c2a96c0c7f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 03:19:50 +0000

    Add package Kala-Qubits/0.1.0/Kala_Qubits-0.1.0-py3-none-any.whl

---
+++ packages/Kala-Qubits/Kala_Qubits-0.1.0-py3-none-any.whl/Qubits/__init__.py
@@ -0,0 +1,12 @@
+# __init__.py
+
+from .Qubits import Gate, QuantumRegister, ClassicalRegister, QuantumCircuit
+
+__all__ = [
+    "Gate",
+    "QuantumRegister",
+    "ClassicalRegister",
+    "QuantumCircuit",
+]
+
+__version__ = "0.1.0"
