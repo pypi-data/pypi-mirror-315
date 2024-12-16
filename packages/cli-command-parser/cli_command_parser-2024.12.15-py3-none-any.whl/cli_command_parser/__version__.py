f1dc6865832ef204797c22c681934179f1d5bf80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 04:05:03 +0000

    Add package cli-command-parser/2024.12.15/cli_command_parser-2024.12.15-py3-none-any.whl

---
+++ packages/cli-command-parser/cli_command_parser-2024.12.15-py3-none-any.whl/cli_command_parser/__version__.py
@@ -0,0 +1,8 @@
+__title__ = 'cli_command_parser'
+__description__ = 'CLI Command Parser'
+__url__ = 'https://github.com/dskrypa/cli_command_parser'
+__version__ = '2024.12.15'
+__author__ = 'Doug Skrypa'
+__author_email__ = 'dskrypa@gmail.com'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2023 Doug Skrypa'
