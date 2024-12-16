cbeb22a1c883be80ee1c68f8ea3b0488bf55c0c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 14:05:22 +0000

    Add package player-performance-ratings/5.18.3/player_performance_ratings-5.18.3.tar.gz

---
+++ packages/player-performance-ratings/player_performance_ratings-5.18.3.tar.gz/player_performance_ratings-5.18.3/player_performance_ratings/ratings/performance_generator/__init__.py
@@ -0,0 +1,7 @@
+
+from player_performance_ratings.ratings.performance_generator.performances_generator import (
+    ColumnWeight,
+    Performance,
+    auto_create_pre_performance_transformations,
+    PerformancesGenerator,
+)
