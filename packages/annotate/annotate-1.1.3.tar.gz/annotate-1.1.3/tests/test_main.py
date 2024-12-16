482b8d92ea9f386cc46e76b2a7c1ac6c237805c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 04:05:07 +0000

    Add package annotate/1.1.3/annotate-1.1.3.tar.gz

---
+++ packages/annotate/annotate-1.1.3.tar.gz/annotate-1.1.3/tests/test_main.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2012 Adam Karpierz
+# SPDX-License-Identifier: Zlib
+
+import unittest
+
+import annotate
+from annotate import annotate
+
+
+class MainTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
