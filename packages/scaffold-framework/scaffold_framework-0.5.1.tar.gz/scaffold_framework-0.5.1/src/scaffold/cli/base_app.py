1b7a70f06df3b107c16a724dfccb9507ba23021e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 03:20:06 +0000

    Add package scaffold-framework/0.5.1/scaffold_framework-0.5.1.tar.gz

---
+++ packages/scaffold-framework/scaffold_framework-0.5.1.tar.gz/scaffold_framework-0.5.1/src/scaffold/cli/base_app.py
@@ -0,0 +1,37 @@
+import argparse
+import asyncio
+
+from .decorators import Command
+
+
+class BaseCLIApp:
+    def __init__(self) -> None:
+        self.parser = argparse.ArgumentParser()
+        self.subparsers = self.parser.add_subparsers()
+        self._register_commands()
+
+    def _register_commands(self) -> None:
+        for attr_name in dir(self):
+            command = getattr(self, attr_name)
+            if isinstance(command, Command):
+                subparser = self.subparsers.add_parser(
+                    command.name,
+                    help=command.help,
+                )
+                for args, kwargs in command.arguments:
+                    subparser.add_argument(*args, **kwargs)
+                subparser.set_defaults(func=command)
+
+    def run(self) -> None:
+        args = self.parser.parse_args()
+
+        if hasattr(args, "func"):
+            kwargs = dict(vars(args))
+            del kwargs["func"]
+            result = args.func(self, **kwargs)
+
+            if asyncio.iscoroutine(result):
+                asyncio.run(result)
+
+        else:
+            self.parser.print_help()
