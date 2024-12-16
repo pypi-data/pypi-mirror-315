ff1414c9e1c8fb1fe6f73178e3489f469b5ea0d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 03:19:45 +0000

    Add package NCBI-id-grab/0.0.3/ncbi_id_grab-0.0.3.tar.gz

---
+++ packages/NCBI-id-grab/ncbi_id_grab-0.0.3.tar.gz/ncbi_id_grab-0.0.3/tests/test_package.py
@@ -0,0 +1,4 @@
+from NCBI_id_grab_CONG_PHAM.NCBI_ID_grab import find_NCBI_ID_from_Bacterial_list
+
+bacteria = ['Eubacterium_sp.', 'Ruminococcaceae_', 'Blautia_', 'Lactiplantibacillus_plantarum']
+find_NCBI_ID_from_Bacterial_list(bacteria)
