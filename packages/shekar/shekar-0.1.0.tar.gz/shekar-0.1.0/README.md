992319c634be40095bc2ef945b7d6da54398da25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 14:05:24 +0000

    Add package shekar/0.1.0/shekar-0.1.0.tar.gz

---
+++ packages/shekar/shekar-0.1.0.tar.gz/shekar-0.1.0/README.md
@@ -0,0 +1,6 @@
+<p align="center">
+  <img src="https://amirivojdan.io/shekar_logo.svg" alt="drawing" width="150"/>
+  <h2 align="center" >فارسی شکر است!</h2>
+</p>
+
+# Simplifying Persian NLP for Everyone
---
+++ packages/shekar/shekar-0.1.0.tar.gz/shekar-0.1.0/tests/test_utils.py
@@ -0,0 +1,15 @@
+import pytest
+from shekar.utils import is_informal
+from shekar import Shekar
+
+def test_is_informal():
+    input_text = "میخوام برم خونه، تو نمیای؟"
+    expected_output = (True, 4)
+    assert is_informal(input_text) == expected_output
+
+    input_text = "دیگه چه خبر؟"
+    expected_output = (True, 1)
+    assert is_informal(input_text) == expected_output
+
+if __name__ == "__main__":
+    pytest.main()
