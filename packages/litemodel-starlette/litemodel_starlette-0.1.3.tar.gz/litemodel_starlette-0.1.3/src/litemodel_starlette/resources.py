940dff95c78c81d025e8753448e9cbd6e22ca270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 14:05:04 +0000

    Add package litemodel-starlette/0.1.3/litemodel_starlette-0.1.3.tar.gz

---
+++ packages/litemodel-starlette/litemodel_starlette-0.1.3.tar.gz/litemodel_starlette-0.1.3/src/litemodel_starlette/resources.py
@@ -0,0 +1,11 @@
+from databases import Database
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+from litemodel_starlette import settings
+
+database = Database(settings.DATABASE_URL)
+templates = Jinja2Templates(directory=settings.TEMPLATES_DIR)
+admin_templates = Jinja2Templates(directory=settings.ADMIN_TEMPLATES_DIR)
+model_templates = Jinja2Templates(directory=settings.MODEL_TEMPLATES_DIR)
+static = StaticFiles(directory=settings.STATIC_DIR)
+# images = StaticFiles(directory=settings.IMAGE_DIR)
