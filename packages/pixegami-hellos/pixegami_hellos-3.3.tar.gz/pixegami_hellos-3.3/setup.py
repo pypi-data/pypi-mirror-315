f73bd9d13ff94e3e5acd130eda530e41b85d5f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 15:05:06 +0000

    Add package pixegami-hellos/3.3/pixegami_hellos-3.3.tar.gz

---
+++ packages/pixegami-hellos/pixegami_hellos-3.3.tar.gz/pixegami_hellos-3.3/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='pixegami_hellos',
+    version='3.3',
+    packages=find_packages(),  # Automatically find all packages in the directory
+    install_requires=[
+        'Flask',
+        'Flask-SQLAlchemy',
+    ],
+)
