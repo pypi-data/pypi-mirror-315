8fd3d251f062c1b6a4f22c9dfdf09b1e7993de21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 14:05:16 +0000

    Add package openpo/0.5.8/openpo-0.5.8.tar.gz

---
+++ packages/openpo/openpo-0.5.8.tar.gz/openpo-0.5.8/openpo/resources/provider/anthropic.py
@@ -0,0 +1,73 @@
+import os
+from typing import Any, Dict, List, Optional, Union
+
+import pandas as pd
+from anthropic import Anthropic as AnthropicClient
+from pydantic import BaseModel
+
+from openpo.internal import prompt as prompt_lib
+from openpo.internal.error import AuthenticationError, ProviderError
+
+from .base import LLMProvider
+
+
+class AnnotateModel(BaseModel):
+    rank: List[int]
+    preferred_score: float
+    rejected_score: float
+    reason: str
+
+
+class Response(BaseModel):
+    preference: List[AnnotateModel]
+
+
+class Anthropic(LLMProvider):
+    def __init__(self, api_key: str):
+        if not api_key:
+            raise AuthenticationError("Anthropic")
+        try:
+            self.client = AnthropicClient(api_key=api_key)
+        except Exception as e:
+            raise AuthenticationError(
+                "Anthropic", message=f"Failed to initialize Anthropic client: {str(e)}"
+            )
+
+    def generate(
+        self,
+        model: str,
+        questions: List[str],
+        responses: List[List],
+        prompt: Optional[str] = None,
+    ):
+        tools = [
+            {
+                "name": "build_response_output",
+                "description": "build response output with predefined structure",
+                "input_schema": Response.model_json_schema(),
+            }
+        ]
+
+        try:
+            res = self.client.messages.create(
+                model=model,
+                system=prompt if prompt else prompt_lib.EVALUATION_PROMPT,
+                messages=[
+                    {
+                        "role": "user",
+                        "content": prompt_lib.EVALUATION_QUERY.format(
+                            questions, responses
+                        ),
+                    },
+                ],
+                max_tokens=4096,
+                tools=tools,
+                tool_choice={"type": "tool", "name": "build_response_output"},
+            )
+
+            return res
+
+        except Exception as e:
+            raise ProviderError(
+                "Anthropic", message=f"Request to Anthropic model failed: {str(e)}"
+            )
