4cb9502181b01fdb4320a0107282c787b9f36328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 04:05:12 +0000

    Add package mdformat_mkdocs/4.0.0/mdformat_mkdocs-4.0.0.tar.gz

---
+++ packages/mdformat_mkdocs/mdformat_mkdocs-4.0.0.tar.gz/mdformat_mkdocs-4.0.0/mdformat_mkdocs/mdit_plugins/__init__.py
@@ -0,0 +1,33 @@
+"""Plugins."""
+
+from ._material_admon import MATERIAL_ADMON_MARKERS, material_admon_plugin
+from ._material_content_tabs import (
+    MATERIAL_CONTENT_TAB_MARKERS,
+    material_content_tabs_plugin,
+)
+from ._mkdocstrings_autorefs import (
+    MKDOCSTRINGS_AUTOREFS_PREFIX,
+    MKDOCSTRINGS_HEADING_AUTOREFS_PREFIX,
+    mkdocstrings_autorefs_plugin,
+)
+from ._mkdocstrings_crossreference import (
+    MKDOCSTRINGS_CROSSREFERENCE_PREFIX,
+    mkdocstrings_crossreference_plugin,
+)
+from ._pymd_abbreviations import PYMD_ABBREVIATIONS_PREFIX, pymd_abbreviations_plugin
+from ._python_markdown_admon import python_markdown_admon_plugin
+
+__all__ = (
+    "MATERIAL_ADMON_MARKERS",
+    "MATERIAL_CONTENT_TAB_MARKERS",
+    "MKDOCSTRINGS_AUTOREFS_PREFIX",
+    "MKDOCSTRINGS_CROSSREFERENCE_PREFIX",
+    "MKDOCSTRINGS_HEADING_AUTOREFS_PREFIX",
+    "PYMD_ABBREVIATIONS_PREFIX",
+    "material_admon_plugin",
+    "material_content_tabs_plugin",
+    "mkdocstrings_autorefs_plugin",
+    "mkdocstrings_crossreference_plugin",
+    "pymd_abbreviations_plugin",
+    "python_markdown_admon_plugin",
+)
