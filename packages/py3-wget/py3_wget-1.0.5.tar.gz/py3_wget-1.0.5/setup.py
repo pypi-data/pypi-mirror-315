fca4c7eaa10312a622884d6d1a5fbfecfd0cf2b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 14:05:22 +0000

    Add package py3-wget/1.0.5/py3_wget-1.0.5.tar.gz

---
+++ packages/py3-wget/py3_wget-1.0.5.tar.gz/py3_wget-1.0.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = "1.0.5"
+DESCRIPTION = "A tool to download files. It supports progress bar, cksum, timeout, retry failed download."
+
+# Setting up
+setup(
+    name="py3_wget",
+    version=VERSION,
+    author="Gabriele Berton",
+    author_email="<berton.gabri@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=["tqdm"],
+    keywords=["python", "download", "progress bar"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
