973df20364289431f8344e5eab5e0cb61a51f239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 03:19:53 +0000

    Add package pyemon/0.1.0/pyemon-0.1.0-py3-none-any.whl

---
+++ packages/pyemon/pyemon-0.1.0-py3-none-any.whl/pyemon/tasks/task/run.py
@@ -0,0 +1,28 @@
+from ...task import *
+from ...directory import *
+import glob
+import importlib
+
+class TaskRunTask(Task):
+  def run(self, argv):
+    sys.path.append(os.getcwd())
+    tasks = {}
+    for path in glob.glob("**/pyetask.py", recursive = True):
+      names = Directory.split(path)
+      names[-1] = os.path.splitext(names[-1])[0]
+      for task in importlib.import_module(".".join(names)).Task.tasks():
+        tasks[task.Name] = task
+        Task.set(task)
+    if len(argv) == 0:
+      print("<Tasks>")
+      for task in tasks.values():
+        print(task.to_string("  "))
+        print("")
+    else:
+      newArgv = copy.deepcopy(argv)
+      name = List.shift(newArgv)
+      if name in tasks:
+        tasks[name].run(newArgv)
+      else:
+        sys.exit(Task.to_undefined_string(name))
+Task.parse_if_main(__name__, TaskRunTask("<task args>"))
