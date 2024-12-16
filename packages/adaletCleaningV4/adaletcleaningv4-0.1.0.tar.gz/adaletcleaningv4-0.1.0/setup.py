eaa6a0de2b12dfa8537833727ff5bc44aba27778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 04:05:16 +0000

    Add package adaletCleaningV4/0.1.0/adaletcleaningv4-0.1.0.tar.gz

---
+++ packages/adaletCleaningV4/adaletcleaningv4-0.1.0.tar.gz/adaletcleaningv4-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name="adaletCleaningV4",
+    version= "0.1.0",
+    description="",
+    packages=find_packages(),
+    include_package_data=True
+)
+
