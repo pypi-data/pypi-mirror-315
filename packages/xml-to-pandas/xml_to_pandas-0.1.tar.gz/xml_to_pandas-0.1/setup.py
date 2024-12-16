a25e8b587b67e87d18a3f2568ebd46c1f1bf7891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 03:20:08 +0000

    Add package xml-to-pandas/0.1/xml_to_pandas-0.1.tar.gz

---
+++ packages/xml-to-pandas/xml_to_pandas-0.1.tar.gz/xml_to_pandas-0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding='utf-8') #Gets the long description from Readme file
+
+setup(
+    name='xml_to_pandas',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+    ],  # Add a comma here
+    author='Pinak Tendulkar',
+    author_email='pdtendulkar140203@gmail.com.com',
+    description='Converts all kinds of XML files to a pandas dataframe',
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+     project_urls={
+           'Source Repository': 'https://github.com/Pi2003/'
+    }
+)
