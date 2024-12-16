cfbcef6581e40493a6d22828eb2b11bb3bfbf5e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 05:05:20 +0000

    Add package reling/1.10.0/reling-1.10.0.tar.gz

---
+++ packages/reling/reling-1.10.0.tar.gz/reling-1.10.0/src/reling/app/commands/exam/types.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+
+from reling.types import DialogueExchangeData, Input
+
+__all__ = [
+    'ExchangeWithTranslation',
+    'PreScoreWithSuggestion',
+    'ScoreWithSuggestion',
+    'SentenceWithTranslation',
+]
+
+
+@dataclass
+class SentenceWithTranslation:
+    sentence: str
+    translation: Input
+
+
+@dataclass
+class ExchangeWithTranslation:
+    exchange: DialogueExchangeData
+    user_translation: Input
+
+
+@dataclass
+class PreScoreWithSuggestion:
+    score: int
+    suggestion: str | None
+
+
+@dataclass
+class ScoreWithSuggestion:
+    score: int
+    suggestion: str | None
