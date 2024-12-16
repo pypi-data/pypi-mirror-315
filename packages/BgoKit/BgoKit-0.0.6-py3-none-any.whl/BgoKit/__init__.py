db84826b14cbe991e361e166d4c6a1033f496864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 15:05:01 +0000

    Add package BgoKit/0.0.6/BgoKit-0.0.6-py3-none-any.whl

---
+++ packages/BgoKit/BgoKit-0.0.6-py3-none-any.whl/BgoKit/__init__.py
@@ -0,0 +1,16 @@
+
+__description__ = 'BgoKit, A tool package for Bgolearn'
+__documents__ = 'https://bgolearn.netlify.app/'
+__author__ = 'Bin Cao, Advanced Materials Thrust, Hong Kong University of Science and Technology (Guangzhou)'
+__author_email__ = 'binjacobcao@gmail.com'
+__url__ = 'https://github.com/Bin-Cao/Bgolearn'
+
+import datetime
+now = datetime.datetime.now()
+formatted_date_time = now.strftime('%Y-%m-%d %H:%M:%S')
+   
+
+print('BgoKit, A tool package for Bgolearn')
+print('If you need any help, please contact me at github.com/Bin-Cao/Bgolearn')
+print('Executed on :',formatted_date_time, ' | Have a great day.')  
+print('='*80)
