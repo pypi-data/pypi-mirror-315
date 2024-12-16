6e97097e8f2adc98c8d5602c4afd958645f6664b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 14:05:32 +0000

    Add package seatools-starter-redis/1.0.3/seatools-starter-redis-1.0.3.tar.gz

---
+++ packages/seatools-starter-redis/seatools-starter-redis-1.0.3.tar.gz/seatools-starter-redis-1.0.3/seatools/ioc/starters/redis/redis.py
@@ -0,0 +1,30 @@
+from seatools.ioc.config import cfg
+from seatools.ioc.base import get_bean_factory
+from seatools.redis.dbconfig import RedisConfig
+from seatools.redis.utils import new_redis
+from seatools.ioc.injects import Bean
+from loguru import logger
+
+
+@Bean
+def init_redis():
+    redis_config = None
+    config = cfg()
+    if 'seatools' in config and 'redis' in config['seatools']:
+        redis_config = config['seatools']['redis']
+
+    if not redis_config:
+        logger.warning('配置[seatools.redis]不存在, 无法自动初始redis bean实例')
+        return
+    if not isinstance(redis_config, dict):
+        logger.error('配置[seatools.redis]属性不是字典类型, 无法自动初始redis bean实例')
+        exit(1)
+    try:
+        config = RedisConfig(**redis_config)
+        client = new_redis(config, config.config)
+        bean_factory = get_bean_factory()
+        # 注册bean, 非延迟注册
+        bean_factory.register_bean(name=config.name or 'redis', cls=client, primary=config.primary, lazy=False)
+    except Exception as e:
+        logger.error(f'配置[seatools.redis]存在不支持的参数, 请检查修改配置后重试')
+        exit(1)
