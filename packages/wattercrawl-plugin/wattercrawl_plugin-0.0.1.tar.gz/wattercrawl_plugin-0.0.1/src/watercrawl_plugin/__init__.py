94cf3177b4d59908413506ad18833572c8151751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 04:04:54 +0000

    Add package wattercrawl-plugin/0.0.1/wattercrawl_plugin-0.0.1.tar.gz

---
+++ packages/wattercrawl-plugin/wattercrawl_plugin-0.0.1.tar.gz/wattercrawl_plugin-0.0.1/src/watercrawl_plugin/__init__.py
@@ -0,0 +1,15 @@
+from .base import AbstractPipelinePlugin, AbstractInputValidator, AbstractPlugin
+
+version = '0.0.1'
+
+__all__ = [
+    'AbstractPipelinePlugin',
+    'AbstractInputValidator',
+    'AbstractPlugin',
+]
+
+__version__ = version
+
+__title__ = "wattercrawl_plugin"
+__description__ =  "Base plugin for wattercrawl"
+__url__ = "https://github.com/wattercrawl/wattercrawl-plugin"
