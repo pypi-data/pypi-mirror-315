e491d9f26a2b0bc2320bb75ca9592fc0d886ef82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 03:19:50 +0000

    Add package idefix-cli/6.0.1/idefix_cli-6.0.1-py3-none-any.whl

---
+++ packages/idefix-cli/idefix_cli-6.0.1-py3-none-any.whl/idefix_cli/_commands/write.py
@@ -0,0 +1,55 @@
+"""write an Idefix inifile from a json string"""
+
+from __future__ import annotations
+
+import argparse
+import json
+import sys
+from pathlib import Path
+
+import inifix
+
+from idefix_cli.lib import print_error
+
+
+def add_arguments(parser) -> None:
+    parser.add_argument("dest", type=str, help="dest inifile")
+    parser.add_argument(
+        "source",
+        nargs="?",
+        type=argparse.FileType("r"),
+        default=sys.stdin,
+        help="json input",
+    )
+    parser.add_argument(
+        "-f",
+        "--force",
+        action="store_true",
+        help="force writing even if savefile already exists",
+    )
+
+
+def command(dest: str, source, force: bool = False) -> int:
+    try:
+        data = json.load(source)
+    except json.decoder.JSONDecodeError:
+        print_error("input is not valid json.")
+        return 1
+
+    try:
+        inifix.validate_inifile_schema(data, sections="require")
+    except ValueError:
+        print_error("input is not Pluto inifile format compliant.")
+        return 1
+
+    pdest = Path(dest)
+    if pdest.is_file() and not force:
+        print_error(
+            f"destination file {dest} already exists. Use -f/--force to overwrite."
+        )
+        return 1
+
+    with open(pdest, "wb") as fh:
+        inifix.dump(data, fh, sections="require")
+
+    return 0
