821144992b5a23655063f5af68a9aa801a6c7e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 15:05:33 +0000

    Add package honcho-ai/0.0.19/honcho_ai-0.0.19.tar.gz

---
+++ packages/honcho-ai/honcho_ai-0.0.19.tar.gz/honcho_ai-0.0.19/src/honcho/types/apps/users/collections/document_query_params.py
@@ -0,0 +1,20 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Dict, Optional
+from typing_extensions import Required, TypedDict
+
+__all__ = ["DocumentQueryParams"]
+
+
+class DocumentQueryParams(TypedDict, total=False):
+    app_id: Required[str]
+
+    user_id: Required[str]
+
+    query: Required[str]
+
+    filter: Optional[Dict[str, object]]
+
+    top_k: int
