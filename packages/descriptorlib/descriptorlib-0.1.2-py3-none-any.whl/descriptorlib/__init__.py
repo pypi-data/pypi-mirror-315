24a62e8353d273c037be7a924d4513f0a50e119b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 03:19:55 +0000

    Add package descriptorlib/0.1.2/descriptorlib-0.1.2-py3-none-any.whl

---
+++ packages/descriptorlib/descriptorlib-0.1.2-py3-none-any.whl/descriptorlib/__init__.py
@@ -0,0 +1,9 @@
+from .monomer.base import Descriptor
+from .monomer.MonomerPropertyDescriptor import MonomerPropertyDescriptor
+from .run_tests import run_tests_with_coverage
+
+__all__ = [
+    'Descriptor', 
+    'MonomerPropertyDescriptor', 
+    'run_tests_with_coverage'
+]
