56725db69494f510d373039b2423136191a83229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 04:05:09 +0000

    Add package icecomet/3.1.5/icecomet-3.1.5.tar.gz

---
+++ packages/icecomet/icecomet-3.1.5.tar.gz/icecomet-3.1.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='icecomet',
+    version='3.1.5',
+    description='debug',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/my_library',  # หรือ URL ของโปรเจกต์
+    author='icecomet',
+    author_email='icecomet634@gmail.com',
+    license='MIT',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'matplotlib',
+        'pandas',
+        'numpy',
+        'pynput',
+        'pyperclip',
+        
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
