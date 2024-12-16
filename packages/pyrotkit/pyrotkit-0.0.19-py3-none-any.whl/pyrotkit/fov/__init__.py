2f65b95b9337d4588ed58382dd1d264daa9d7703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 04:06:59 +0000

    Add package pyrotkit/0.0.19/pyrotkit-0.0.19-py3-none-any.whl

---
+++ packages/pyrotkit/pyrotkit-0.0.19-py3-none-any.whl/pyrotkit/fov/__init__.py
@@ -0,0 +1,7 @@
+"""Classes which implement various Field of View algorithms."""
+
+from .precise_permissive import PrecisePermissiveView  # noqa: F401
+
+__all__ = [
+    "PrecisePermissiveView",
+]
