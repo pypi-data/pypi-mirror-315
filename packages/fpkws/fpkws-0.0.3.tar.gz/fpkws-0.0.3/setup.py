a64ca3ab3cc2e37fccd2bfd21e1f58816f28e77b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 03:19:46 +0000

    Add package fpkws/0.0.3/fpkws-0.0.3.tar.gz

---
+++ packages/fpkws/fpkws-0.0.3.tar.gz/fpkws-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup (
+    name="fpkws",
+    version="0.0.3",
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    package_data={
+        "fpkws": ["WideResNet12WithEMA.pth"],
+    },
+    author="FanPars",
+    description="Spotting keywords in audio files",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown"
+)
+
