28d583884fdbfc646872f97c578586496a468fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 04:04:53 +0000

    Add package fileMapping/0.3.7/filemapping-0.3.7.tar.gz

---
+++ packages/fileMapping/filemapping-0.3.7.tar.gz/filemapping-0.3.7/fileMapping/config.py
@@ -0,0 +1,22 @@
+import sys
+
+
+functionsName = {
+    # 保留参数
+    "__file__": "__fileName__",
+    "__function__": "__function__",
+    "__run__": "__run__",
+}
+
+functions = {
+    # 保留参数
+    "__file__":  "main.py",
+    "__fileName__":  "main.py",
+    "__function__": "main",
+    "__run__": True,
+}
+
+log = {
+    "printPosition": sys.stdout,
+    "printLog": False
+}
