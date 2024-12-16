fc213166a38385a9ab0522dc0f776295f5d911b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 14:05:07 +0000

    Add package langchain-cratedb/0.0.0/langchain_cratedb-0.0.0.tar.gz

---
+++ packages/langchain-cratedb/langchain_cratedb-0.0.0.tar.gz/langchain_cratedb-0.0.0/langchain_cratedb/__init__.py
@@ -0,0 +1,28 @@
+# ruff: noqa: E402  # Module level import not at top of file
+from importlib import metadata
+
+from langchain_cratedb.patches import patch_sqlalchemy_dialect
+
+patch_sqlalchemy_dialect()
+
+from langchain_cratedb.chat_history import CrateDBChatMessageHistory
+from langchain_cratedb.loaders import CrateDBLoader
+from langchain_cratedb.vectorstores import (
+    CrateDBVectorStore,
+    CrateDBVectorStoreMultiCollection,
+)
+
+try:
+    __version__ = metadata.version(__package__)
+except metadata.PackageNotFoundError:  # pragma: no cover
+    # Case where package metadata is not available.
+    __version__ = ""
+del metadata  # optional, avoids polluting the results of dir(__package__)
+
+__all__ = [
+    "CrateDBChatMessageHistory",
+    "CrateDBLoader",
+    "CrateDBVectorStore",
+    "CrateDBVectorStoreMultiCollection",
+    "__version__",
+]
