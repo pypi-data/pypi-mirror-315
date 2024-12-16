49e39974f4f7b2f7c8c1a406a5074ff8cb326ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 03:20:02 +0000

    Add package pytest-xvirt/0.3.1/pytest_xvirt-0.3.1-py3-none-any.whl

---
+++ packages/pytest-xvirt/pytest_xvirt-0.3.1-py3-none-any.whl/xvirt/__init__.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+from pytest import Config
+
+
+class XVirt:
+    config: Config
+
+    def virtual_path(self) -> str:
+        """Returns the path to be ignored locally and executed remotely."""
+        raise NotImplementedError()
+
+    def run(self):
+        """
+        Should fire the remote execution of the tests and setup the
+        infrastructure to receive the events from the remote.
+        """
+        pass
+
+    def recv_event(self) -> str:
+        """Returns the next event received from the remote pytest."""
+        pass
+
+    def finalize(self):
+        """Finalize the remote execution."""
+        pass
+
+    def remote_invocation_params(self, remote_root: str) -> (str, str):
+        idir = Path(self.config.invocation_dir).as_posix()
+        rp = Path(self.config.rootpath).as_posix()
+        rr = remote_root
+        invocation_dir = path_rewrite(idir, rp, rr)
+        args = [path_rewrite(Path(a).as_posix(), rp, rr) for a in self.config.args]
+        return invocation_dir, args
+
+def path_rewrite(invocation_dir, root_path: Path, remote_root: str) -> str:
+    root_path_str = str(root_path)
+    return str(invocation_dir).replace(root_path_str, remote_root)
