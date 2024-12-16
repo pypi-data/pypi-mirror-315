6fa8da9653e1ec1568af23836ba140b54552636d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-16 04:09:15 +0000

    Add package textutilsSohaib/1.1.0/textutilsSohaib-1.1.0-py3-none-any.whl

---
+++ packages/textutilsSohaib/textutilsSohaib-1.1.0-py3-none-any.whl/textutilsSohaib/__init__.py
@@ -0,0 +1,15 @@
+# textutilsSohaib/__init__.py
+
+from .transformations import (
+    to_uppercase,
+    to_lowercase,
+    reverse_text,
+    capitalize_words,
+    remove_whitespace,
+    word_count,
+    replace_spaces_with_underscore,
+    remove_punctuation,
+    reverse_words,
+    count_vowels,
+    is_palindrome
+)
