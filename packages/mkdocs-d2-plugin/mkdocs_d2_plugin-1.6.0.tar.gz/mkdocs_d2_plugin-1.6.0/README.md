4bf47e245f4eea6a3a2d074c76ec8db2d64fb5c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 03:18:07 +0000

    Add package mkdocs-d2-plugin/1.6.0/mkdocs_d2_plugin-1.6.0.tar.gz

---
+++ packages/mkdocs-d2-plugin/mkdocs_d2_plugin-1.6.0.tar.gz/mkdocs_d2_plugin-1.6.0/README.md
@@ -0,0 +1,70 @@
+# mkdocs-d2-plugin
+
+A plugin for embedding D2 diagrams in MkDocs.
+
+**Documentation and live demo can be found
+[here](https://landmaj.github.io/mkdocs-d2-plugin/).**
+
+## Requirements
+
+* [Python](https://www.python.org/) >= 3.9
+* [MkDocs](https://www.mkdocs.org/) >= 1.6.0
+* [D2](https://d2lang.com) >= 0.6.3
+
+## Installation
+
+Install the plugin using pip:
+
+```bash
+pip install mkdocs-d2-plugin
+```
+
+And add it to your `mkdocs.yml`:
+
+```yaml
+plugins:
+  - d2
+```
+
+## Usage
+
+### Fenced code block
+
+````md
+```d2
+shape: sequence_diagram
+Alice -> John: Hello John, how are you?
+Alice -> John.ack: John, can you hear me?
+John.ack -> Alice: Hi Alice, I can hear you!
+John -> Alice: I feel great!
+```
+````
+
+### Image tag
+
+```md
+![Diagram](diagram.d2)
+```
+
+## Demo app
+
+You can find demo app in the `docs` directory.
+Live version is available [here](https://landmaj.github.io/mkdocs-d2-plugin/).
+
+To run in locally:
+
+```bash
+cd docs
+python3 -m venv .venv
+source .venv/bin/activate
+pip install mkdocs-material mkdocs-d2-plugin
+mkdocs serve
+```
+
+To run it using Docker:
+
+```bash
+cd docs
+docker build --tag mkdocs-d2-plugin:latest .
+docker run --rm -it -p 8000:8000 -v ${PWD}:/docs mkdocs-d2-plugin:latest
+```
---
+++ packages/mkdocs-d2-plugin/mkdocs_d2_plugin-1.6.0.tar.gz/mkdocs_d2_plugin-1.6.0/setup.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+PROJ_DIR = Path(__file__).resolve().parent
+with open(PROJ_DIR / "README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="mkdocs-d2-plugin",
+    version="1.6.0",
+    description="MkDocs plugin for D2",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="mkdocs python markdown d2 diagram",
+    url="https://github.com/landmaj/mkdocs-d2-plugin",
+    author="Michał Wieluński",
+    author_email="michal@wielunski.net",
+    license="MIT",
+    python_requires=">=3.9",
+    install_requires=[
+        "mkdocs>=1.6.0",
+        "pymdown-extensions>=9.0",
+        "pydantic>=2.0",
+        "packaging",
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    packages=find_packages(),
+    package_data={"d2.css": ["mkdocs_d2_plugin.css"]},
+    include_package_data=True,
+    entry_points={
+        "mkdocs.plugins": ["d2 = d2.plugin:Plugin"],
+        "markdown.extensions": ["d2_img = d2.img:D2ImgExtension"],
+    },
+)
