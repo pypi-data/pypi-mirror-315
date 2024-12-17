69d6df2efeabbc86bf4fb5bf20dd099d7cc9f798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 15:06:34 +0000

    Add package vutils/1.1.6/vutils-1.1.6.tar.gz

---
+++ packages/vutils/vutils-1.1.6.tar.gz/vutils-1.1.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vutils",
+    version="1.1.6",
+    author="Vichayturen",
+    author_email="vichayturen@gmail.com",
+    description="vichayturen's utils",
+    url="https://github.com/RankKCodeTalker/vutils",
+    license="MIT",
+    install_requires=[],
+    extra_requires=[
+        'openai',
+        'torch',
+        'pandas',
+        'openpyxl'
+    ],
+    package_dir={"": "src"},
+    packages=find_packages("src")
+)
