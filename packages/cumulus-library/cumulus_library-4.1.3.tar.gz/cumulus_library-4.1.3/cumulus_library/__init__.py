1666fc0ddec0e14050ae13175389f8ffb58e3e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 06:05:35 +0000

    Add package cumulus-library/4.1.3/cumulus_library-4.1.3.tar.gz

---
+++ packages/cumulus-library/cumulus_library-4.1.3.tar.gz/cumulus_library-4.1.3/cumulus_library/__init__.py
@@ -0,0 +1,9 @@
+"""Package metadata"""
+
+from cumulus_library.base_utils import StudyConfig
+from cumulus_library.builders.base_table_builder import BaseTableBuilder
+from cumulus_library.builders.counts import CountsBuilder
+from cumulus_library.study_manifest import StudyManifest
+
+__all__ = ["BaseTableBuilder", "CountsBuilder", "StudyConfig", "StudyManifest"]
+__version__ = "4.1.3"
