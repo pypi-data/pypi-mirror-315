30d75d3304f076af05378bd7849185d5552e2fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 08:05:31 +0000

    Add package subnotebook/0.0.1/subnotebook-0.0.1.tar.gz

---
+++ packages/subnotebook/subnotebook-0.0.1.tar.gz/subnotebook-0.0.1/README.md
@@ -0,0 +1,25 @@
+# Subnotebook
+
+Run a notebook as you would call a Python function.
+
+Two modes are currently supported:
+
+- An in-process mode, where the subnotebook is executed in the same kernel your
+interpreter is running in (but in a different name space). This allows to pass
+parameters and get results back, including widgets.
+```python
+from subnotebook import run_nb
+
+ab, ba, slider, output = run_nb('sub_notebook.ipynb', a='c', b='d')
+```
+
+- An out-of-process mode, where the subnotebook is executed and served using
+[Voila](https://voila.readthedocs.io), and included in the main notebook as an
+IFrame. This mode only allows to display the outputs of the subnotebook, which
+is useful for offloading the main notebook but also to offer a UI to resources
+you would not have access to, such as big data or protected data.
+```python
+from subnotebook import display_nb
+
+display_nb('widget_notebook.ipynb')
+```
---
+++ packages/subnotebook/subnotebook-0.0.1.tar.gz/subnotebook-0.0.1/src/subnotebook/_subnotebook.py
@@ -0,0 +1,103 @@
+import nbformat
+import inspect
+#import voila.app
+#import nest_asyncio
+import subprocess
+from IPython.display import IFrame, display
+import atexit
+
+
+voila_processes = []
+
+
+def run_nb(path, **kwargs):
+    nb = open_nb(path)
+    results = nb.run(**kwargs)
+    return results
+
+
+def open_nb(path):
+    nb = nbformat.read(path, nbformat.NO_CONVERT)
+    subnb = SubNotebook(nb)
+    return subnb
+
+
+def default_value(**kwargs):
+    frame = inspect.currentframe()
+    try:
+        out_locals = frame.f_back.f_locals
+    finally:
+        del frame
+    for k, v in kwargs.items():
+        if k not in out_locals:
+            out_locals[k] = v
+
+
+class Return:
+    Result_i = 0
+    def __init__(self, *args):
+        frame = inspect.currentframe()
+        try:
+            out_locals = frame.f_back.f_locals
+        finally:
+            del frame
+        for i, arg in enumerate(args):
+            result_i = self.Result_i + i
+            out_locals[f'__result_{result_i}__'] = arg
+        Return.Result_i += len(args)
+
+
+class SubNotebook:
+
+    def __init__(self, nb):
+        self.nb = nb
+        self.kwargs = {}
+
+    def run(self, **kwargs):
+        self.namespace = kwargs
+        for cell in self.nb.cells:
+            if cell['cell_type'] == 'code':
+                exec(cell['source'], self.namespace)
+        results = self.get_results()
+        return results
+
+    def get_results(self):
+        results = [self.namespace[name] for name in self.namespace if name.startswith('__result_')]
+        if len(results) == 1:
+            return results[0]
+        return tuple(results)
+
+
+def get_lines(std_pipe):
+    '''Generator that yields lines from a standard pipe as they are printed.'''
+    for line in iter(std_pipe.readline, ''):
+        yield line
+    std_pipe.close()
+
+
+def display_nb(path, server_address='http://*:*/', width='100%', height=1000):
+    #nest_asyncio.apply()
+    #voila_app = voila.app.Voila()
+    #voila_app.initialize([path, '--no-browser', "--Voila.tornado_settings={'headers':{'Content-Security-Policy':\"frame-ancestors 'self' " + server_address + "\"}}"])
+    #voila_app.start()
+    #print(voila_app.server_url)
+
+    cmd = ['voila', '--no-browser', "--Voila.tornado_settings={'headers':{'Content-Security-Policy':\"frame-ancestors 'self' " + server_address + "\"}}", path]
+    voila = subprocess.Popen(cmd, stderr=subprocess.PIPE, universal_newlines=True)
+    voila_processes.append(voila)
+
+    # wait until server is ready and get the address
+    for line in get_lines(voila.stderr):
+        if line.startswith('http://'):
+            voila_address = line.strip()
+            break
+
+    display(IFrame(src=voila_address, width=width, height=height))
+
+
+def kill_processes(processes):
+    for p in processes:
+        p.kill()
+
+
+atexit.register(kill_processes, voila_processes)
