9f39ce232709af96de5931a6f5bb616eedc4171c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 07:04:52 +0000

    Add package kamu/0.1.1/kamu-0.1.1-py3-none-any.whl

---
+++ packages/kamu/kamu-0.1.1-py3-none-any.whl/kamu/__init__.py
@@ -0,0 +1,23 @@
+from . import _connection
+
+
+__version__ = "0.1.1"
+
+
+def connect(url):
+    """
+    Open connection to a Kamu node.
+
+    Examples
+    --------
+    >>> import kamu
+    >>>
+    >>> # Connect to secure node
+    >>> with kamu.connect("grpc+tls://node.demo.kamu.dev:50050") as con:
+    >>>     pass
+    >>>
+    >>> # Connect to local insecure node
+    >>> with kamu.connect("grpc://localhost:50050") as con:
+    >>>     pass
+    """
+    return _connection.KamuConnection(url=url)
