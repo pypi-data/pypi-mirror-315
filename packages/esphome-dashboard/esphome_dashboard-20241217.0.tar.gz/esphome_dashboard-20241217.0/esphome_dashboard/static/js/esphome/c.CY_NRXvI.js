c026f510ccee7720c011d6d4893a89eff984c0f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 07:05:22 +0000

    Add package esphome-dashboard/20241217.0/esphome_dashboard-20241217.0.tar.gz

---
+++ packages/esphome-dashboard/esphome_dashboard-20241217.0.tar.gz/esphome_dashboard-20241217.0/esphome_dashboard/static/js/esphome/c.CY_NRXvI.js
@@ -0,0 +1,8 @@
+import{b as e,n as s,s as l,x as o}from"./index-D-cxILmb.js";import"./c.Bu6MtT5R.js";import"./c.CyiVjy7U.js";let d=class extends l{render(){return o`
+      <esphome-process-dialog
+        heading="Update All"
+        .type=${"update-all"}
+        @closed=${this._handleClose}
+      ></esphome-process-dialog>
+    `}_handleClose(){this.parentNode.removeChild(this)}};d=e([s("esphome-update-all-dialog")],d);
+//# sourceMappingURL=c.CY_NRXvI.js.map
