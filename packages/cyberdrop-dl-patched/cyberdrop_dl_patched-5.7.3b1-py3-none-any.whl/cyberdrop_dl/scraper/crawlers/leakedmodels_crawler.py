72112513ef632d9f36a48927543c75bbdc1b0864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 07:05:19 +0000

    Add package cyberdrop-dl-patched/5.7.3b1/cyberdrop_dl_patched-5.7.3b1-py3-none-any.whl

---
+++ packages/cyberdrop-dl-patched/cyberdrop_dl_patched-5.7.3b1-py3-none-any.whl/cyberdrop_dl/scraper/crawlers/leakedmodels_crawler.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from yarl import URL
+
+from .xenforo_crawler import PostSelectors, Selector, XenforoCrawler, XenforoSelectors
+
+if TYPE_CHECKING:
+    from cyberdrop_dl.managers.manager import Manager
+
+
+class LeakedModelsCrawler(XenforoCrawler):
+    primary_base_domain = URL("https://LeakedModels.com")
+    post_selectors = PostSelectors(
+        date=Selector("time", "data-time"),
+    )
+    selectors = XenforoSelectors(posts=post_selectors)
+
+    def __init__(self, manager: Manager) -> None:
+        super().__init__(manager, "leakedmodels", "LeakedModels")
