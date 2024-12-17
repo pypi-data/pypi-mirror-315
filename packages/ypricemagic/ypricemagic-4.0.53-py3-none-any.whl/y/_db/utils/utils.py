bbf6cc960405648275102d243275212bb4920aad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 04:05:32 +0000

    Add package ypricemagic/4.0.53/ypricemagic-4.0.53-py3-none-any.whl

---
+++ packages/ypricemagic/ypricemagic-4.0.53-py3-none-any.whl/y/_db/utils/utils.py
@@ -0,0 +1,287 @@
+import logging
+from datetime import datetime, timezone
+from dateutil import parser
+from functools import lru_cache
+from typing import Dict, Optional, Set
+
+import a_sync
+from pony.orm import commit, select
+from brownie import chain
+
+from y._db.decorators import (
+    a_sync_read_db_session,
+    a_sync_write_db_session,
+    a_sync_write_db_session_cached,
+    log_result_count,
+)
+from y._db.entities import Block, BlockAtTimestamp, Chain, insert
+
+logger = logging.getLogger(__name__)
+
+
+@a_sync_read_db_session
+def get_chain() -> Chain:
+    """Retrieve the current blockchain chain object from the database.
+
+    If the chain object does not exist, it inserts a new one.
+
+    Returns:
+        The Chain object for the current blockchain.
+
+    Examples:
+        >>> chain = get_chain()
+        >>> print(chain.id)
+
+    See Also:
+        - :class:`Chain`
+        - :func:`insert`
+    """
+    return Chain.get(id=chain.id) or insert(type=Chain, id=chain.id) or Chain[chain.id]
+
+
+@lru_cache
+def ensure_chain() -> None:
+    """Ensures that the chain object for the connected chain has been inserted to the db.
+
+    This function commits the chain object to the database if it does not already exist.
+
+    Examples:
+        >>> ensure_chain()
+
+    See Also:
+        - :func:`get_chain`
+    """
+    get_chain(sync=True)
+    commit()
+
+
+@a_sync_read_db_session
+def get_block(number: int) -> Block:
+    """Retrieve a block by its number from the database.
+
+    If the block does not exist, it inserts a new one.
+
+    Args:
+        number: The block number to retrieve.
+
+    Returns:
+        The Block object for the specified block number.
+
+    Examples:
+        >>> block = get_block(123456)
+        >>> print(block.number)
+
+    See Also:
+        - :class:`Block`
+        - :func:`insert`
+    """
+    if block := Block.get(chain=chain.id, number=number):
+        return block
+    return insert(type=Block, chain=chain.id, number=number) or get_block(
+        number, sync=True
+    )
+
+
+@a_sync_write_db_session_cached
+def ensure_block(number: int) -> None:
+    """Ensure that a block is known in the database.
+
+    If the block is not known, it retrieves and caches the block.
+
+    Args:
+        number: The block number to ensure.
+
+    Examples:
+        >>> ensure_block(123456)
+
+    See Also:
+        - :func:`get_block`
+        - :func:`known_blocks`
+    """
+    if number not in known_blocks():
+        from y._db.utils._ep import _get_get_block
+
+        get_block = _get_get_block()
+        get_block(number, sync=True)
+
+
+@a_sync_read_db_session
+def get_block_timestamp(number: int) -> Optional[int]:
+    """Retrieve the timestamp for a given block number.
+
+    If the timestamp is not known, it retrieves and caches the block's timestamp.
+
+    Args:
+        number: The block number to retrieve the timestamp for.
+
+    Returns:
+        The timestamp of the block as a Unix timestamp, or None if not found.
+
+    Examples:
+        >>> timestamp = get_block_timestamp(123456)
+        >>> print(timestamp)
+
+    See Also:
+        - :func:`known_block_timestamps`
+    """
+    if (ts := known_block_timestamps().pop(number, None)) is None:
+        from y._db.utils._ep import _get_get_block
+
+        get_block = _get_get_block()
+        ts = get_block(number, sync=True).timestamp
+    if ts:
+        # some db providers return a string here, we must parse it
+        if isinstance(ts, str):
+            ts = parser.parse(ts)
+        unix = ts.timestamp()
+        logger.debug(
+            "got Block[%s, %s].timestamp from cache: %s, %s", chain.id, number, unix, ts
+        )
+        return unix
+
+
+@a_sync_read_db_session
+def get_block_at_timestamp(timestamp: datetime) -> Optional[int]:
+    """Retrieve the block number at a specific timestamp.
+
+    If the block number is not known, it attempts to find it in the database.
+
+    Args:
+        timestamp: The timestamp to find the block number for.
+
+    Returns:
+        The block number at the given timestamp, or None if not found.
+
+    Examples:
+        >>> block_number = get_block_at_timestamp(datetime.now())
+        >>> print(block_number)
+
+    See Also:
+        - :class:`BlockAtTimestamp`
+    """
+    if block := known_blocks_for_timestamps().pop(timestamp, None):
+        logger.debug("found block %s for %s in ydb", block, timestamp)
+        return block
+    elif entity := BlockAtTimestamp.get(chainid=chain.id, timestamp=timestamp):
+        block = entity.block
+        logger.debug("found block %s for %s in ydb", block, timestamp)
+        return block
+
+
+@a_sync_write_db_session
+def _set_block_timestamp(block: int, timestamp: int) -> None:
+    """Set the timestamp for a specific block in the database.
+
+    Args:
+        block: The block number to set the timestamp for.
+        timestamp: The timestamp to set for the block.
+
+    Examples:
+        >>> _set_block_timestamp(123456, 1609459200)
+
+    See Also:
+        - :func:`get_block`
+    """
+    from y._db.utils._ep import _get_get_block
+
+    get_block = _get_get_block()
+    block = get_block(block, sync=True)
+    timestamp = datetime.fromtimestamp(timestamp, tz=timezone.utc)
+    block.timestamp = timestamp
+    logger.debug("cached %s.timestamp %s", block, timestamp)
+
+
+set_block_timestamp = a_sync.ProcessingQueue(
+    _set_block_timestamp, num_workers=10, return_data=False
+)
+
+
+@a_sync_write_db_session
+def _set_block_at_timestamp(timestamp: datetime, block: int) -> None:
+    """Insert a block number for a specific timestamp in the database.
+
+    Args:
+        timestamp: The timestamp to associate with the block number.
+        block: The block number to insert.
+
+    Examples:
+        >>> _set_block_at_timestamp(datetime.now(), 123456)
+
+    See Also:
+        - :class:`BlockAtTimestamp`
+    """
+    insert(BlockAtTimestamp, chainid=chain.id, timestamp=timestamp, block=block)
+    logger.debug("inserted block %s for %s", block, timestamp)
+
+
+set_block_at_timestamp = a_sync.ProcessingQueue(
+    _set_block_at_timestamp, num_workers=10, return_data=False
+)
+
+# startup caches
+
+
+@lru_cache(maxsize=1)
+@log_result_count("blocks")
+def known_blocks() -> Set[int]:
+    """Cache and return all known blocks for this chain to minimize db reads.
+
+    Returns:
+        A set of all known block numbers for the current chain.
+
+    Examples:
+        >>> blocks = known_blocks()
+        >>> print(len(blocks))
+
+    See Also:
+        - :class:`Block`
+    """
+    return set(select(b.number for b in Block if b.chain.id == chain.id))
+
+
+@lru_cache(maxsize=1)
+@log_result_count("block timestamps")
+def known_block_timestamps() -> Dict[int, datetime]:
+    """Cache and return all known block timestamps for this chain to minimize db reads.
+
+    Returns:
+        A dictionary mapping block numbers to their timestamps.
+
+    Examples:
+        >>> timestamps = known_block_timestamps()
+        >>> print(len(timestamps))
+
+    See Also:
+        - :class:`Block`
+    """
+    query = select(
+        (b.number, b.timestamp) for b in Block if b.chain.id == chain.id and b.timestamp
+    )
+    page_size = 100_000
+    timestamps = {}
+    for i in range((query.count() // page_size) + 1):
+        for block, timestamp in query.page(i + 1, page_size):
+            timestamps[block] = timestamp
+    return timestamps
+
+
+@lru_cache(maxsize=1)
+@log_result_count("blocks for timestamps")
+def known_blocks_for_timestamps() -> Dict[datetime, int]:
+    """Cache and return all known blocks for timestamps for this chain to minimize db reads.
+
+    Returns:
+        A dictionary mapping timestamps to block numbers.
+
+    Examples:
+        >>> blocks_for_timestamps = known_blocks_for_timestamps()
+        >>> print(len(blocks_for_timestamps))
+
+    See Also:
+        - :class:`BlockAtTimestamp`
+    """
+    return dict(
+        select(
+            (x.timestamp, x.block) for x in BlockAtTimestamp if x.chainid == chain.id
+        )
+    )
