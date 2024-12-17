483404056864594789d54b07b0325e1142eeead9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 14:05:07 +0000

    Add package redshot/0.1.0/redshot-0.1.0-py3-none-any.whl

---
+++ packages/redshot/redshot-0.1.0-py3-none-any.whl/redshot/object/__init__.py
@@ -0,0 +1,10 @@
+from .message import Message, MessageInfo, MessageQuote, MessageLink
+from .search import SearchResult
+
+__all__ = [
+    "Message",
+    "MessageInfo",
+    "MessageQuote",
+    "MessageLink",
+    "SearchResult"
+]
