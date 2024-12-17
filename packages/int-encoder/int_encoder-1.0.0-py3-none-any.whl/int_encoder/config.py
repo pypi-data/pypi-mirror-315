7a7eaf3028c80507e0b7d1965c11bc583e46ddde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 14:04:58 +0000

    Add package int-encoder/1.0.0/int_encoder-1.0.0-py3-none-any.whl

---
+++ packages/int-encoder/int_encoder-1.0.0-py3-none-any.whl/int_encoder/config.py
@@ -0,0 +1,11 @@
+# int_encoder/config.py
+#
+# Configuration for the int_encoder library
+
+
+
+# Encode key used for encoding and decoding integers
+INT_ENCODE_KEY = 2034569
+
+# Maximum value for the encoded integer
+INT_ENCODED_MAX = 2 ** 32
