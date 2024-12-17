e55ab94c80c4bc13a683e58954b2fddf4dc3007a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 05:05:25 +0000

    Add package psychopy-cedrus/0.0.10/psychopy_cedrus-0.0.10.tar.gz

---
+++ packages/psychopy-cedrus/psychopy_cedrus-0.0.10.tar.gz/psychopy_cedrus-0.0.10/psychopy_cedrus/components/lumina.py
@@ -0,0 +1,22 @@
+from psychopy.experiment.components.buttonBox import ButtonBoxComponent
+from psychopy.experiment.plugins import DeviceBackend
+from psychopy.localization import _translate
+from . import util
+
+
+class LuminaButtonBoxBackend(DeviceBackend):
+    key = "lumina"
+    label = _translate("Cedrus Lumina Series")
+    component = ButtonBoxComponent
+    deviceClasses = ["psychopy_cedrus.lumina.LuminaButtonGroup"]
+
+    def getParams(self):
+        return util.getXidButtonBoxParams(key="lumina")
+    
+    def writeDeviceCode(self, buff):
+        return util.writeXidButtonBoxCode(
+            self,
+            buff,
+            cls="psychopy_cedrus.lumina.LuminaButtonGroup",
+            key="lumina"
+        )
