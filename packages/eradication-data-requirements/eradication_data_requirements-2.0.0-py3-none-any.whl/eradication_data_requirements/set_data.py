84b0ae193e109a0520fe245e5851e6ef402fb55e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 06:05:40 +0000

    Add package eradication-data-requirements/2.0.0/eradication_data_requirements-2.0.0-py3-none-any.whl

---
+++ packages/eradication-data-requirements/eradication_data_requirements-2.0.0-py3-none-any.whl/eradication_data_requirements/set_data.py
@@ -0,0 +1,9 @@
+def filter_data_by_method(raw_data, method):
+    filtered_by_method = raw_data[raw_data.Tecnica == method]
+    filtered_by_method.loc[:, ["Acumulado"]] = filtered_by_method.Capturas.cumsum()
+    return filtered_by_method
+
+
+def select_december_of_every_year(data):
+    month_to_plot = "-12-"
+    return data[data.Fecha.str.contains(month_to_plot)]
