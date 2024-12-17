e3dd43446f9ee513f7b59e06b01cc154165c6178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 05:06:37 +0000

    Add package bitcart/1.17.0.0/bitcart-1.17.0.0.tar.gz

---
+++ packages/bitcart/bitcart-1.17.0.0.tar.gz/bitcart-1.17.0.0/tests/utils.py
@@ -0,0 +1,23 @@
+import subprocess
+
+
+def run_shell(args=None, timeout=30):
+    if args is None:
+        args = []
+    process = subprocess.Popen(["tests/regtest/regtest.sh"] + args, stdout=subprocess.PIPE, universal_newlines=True)
+    process.wait(timeout=timeout)
+    assert process.returncode == 0
+    return process.stdout.read().strip()
+
+
+def data_check(data, key, check_type, length=None):
+    assert key in data
+    assert isinstance(data[key], check_type)
+    if length:
+        assert len(data[key]) == length
+
+
+def patch_session(mocker, patched_session):
+    mocker.patch(
+        "bitcart.providers.jsonrpcrequests.RPCProxy.session", new_callable=mocker.PropertyMock(return_value=patched_session)
+    )
