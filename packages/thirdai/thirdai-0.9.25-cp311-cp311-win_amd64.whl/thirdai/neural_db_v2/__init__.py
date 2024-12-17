ab5af0a5da1996784244ca5f5d50ee5545db3598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 14:05:38 +0000

    Add package thirdai/0.9.25/thirdai-0.9.25-cp311-cp311-win_amd64.whl

---
+++ packages/thirdai/thirdai-0.9.25-cp311-cp311-win_amd64.whl/thirdai/neural_db_v2/__init__.py
@@ -0,0 +1,13 @@
+from .documents import (
+    CSV,
+    DOCX,
+    PDF,
+    PPTX,
+    URL,
+    Document,
+    Email,
+    InMemoryText,
+    TextFile,
+)
+from .neural_db import NeuralDB
+from .supervised import CsvSupervised, InMemorySupervised
