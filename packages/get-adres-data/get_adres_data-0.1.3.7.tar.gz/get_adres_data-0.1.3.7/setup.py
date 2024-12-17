ff0ee78f2219536629c120e927ab4e422c80fdc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 03:18:09 +0000

    Add package get-adres-data/0.1.3.7/get_adres_data-0.1.3.7.tar.gz

---
+++ packages/get-adres-data/get_adres_data-0.1.3.7.tar.gz/get_adres_data-0.1.3.7/setup.py
@@ -0,0 +1,13 @@
+# https://medium.com/@tushar_datascience/creating-a-python-library-a-step-by-step-guide-with-a-simple-example-c87b653b9a4e
+# https://medium.com/analytics-vidhya/how-to-create-a-python-library-7d5aea80cc3f
+
+from setuptools import setup
+
+setup(name='get_adres_data',
+      version='0.1.3.7',
+      description='Get ADRES Data in CSV format',
+      url='https://www.adres-risa.org',
+      author='Joseph Hui',
+      author_email='joseph-ch.hui@polyu.edu.hk',
+      license='MIT',
+      packages=['get_adres_data'])
