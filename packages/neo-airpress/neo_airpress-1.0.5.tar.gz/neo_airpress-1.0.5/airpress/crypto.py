98524aee11224ebb11ca03faff127481f8941e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 14:05:07 +0000

    Add package neo-airpress/1.0.5/neo_airpress-1.0.5.tar.gz

---
+++ packages/neo-airpress/neo_airpress-1.0.5.tar.gz/neo_airpress-1.0.5/airpress/crypto.py
@@ -0,0 +1,50 @@
+# pkcs signature implementation was generously provided
+# by gentlemen of "Develatio Technologies SL", which is much appreciated.
+# To learn more about their projects visit https://develat.io/ or check out
+# original implementation at https://github.com/Develatio/django-walletpass
+# pylint: disable=protected-access, invalid-name, too-many-locals
+
+
+from cryptography import x509
+from cryptography.hazmat.bindings.openssl.binding import Binding
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.serialization import pkcs7
+
+copenssl = Binding.lib
+cffi = Binding.ffi
+
+
+# SMIME isn't supported by pyca/cryptography:
+# https://github.com/pyca/cryptography/issues/1621
+# adjusted with latest cryptography code from https://github.com/devartis/passbook/pull/60/files
+def pkcs7_sign(
+    certcontent,
+    keycontent,
+    wwdr_certificate,
+    data,
+    key_password=None,
+):
+    """Sign data with PKCS#7.
+
+    Args:
+        keycontent (bytes): Content of pem file certificate
+        keycontent (bytes): Content of key file
+        wwdr_certificate (bytes): Content of Intermediate cert file
+        data (bytes): Data to be signed
+        key_password (bytes, optional): key file passwd. Defaults to None.
+    """
+
+    cert = x509.load_pem_x509_certificate(certcontent)
+    priv_key = serialization.load_pem_private_key(
+        keycontent, password=key_password
+    )
+    wwdr_cert = x509.load_der_x509_certificate(wwdr_certificate)
+
+    options = [pkcs7.PKCS7Options.DetachedSignature]
+    return (
+        pkcs7.PKCS7SignatureBuilder()
+        .set_data(data)
+        .add_signer(cert, priv_key, hashes.SHA256())
+        .add_certificate(wwdr_cert)
+        .sign(serialization.Encoding.DER, options)
+    )
---
+++ packages/neo-airpress/neo_airpress-1.0.5.tar.gz/neo_airpress-1.0.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="neo-airpress",
+    version="1.0.5",
+    author="Jstyles",
+    author_email="jstyles@styl.dev",
+    description="A frustration-free compression tool for PKPass archives.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/captain-fox/airpress",
+    packages=["airpress"],
+    install_requires=[
+        "cryptography>=44.0.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
