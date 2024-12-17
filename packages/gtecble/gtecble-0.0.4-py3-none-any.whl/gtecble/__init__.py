eaf6887671fb6d9a0fce5d768bac82a7030231bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 16:06:48 +0000

    Add package gtecble/0.0.4/gtecble-0.0.4-py3-none-any.whl

---
+++ packages/gtecble/gtecble-0.0.4-py3-none-any.whl/gtecble/__init__.py
@@ -0,0 +1,18 @@
+"""
+GtecBLE
+-------
+
+A library for communicating with g.tec BLE devices
+
+:copyright: (c) 2024 g.tec medical engineering GmbH
+
+"""
+
+# compatibility
+from __future__ import absolute_import, division, print_function
+
+# get version
+from .__version__ import __version__
+
+# allow lazy loading
+from .gtecble import GtecBLE
