e604e1a5d42b9675997e1fe9cf11b2539f209134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 05:06:43 +0000

    Add package spaceone-dashboard/2.0.dev104/spaceone-dashboard-2.0.dev104.tar.gz

---
+++ packages/spaceone-dashboard/spaceone-dashboard-2.0.dev104.tar.gz/spaceone-dashboard-2.0.dev104/spaceone/dashboard/error/data_table.py
@@ -0,0 +1,33 @@
+from spaceone.core.error import *
+
+
+class ERROR_NOT_SUPPORTED_SOURCE_TYPE(ERROR_INVALID_ARGUMENT):
+    _message = "Data table does not support source type. (source_type = {source_type})"
+
+
+class ERROR_QUERY_OPTION(ERROR_INVALID_ARGUMENT):
+    _message = "Query option is invalid. (key = {key})"
+
+
+class ERROR_NOT_SUPPORTED_OPERATOR(ERROR_INVALID_ARGUMENT):
+    _message = "Data table does not support operator. (operator = {operator})"
+
+
+class ERROR_UNAVAILABLE_DATA_TABLE(ERROR_INVALID_ARGUMENT):
+    _message = "Data table is not available. (data_table_id = {data_table_id})"
+
+
+class ERROR_DUPLICATED_DATA_FIELDS(ERROR_INVALID_ARGUMENT):
+    _message = "Data fields for join are duplicated. (fields = {fields})"
+
+
+class ERROR_NO_FIELDS_TO_JOIN(ERROR_INVALID_ARGUMENT):
+    _message = "There is no fields to join."
+
+
+class ERROR_NOT_GLOBAL_VARIABLE_KEY(ERROR_INVALID_ARGUMENT):
+    _message = "The variable is not a global variable. (global_variable_key = {global_variable_key})"
+
+
+class ERROR_NO_FIELDS_TO_GLOBAL_VARIABLES(ERROR_INVALID_ARGUMENT):
+    _message = "There is no fields in GLOBAL_VARIABLES. (vars = {vars})"
