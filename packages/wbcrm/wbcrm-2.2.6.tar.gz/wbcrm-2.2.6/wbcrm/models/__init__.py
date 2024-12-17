d16553b16ce6e950e1eacf93251584c6f8566743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 05:05:44 +0000

    Add package wbcrm/2.2.6/wbcrm-2.2.6.tar.gz

---
+++ packages/wbcrm/wbcrm-2.2.6.tar.gz/wbcrm-2.2.6/wbcrm/models/__init__.py
@@ -0,0 +1,4 @@
+from .accounts import Account, AccountRole, AccountRoleType, AccountRoleValidity
+from .activities import Activity, ActivityParticipant, ActivityType, add_employer_to_activities
+from .groups import Group
+from .products import Product, ProductCompanyRelationship
