51df29ca237d9226698ae4222ee15314b95ee703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 08:05:24 +0000

    Add package l2winddir/0.0.2/l2winddir-0.0.2.tar.gz

---
+++ packages/l2winddir/l2winddir-0.0.2.tar.gz/l2winddir-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='l2winddir',
+    version='0.0.2',
+    description='A package for l2 wind direction',
+    author='jean2262',
+    author_email='jrenaud495@gmail.com',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        'hydra-core',
+        'hydra-zen',
+        'pandas',
+        'xarray',
+        'numpy',
+        'lightning',
+        'omegaconf',
+        'tqdm',
+        'einops'
+    ],
+    python_requires='>=3.11',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
