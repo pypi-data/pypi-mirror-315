30b6a2c72cd45efc865e73b30f5cb7c9133cd4a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 14:04:59 +0000

    Add package pipez/0.0.130/pipez-0.0.130.tar.gz

---
+++ packages/pipez/pipez-0.0.130.tar.gz/pipez-0.0.130/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+
+setup(
+    name='pipez',
+    version='0.0.130',
+    author="Alexander Timofeev",
+    author_email="tam2511@mail.ru",
+    python_requires=">=3.9",
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tam2511/pipez",
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'fastapi',
+        'uvicorn',
+        'Jinja2'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True
+)
