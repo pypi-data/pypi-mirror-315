a41253e85a03bb850816b69b6a017a67385765fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 14:04:56 +0000

    Add package codegen-sh/1.7.0/codegen_sh-1.7.0.tar.gz

---
+++ packages/codegen-sh/codegen_sh-1.7.0.tar.gz/codegen_sh-1.7.0/src/codegen/api/endpoints.py
@@ -0,0 +1,11 @@
+from codegen.api.modal import MODAL_WORKSPACE
+
+RUN_CODEMOD_ENDPOINT = f"https://{MODAL_WORKSPACE}--cli-run-codemod.modal.run"
+DOCS_ENDPOINT = f"https://{MODAL_WORKSPACE}--cli-docs.modal.run"
+EXPERT_ENDPOINT = f"https://{MODAL_WORKSPACE}--cli-ask-expert.modal.run"
+
+# DEPRECATED
+LIST_DOCS_ENDPOINT = f"https://{MODAL_WORKSPACE}--cli-list-docs.modal.run"
+LIST_SKILLS_ENDPOINT = f"https://{MODAL_WORKSPACE}--cli-list-skills.modal.run"
+
+CREATE_ENDPOINT = "https://codegen-sh-staging--cli-create.modal.run"
