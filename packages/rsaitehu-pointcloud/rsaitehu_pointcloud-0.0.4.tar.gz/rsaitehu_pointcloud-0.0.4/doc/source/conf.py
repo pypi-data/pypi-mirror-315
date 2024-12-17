55f184e65d5db85f1c598ba78f0589e9683113fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 06:05:26 +0000

    Add package rsaitehu-pointcloud/0.0.4/rsaitehu_pointcloud-0.0.4.tar.gz

---
+++ packages/rsaitehu-pointcloud/rsaitehu_pointcloud-0.0.4.tar.gz/rsaitehu_pointcloud-0.0.4/doc/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../rsaitehu_pointcloud'))
+
+project = 'rsaitehu_pointcloud'
+copyright = '2024, José María Martínez-Otzeta'
+author = 'José María Martínez-Otzeta'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # For Google or NumPy docstrings
+    'sphinx.ext.viewcode',
+]
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+add_module_names = False
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ packages/rsaitehu-pointcloud/rsaitehu_pointcloud-0.0.4.tar.gz/rsaitehu_pointcloud-0.0.4/doc/source/index.rst
@@ -0,0 +1,26 @@
+.. rsaitehu_pointcloud documentation master file, created by
+   sphinx-quickstart on Mon Dec 16 16:02:14 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to rsaitehu_pointcloud's documentation!
+===============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Module Reference
+================
+
+.. automodule:: main
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
