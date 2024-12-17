26cf6fbe98d61a1f2b6148f497fa89ebad115940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 16:06:19 +0000

    Add package pip-start/0.1.1/pip_start-0.1.1.tar.gz

---
+++ packages/pip-start/pip_start-0.1.1.tar.gz/pip_start-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pip_start",
+    version="v0.1.1",
+    author="Veillax",
+    author_email="contact@veillax.com",
+    description="A Python Package allowing thr automatic checking and updating of a python package upon import",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Veillax/pip_start", 
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "requests"
+    ],
+)
