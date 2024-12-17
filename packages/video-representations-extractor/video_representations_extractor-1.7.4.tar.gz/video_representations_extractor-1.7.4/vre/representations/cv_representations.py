7effb2a16b22d526f6358ac49fa3d35929ccd292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 06:05:10 +0000

    Add package video-representations-extractor/1.7.4/video_representations_extractor-1.7.4.tar.gz

---
+++ packages/video-representations-extractor/video_representations_extractor-1.7.4.tar.gz/video_representations_extractor-1.7.4/vre/representations/cv_representations.py
@@ -0,0 +1,139 @@
+"""Computer Vision stored representations"""
+from __future__ import annotations
+import numpy as np
+from overrides import overrides
+
+from vre.utils import colorize_semantic_segmentation, VREVideo, colorize_optical_flow, colorize_depth
+from .np_io_representation import NpIORepresentation, DiskData, MemoryData
+from .normed_representation_mixin import NormedRepresentationMixin
+from .representation import Representation
+from .compute_representation_mixin import ComputeRepresentationMixin
+from .color.hsv import rgb2hsv
+
+class ExternalRepresentation(Representation, ComputeRepresentationMixin, NormedRepresentationMixin):
+    """External Representations wrapper, so we can load stuff on the disk w/o explicit representations"""
+    def __init__(self, name, dependencies = None):
+        Representation.__init__(self, name=name, dependencies=dependencies)
+        ComputeRepresentationMixin.__init__(self)
+        NormedRepresentationMixin.__init__(self)
+
+    @overrides
+    def compute(self, video: VREVideo, ixs: list[int]):
+        raise NotImplementedError(f"{self} supposed to be used only as external representation")
+
+class ColorRepresentation(ExternalRepresentation, NpIORepresentation):
+    """ColorRepresentation -- a wrapper over all 3-channeled colored representations"""
+    def __init__(self, name: str, dependencies: list[Representation] | None = None):
+        ExternalRepresentation.__init__(self, name=name, dependencies=dependencies)
+        NpIORepresentation.__init__(self)
+
+    @property
+    @overrides
+    def n_channels(self) -> int:
+        return 3
+
+    @overrides
+    def make_images(self) -> np.ndarray:
+        y = self.unnormalize(self.data.output) if self.normalization is not None else self.data.output
+        return y.astype(np.uint8)
+
+class NormalsRepresentation(ColorRepresentation):
+    """NormalsRepresentation -- CV representation for world and camera normals"""
+    @overrides
+    def make_images(self) -> np.ndarray:
+        y = self.unnormalize(self.data.output) if self.normalization is not None else self.data.output
+        return (y * 255).astype(np.uint8)
+
+class HSVRepresentation(ColorRepresentation):
+    """HSVRepresentation -- CV representation for HSV derived from RGB directly"""
+    @overrides
+    def disk_to_memory_fmt(self, disk_data: DiskData) -> MemoryData:
+        return MemoryData(rgb2hsv(disk_data))
+
+    @overrides
+    def make_images(self) -> np.ndarray:
+        y = self.unnormalize(self.data.output) if self.normalization is not None else self.data.output
+        return (y * 255).astype(np.uint8)
+
+class EdgesRepresentation(ExternalRepresentation, NpIORepresentation):
+    """EdgesRepresentation -- CV representation for 1-channeled edges/boundaries"""
+    def __init__(self, *args, **kwargs):
+        ExternalRepresentation.__init__(self, *args, **kwargs)
+        NpIORepresentation.__init__(self)
+
+    @property
+    @overrides
+    def n_channels(self) -> int:
+        return 1
+
+    def make_images(self) -> np.ndarray:
+        y = self.unnormalize(self.data.output) if self.normalization is not None else self.data.output
+        return (np.repeat(y, 3, axis=-1) * 255).astype(np.uint8)
+
+class DepthRepresentation(ExternalRepresentation, NpIORepresentation):
+    """DepthRepresentation. Implements depth task-specific stuff, like spectral map for plots."""
+    def __init__(self, name: str, min_depth: float, max_depth: float, **kwargs):
+        ExternalRepresentation.__init__(self, name=name, **kwargs)
+        NpIORepresentation.__init__(self)
+        self.min_depth = min_depth
+        self.max_depth = max_depth
+
+    @property
+    @overrides
+    def n_channels(self) -> int:
+        return 1
+
+    @overrides
+    def disk_to_memory_fmt(self, disk_data: DiskData) -> MemoryData:
+        return MemoryData(disk_data.clip(self.min_depth, self.max_depth))
+
+    @overrides
+    def make_images(self) -> np.ndarray:
+        assert self.data is not None, f"[{self}] data must be first computed using compute()"
+        return (colorize_depth(self.data.output, percentiles=[1, 95]) * 255).astype(np.uint8)
+
+class OpticalFlowRepresentation(ExternalRepresentation, NpIORepresentation):
+    """OpticalFlowRepresentation. Implements flow task-specific stuff."""
+    def __init__(self, *args, **kwargs):
+        ExternalRepresentation.__init__(self, *args, **kwargs)
+        NpIORepresentation.__init__(self)
+
+    @property
+    @overrides
+    def n_channels(self) -> int:
+        return 2
+
+    @overrides
+    def make_images(self) -> np.ndarray:
+        _min, _max = self.data.output.min(0).min(0), self.data.output.max(0).max(0)
+        y = np.nan_to_num(((self.data.output - _min) / (_max - _min)), False, 0, 0, 0).astype(np.float32)
+        return colorize_optical_flow(y)
+
+class SemanticRepresentation(Representation, ComputeRepresentationMixin, NpIORepresentation):
+    """SemanticRepresentation. Implements semantic task-specific stuff, like argmaxing if needed"""
+    def __init__(self, *args, classes: int | list[str], color_map: list[tuple[int, int, int]], **kwargs):
+        self.n_classes = len(list(range(classes)) if isinstance(classes, int) else classes)
+        Representation.__init__(self, *args, **kwargs)
+        ComputeRepresentationMixin.__init__(self)
+        NpIORepresentation.__init__(self)
+        self.classes = list(range(classes)) if isinstance(classes, int) else classes
+        self.color_map = color_map
+        assert len(color_map) == self.n_classes and self.n_classes > 1, (color_map, self.n_classes)
+
+    @property
+    @overrides
+    def n_channels(self) -> int:
+        return self.n_classes
+
+    @overrides
+    def disk_to_memory_fmt(self, disk_data: DiskData) -> MemoryData:
+        assert disk_data.dtype in (np.uint8, np.uint16), disk_data.dtype
+        return MemoryData(np.eye(self.n_classes)[disk_data].astype(np.float32))
+
+    @overrides
+    def make_images(self) -> np.ndarray:
+        return colorize_semantic_segmentation(self.data.output.argmax(-1), self.classes, self.color_map)
+
+    @overrides
+    def compute(self, video: VREVideo, ixs: list[int]):
+        raise NotImplementedError(f"{self} supposed to be used only as external representation")
---
+++ packages/video-representations-extractor/video_representations_extractor-1.7.4.tar.gz/video_representations_extractor-1.7.4/vre/representations/semantic_segmentation/safeuav/safeuav.py
@@ -0,0 +1,109 @@
+"""SafeUAV semanetic segmentation representation"""
+from overrides import overrides
+import numpy as np
+import torch as tr
+from torch import nn
+from torch.nn import functional as F
+
+from vre.utils import (
+    image_resize_batch, fetch_weights, vre_load_weights, colorize_semantic_segmentation, VREVideo, MemoryData)
+from vre.logger import vre_logger as logger
+from vre.representations import (
+    Representation, ReprOut, LearnedRepresentationMixin, ComputeRepresentationMixin, NpIORepresentation)
+from vre.representations.semantic_segmentation.safeuav.Map2Map import EncoderMap2Map, DecoderMap2Map
+
+class _SafeUavWrapper(nn.Module):
+    """Wrapper. TODO: Replace with nn.Sequential"""
+    def __init__(self, ch_in: int, ch_out: int):
+        super().__init__()
+        tr.manual_seed(42)
+        self.encoder = EncoderMap2Map(ch_in)
+        self.decoder = DecoderMap2Map(ch_out)
+
+    def forward(self, x):
+        """forward function"""
+        y_encoder = self.encoder(x)
+        y_decoder = self.decoder(y_encoder)
+        return y_decoder
+
+class SafeUAV(Representation, LearnedRepresentationMixin, ComputeRepresentationMixin, NpIORepresentation):
+    """SafeUAV semantic segmentation representation"""
+    def __init__(self, num_classes: int, train_height: int, train_width: int, color_map: list[tuple[int, int, int]],
+                 semantic_argmax_only: bool, weights_file: str | None = None, **kwargs):
+        Representation.__init__(self, **kwargs)
+        LearnedRepresentationMixin.__init__(self)
+        ComputeRepresentationMixin.__init__(self)
+        NpIORepresentation.__init__(self)
+        self.num_classes = num_classes
+        assert len(color_map) == num_classes, f"{color_map} ({len(color_map)}) vs {num_classes}"
+        self.color_map = color_map
+        self.train_height = train_height
+        self.train_width = train_width
+        self.semantic_argmax_only = semantic_argmax_only
+        self.weights_file = weights_file
+        self.classes = list(range(num_classes))
+        self.model: _SafeUavWrapper | None = None
+        self.output_dtype = "uint8" if semantic_argmax_only else "float16"
+
+    @property
+    @overrides
+    def n_channels(self) -> int:
+        raise len(self.classes)
+
+    @overrides
+    def compute(self, video: VREVideo, ixs: list[int]):
+        assert self.data is None, f"[{self}] data must not be computed before calling this"
+        tr_frames = tr.from_numpy(video[ixs]).to(self.device)
+        frames_norm = tr_frames.permute(0, 3, 1, 2) / 255
+        frames_resized = F.interpolate(frames_norm, (self.train_height, self.train_width), mode="bilinear")
+        with tr.no_grad():
+            prediction = self.model.forward(frames_resized)
+        np_pred = prediction.permute(0, 2, 3, 1).cpu().numpy().astype(np.float32)
+        y_out = np.argmax(np_pred, axis=-1).astype(np.uint8) if self.semantic_argmax_only else np_pred
+        self.data = ReprOut(frames=video[ixs], output=MemoryData(y_out), key=ixs)
+
+    @overrides
+    def make_images(self) -> np.ndarray:
+        assert self.data is not None, f"[{self}] data must be first computed using compute()"
+        frames_rsz = image_resize_batch(self.data.frames, *self.data.output.shape[1:3])
+        preds = self.data.output if self.semantic_argmax_only else self.data.output.argmax(-1)
+        return colorize_semantic_segmentation(preds, self.classes, self.color_map, rgb=frames_rsz)
+
+    @overrides
+    def vre_setup(self, load_weights: bool = True):
+        assert self.setup_called is False
+        self.model = _SafeUavWrapper(ch_in=3, ch_out=self.num_classes)
+
+        if load_weights:
+            if self.weights_file is None:
+                logger.warning("No weights file provided, using random weights.")
+                self.model = self.model.eval().to(self.device)
+                return
+
+            def _convert(data: dict[str, tr.Tensor]) -> dict[str, tr.Tensor]:
+                logger.warning("GET RID OF THIS WHEN THERE'S TIME")
+                new_data = {}
+                for k in data.keys():
+                    if k.startswith("model.0."):
+                        other = k.replace("model.0.", "encoder.")
+                    elif k.startswith("model.1."):
+                        other = k.replace("model.1.", "decoder.")
+                    else:
+                        assert False, k
+                    new_data[other] = data[k]
+                return new_data
+
+            weights_file_abs = fetch_weights(__file__) / self.weights_file
+            data = _convert(vre_load_weights(weights_file_abs)["state_dict"])
+            self.model.load_state_dict(data)
+        self.model = self.model.eval().to(self.device)
+        self.setup_called = True
+
+    @overrides
+    def vre_free(self):
+        assert self.setup_called is True and self.model is not None, (self.setup_called, self.model is not None)
+        if str(self.device).startswith("cuda"):
+            self.model.to("cpu")
+            tr.cuda.empty_cache()
+        self.model = None
+        self.setup_called = False
