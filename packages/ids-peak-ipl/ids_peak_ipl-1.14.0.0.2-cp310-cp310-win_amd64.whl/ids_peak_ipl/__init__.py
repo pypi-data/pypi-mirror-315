6a32b5223e7121a1cef5200366a3bcc6a7ab868d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 05:05:04 +0000

    Add package ids-peak-ipl/1.14.0.0.2/ids_peak_ipl-1.14.0.0.2-cp310-cp310-win_amd64.whl

---
+++ packages/ids-peak-ipl/ids_peak_ipl-1.14.0.0.2-cp310-cp310-win_amd64.whl/ids_peak_ipl/__init__.py
@@ -0,0 +1,28 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_7_2():
+    import os
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'ids_peak_ipl.libs'))
+    if os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_7_2()
+del _delvewheel_patch_1_7_2
+# end delvewheel patch
+
+import os
+import sys
+
+MODULE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+if (sys.version_info[0] < 3) or ((sys.version_info[0] == 3) and (sys.version_info[1] < 8)):
+    os.environ["Path"] += os.pathsep + MODULE_DIR
+else:
+    os.add_dll_directory(MODULE_DIR)
+    # Workaround for Conda Python 3.8 environments under Windows.PATHSEP_STRING
+    # Although Python changed the DLL search mechanism in Python 3.8,
+    # Windows Conda Python 3.8 environments still use the old mechanism...
+    os.environ["Path"] += os.pathsep + MODULE_DIR
+
+
+
