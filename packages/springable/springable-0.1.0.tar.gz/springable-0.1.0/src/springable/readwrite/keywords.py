8fc63f8e7d6625eb758512431bea8a7924dc5505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 06:05:52 +0000

    Add package springable/0.1.0/springable-0.1.0.tar.gz

---
+++ packages/springable/springable-0.1.0.tar.gz/springable-0.1.0/src/springable/readwrite/keywords.py
@@ -0,0 +1,23 @@
+from ..mechanics import shape
+from ..mechanics import mechanical_behavior
+from .keywordmapping import KeywordMapping
+
+usable_shapes = KeywordMapping({shape.Segment: '',
+                                shape.Area: 'AREA',
+                                shape.Path: 'LINE',
+                                shape.Angle: 'ROTATION',
+                                shape.SignedDistancePointLine: 'DISTANCE',
+                                shape.SquaredDistancePointSegment: 'GAP',
+                                })
+
+usable_behaviors = KeywordMapping({mechanical_behavior.LinearBehavior: 'LINEAR',
+                                   mechanical_behavior.NaturalBehavior: 'NATURAL',
+                                   mechanical_behavior.BezierBehavior: 'BEZIER',
+                                   mechanical_behavior.Bezier2Behavior: 'BEZIER2',
+                                   mechanical_behavior.ZigZagBehavior: 'ZIGZAG',
+                                   mechanical_behavior.ZigZag2Behavior: 'ZIGZAG2',
+                                   mechanical_behavior.ContactBehavior: 'CONTACT',
+                                   mechanical_behavior.IsothermicGas: 'ISOTHERMIC_GAS',
+                                   mechanical_behavior.IsentropicGas: 'ISENTROPIC_GAS',
+                                   })
+
