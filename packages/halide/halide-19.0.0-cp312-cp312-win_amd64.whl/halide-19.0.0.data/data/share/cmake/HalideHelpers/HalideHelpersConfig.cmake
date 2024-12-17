db3d1cc1f9b9bf95003a8c37a58ebca7ab245429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 15:06:52 +0000

    Add package halide/19.0.0/halide-19.0.0-cp312-cp312-win_amd64.whl

---
+++ packages/halide/halide-19.0.0-cp312-cp312-win_amd64.whl/halide-19.0.0.data/data/share/cmake/HalideHelpers/HalideHelpersConfig.cmake
@@ -0,0 +1,6 @@
+cmake_minimum_required(VERSION 3.28)
+
+include(CMakeFindDependencyMacro)
+find_dependency(Python 3)
+
+include("${Python_SITEARCH}/halide/lib/cmake/HalideHelpers/HalideHelpersConfig.cmake")
