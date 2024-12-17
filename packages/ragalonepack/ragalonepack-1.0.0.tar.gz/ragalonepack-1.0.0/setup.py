cc4ca0600cc06b291f443184777eb9266787392c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 04:05:32 +0000

    Add package ragalonepack/1.0.0/ragalonepack-1.0.0.tar.gz

---
+++ packages/ragalonepack/ragalonepack-1.0.0.tar.gz/ragalonepack-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ragalonepack",
+    version="1.0.0",
+    description="A Python package for RAG pipelines using OpenAI and FAISS.",
+    author="Tarun",
+    author_email="gangadhartarun21@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+    'openai==0.28.0',
+    'sentence-transformers==3.2.1',
+    'faiss-cpu==1.9.0',
+    'datasets==3.2.0',
+    'transformers==4.46.3',
+    'gcsfs>=2024.10.0',
+    'fsspec==2024.10.0'
+    ],
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
