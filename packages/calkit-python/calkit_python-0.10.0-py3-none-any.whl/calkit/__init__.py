d3e98c527b44eb0a6784be0b180e18b85a0a252d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 06:05:38 +0000

    Add package calkit-python/0.10.0/calkit_python-0.10.0-py3-none-any.whl

---
+++ packages/calkit-python/calkit_python-0.10.0-py3-none-any.whl/calkit/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.10.0"
+
+from .core import *
+from . import git
+from . import dvc
+from . import cloud
+from . import jupyter
+from . import config
+from . import models
+from . import office
+from . import templates
+from . import conda
