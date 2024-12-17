2a99289fb1b0c86bbef287ecd8d92b5fb70d2882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 15:06:55 +0000

    Add package rapidstats/0.0.23/rapidstats-0.0.23-cp312-cp312-win32.whl

---
+++ packages/rapidstats/rapidstats-0.0.23-cp312-cp312-win32.whl/rapidstats/__init__.py
@@ -0,0 +1,14 @@
+from ._bootstrap import Bootstrap
+from ._corr import correlation_matrix
+from ._distributions import norm
+from ._metrics import (
+    adverse_impact_ratio,
+    brier_loss,
+    confusion_matrix,
+    confusion_matrix_at_thresholds,
+    max_ks,
+    mean,
+    mean_squared_error,
+    roc_auc,
+    root_mean_squared_error,
+)
