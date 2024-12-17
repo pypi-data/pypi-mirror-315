8fe545cabb1067723d9c561119c2453ea0020319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 06:05:54 +0000

    Add package aeif-dataset/0.9.8/aeif_dataset-0.9.8-py3-none-any.whl

---
+++ packages/aeif-dataset/aeif_dataset-0.9.8-py3-none-any.whl/aeifdataset/miscellaneous/contstants.py
@@ -0,0 +1,4 @@
+# Named constants
+SHA256_CHECKSUM_LENGTH = 32  # Length of SHA-256 checksum
+INT_LENGTH = 4  # Length of an integer file length
+REPACK = False  # True when Image and Points should be passed through
