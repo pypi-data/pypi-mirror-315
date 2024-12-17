84306fcbe32ae8782cc0c86c809afa3813f7ee71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 05:05:49 +0000

    Add package dify_plugin/0.0.1b51/dify_plugin-0.0.1b51-py3-none-any.whl

---
+++ packages/dify_plugin/dify_plugin-0.0.1b51-py3-none-any.whl/dify_plugin/__init__.py
@@ -0,0 +1,48 @@
+from gevent import monkey
+
+# patch all the blocking calls
+monkey.patch_all(sys=True)
+
+from .config.config import DifyPluginEnv
+from .interfaces.endpoint import Endpoint
+from .interfaces.model import ModelProvider
+from .interfaces.model.large_language_model import LargeLanguageModel
+from .interfaces.model.moderation_model import ModerationModel
+from .interfaces.model.openai_compatible.llm import OAICompatLargeLanguageModel
+from .interfaces.model.openai_compatible.provider import OAICompatProvider
+from .interfaces.model.openai_compatible.rerank import OAICompatRerankModel
+from .interfaces.model.openai_compatible.speech2text import OAICompatSpeech2TextModel
+from .interfaces.model.openai_compatible.text_embedding import OAICompatEmbeddingModel
+from .interfaces.model.openai_compatible.tts import OAICompatText2SpeechModel
+from .interfaces.model.rerank_model import RerankModel
+from .interfaces.model.speech2text_model import Speech2TextModel
+from .interfaces.model.text_embedding_model import TextEmbeddingModel
+from .interfaces.model.tts_model import TTSModel
+from .interfaces.tool import Tool, ToolProvider
+from .interfaces.agent import AgentProvider, AgentStrategy
+from .invocations.file import File
+from .plugin import Plugin
+
+__all__ = [
+    "Plugin",
+    "DifyPluginEnv",
+    "Endpoint",
+    "ToolProvider",
+    "Tool",
+    "ModelProvider",
+    "LargeLanguageModel",
+    "TextEmbeddingModel",
+    "RerankModel",
+    "TTSModel",
+    "Speech2TextModel",
+    "ModerationModel",
+    "OAICompatProvider",
+    "OAICompatLargeLanguageModel",
+    "OAICompatEmbeddingModel",
+    "OAICompatSpeech2TextModel",
+    "OAICompatText2SpeechModel",
+    "OAICompatRerankModel",
+    "File",
+    "AgentProvider",
+    "AgentStrategy",
+]
