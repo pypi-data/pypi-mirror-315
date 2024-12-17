2fb5c5fd96faee822949bd66253e0265178987bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 04:06:40 +0000

    Add package mlinsightlab/0.0.9/mlinsightlab-0.0.9.tar.gz

---
+++ packages/mlinsightlab/mlinsightlab-0.0.9.tar.gz/mlinsightlab-0.0.9/src/mlinsightlab/__init__.py
@@ -0,0 +1,11 @@
+"""
+Utilities for interacting with the Machine Learning Insights Lab (MLIL) platform.
+
+This package is based around the 'MLIL_client' class, which has the capabilities to
+interact with the functionality of the MLIL platform from a Python CLI environment.
+"""
+
+from .MLILClient import MLILClient
+
+__all__ = ['MLILClient']
+__version__ = '0.0.9'
