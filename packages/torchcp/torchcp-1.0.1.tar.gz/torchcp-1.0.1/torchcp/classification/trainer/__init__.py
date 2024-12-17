48f40328353457a02f3c73b773451933bf8ec76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 03:17:59 +0000

    Add package torchcp/1.0.1/torchcp-1.0.1.tar.gz

---
+++ packages/torchcp/torchcp-1.0.1.tar.gz/torchcp-1.0.1/torchcp/classification/trainer/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2023-present, SUSTech-ML.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+from .base_trainer import Trainer
+from .ordinal_trainer import OrdinalTrainer
+from .ts_trainer import TSTrainer
+from .confts_trainer import ConfTSTrainer
+from .model import TemperatureScalingModel
