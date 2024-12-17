8945a1187225940fd75476b48fe12831742014c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 05:06:04 +0000

    Add package lamin_spatial/0.1.0/lamin_spatial-0.1.0-py2.py3-none-any.whl

---
+++ packages/lamin_spatial/lamin_spatial-0.1.0-py2.py3-none-any.whl/lamin_spatial/__init__.py
@@ -0,0 +1,11 @@
+"""Manage spatial data using LaminDB.
+
+Import the package::
+
+   import lamin_spatial
+
+"""
+
+__version__ = "0.1.0"  # denote a pre-release for 0.1.0 with 0.1rc1
+
+from lamin_spatial.spatialdata_curator import SpatialDataCurator
