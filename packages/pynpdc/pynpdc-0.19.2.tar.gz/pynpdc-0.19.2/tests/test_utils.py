c9a3db110b31fa58ce934c0e158d85c4a0cbfb38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 16:05:57 +0000

    Add package pynpdc/0.19.2/pynpdc-0.19.2.tar.gz

---
+++ packages/pynpdc/pynpdc-0.19.2.tar.gz/pynpdc-0.19.2/tests/test_utils.py
@@ -0,0 +1,39 @@
+import os
+import pytest
+import unittest
+import uuid
+from pynpdc.utils import guard_dir, guard_path
+
+
+class TestGuardDir(unittest.TestCase):
+    def test_with_dir(self) -> None:
+        path = os.path.dirname(__file__)
+        guard_dir(path)
+        self.assertTrue(True)
+
+    def test_with_file(self) -> None:
+        path = __file__
+        with pytest.raises(FileNotFoundError):
+            guard_dir(path)
+
+    def test_with_unknown_path(self) -> None:
+        path = f"/tmp/{uuid.uuid4()}"
+        with pytest.raises(FileNotFoundError):
+            guard_dir(path)
+
+
+class TestGuardPath(unittest.TestCase):
+    def test_with_dir(self) -> None:
+        path = os.path.dirname(__file__)
+        with pytest.raises(FileNotFoundError):
+            guard_path(path)
+
+    def test_with_file(self) -> None:
+        path = __file__
+        guard_path(path)
+        self.assertTrue(True)
+
+    def test_with_unknown_path(self) -> None:
+        path = f"/tmp/{uuid.uuid4()}"
+        with pytest.raises(FileNotFoundError):
+            guard_path(path)
