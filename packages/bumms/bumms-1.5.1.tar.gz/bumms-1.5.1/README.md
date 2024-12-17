87802798c8f292b0d5e6fe07c9cf249fc0172713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 14:05:10 +0000

    Add package bumms/1.5.1/bumms-1.5.1.tar.gz

---
+++ packages/bumms/bumms-1.5.1.tar.gz/bumms-1.5.1/README.md
@@ -0,0 +1,7 @@
+# Bumms
+
+Eine lustige Python-Bibliothek, die eine MP3-Datei abspielt und zufällige Texte anzeigt.
+
+## Installation
+```bash
+pip install bumms
---
+++ packages/bumms/bumms-1.5.1.tar.gz/bumms-1.5.1/bumms/__main__.py
@@ -0,0 +1,13 @@
+from bumms.core import play_random_sound, display_ascii_art, display_random_text, play_video_in_ascii
+
+def bumms_hd():
+    play_video_in_ascii()
+
+def main():
+    play_random_sound()
+    display_ascii_art()
+    display_random_text()
+
+if __name__ == "__main__":
+    bumms_hd()  # Befehl "bumms-hd"
+    # main()  # Alternativ Standardfunktion "bumms"
