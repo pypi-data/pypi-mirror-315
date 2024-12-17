93933105db5efd5f281ab2389731577f9bd2a270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 15:07:01 +0000

    Add package pdf-utility/0.0.1/pdf_utility-0.0.1.tar.gz

---
+++ packages/pdf-utility/pdf_utility-0.0.1.tar.gz/pdf_utility-0.0.1/README.md
@@ -0,0 +1,29 @@
+# PDF Utility
+A simple command-line utility to combine multiple PDF/image files into a single PDF.
+
+## Features
+- Combine individual PDF files or entire folders of PDFs.
+- Support for recursive folder traversal.
+- Specify custom output file paths.
+- Works as a CLI tool for seamless integration into your workflows.
+
+### Coming Soon
+- File Size Reduction
+
+## Installation
+Using pip: `pip install pdf_util`
+
+*Note: the shortcut `pdf_util` can be directly used, but sometimes pip shortcuts do not work on windows. In this case, the module can be run with `python -m pdf_util <args>`.*
+
+Using executable:
+- Download the executable artifact from the latest `Build Windows Executable` job
+- Execute the executable, ex: `path/to/pdf_util.exe --help`
+
+## Usage
+Combine Specific Files
+To combine specific PDF files into a single output: `pdf_util --operation combine path/to/file1.pdf path/to/file2.png --output_path output.pdf`
+
+Combine All PDFs in a Folder
+To combine all PDF files in a specific folder: `pdf_util --operation combine -r path/to/folder --output_path output.pdf`
+
+Note: images (.png, .jpg) are supported as file input
---
+++ packages/pdf-utility/pdf_utility-0.0.1.tar.gz/pdf_utility-0.0.1/pdf_util/input.py
@@ -0,0 +1,117 @@
+import argparse
+from pdf_util import util
+import os
+import click
+
+INPUT_EXTENSIONS = (
+    ".pdf",
+    ".png",
+    ".jpg",
+    ".jpeg",
+    ".PDF",
+)
+
+SUPPORTED_OPERATIONS = {
+    "combine": "Combine multiple PDFs into one.",
+}
+
+
+class PdfInputError(Exception):
+    def __str__(self):
+        # Apply click styling only when the exception is explicitly printed
+        return click.style(self.args[0], fg="red", bold=False)
+
+
+class PdfInputData:
+    def __init__(self, args):
+        self.recursive = args.recursive
+
+        if self.recursive:
+            self.input_files = []
+            for path in args.input_paths:
+                if os.path.isdir(path):
+                    files_with_extensions = util.get_files_with_extensions(
+                        INPUT_EXTENSIONS, [], path
+                    )
+                    # Sort by filename (basename) before extending the list
+                    files_with_extensions.sort(key=os.path.basename)
+
+                    self.input_files.extend(files_with_extensions)
+                else:
+                    self.input_files.append(path)
+        else:
+            self.input_files = args.input_paths
+            for path in self.input_files:
+                if os.path.isdir(path):
+                    raise PdfInputError(
+                        f"Invalid input path - {path} is directory!\nUse -r option to select all files in the directory."
+                    )
+                elif not os.path.exists(path):
+                    raise PdfInputError(f"Invalid input path - does not exist: {path}")
+
+        if args.output_path:
+            self.output_path = args.output_path
+            # get the output path extension
+            _, output_extension = os.path.splitext(self.output_path)
+            if output_extension != ".pdf":
+                raise PdfInputError("Output file must be a PDF.")
+        else:
+            self.output_path = os.path.join(os.getcwd(), "converted_output")
+
+            for path in self.input_files:
+                file_name = os.path.basename(path)
+                # remove the extension
+                file_name = file_name[: file_name.rfind(".")]
+                self.output_path += f"_{file_name}"
+
+            self.output_path += ".pdf"
+
+        if os.path.exists(self.output_path) and not (args.override_output_path):
+            raise PdfInputError(
+                f"\nOutput file already exists: {self.output_path}.\n Use --override_output_path to overwrite."
+            )
+
+        input_files_printable = ", ".join(self.input_files)
+        click.secho(f"Collected input files: {input_files_printable}", fg="green")
+
+
+def process_inputs():
+    parser = argparse.ArgumentParser(
+        description="PDF utility for perfoming common operations on PDFs."
+    )
+
+    supported_operation_message = "\nSupported operations:\n"
+    for operation, description in SUPPORTED_OPERATIONS.items():
+        supported_operation_message += f"\n{operation}: {description}\n"
+
+    parser.add_argument(
+        "--operation",
+        choices=SUPPORTED_OPERATIONS.keys(),
+        help=f"The operation to perform on the input files. {supported_operation_message}",
+        required=True,
+    )
+
+    parser.add_argument("-o", "--output_path", help="Path to the output PDF.")
+    parser.add_argument(
+        "-r",
+        "--recursive",
+        action="store_true",
+        help="Recursively search directories for input files.",
+    )
+    parser.add_argument("input_paths", nargs="+", help="Paths to the input PDFs.")
+    parser.add_argument(
+        "--override_output_path",
+        action="store_true",
+        help="Override the output file if it exists",
+    )
+
+    args = parser.parse_args()
+
+    # make the output path absolute
+    if args.output_path:
+        args.output_path = os.path.abspath(args.output_path)
+
+    # make the input paths absolute
+    args.input_paths = [os.path.abspath(path) for path in args.input_paths]
+
+    return PdfInputData(args)
