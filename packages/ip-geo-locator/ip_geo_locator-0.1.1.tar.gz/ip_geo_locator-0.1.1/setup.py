5fa0ebfaa19c0a096da3713619d93ec08ac53e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 16:06:59 +0000

    Add package ip-geo-locator/0.1.1/ip_geo_locator-0.1.1.tar.gz

---
+++ packages/ip-geo-locator/ip_geo_locator-0.1.1.tar.gz/ip_geo_locator-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ip_geo_locator",
+    version="0.1.1",
+    author="ip_finder",
+    author_email="your.email@example.com",
+    description="A Python SDK for IP Finder",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Three-mavericks/ip_finder_python.git",  # Update with your repo
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.0.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
+
