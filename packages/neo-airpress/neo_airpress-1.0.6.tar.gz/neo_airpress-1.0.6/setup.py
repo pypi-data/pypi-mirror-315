0d5dbe9e975b3f0b5f1ac090dd9bb6b2b91a6ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 15:06:16 +0000

    Add package neo-airpress/1.0.6/neo_airpress-1.0.6.tar.gz

---
+++ packages/neo-airpress/neo_airpress-1.0.6.tar.gz/neo_airpress-1.0.6/setup.py
@@ -0,0 +1,30 @@
+# SPDX-License-Identifier: MIT
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="neo-airpress",
+    version="1.0.6",
+    author="Jstyles",
+    author_email="jstyles@styl.dev",
+    description="A frustration-free compression tool for PKPass archives.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jontyms/neo-airpress",
+    packages=["airpress"],
+    install_requires=[
+        "cryptography>=44.0.0",
+    ],
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
