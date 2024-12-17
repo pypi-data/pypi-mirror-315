7bc47410e12a19423d38d1cd33ef60647d2bf4ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-17 05:04:58 +0000

    Add package RuleTree/0.0.2.post3/ruletree-0.0.2.post3.tar.gz

---
+++ packages/RuleTree/ruletree-0.0.2.post3.tar.gz/ruletree-0.0.2.post3/RuleTree/base/RuleTreeBaseStump.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+from sklearn.base import BaseEstimator
+
+from RuleTree.utils.define import DATA_TYPE_TABULAR
+
+
+class RuleTreeBaseStump(BaseEstimator, ABC):
+    @abstractmethod
+    def get_rule(self, columns_names=None, scaler=None, float_precision:int|None=3):
+        pass
+
+    @abstractmethod
+    def node_to_dict(self):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def dict_to_node(self, node_dict, X):
+        pass
+
+    @staticmethod
+    def supports(data_type):
+        return data_type in [DATA_TYPE_TABULAR]
