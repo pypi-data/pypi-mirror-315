2f71c0d989bc3b3f6e8019985617ca723e56500a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 03:14:56 +0000

    Add package bscintillaedit/0.0.3/bscintillaedit-0.0.3-cp312-cp312-win_amd64.whl

---
+++ packages/bscintillaedit/bscintillaedit-0.0.3-cp312-cp312-win_amd64.whl/bscintillaedit/__init__.py
@@ -0,0 +1,6 @@
+"""Provides a `BScintillaEdit` text edit control based on
+[QScrollArea](https://doc.qt.io/qt-6/qscrollarea.html) and embedding a
+`ScintillaEditBase` from [Scintilla](https://www.scintilla.org/).
+"""
+
+__version__ = "0.0.3"
