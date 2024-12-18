2b3e2ee6115ca590e3689d81eff51692209813ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 03:14:30 +0000

    Add package flowpyter-task/1.3.0/flowpyter_task-1.3.0-py3-none-any.whl

---
+++ packages/flowpyter-task/flowpyter_task-1.3.0-py3-none-any.whl/flowpytertask/__init__.py
@@ -0,0 +1,18 @@
+from . import flowpytertask
+
+from .flowpytertask import \
+    NotebookExtensionError, \
+    ReservedParameterError, \
+    ReservedMountError, \
+    RelativePathError, \
+    BadParameterError, \
+    FlowkitEnvError, \
+    ParameterFileError, \
+    PapermillOperator, \
+    FlowpyterOperator, \
+    TemplateOperator, \
+    is_running_on_docker
+
+
+from . import _version
+__version__ = _version.get_versions()['version']
