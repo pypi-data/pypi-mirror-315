4e3c86c2fb89ef2ab2161db3053f2d3ddee08904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 03:15:00 +0000

    Add package onego/1.1.0/onego-1.1.0-py3-none-any.whl

---
+++ packages/onego/onego-1.1.0-py3-none-any.whl/polinv/sessions/__init__.py
@@ -0,0 +1,4 @@
+import polinv.sessions.chrome as chrome
+from polinv.sessions.chrome import SessionChrome
+
+from polinv.sessions.get_chrome_version import get_chrome_version, get_chrome_version_soft
