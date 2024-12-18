1053ac54a6a08fc58a1345555e3e66cf08771174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 03:14:26 +0000

    Add package crawlab-sdk/0.7.0rc5/crawlab_sdk-0.7.0rc5-py3-none-any.whl

---
+++ packages/crawlab-sdk/crawlab_sdk-0.7.0rc5-py3-none-any.whl/crawlab/__init__.py
@@ -0,0 +1,8 @@
+__all__ = [
+    "save_item",
+    "save_items",
+    "CrawlabPipeline",
+]
+
+from crawlab.core.item import save_item, save_items
+from crawlab.core.scrapy.pipelines import CrawlabPipeline
