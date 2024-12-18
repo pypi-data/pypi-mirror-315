876d8f8d832ec29021ba5b99f0e6b435fe92fc29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 04:05:51 +0000

    Add package edflib/0.86.4/edflib-0.86.4-cp39-cp39-musllinux_1_2_i686.whl

---
+++ packages/edflib/edflib-0.86.4-cp39-cp39-musllinux_1_2_i686.whl/edflib/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*
+from __future__ import print_function, division, absolute_import
+
+from .edfwriter import EdfWriter
+from .edfreader import Edfinfo, EdfReader
+
+__version__= "0.86.4"
