3532962b1b5260a9918bd3dcd5288c68ab922dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 03:14:23 +0000

    Add package omni-photo-lib/0.1.35/omni-photo-lib-0.1.35.tar.gz

---
+++ packages/omni-photo-lib/omni-photo-lib-0.1.35.tar.gz/omni-photo-lib-0.1.35/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="omni-photo-lib",  # Unique package name
+    version="0.1.35",  # Start with a low version
+    description="A library for generating images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Markdown for PyPI
+    packages=find_packages(),
+    install_requires=[
+        "Pillow",
+        "requests",
+        "together",
+        "celery",
+    ],
+    author="Adhithyan",
+    author_email="adhithyan@omnineura.ai",
+    url="",  
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
