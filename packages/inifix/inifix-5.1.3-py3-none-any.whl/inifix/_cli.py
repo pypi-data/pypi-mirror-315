0055d6cfa600043996f91f38b340e7cb2d9c752a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 05:06:25 +0000

    Add package inifix/5.1.3/inifix-5.1.3-py3-none-any.whl

---
+++ packages/inifix/inifix-5.1.3-py3-none-any.whl/inifix/_cli.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from typing import Literal, NamedTuple, TextIO
+
+
+class Message(NamedTuple):
+    content: str
+    dest: TextIO
+
+
+class TaskResults(NamedTuple):
+    status: Literal[0, 1]
+    messages: list[Message]
+
+
+def get_cpu_count() -> int:
+    # this function exists primarily to be mocked
+    # instead of something we don't own
+    base_cpu_count: int | None
+    if sys.version_info >= (3, 13):
+        base_cpu_count = os.process_cpu_count()
+    elif hasattr(os, "sched_getaffinity"):
+        # this function isn't available on all platforms
+        base_cpu_count = len(os.sched_getaffinity(0))
+    else:  # pragma: no cover
+        # this proxy is good enough in most situations
+        base_cpu_count = os.cpu_count()
+    return base_cpu_count or 1
