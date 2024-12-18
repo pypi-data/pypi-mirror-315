5a99dffa90e745009b4c86f517a4c7b8e65be28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 04:06:47 +0000

    Add package sms8-api/0.1.2/sms8_api-0.1.2.tar.gz

---
+++ packages/sms8-api/sms8_api-0.1.2.tar.gz/sms8_api-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sms8_api',
+    version='0.1.2',
+    description='Opis Twojego pakietu',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Patryk',
+    author_email='patryk.okonn@gmail.com',
+    url='https://github.com/ryba-22/sms8_api',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'requests',  # Poprawna składnia - dodajemy nazwę pakietu jako ciąg znaków
+    ],
+)
