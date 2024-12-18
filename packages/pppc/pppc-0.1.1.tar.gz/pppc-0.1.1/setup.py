c7620578b7201b7843d72fabf1b47151bed5177c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 05:06:29 +0000

    Add package pppc/0.1.1/pppc-0.1.1.tar.gz

---
+++ packages/pppc/pppc-0.1.1.tar.gz/pppc-0.1.1/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pppc",
+    version="0.1.1",
+    author="retrotee",
+    description="Create Minecraft Paper plugins using Python",
+    url="https://github.com/retrotee/pppc",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Games/Entertainment",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "javabridge>=1.0.19",  # For Java integration
+        "PyYAML>=6.0",         # For YAML file handling
+        "typing-extensions>=4.0.0",  # For enhanced type hints
+        "dataclasses>=0.6",    # For Python 3.6 compatibility
+        "jinja2>=3.0.0",       # For template rendering
+        "black>=22.0.0",       # For code formatting
+        "isort>=5.0.0"         # For import sorting
+    ],
+    extras_require={
+        'dev': [
+            'pytest>=7.0.0',
+            'pytest-cov>=3.0.0',
+            'mypy>=0.950',
+            'flake8>=4.0.0',
+            'tox>=3.24.0'
+        ]
+    }
+)
