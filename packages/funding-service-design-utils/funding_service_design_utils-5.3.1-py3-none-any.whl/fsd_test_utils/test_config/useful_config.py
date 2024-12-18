7c2ca37ec2150f5b12b72b1a5bd2bde2cb05057f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 04:06:01 +0000

    Add package funding-service-design-utils/5.3.1/funding_service_design_utils-5.3.1-py3-none-any.whl

---
+++ packages/funding-service-design-utils/funding_service_design_utils-5.3.1-py3-none-any.whl/fsd_test_utils/test_config/useful_config.py
@@ -0,0 +1,12 @@
+class UsefulConfig:
+    COF_FUND_ID = "47aef2f5-3fcb-4d45-acb5-f0152b5f03c4"
+    COF_ROUND_2_ID = "c603d114-5364-4474-a0c4-c41cbf4d3bbd"
+    COF_ROUND_2_W3_ID = "5cf439bf-ef6f-431e-92c5-a1d90a4dd32f"
+    COF_ROUND_3_W1_ID = "e85ad42f-73f5-4e1b-a1eb-6bc5d7f3d762"
+    COF_ROUND_3_W2_ID = "6af19a5e-9cae-4f00-9194-cf10d2d7c8a7"
+
+    NSTF_FUND_ID = "13b95669-ed98-4840-8652-d6b7a19964db"
+    NSTF_ROUND_2_ID = "fc7aa604-989e-4364-98a7-d1234271435a"
+
+    HSRA_FUND_ID = "1e4bd8b0-b399-466d-bbd1-572171bbc7bd"
+    HSRA_ROUND_1_ID = "50062ff6-e696-474d-a560-4d9af784e6e5"
