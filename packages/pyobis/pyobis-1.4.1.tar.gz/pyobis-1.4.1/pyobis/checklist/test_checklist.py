b6b80898e399c260c08a83cb8d1408b624d16fe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 05:06:35 +0000

    Add package pyobis/1.4.1/pyobis-1.4.1.tar.gz

---
+++ packages/pyobis/pyobis-1.4.1.tar.gz/pyobis-1.4.1/pyobis/checklist/test_checklist.py
@@ -0,0 +1,55 @@
+"""Tests for checklist module"""
+
+import pytest
+import requests
+
+from pyobis import checklist
+
+
+@pytest.mark.vcr()
+def test_checklist():
+    """
+    checklist.list - basic test for data, check type, size and other methods
+    """
+    query = checklist.list(scientificname="Mola mola")
+    assert not query.data
+    query.execute()
+    assert "dict" == query.data.__class__.__name__
+    assert 2 == len(query.data)
+    assert dict == query.data["results"][0].__class__
+    assert int == query.data["results"][0]["taxonID"].__class__
+    assert "Mola mola" == query.data["results"][0]["species"]
+    assert requests.get(query.api_url).status_code == 200
+
+
+@pytest.mark.vcr()
+def test_checklist_redlist():
+    """
+    checklist.redlist - basic test for data, check type, size and other methods
+    """
+    query = checklist.redlist(scientificname="Mola mola")
+    assert not query.data
+    query.execute()
+    assert "dict" == query.data.__class__.__name__
+    assert 2 == len(query.data)
+    assert dict == query.data["results"][0].__class__
+    assert int == query.data["results"][0]["taxonID"].__class__
+    assert "Mola mola" == query.data["results"][0]["species"]
+    assert requests.get(query.api_url).status_code == 200
+
+
+@pytest.mark.vcr()
+def test_checklist_newest():
+    """
+    checklist.newest - basic test for data, check type, size and other methods
+    """
+    query = checklist.newest(scientificname="Mola mola")
+    assert not query.data
+    query.execute()
+    assert "dict" == query.data.__class__.__name__
+    assert 1 == len(query.data)
+    assert dict == query.data["results"][0].__class__
+    assert int == query.data["results"][0]["taxonID"].__class__
+    assert "Mola mola" == query.data["results"][0]["species"]
+    assert requests.get(query.api_url).status_code == 200
+    assert query.to_pandas().__class__.__name__ == "DataFrame"
---
+++ packages/pyobis/pyobis-1.4.1.tar.gz/pyobis-1.4.1/pyobis/dataset/test_dataset.py
@@ -0,0 +1,44 @@
+"""Tests for dataset module"""
+
+import pytest
+import requests
+
+from pyobis import dataset
+
+
+@pytest.mark.vcr()
+def test_dataset_get_data():
+    "dataset.get - test data"
+    query = dataset.get(id="ec9df3b9-3b2b-4d83-881b-27bcbcd57b95")
+    assert not query.data
+    query.execute()
+    assert query.data.__class__.__name__ == "dict"
+    assert 2 == len(query.data)
+    assert dict == query.data["results"][0].__class__
+    assert query.to_pandas().__class__.__name__ == "DataFrame"
+
+
+@pytest.mark.vcr()
+def test_dataset_get_url():
+    query = dataset.get(id="ec9df3b9-3b2b-4d83-881b-27bcbcd57b95")
+    assert requests.get(query.api_url).status_code == 200
+    assert requests.get(query.mapper_url).status_code == 200
+
+
+@pytest.mark.vcr()
+def test_dataset_search_data():
+    "dataset.search - test for data"
+    query = dataset.search(scientificname="Mola mola")
+    assert not query.data
+    query.execute()
+    assert "dict" == query.data.__class__.__name__
+    assert 2 == len(query.data)
+    assert dict == query.data["results"][0].__class__
+    assert query.to_pandas().__class__.__name__ == "DataFrame"
+
+
+@pytest.mark.vcr()
+def test_dataset_search_url():
+    query = dataset.search(scientificname="Mola mola")
+    assert requests.get(query.api_url).status_code == 200
+    assert not query.mapper_url
