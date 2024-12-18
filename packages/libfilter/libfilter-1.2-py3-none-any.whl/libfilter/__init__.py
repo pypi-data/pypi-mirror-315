5b2a6117122e12d25335f0e00f17b947b0a7f9b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 04:05:56 +0000

    Add package libfilter/1.2/libfilter-1.2-py3-none-any.whl

---
+++ packages/libfilter/libfilter-1.2-py3-none-any.whl/libfilter/__init__.py
@@ -0,0 +1,12 @@
+from .base_classes import *
+from .audio_filters import *
+from .oscillators import *
+from .samplers import *
+from .other import *
+from .transformers import *
+from .data_encoders import *
+
+__lib__ = "libfilter"
+__lib_creator__ = "radio95"
+__lib_version__ = 1.2
+__version__ = __lib_version__
