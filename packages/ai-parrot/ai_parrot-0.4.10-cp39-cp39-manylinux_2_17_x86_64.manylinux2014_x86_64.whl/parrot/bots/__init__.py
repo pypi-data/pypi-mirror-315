9b6e1b10383b0122e11ad16b04bcabbc0c3206be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 06:06:19 +0000

    Add package ai-parrot/0.4.10/ai_parrot-0.4.10-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ai-parrot/ai_parrot-0.4.10-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/parrot/bots/__init__.py
@@ -0,0 +1,9 @@
+from .abstract import AbstractBot
+from .basic import BasicBot
+from .troc import AskTROC
+from .chatbot import Chatbot
+from .hrbot import HRAgent
+
+__all__ = (
+    'AbstractBot',
+)
