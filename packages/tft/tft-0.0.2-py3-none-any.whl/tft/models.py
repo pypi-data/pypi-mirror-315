807fa9c2bedf19112f90ba1e4cd851604161d66f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 04:06:57 +0000

    Add package tft/0.0.2/tft-0.0.2-py3-none-any.whl

---
+++ packages/tft/tft-0.0.2-py3-none-any.whl/tft/models.py
@@ -0,0 +1,20 @@
+import torch
+from timm.models.maxxvit import TransformerBlock2d
+from timm.models._efficientnet_blocks import UniversalInvertedResidual
+from pytorch_ggd.GGD import DynamicGGD
+
+class AsCAN2D(torch.nn.Module):
+    def __init__(self, input_dim, embed_dim):
+        super().__init__()
+        ggd=lambda num_features,**kw:DynamicGGD(num_features,True,False)
+        C=lambda:UniversalInvertedResidual(embed_dim,embed_dim,act_layer=torch.nn.Identity,norm_layer=ggd)
+        T=lambda:TransformerBlock2d(embed_dim,embed_dim)
+        self.layers=torch.nn.Sequential(
+            torch.nn.Conv2d(i,embed_dim,kernel_size=1),
+            ggd(embed_dim),
+            C(),C(),C(),T(),
+            C(),C(),T(),T(),
+            C(),T(),T(),T()
+        )
+    def forward(self,x):
+        return self.layers(x)
