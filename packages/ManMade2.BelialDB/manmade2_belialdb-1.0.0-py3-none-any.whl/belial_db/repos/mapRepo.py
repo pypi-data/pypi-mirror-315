7d9a23986c1a7e69659fe4a20b2de37f233335b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 04:06:52 +0000

    Add package ManMade2.BelialDB/1.0.0/manmade2_belialdb-1.0.0-py3-none-any.whl

---
+++ packages/ManMade2.BelialDB/manmade2_belialdb-1.0.0-py3-none-any.whl/belial_db/repos/mapRepo.py
@@ -0,0 +1,69 @@
+from sqlalchemy import Engine
+from sqlalchemy.orm import Session, joinedload
+
+from belial_db.models import MapModel
+
+
+class MapRepo:
+    def __init__(self, engine: Engine):
+        """Initialize the MapRepo with a SQLAlchemy engine.
+
+        Args:
+            engine (Engine): The SQLAlchemy engine to connect to the database.
+        """
+        self._engine = engine
+
+    def get_map(self, id: int) -> MapModel | None:
+        """Retrieve a map by its ID.
+
+        Args:
+            id (int): The ID of the map to retrieve.
+
+        Returns:
+            MapModel | None: The map model if found, otherwise None.
+        """
+        with Session(self._engine) as session:
+            return (
+                session.query(MapModel)
+                .options(joinedload(MapModel.Assets), joinedload(MapModel.AssetFiles))
+                .filter(MapModel.Id == id)
+                .first()
+            )
+
+    def create_map(self, map: MapModel) -> MapModel:
+        """Create a new map in the database.
+
+        Args:
+            map (MapModel): The map model to create.
+
+        Returns:
+            MapModel: The created map model.
+        """
+        with Session(self._engine) as session:
+            session.add(map)
+            session.commit()
+            return map
+
+    def update_map(self, map: MapModel) -> MapModel:
+        """Update an existing map in the database.
+
+        Args:
+            map (MapModel): The map model to update.
+
+        Returns:
+            MapModel: The updated map model.
+        """
+        with Session(self._engine) as session:
+            session.merge(map)
+            session.commit()
+            return map
+
+    def delete_map(self, id: int) -> None:
+        """Delete a map from the database by its ID.
+
+        Args:
+            id (int): The ID of the map to delete.
+        """
+        with Session(self._engine) as session:
+            session.query(MapModel).filter(MapModel.Id == id).delete()
+            session.commit()
