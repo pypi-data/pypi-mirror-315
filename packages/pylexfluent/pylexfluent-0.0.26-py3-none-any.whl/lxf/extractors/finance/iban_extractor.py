c95f13e25cbbac6f27120f2a78f8a8bc310ee1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 04:06:48 +0000

    Add package pylexfluent/0.0.26/pylexfluent-0.0.26-py3-none-any.whl

---
+++ packages/pylexfluent/pylexfluent-0.0.26-py3-none-any.whl/lxf/extractors/finance/iban_extractor.py
@@ -0,0 +1,31 @@
+
+###################################################################
+
+import logging
+
+from lxf.ai.ocr.ocr import doOcr_from_pdf
+from lxf import settings
+
+
+
+logger = logging.getLogger('iban extractor')
+fh = logging.FileHandler('./logs/iban_extractor.log')
+fh.setLevel(settings.get_logging_level())
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.setLevel(settings.get_logging_level())
+logger.addHandler(fh)
+#################################################################
+from lxf.extractors.finance.ibans.iban_analyzer import IbanAnalyzer
+from lxf.services.try_safe import try_safe_execute_async
+
+
+async def extract_data(file_path:str)->str:
+    """
+    Extrait les données d'un iban 
+    """
+    result = await try_safe_execute_async(logger,doOcr_from_pdf, path_filename=file_path)
+    if result!=None :
+        analyzer:IbanAnalyzer= IbanAnalyzer(result)
+        return await try_safe_execute_async(logger,analyzer.do_analyze)
+    return result
