40d655522d5d37ef33ad892263912f70af01dc0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 05:06:14 +0000

    Add package nonebot-plugin-disconnect-notice/0.3.3/nonebot_plugin_disconnect_notice-0.3.3.tar.gz

---
+++ packages/nonebot-plugin-disconnect-notice/nonebot_plugin_disconnect_notice-0.3.3.tar.gz/nonebot_plugin_disconnect_notice-0.3.3/nonebot_plugin_disconnect_notice/dataClass.py
@@ -0,0 +1,58 @@
+class MailConfig:
+    def __init__(self,
+                 user: str,
+                 password: str,
+                 server: str,
+                 port: int,
+                 notice_email: str):
+        self.user = user
+        self.password = password
+        self.server = server
+        self.port = port
+        self.notice_email = notice_email
+
+    def check_params(self) -> bool:
+        """检查参数是否填写完整"""
+        if self.user and self.password and self.server and self.port and self.notice_email:
+            return True
+        else:
+            return False
+
+# https://www.pushplus.plus/doc/
+class PushPlusConfig:
+    def __init__(self,
+                 token: str
+                 ):
+        self.token = token
+
+    def check_params(self) -> bool:
+        """检查参数是否填写完整"""
+        if self.token:
+            return True
+        else:
+            return False
+
+class ServerConfig:
+    def __init__(self,
+                 key: str
+                 ):
+        self.key = key
+
+    def check_params(self) -> bool:
+        """检查参数是否填写完整"""
+        if self.key:
+            return True
+        else:
+            return False
+
+
+class BotParams:
+    def __init__(self,
+                 adapter_name: str,
+                 bot_id: str,
+                 tag:str=None,
+                 ):
+        self.adapter_name = adapter_name
+        self.bot_id = bot_id
+        self.tag = tag
+
---
+++ packages/nonebot-plugin-disconnect-notice/nonebot_plugin_disconnect_notice-0.3.3.tar.gz/nonebot_plugin_disconnect_notice-0.3.3/nonebot_plugin_disconnect_notice/utils.py
@@ -0,0 +1,21 @@
+import httpx
+from httpx import Response
+
+HTTP_TIME_OUT = 10 # 请求超时，秒
+
+class AsHttpReq(object):
+    """httpx 异步请求封装"""
+
+    @staticmethod
+    async def get(url, **kwargs) -> Response:
+        proxies = None
+        async with httpx.AsyncClient(proxies=proxies) as client:
+            response = await client.get(url, timeout=HTTP_TIME_OUT, **kwargs)
+            return response
+
+    @staticmethod
+    async def post(url, **kwargs) -> Response:
+        proxies = None
+        async with httpx.AsyncClient(proxies=proxies) as client:
+            response = await client.post(url, timeout=HTTP_TIME_OUT, **kwargs)
+            return response
