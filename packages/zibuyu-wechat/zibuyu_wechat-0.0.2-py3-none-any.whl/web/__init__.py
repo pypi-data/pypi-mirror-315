4598f3d5f377391ae8d276402cb7a6347b0e319f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 03:14:24 +0000

    Add package zibuyu-wechat/0.0.2/zibuyu_wechat-0.0.2-py3-none-any.whl

---
+++ packages/zibuyu-wechat/zibuyu_wechat-0.0.2-py3-none-any.whl/web/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+--------------------------------------------
+project: zibuyu_wechat
+author: 子不语
+date: 2024/12/14
+contact: 【公众号】思维兵工厂
+description: 
+--------------------------------------------
+"""
+
+from . import official
+
+__all__ = ['official']
