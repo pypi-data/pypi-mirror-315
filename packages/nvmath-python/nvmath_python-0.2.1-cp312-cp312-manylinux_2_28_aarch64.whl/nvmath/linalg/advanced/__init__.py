421ed9a1ed8161a79eb6460b1ca896e181268d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 04:06:55 +0000

    Add package nvmath-python/0.2.1/nvmath_python-0.2.1-cp312-cp312-manylinux_2_28_aarch64.whl

---
+++ packages/nvmath-python/nvmath_python-0.2.1-cp312-cp312-manylinux_2_28_aarch64.whl/nvmath/linalg/advanced/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from ._algorithmmod import *
+from ._configuration import *
+from .matmulmod import *
