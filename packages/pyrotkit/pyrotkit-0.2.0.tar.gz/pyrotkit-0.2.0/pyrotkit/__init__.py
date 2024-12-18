e787f0cc60590e96eb770205756a06bcd83aaba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 04:06:44 +0000

    Add package pyrotkit/0.2.0/pyrotkit-0.2.0.tar.gz

---
+++ packages/pyrotkit/pyrotkit-0.2.0.tar.gz/pyrotkit-0.2.0/pyrotkit/__init__.py
@@ -0,0 +1,5 @@
+from . import fov, generators, los, path, tools, types
+
+__version__ = "0.2.0"
+
+__all__ = ["fov", "generators", "los","path", "tools", "types"]
---
+++ packages/pyrotkit/pyrotkit-0.2.0.tar.gz/pyrotkit-0.2.0/pyrotkit/types/rectroom.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+from typing import Self
+
+class RectangularRoom:
+    def __init__(self, x: int, y: int, width: int, height: int):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + width
+        self.y2 = y + height
+
+    @property
+    def center(self) -> tuple[int, int]:
+        center_x = (self.x1 + self.x2) // 2
+        center_y = (self.y1 + self.y2) // 2
+
+        return center_x, center_y
+
+    @property
+    def inner(self) -> tuple[slice, slice]:
+        return slice(self.x1 + 1, self.x2), slice(self.y1 + 1, self.y2)
+
+    def intersects(self, other: Self) -> bool:
+        return(
+            self.x1 <= other.x2
+            and self.x2 >= other.x1
+            and self.y1 <= other.y2
+            and self.y2 >= other.y1
+        )
+    
+class CircularRoom:
+    pass
