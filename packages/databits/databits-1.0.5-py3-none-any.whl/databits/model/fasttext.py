aaba5dc174b832829d3e78366f21b362b5d6286d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 04:06:09 +0000

    Add package databits/1.0.5/databits-1.0.5-py3-none-any.whl

---
+++ packages/databits/databits-1.0.5-py3-none-any.whl/databits/model/fasttext.py
@@ -0,0 +1,21 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class FASTTEXTModel(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, output_dim, n_layers, dropout=0.1):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.hidden = nn.Linear(embedding_dim, embedding_dim)
+        self.layers = [nn.Linear(embedding_dim, embedding_dim) for _ in range(n_layers)]
+        self.fc = nn.Linear(embedding_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        embeddings = self.embedding(x)
+        avg_embeddings = embeddings.mean(dim=1).squeeze(1)
+        for layer in self.layers:
+            avg_embeddings = self.dropout(F.leaky_relu(self.hidden(avg_embeddings)))
+        out = self.fc(avg_embeddings)
+        return out
+        
