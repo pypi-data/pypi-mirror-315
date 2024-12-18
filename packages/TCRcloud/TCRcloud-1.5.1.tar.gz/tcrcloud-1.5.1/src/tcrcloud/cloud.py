ecf967619ab4fe94231e0437ec021f25ec310a80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 04:06:24 +0000

    Add package TCRcloud/1.5.1/TCRcloud-1.5.1.tar.gz

---
+++ packages/TCRcloud/TCRcloud-1.5.1.tar.gz/tcrcloud-1.5.1/src/tcrcloud/cloud.py
@@ -0,0 +1,185 @@
+import json
+import sys
+import re
+
+import numpy as np
+
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+import tcrcloud.colours
+import tcrcloud.format
+
+# Import default colours based on the V gene
+TRAV = tcrcloud.colours.TRAV
+TRBV = tcrcloud.colours.TRBV
+TRGV = tcrcloud.colours.TRGV
+TRDV = tcrcloud.colours.TRDV
+IGHV = tcrcloud.colours.IGHV
+IGKV = tcrcloud.colours.IGKV
+IGLV = tcrcloud.colours.IGLV
+
+
+# This colours the wordclouds
+class SimpleGroupedColorFunc(object):
+    """Create a color function object which assigns EXACT colors
+    to certain words based on the color to words mapping
+
+    Parameters
+    ----------
+    color_to_words : dict(str -> list(str))
+        A dictionary that maps a color to the list of words.
+
+    default_color : str
+        Color that will be assigned to a word that's not a member
+        of any value from color_to_words.
+    """
+
+    def __init__(self, color_to_words, default_color):
+        self.word_to_color = {
+            word: color for (color, words) in color_to_words.items() for word in words
+        }
+
+        self.default_color = default_color
+
+    def __call__(self, word, **kwargs):
+        return self.word_to_color.get(word, self.default_color)
+
+
+def separate(text):
+    return int(text) if text.isdigit() else text
+
+
+def natural_sort(text):
+    return [separate(c) for c in re.split(r"(\d+)", text)]
+
+
+def handle_duplicates(df):
+    df["junction_aa"] = np.where(
+        df["junction_aa"].duplicated(), " " + df["junction_aa"], df["junction_aa"]
+    )
+
+    if df["junction_aa"].is_unique is False:
+        handle_duplicates(df)
+
+    return df
+
+
+def wordcloud(args):
+    if args.legend.lower() != "true":
+        if args.legend.lower() != "false":
+            sys.stderr.write(
+                "TCRcloud error: please indicate legend \
+True or False\n"
+            )
+            exit()
+
+    samples_df = tcrcloud.format.format_data(args)
+
+    formatted_samples = tcrcloud.format.format_cloud(samples_df)
+
+    if formatted_samples["junction_aa"].is_unique is False:
+        handle_duplicates(formatted_samples)
+
+    samples = formatted_samples.groupby(["chain", "repertoire_id"])
+    keys = [key for key, _ in samples]
+
+    for j in keys:
+        df = samples.get_group(j)
+
+        if len(df) > 1:
+            family = (
+                df[["junction_aa", "v_call"]]
+                .set_index("junction_aa")
+                .squeeze()
+                .to_dict()
+            )
+            text = (
+                df[["junction_aa", "counts"]]
+                .set_index("junction_aa")
+                .squeeze()
+                .to_dict()
+            )
+        else:
+            family = {df["junction_aa"].iloc[0]: df["v_call"].iloc[0]}
+            text = {df["junction_aa"].iloc[0]: df["counts"].iloc[0]}
+
+        # create the wordcloud
+        wordcloud = WordCloud(
+            width=1000,
+            height=args.size,
+            background_color="white",
+            relative_scaling=0.7,
+            prefer_horizontal=1.0,
+            scale=2.5,
+            max_font_size=3000,
+            max_words=len(df),
+        ).generate_from_frequencies(text)
+        color_to_words = {}
+        if args.colours is not None:
+            try:
+                with open(args.colours) as json_file:
+                    color_to_words = json.load(json_file)
+            except FileNotFoundError:
+                sys.stderr.write(
+                    "TCRcloud error: " + args.colours + " doesn't seem to exist\n"
+                )
+                exit()
+            except json.decoder.JSONDecodeError:
+                sys.stderr.write(
+                    "TCRcloud error: "
+                    + args.colours
+                    + " doesn't seem properly formatted. Check \
+https://github.com/oldguyeric/TCRcloud for more information\n"
+                )
+                exit()
+        else:
+            for i in family:
+                color_to_words.setdefault(
+                    eval(family.get(i)[:4]).get(family.get(i), "grey"), []
+                ).append(i)
+
+        default_color = "grey"
+        try:
+            grouped_color_func = SimpleGroupedColorFunc(color_to_words, default_color)
+        except TypeError:
+            sys.stderr.write(
+                "TCRcloud error: "
+                + args.colours
+                + " doesn't seem properly formatted. Check \
+https://github.com/oldguyeric/TCRcloud for more information\n"
+            )
+            exit()
+        wordcloud.recolor(color_func=grouped_color_func)
+
+        plt.figure(dpi=300.0)
+        plt.imshow(wordcloud, interpolation="bilinear")
+        plt.xticks([])
+        plt.yticks([])
+
+        if args.legend.lower() == "true":
+            colours_for_legend = {}
+            if args.colours is None:
+                for i in family:
+                    tempdict = eval(family.get(i)[:4]).get(family.get(i), "grey")
+                    colours_for_legend[family.get(i)] = tempdict
+
+                sorted_legend = sorted(colours_for_legend)
+                sorted_legend.sort(key=natural_sort)
+                patchList = []
+                for key in sorted_legend:
+                    data_key = mpatches.Patch(color=colours_for_legend[key], label=key)
+                    patchList.append(data_key)
+
+                plt.legend(
+                    handles=patchList,
+                    bbox_to_anchor=(0.5, -0.01),
+                    loc="upper center",
+                    ncol=4,
+                    prop={"size": 6},
+                )
+        outputname = args.rearrangements[:-4] + "_" + j[1] + "_" + j[0] + ".png"
+        plt.tight_layout()
+        plt.savefig(outputname, dpi=300, bbox_inches="tight")
+        print("Word cloud saved as " + outputname)
---
+++ packages/TCRcloud/TCRcloud-1.5.1.tar.gz/tcrcloud-1.5.1/src/tcrcloud/testdata.py
@@ -0,0 +1,106 @@
+import requests
+import airr
+
+
+def download(args):
+    host_url = " https://ipa6.ireceptor.org/airr/v1"
+
+    query1 = {
+        "filters": {
+            "op": "and",
+            "content": [
+                {
+                    "op": "contains",
+                    "content": {"field": "subject.subject_id", "value": "su008"},
+                },
+                {
+                    "op": "in",
+                    "content": {
+                        "field": "sample.pcr_target.pcr_target_locus",
+                        "value": ["TRA"],
+                    },
+                },
+            ],
+        }
+    }
+
+    query2 = {
+        "filters": {
+            "op": "and",
+            "content": [
+                {
+                    "op": "contains",
+                    "content": {"field": "subject.subject_id", "value": "su008"},
+                },
+                {
+                    "op": "in",
+                    "content": {
+                        "field": "sample.pcr_target.pcr_target_locus",
+                        "value": ["TRB"],
+                    },
+                },
+                {
+                    "op": "contains",
+                    "content": {
+                        "field": "study.keywords_study",
+                        "value": "contains_schema_rearrangement",
+                    },
+                },
+            ],
+        }
+    }
+    # Send the query
+    resp = requests.post(host_url + "/repertoire", json=query1)
+
+    # The data is returned as JSON, use AIRR library to write out data
+    data = resp.json()
+    airr.write_repertoire(
+        "alpharepertoire.airr.json", data["Repertoire"], info=data["Info"]
+    )
+
+    # Print out some Info
+    print("       Info: " + data["Info"]["Info"]["title"])
+    print("    version: " + str(data["Info"]["Info"]["version"]))
+    print("description: " + data["Info"]["Info"]["description"])
+
+    # Save them out to a file
+    print(
+        "Received "
+        + str(len(data["Repertoire"]))
+        + " alpha repertoires. Saved as alpharepertoire.airr.json"
+    )
+
+    # Send the query
+    resp = requests.post(host_url + "/repertoire", json=query2)
+
+    # The data is returned as JSON, use AIRR library to write out data
+    data = resp.json()
+    airr.write_repertoire(
+        "betarepertoire.airr.json", data["Repertoire"], info=data["Info"]
+    )
+
+    # Save them out to a file
+    print(
+        "Received "
+        + str(len(data["Repertoire"]))
+        + " beta repertoires. Saved as betarepertoire.airr.json"
+    )
+
+    print()
+
+    with open("legend.json", "w") as fileout:
+        print("{", file=fileout)
+        print(
+            '    "PRJNA509910-su008_pre-TRA":"Subject 8 pre-treatment",', file=fileout
+        )
+        print(
+            '    "PRJNA509910-su008_post-TRA":"Subject 8 post-treatment",', file=fileout
+        )
+        print(
+            '    "PRJNA509910-su008_pre-TRB":"Subject 8 pre-treatment",', file=fileout
+        )
+        print(
+            '    "PRJNA509910-su008_post-TRB":"Subject 8 post-treatment"', file=fileout
+        )
+        print("}", file=fileout)
+    print("json file for legend saved as legend.json")
