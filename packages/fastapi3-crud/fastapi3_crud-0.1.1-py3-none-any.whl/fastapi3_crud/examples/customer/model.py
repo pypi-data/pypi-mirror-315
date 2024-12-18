50b7588ed2cb54a9507f8e7ceb6985eec39dcb5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-18 03:15:19 +0000

    Add package fastapi3-crud/0.1.1/fastapi3_crud-0.1.1-py3-none-any.whl

---
+++ packages/fastapi3-crud/fastapi3_crud-0.1.1-py3-none-any.whl/fastapi3_crud/examples/customer/model.py
@@ -0,0 +1,18 @@
+# --8<-- [start:imports]
+from sqlalchemy3 import Column, Integer, String
+from sqlalchemy3.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+# --8<-- [end:imports]
+# --8<-- [start:model]
+class Customer(Base):
+    __tablename__ = "customer"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+
+# --8<-- [end:model]
